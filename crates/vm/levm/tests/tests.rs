17d04ecd406ac2d6d5c50fd4956f5e39362ba742
maximopalopoli <mpalopoli@fi.uba.ar>
2024-12-27 09:47:57 -0300

    Fix blake2f unit test

--- crates/vm/levm/tests/tests.rs
+++ crates/vm/levm/tests/tests.rs
@@ -4634,5 +4634,5 @@ fn blake2f_test() {
     let expected_result = Bytes::from(hex::decode("ba80a53f981c4d0d6a2797b69f12f6e94c212f14685ac4b74b12bb6fdbffa2d17d87c5392aab792dc252d5de4533cc9518d38aa8dbf1925ab92386edd4009923").unwrap());
 
     assert_eq!(result, expected_result);
-    assert_eq!(consumed_gas, BLAKE2F_ROUND_COST);
+    assert_eq!(consumed_gas, 12 * BLAKE2F_ROUND_COST);
 }
