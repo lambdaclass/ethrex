eee8608169fdf361457bbba94b5f1fc3e3aaa05d
tesol2y090 <gliese090@gmail.com>
2025-01-01 12:40:31 +0700

    feat: add support exchange capabilities

---
+++ crates/networking/rpc/engine/exchange_capabilities.rs
@@ -0,0 +1,53 @@
+use crate::{utils::RpcRequest, RpcApiContext, RpcErr, RpcHandler};
+use serde_json::{json, Value};
+
+pub type ExchangeCapabilitiesRequest = Vec<String>;
+
+/// Supported engine capabilities
+const SUPPORTED_CAPABILITIES: [&str; 10] = [
+    "engine_forkchoiceUpdatedV1",
+    "engine_forkchoiceUpdatedV2",
+    "engine_forkchoiceUpdatedV3",
+    "engine_newPayloadV1",
+    "engine_newPayloadV2",
+    "engine_newPayloadV3",
+    "engine_getPayloadV1",
+    "engine_getPayloadV2",
+    "engine_getPayloadV3",
+    "engine_exchangeTransitionConfigurationV1",
+];
+
+impl From<ExchangeCapabilitiesRequest> for RpcRequest {
+    fn from(val: ExchangeCapabilitiesRequest) -> Self {
+        RpcRequest {
+            method: "engine_exchangeCapabilities".to_string(),
+            params: Some(vec![serde_json::json!(val)]),
+            ..Default::default()
+        }
+    }
+}
+
+impl RpcHandler for ExchangeCapabilitiesRequest {
+    fn parse(params: &Option<Vec<Value>>) -> Result<Self, RpcErr> {
+        params
+            .as_ref()
+            .ok_or(RpcErr::BadParams("No params provided".to_owned()))?
+            .first()
+            .ok_or(RpcErr::BadParams("Expected 1 param".to_owned()))
+            .and_then(|v| {
+                serde_json::from_value(v.clone())
+                    .map_err(|error| RpcErr::BadParams(error.to_string()))
+            })
+    }
+
+    fn handle(&self, _context: RpcApiContext) -> Result<Value, RpcErr> {
+        // Return only the capabilities that are supported by both client and server
+        let supported: Vec<String> = SUPPORTED_CAPABILITIES
+            .iter()
+            .filter(|cap| self.contains(&cap.to_string()))
+            .map(|s| s.to_string())
+            .collect();
+
+        Ok(json!(supported))
+    }
+}
