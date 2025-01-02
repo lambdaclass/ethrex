76a2de98e6eff2a00c1add40dc6196ecd4cdab93
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-01-02 16:55:56 -0300

    Improve error comment

--- crates/vm/levm/src/errors.rs
+++ crates/vm/levm/src/errors.rs
@@ -102,7 +102,7 @@ pub enum TxValidationError {
     InsufficientMaxFeePerGas,
     #[error("Insufficient max fee per blob gas")]
     InsufficientMaxFeePerBlobGas,
-    #[error("Type 3 transaction not allowed in current fork")]
+    #[error("Type 3 transactions are not supported before the Cancun fork")]
     Type3TxPreFork,
     #[error("Type3TxZeroBlobs")]
     Type3TxZeroBlobs,
