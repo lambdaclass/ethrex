8bb007710dc4df514675cdd8202cf27ca47aa0c8
fborello-lambda <federicoborello@lambdaclass.com>
2025-01-08 16:25:10 -0300

    fix: test lowering the iterations and wait for perf

--- .github/scripts/flamegraph_watcher.sh
+++ .github/scripts/flamegraph_watcher.sh
@@ -4,7 +4,7 @@
 # then polls the account balance until the expected balance has been reached
 # and then kills the process. It also measures the elapsed time of the test and
 # outputs it to Github Action's outputs.
-iterations=500
+iterations=50
 value=1
 account=0x33c6b73432B3aeA0C1725E415CC40D04908B85fd
 
@@ -18,7 +18,7 @@ seconds=$((elapsed % 60))
 output=$(ethrex_l2 info -b -a $account --wei 2>&1)
 echo "Balance of $output reached in $minutes min $seconds s, killing process"
 
-sudo pkill "$PROGRAM" && while pgrep -l "flamegraph"; do
+sudo pkill "$PROGRAM" && while pgrep -l "perf"; do
     echo "waiting for $PROGRAM to exit... "
     sleep 1
 done
