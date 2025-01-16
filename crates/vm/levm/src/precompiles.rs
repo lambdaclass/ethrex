4bcc16b169cbe94f02d8d345d11f8379f322d496
Tomás Paradelo <tomas.paradelo@lambdaclass.com>
2025-01-16 15:41:54 -0300

    resolve conflicts

--- crates/vm/levm/src/precompiles.rs
+++ crates/vm/levm/src/precompiles.rs
@@ -1286,7 +1286,7 @@ pub fn bls12_g2add(
     };
 
     let mut padded_result = Vec::new();
-    // The crate bls12_381 deserialize the G2 point as x_2 || x_1 || y_2 || y_1
+    // The crate bls12_381 deserialize the G2 point as x_1 || x_0 || y_1 || y_0
     // https://docs.rs/bls12_381/0.8.0/src/bls12_381/g2.rs.html#284-299
     add_padded_coordinate(&mut padded_result, result_bytes.get(48..96))?;
     add_padded_coordinate(&mut padded_result, result_bytes.get(0..48))?;
@@ -1335,7 +1335,7 @@ pub fn bls12_g2msm(
     };
 
     let mut padded_result = Vec::new();
-    // The crate bls12_381 deserialize the G2 point as x_2 || x_1 || y_2 || y_1
+    // The crate bls12_381 deserialize the G2 point as x_1 || x_0 || y_1 || y_0
     // https://docs.rs/bls12_381/0.8.0/src/bls12_381/g2.rs.html#284-299
     add_padded_coordinate(&mut padded_result, result_bytes.get(48..96))?;
     add_padded_coordinate(&mut padded_result, result_bytes.get(0..48))?;
@@ -1350,7 +1350,55 @@ pub fn bls12_pairing_check(
     gas_for_call: u64,
     consumed_gas: &mut u64,
 ) -> Result<Bytes, VMError> {
-    Ok(Bytes::new())
+    if calldata.is_empty() || calldata.len() % BLS12_381_PAIRING_CHECK_PAIR_LENGTH != 0 {
+        return Err(VMError::PrecompileError(PrecompileError::ParsingInputError));
+    }
+
+    // GAS
+    let k = calldata.len() / BLS12_381_PAIRING_CHECK_PAIR_LENGTH;
+    let gas_cost = gas_cost::bls12_pairing_check(k)?;
+    increase_precompile_consumed_gas(gas_for_call, gas_cost, consumed_gas)?;
+
+    let mut points: Vec<(G1Affine, G2Prepared)> = Vec::new();
+    for i in 0..k {
+        let g1_point_offset = i
+            .checked_mul(BLS12_381_PAIRING_CHECK_PAIR_LENGTH)
+            .ok_or(InternalError::ArithmeticOperationOverflow)?;
+        let g2_point_offset = g1_point_offset
+            .checked_add(128)
+            .ok_or(InternalError::ArithmeticOperationOverflow)?;
+        let pair_end = g2_point_offset
+            .checked_add(256)
+            .ok_or(InternalError::ArithmeticOperationOverflow)?;
+
+        // The check for the subgroup is required
+        // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-2537.md?plain=1#L194
+        let g1 = G1Affine::from(parse_g1_point(
+            calldata.get(g1_point_offset..g2_point_offset),
+            false,
+        )?);
+        let g2 = G2Affine::from(parse_g2_point(
+            calldata.get(g2_point_offset..pair_end),
+            false,
+        )?);
+        points.push((g1, G2Prepared::from(g2)));
+    }
+
+    // The crate bls12_381 expects a reference to the points
+    let points: Vec<(&G1Affine, &G2Prepared)> = points.iter().map(|(g1, g2)| (g1, g2)).collect();
+
+    // perform the final exponentiation to get the result of the pairing check
+    // https://docs.rs/bls12_381/0.8.0/src/bls12_381/pairings.rs.html#43-48
+    let result: Gt = multi_miller_loop(&points).final_exponentiation();
+
+    // follows this https://github.com/ethereum/EIPs/blob/master/EIPS/eip-2537.md?plain=1#L188
+    if result == Gt::identity() {
+        let mut result = Vec::from([0_u8; 31]);
+        result.push(1);
+        Ok(Bytes::from(result))
+    } else {
+        Ok(Bytes::copy_from_slice(&[0_u8; 32]))
+    }
 }
 
 pub fn bls12_map_fp_to_g1(
