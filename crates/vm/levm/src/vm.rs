b62ac2091a67ae52e1cda5f08a4e36c843010661
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2025-01-21 15:41:20 -0300

    Add if branch for when there's delegation code inside the call

--- crates/vm/levm/src/vm.rs
+++ crates/vm/levm/src/vm.rs
@@ -306,6 +306,22 @@ impl VM {
                 logs: std::mem::take(&mut current_call_frame.logs),
                 created_address: None,
             });
+        } else if current_call_frame.is_delegation
+            && was_delegated_from_bytecode(&current_call_frame.bytecode)?
+        {
+            self.call_frames.push(current_call_frame.clone());
+
+            return Ok(TransactionReport {
+                result: TxResult::Success,
+                new_state: self.cache.clone(),
+                // Here we use the gas used and not check for the floor cost
+                // for Prague fork because the precompiles have constant gas cost
+                gas_used: current_call_frame.gas_used,
+                gas_refunded: self.env.refunded_gas,
+                output: Bytes::new(),
+                logs: std::mem::take(&mut current_call_frame.logs),
+                created_address: None,
+            });
         }
 
         if is_precompile(&current_call_frame.code_address, self.env.spec_id) {
