d4b03502f241dc05df7fd7c536d29415b3b2ef52
fmoletta <fedemoletta@hotmail.com>
2025-01-21 15:08:44 -0300

    Soft reset

--- cmd/ethrex/ethrex.rs
+++ cmd/ethrex/ethrex.rs
@@ -148,6 +148,9 @@ async fn main() {
     store
         .add_initial_state(genesis.clone())
         .expect("Failed to create genesis block");
+    // Fix latest corrupted state
+    // Add block from this morning where state was okay
+    store.set_header_download_checkpoint(ethrex_core::H256::from_str("0x9adf8633ffff3f94ca0a5692bfedb9dcf961ecc71ddf9279773f961c4ec4e7db").unwrap()).unwrap();
 
     if let Some(chain_rlp_path) = matches.get_one::<String>("import") {
         info!("Importing blocks from chain file: {}", chain_rlp_path);
