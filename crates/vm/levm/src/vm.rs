c9ab6ca07d3e78416f2af536f56724091ba8aee3
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2025-01-23 09:17:45 -0300

    Revert "Remove one linter error"
    This reverts commit 4849d060c1649d90dc27e1eb4abd3ee1e2989b9b.

--- crates/vm/levm/src/vm.rs
+++ crates/vm/levm/src/vm.rs
@@ -3,7 +3,7 @@ use crate::{
     call_frame::CallFrame,
     constants::*,
     db::{
-        cache::{self, remove_account},
+        cache::{self, get_account_mut, remove_account},
         CacheDB, Database,
     },
     environment::Environment,
