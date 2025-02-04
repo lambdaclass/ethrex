44f1a4e8c103f9bba3a9ec7c3e71aade5491c86e
fmoletta <fedemoletta@hotmail.com>
2025-02-04 14:20:34 -0300

    Revert "Mute p2p errors"
    This reverts commit 6fe127197e9accae0a8ab7f6da07c32687c857ee.

--- crates/networking/p2p/rlpx/eth/blocks.rs
+++ crates/networking/p2p/rlpx/eth/blocks.rs
@@ -11,7 +11,7 @@ use ethrex_rlp::{
     structs::{Decoder, Encoder},
 };
 use ethrex_storage::Store;
-use tracing::{debug, error};
+use tracing::error;
 
 pub const HASH_FIRST_BYTE_DECODER: u8 = 160;
 
@@ -93,7 +93,7 @@ impl GetBlockHeaders {
                 if let Ok(Some(block_number)) = storage.get_block_number(block_hash) {
                     block_number
                 } else {
-                    debug!("Could not fetch block number for hash {block_hash}");
+                    error!("Could not fetch block number for hash {block_hash}");
                     return vec![];
                 }
             }
