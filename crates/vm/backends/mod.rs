a3e76a7206923f89333dcb2380111b0164c87a96
fborello-lambda <federicoborello@lambdaclass.com>
2025-02-07 18:22:36 -0300

    [wip]feat(levm): EVM trait

--- crates/vm/backends/mod.rs
+++ crates/vm/backends/mod.rs
@@ -1,7 +1,9 @@
 pub mod levm;
 pub mod revm;
 
-use crate::errors::EvmError;
+use crate::{errors::EvmError, EvmState};
+use ethrex_core::types::Block;
+use ethrex_storage::AccountUpdate;
 use std::str::FromStr;
 
 #[derive(Debug, Clone, Default)]
@@ -20,4 +22,34 @@ impl FromStr for EVM {
             _ => Err(EvmError::InvalidEVM(s.to_string())),
         }
     }
+}
+
+pub trait IEVM {
+    /// The error type for this trait's error. `EvmError` is the default, but it could be modified if needed.
+    type Error;
+
+    /// Output for `execute_block`. The default is `(Vec<Receipt>, Vec<AccountUpdate>)`, but it could be modified if needed.
+    type BlockExecutionOutput;
+
+    /// Input for `execute_tx`. This must be defined by the implementor. It may vary depending on the backend EVM.
+    type TransactionExecutionInput;
+
+    /// Output for `execute_tx`. This must be defined by the implementor. It may vary depending on the backend EVM.
+    type TransactionExecutionResult;
+
+    /// Input for `get_state_transitions`. This must be defined by the implementor. It may vary depending on the backend EVM.
+    type GetStateTransitionsInput;
+
+    /// Executes every transaction of a block returning a list of their receipts executed and a list of accounts that were updated in the execution.
+    fn execute_block(
+        block: &Block,
+        state: &mut EvmState,
+    ) -> Result<Self::BlockExecutionOutput, Self::Error>;
+
+    /// Executes a transaction returning its execution result. It may vary depending on the EVM used.
+    fn execute_tx(
+        input: Self::TransactionExecutionInput,
+    ) -> Result<Self::TransactionExecutionResult, Self::Error>;
+
+    fn get_state_transitions(input: Self::GetStateTransitionsInput) -> Vec<AccountUpdate>;
 }
