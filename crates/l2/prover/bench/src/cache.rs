ec24436f1b1bc86bfd0a19432096ef9993d9d589
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2025-02-10 17:35:35 -0300

    fix cache deser

--- crates/l2/prover/bench/src/cache.rs
+++ crates/l2/prover/bench/src/cache.rs
@@ -2,6 +2,7 @@ use ethrex_core::types::{Block, BlockHeader};
 use ethrex_vm::execution_db::ExecutionDB;
 
 use serde::{Deserialize, Serialize};
+use bincode::Options;
 
 #[derive(Serialize, Deserialize)]
 pub struct Cache {
@@ -13,7 +14,8 @@ pub struct Cache {
 pub fn load_cache(block_number: usize) -> Result<Cache, String> {
     let file_name = format!("cache_{}.bin", block_number);
     let file = std::fs::File::open(file_name).map_err(|err| err.to_string())?;
-    bincode::deserialize_from(file).map_err(|err| err.to_string())
+    bincode::DefaultOptions::new().with_fixint_encoding().
+    deserialize_from(file).map_err(|err| err.to_string())
 }
 
 pub fn write_cache(cache: &Cache) -> Result<(), String> {
