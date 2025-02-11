985a87a2c929b12e966fbf54357ccbee07986a13
Tomás Paradelo <tomas.paradelo@lambdaclass.com>
2025-02-11 16:58:35 -0300

    add 2929

--- crates/vm/levm/src/vm.rs
+++ crates/vm/levm/src/vm.rs
@@ -323,10 +323,21 @@ impl VM {
             return self.handle_precompile_result(precompile_result, current_call_frame, backup);
         }
 
+        dbg!(&current_call_frame.msg_sender);
+        dbg!(&current_call_frame.to);
+        dbg!(&current_call_frame.gas_used);
+        dbg!(&current_call_frame.gas_limit);
+
         loop {
             let opcode = current_call_frame.next_opcode();
 
+            dbg!(&opcode);
+
             let op_result = self.handle_current_opcode(opcode, current_call_frame);
+
+            dbg!(&current_call_frame.gas_used);
+            dbg!(&current_call_frame.stack);
+            dbg!(&op_result);
 
             match op_result {
                 Ok(OpcodeResult::Continue { pc_increment }) => {
