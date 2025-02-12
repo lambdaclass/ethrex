a679f5044124c488221fda4d063cb9c9748da913
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2025-02-12 14:22:20 -0300

    Refactor

--- crates/vm/levm/src/gas_cost.rs
+++ crates/vm/levm/src/gas_cost.rs
@@ -578,37 +578,44 @@ pub fn selfdestruct(
     balance_to_transfer: U256,
     fork: Fork,
 ) -> Result<u64, OutOfGasError> {
-    match fork {
-        f if f <= Fork::DaoFork => Ok(SELFDESTRUCT_STATIC_PRE_TANGERINE),
+    let (static_cost, dynamic_cost) = match fork {
+        f if f <= Fork::DaoFork => (SELFDESTRUCT_STATIC_PRE_TANGERINE, 0),
         Fork::Tangerine => {
             let dynamic_cost = if account_is_empty {
                 SELFDESTRUCT_DYNAMIC
             } else {
                 0
             };
-            Ok(SELFDESTRUCT_STATIC
-                .checked_add(dynamic_cost)
-                .ok_or(OutOfGasError::GasCostOverflow)?)
+            (SELFDESTRUCT_STATIC, dynamic_cost)
+        }
+        f if f > Fork::Tangerine && f < Fork::Berlin => {
+            let dynamic_cost = if account_is_empty && balance_to_transfer > U256::zero() {
+                SELFDESTRUCT_DYNAMIC
+            } else {
+                0
+            };
+
+            (SELFDESTRUCT_STATIC, dynamic_cost)
         }
         _ => {
-            let mut gas_cost = SELFDESTRUCT_STATIC;
-
-            if fork >= Fork::Berlin && address_was_cold {
-                gas_cost = gas_cost
-                    .checked_add(COLD_ADDRESS_ACCESS_COST)
-                    .ok_or(OutOfGasError::GasCostOverflow)?;
-            }
+            let mut dynamic_cost = if address_was_cold {
+                COLD_ADDRESS_ACCESS_COST
+            } else {
+                0
+            };
 
             // If a positive balance is sent to an empty account, the dynamic gas is 25000
             if account_is_empty && balance_to_transfer > U256::zero() {
-                gas_cost = gas_cost
+                dynamic_cost = dynamic_cost
                     .checked_add(SELFDESTRUCT_DYNAMIC)
                     .ok_or(OutOfGasError::GasCostOverflow)?;
             }
-
-            Ok(gas_cost)
+            (SELFDESTRUCT_STATIC, dynamic_cost)
         }
-    }
+    };
+    static_cost
+        .checked_add(dynamic_cost)
+        .ok_or(OutOfGasError::GasCostOverflow)
 }
 
 pub fn tx_calldata(calldata: &Bytes, fork: Fork) -> Result<u64, OutOfGasError> {
