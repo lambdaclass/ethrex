d9721f000ae69f1b518cc0a8a5a3c7836d70a372
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2025-02-13 12:33:59 -0300

    Try delegatecall

--- crates/vm/levm/src/gas_cost.rs
+++ crates/vm/levm/src/gas_cost.rs
@@ -879,13 +879,28 @@ pub fn delegatecall(
 ) -> Result<(u64, u64), VMError> {
     let memory_expansion_cost = memory::expansion_cost(new_memory_size, current_memory_size)?;
 
-    let address_access_cost = address_access_cost(
-        address_was_cold,
-        DELEGATECALL_STATIC,
-        DELEGATECALL_COLD_DYNAMIC,
-        DELEGATECALL_WARM_DYNAMIC,
-        fork,
-    )?;
+    let (static_cost, cold_dynamic_cost, warm_dynamic_cost) = match fork {
+        f if f < Fork::Tangerine => (40, 0, 0),
+        f if f < Fork::Berlin => (700, 0, 0),
+        f if f < Fork::Berlin => (0, 0, 0),
+        _ => (
+            DELEGATECALL_STATIC,
+            DELEGATECALL_COLD_DYNAMIC,
+            DELEGATECALL_WARM_DYNAMIC,
+        ),
+    };
+
+    let dynamic_cost: u64 = if address_was_cold {
+        cold_dynamic_cost
+    } else {
+        warm_dynamic_cost
+    };
+
+    //TODO: CHANGE BEFORE COMMIT
+    let address_access_cost = static_cost
+        .checked_add(dynamic_cost)
+        .ok_or(OutOfGasError::GasCostOverflow)?;
+
     let call_gas_costs = memory_expansion_cost
         .checked_add(address_access_cost)
         .ok_or(OutOfGasError::GasCostOverflow)?;
