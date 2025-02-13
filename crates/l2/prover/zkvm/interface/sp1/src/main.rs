782e1a30912468309fefbc6ad9e1abb712249edd
avilagaston9 <gavila@fi.uba.ar>
2025-02-13 16:06:41 -0300

    fix: sp1 import chain_error

--- crates/l2/prover/zkvm/interface/sp1/src/main.rs
+++ crates/l2/prover/zkvm/interface/sp1/src/main.rs
@@ -1,6 +1,6 @@
 #![no_main]
 
-use ethrex_blockchain::{validate_block, validate_gas_used};
+use ethrex_blockchain::{error::ChainError, validate_block, validate_gas_used};
 use ethrex_vm::{backends::revm::execute_block, db::EvmState, get_state_transitions};
 use zkvm_interface::{
     io::{ProgramInput, ProgramOutput},
