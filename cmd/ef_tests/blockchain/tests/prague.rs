a3159bd324b3515d4735ce44c2a2ca830a742b83
avilagaston9 <gavila@fi.uba.ar>
2025-02-14 13:50:36 -0300

    test(l1): enable ef_tests for EIP-7702

--- cmd/ef_tests/blockchain/tests/prague.rs
+++ cmd/ef_tests/blockchain/tests/prague.rs
@@ -5,12 +5,32 @@ use ef_tests_blockchain::{
     test_runner::{parse_test_file, run_ef_test},
 };
 
+// TODO: enable these tests once the evm is updated.
+const SKIPPED_TEST: [&str; 17] = [
+"tests/prague/eip7702_set_code_tx/test_set_code_txs.py::test_ext_code_on_chain_delegating_set_code[fork_Prague-blockchain_test]",
+"tests/prague/eip7702_set_code_tx/test_set_code_txs.py::test_ext_code_on_self_delegating_set_code[fork_Prague-blockchain_test-balance_1]",
+"tests/prague/eip7702_set_code_tx/test_set_code_txs.py::test_ext_code_on_self_set_code[fork_Prague-blockchain_test-balance_0]",
+"tests/prague/eip7702_set_code_tx/test_set_code_txs.py::test_ext_code_on_set_code[fork_Prague-blockchain_test-EMPTY_ACCOUNT-balance_0]",
+"tests/prague/eip7702_set_code_tx/test_set_code_txs.py::test_set_code_to_non_empty_storage[fork_Prague-blockchain_test-zero_nonce]",
+"tests/prague/eip7702_set_code_tx/test_set_code_txs_2.py::test_call_to_precompile_in_pointer_context[fork_Prague-precompile_0x000000000000000000000000000000000000000b-blockchain_test]",
+"tests/prague/eip7702_set_code_tx/test_set_code_txs_2.py::test_pointer_measurements[fork_Prague-blockchain_test]",
+"tests/prague/eip7702_set_code_tx/test_set_code_txs.py::test_ext_code_on_self_delegating_set_code[fork_Prague-blockchain_test-balance_0]",
+"tests/prague/eip7702_set_code_tx/test_set_code_txs.py::test_ext_code_on_self_set_code[fork_Prague-blockchain_test-balance_1]",
+"tests/prague/eip7702_set_code_tx/test_set_code_txs.py::test_ext_code_on_set_code[fork_Prague-blockchain_test-EOA_WITH_SET_CODE-balance_0]",
+"tests/prague/eip7702_set_code_tx/test_set_code_txs.py::test_valid_tx_invalid_chain_id[fork_Prague-blockchain_test-auth_chain_id=2**256-1]",
+"tests/prague/eip7702_set_code_tx/test_set_code_txs.py::test_ext_code_on_set_code[fork_Prague-blockchain_test-CONTRACT-balance_0]",
+"tests/prague/eip7702_set_code_tx/test_set_code_txs.py::test_ext_code_on_set_code[fork_Prague-blockchain_test-EOA_WITH_SET_CODE-balance_1]",
+"tests/prague/eip7702_set_code_tx/test_set_code_txs.py::test_ext_code_on_set_code[fork_Prague-blockchain_test-EMPTY_ACCOUNT-balance_1]",
+"tests/prague/eip7702_set_code_tx/test_set_code_txs.py::test_ext_code_on_set_code[fork_Prague-blockchain_test-EOA-balance_1]",
+"tests/prague/eip7702_set_code_tx/test_set_code_txs.py::test_ext_code_on_set_code[fork_Prague-blockchain_test-CONTRACT-balance_1]",
+"tests/prague/eip7702_set_code_tx/test_set_code_txs.py::test_ext_code_on_set_code[fork_Prague-blockchain_test-EOA-balance_0]"];
+
 #[allow(dead_code)]
 fn parse_and_execute(path: &Path) -> datatest_stable::Result<()> {
     let tests = parse_test_file(path);
 
     for (test_key, test) in tests {
-        if test.network < Network::Merge {
+        if test.network < Network::Merge || SKIPPED_TEST.contains(&test_key.as_str()) {
             // Discard this test
             continue;
         }
@@ -23,5 +43,8 @@ fn parse_and_execute(path: &Path) -> datatest_stable::Result<()> {
 datatest_stable::harness!(
     parse_and_execute,
     "vectors/prague/eip2935_historical_block_hashes_from_state",
+    r".*/.*\.json",
+    parse_and_execute,
+    "vectors/prague/eip7702_set_code_tx",
     r".*/.*\.json"
 );
