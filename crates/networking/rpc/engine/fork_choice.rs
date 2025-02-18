ac8383e535838dbfd9465e417995233bb4d812eb
ilitteri <ilitteri@fi.uba.ar>
2025-02-18 18:48:46 +0000

    Relay engine_forkchoiceUpdatedV3 to gateway

--- crates/networking/rpc/engine/fork_choice.rs
+++ crates/networking/rpc/engine/fork_choice.rs
@@ -115,6 +115,38 @@ impl RpcHandler for ForkChoiceUpdatedV3 {
         })
     }
 
+    async fn call(req: &RpcRequest, context: RpcApiContext) -> Result<Value, RpcErr> {
+        let request = Self::parse(&req.params)?;
+        cfg_if::cfg_if! {
+            if #[cfg(feature = "l2")] {
+                info!("Relaying engine_forkchoiceUpdatedV3 to gateway");
+                let gateway_response = context.gateway_auth_client.engine_forkchoice_updated_v3(request.fork_choice_state, request.payload_attributes)
+                    .await
+                    .map_err(|err| {
+                        RpcErr::Internal(format!(
+                            "Could not relay engine_forkchoiceUpdatedV3 to gateway: {err}",
+                        ))
+                    }).and_then(|response| {
+                        serde_json::to_value(response).map_err(|error| RpcErr::Internal(error.to_string()))
+                    });
+
+                // Parse ir again as it was consumed for gateway_response and it is the same as cloning it.
+                let request = Self::parse(&req.params)?;
+                let client_response = request.handle(context);
+
+                if gateway_response.is_err() {
+                    warn!(error = ?gateway_response, "Gateway engine_forkchoiceUpdatedV3 failed, falling back to local node");
+                } else {
+                    info!("Successfully relayed engine_forkchoiceUpdatedV3 to gateway");
+                }
+
+                gateway_response.or(client_response)
+            } else {
+                request.handle(context)
+            }
+        }
+    }
+
     fn handle(&self, context: RpcApiContext) -> Result<Value, RpcErr> {
         let (head_block_opt, mut response) =
             handle_forkchoice(&self.fork_choice_state, context.clone(), 3)?;
