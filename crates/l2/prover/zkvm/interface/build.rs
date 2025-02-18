9b6fe1f6086356b7d538c3ca770e40e46bea944c
fkrause98 <fkrausear@gmail.com>
2025-02-18 18:30:18 -0300

    Revert "fix: bump sp1 to work with rust 1.82"
    This reverts commit c5acfdbf1214aca99877de612e2f930d2cd96c1b.

--- crates/l2/prover/zkvm/interface/build.rs
+++ crates/l2/prover/zkvm/interface/build.rs
@@ -3,16 +3,7 @@ fn main() {
     #[cfg(feature = "build_risc0")]
     risc0_build::embed_methods();
 
-    // We should use include_elf! instead of doing this.
-    // I'm leaving this to avoid complex changes.
     #[cfg(not(clippy))]
     #[cfg(feature = "build_sp1")]
-    sp1_build::build_program_with_args(
-        "./sp1",
-        sp1_build::BuildArgs {
-            output_directory: Some("./sp1/elf".to_string()),
-            elf_name: Some("riscv32im-succinct-zkvm-elf".to_string()),
-            ..Default::default()
-        },
-    )
+    sp1_build::build_program("./sp1");
 }
