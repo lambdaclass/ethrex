13ed723b71f56854e110097100245a1f960bdfe6
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2025-02-19 14:22:58 -0300

    Revert "Add Revm's refunded gas and gas used when showing differences"
    This reverts commit 616f27ff086b2e5f00988f217d4d3a5a2d550488.

--- cmd/ef_tests/state/runner/revm_runner.rs
+++ cmd/ef_tests/state/runner/revm_runner.rs
@@ -241,11 +241,11 @@ pub fn compare_levm_revm_execution_results(
                         output: _,
                     },
                 ) => {
-                    if levm_tx_report.gas_used != revm_gas_used + revm_gas_refunded {
+                    if levm_tx_report.gas_used != revm_gas_used {
                         re_run_report.register_gas_used_mismatch(
                             *vector,
                             levm_tx_report.gas_used,
-                            revm_gas_used + revm_gas_refunded,
+                            revm_gas_used,
                             *fork,
                         );
                     }
