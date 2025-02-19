f4a2b48803907d2710c1b61ffcb005d698734e00
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2025-02-19 18:19:49 -0300

    add child nodes and exclusion proofs to executiondb from store

--- crates/vm/db.rs
+++ crates/vm/db.rs
@@ -5,6 +5,7 @@ use ethrex_common::{
     Address as CoreAddress, H256 as CoreH256,
 };
 use ethrex_storage::{error::StoreError, hash_address, hash_key, Store};
+use ethrex_trie::{Node, NodeRLP, PathRLP, Trie, TrieError};
 use revm::primitives::{
     AccountInfo as RevmAccountInfo, Address as RevmAddress, Bytecode as RevmBytecode,
     Bytes as RevmBytes, B256 as RevmB256, U256 as RevmU256,
@@ -228,8 +229,19 @@ impl ToExecDB for StoreWrapper {
             spec_id(&chain_config, block.header.timestamp),
             self,
         )
-        .map_err(|err| Box::new(EvmError::from(err)))?; // TODO: must be a better way
+        .map_err(|err| Box::new(EvmError::from(err)))?; // TODO: ugly error handling
         let store_wrapper = cache.db;
+
+        // index read and touched account addresses and storage keys
+        let index = cache.accounts.iter().map(|(address, account)| {
+            let address = CoreAddress::from(address.0.as_ref());
+            let storage_keys: Vec<_> = account
+                .storage
+                .keys()
+                .map(|key| CoreH256::from_slice(&key.to_be_bytes_vec()))
+                .collect();
+            (address, storage_keys)
+        });
 
         // fetch all read/written values from store
         let already_existing_accounts = cache
@@ -302,23 +314,70 @@ impl ToExecDB for StoreWrapper {
             .collect();
         // WARN: unwrapping because revm wraps a u64 as a U256
 
-        // get proofs
+        // get account proofs
         let state_trie = self
+            .store
+            .state_trie(block.hash())?
+            .ok_or(ExecutionDBError::NewMissingStateTrie(parent_hash))?;
+        let parent_state_trie = self
             .store
             .state_trie(parent_hash)?
             .ok_or(ExecutionDBError::NewMissingStateTrie(parent_hash))?;
+        let hashed_addresses: Vec<_> = index
+            .clone()
+            .map(|(address, _)| hash_address(&address))
+            .collect();
+        let initial_state_proofs = state_trie.get_proofs(&hashed_addresses)?;
+        let final_state_proofs: Vec<_> = hashed_addresses
+            .iter()
+            .map(|hashed_address| {
+                Ok((hashed_address, parent_state_trie.get_proof(hashed_address)?))
+            })
+            .collect::<Result<_, TrieError>>()?;
+        let potential_account_child_nodes = final_state_proofs
+            .iter()
+            .filter_map(|(hashed_address, proof)| get_potential_child_nodes(proof, hashed_address))
+            .flat_map(|nodes| nodes.into_iter().map(|node| node.encode_raw()))
+            .collect();
+        let state_proofs = (
+            initial_state_proofs.0,
+            [initial_state_proofs.1, potential_account_child_nodes].concat(),
+        );
 
-        let state_proofs =
-            state_trie.get_proofs(&accounts.keys().map(hash_address).collect::<Vec<_>>())?;
-
-        let mut storage_proofs = HashMap::new();
-        for (address, storages) in &storage {
-            let storage_trie = self.store.storage_trie(parent_hash, *address)?.ok_or(
-                ExecutionDBError::NewMissingStorageTrie(parent_hash, *address),
+        // get storage proofs
+        let mut initial_storage_proofs = HashMap::new();
+        let mut final_storage_proofs = HashMap::new();
+        for (address, storage_keys) in index {
+            let storage_trie = self.store.storage_trie(block.hash(), address)?.ok_or(
+                ExecutionDBError::NewMissingStorageTrie(block.hash(), address),
             )?;
+            let parent_storage_trie = self.store.storage_trie(parent_hash, address)?.ok_or(
+                ExecutionDBError::NewMissingStorageTrie(parent_hash, address),
+            )?;
+            let paths = storage_keys.iter().map(hash_key).collect::<Vec<_>>();
 
-            let paths = storages.keys().map(hash_key).collect::<Vec<_>>();
-            storage_proofs.insert(*address, storage_trie.get_proofs(&paths)?);
+            let initial_proofs = parent_storage_trie.get_proofs(&paths)?;
+            let final_proofs: Vec<(_, Vec<_>)> = storage_keys
+                .iter()
+                .map(|key| {
+                    let hashed_key = hash_key(key);
+                    let proof = storage_trie.get_proof(&hashed_key)?;
+                    Ok((hashed_key, proof))
+                })
+                .collect::<Result<_, TrieError>>()?;
+
+            let potential_child_nodes: Vec<NodeRLP> = final_proofs
+                .iter()
+                .filter_map(|(hashed_key, proof)| get_potential_child_nodes(proof, hashed_key))
+                .flat_map(|nodes| nodes.into_iter().map(|node| node.encode_raw()))
+                .collect();
+            let proofs = (
+                initial_proofs.0,
+                [initial_proofs.1, potential_child_nodes].concat(),
+            );
+
+            initial_storage_proofs.insert(address, proofs);
+            final_storage_proofs.insert(address, final_proofs);
         }
 
         Ok(ExecutionDB {
@@ -328,7 +387,49 @@ impl ToExecDB for StoreWrapper {
             block_hashes,
             chain_config,
             state_proofs,
-            storage_proofs,
+            storage_proofs: initial_storage_proofs,
         })
+    }
+}
+
+/// Get all potential child nodes of a node whose value was deleted.
+///
+/// After deleting a value from a (partial) trie it's possible that the node containing the value gets
+/// replaced by its child, whose prefix is possibly modified by appending some nibbles to it.
+/// If we don't have this child node (because we're modifying a partial trie), then we can't
+/// perform the deletion. If we have the final proof of exclusion of the deleted value, we can
+/// calculate all posible child nodes.
+fn get_potential_child_nodes(proof: &[NodeRLP], key: &PathRLP) -> Option<Vec<Node>> {
+    // TODO: Perhaps it's possible to calculate the child nodes instead of storing all possible ones.
+    let trie = Trie::from_nodes(
+        proof.first(),
+        &proof.iter().skip(1).cloned().collect::<Vec<_>>(),
+    )
+    .unwrap();
+
+    // return some only if this is a proof of exclusion
+    if trie.get(key).unwrap().is_none() {
+        let final_node = Node::decode_raw(proof.last().unwrap()).unwrap();
+        match final_node {
+            Node::Extension(mut node) => {
+                let mut variants = Vec::with_capacity(node.prefix.len());
+                while {
+                    variants.push(Node::from(node.clone()));
+                    node.prefix.next().is_some()
+                } {}
+                Some(variants)
+            }
+            Node::Leaf(mut node) => {
+                let mut variants = Vec::with_capacity(node.partial.len());
+                while {
+                    variants.push(Node::from(node.clone()));
+                    node.partial.next().is_some()
+                } {}
+                Some(variants)
+            }
+            _ => None,
+        }
+    } else {
+        None
     }
 }
