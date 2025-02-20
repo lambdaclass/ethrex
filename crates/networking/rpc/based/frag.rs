851603d6181c6fcecc8a07423686c5a6835f6354
ilitteri <ilitteri@fi.uba.ar>
2025-02-20 12:55:19 +0000

    Fix log message

--- crates/networking/rpc/based/frag.rs
+++ crates/networking/rpc/based/frag.rs
@@ -4,7 +4,7 @@ pub struct FragV0;
 
 impl RpcHandler for FragV0 {
     fn parse(_params: &Option<Vec<serde_json::Value>>) -> Result<Self, crate::utils::RpcErr> {
-        tracing::info!("parsing seal");
+        tracing::info!("parsing frag");
         Ok(Self)
     }
 
@@ -12,7 +12,7 @@ impl RpcHandler for FragV0 {
         &self,
         _context: crate::RpcApiContext,
     ) -> Result<serde_json::Value, crate::utils::RpcErr> {
-        tracing::info!("handling seal");
+        tracing::info!("handling frag");
         Ok(serde_json::Value::Null)
     }
 }
