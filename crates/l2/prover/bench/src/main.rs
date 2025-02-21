7e07a8d375a1229190160341f4ef1bb1203bc60b
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2025-02-21 11:49:17 -0300

    fix bench crate name

--- crates/l2/prover/bench/src/main.rs
+++ crates/l2/prover/bench/src/main.rs
@@ -1,6 +1,6 @@
 use std::{fs::File, io::Write};
 
-use bench::{
+use ethrex_prover_bench::{
     cache::{load_cache, write_cache, Cache},
     rpc::{db::RpcDB, get_block, get_latest_block_number},
 };
