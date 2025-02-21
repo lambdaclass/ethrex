33bfb8dfa0c5f1e2fe0a1ac30902279be9c15e3c
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2025-02-21 16:41:07 -0300

    replace cache by program input

--- crates/l2/prover/bench/src/cache.rs
+++ crates/l2/prover/bench/src/cache.rs
@@ -1,28 +1,18 @@
 use std::{
     fs::File,
-    io::{BufReader, BufWriter, Read},
+    io::{BufReader, BufWriter},
 };
 
-use ethrex_common::types::{Block, BlockHeader};
-use ethrex_vm::execution_db::ExecutionDB;
+use zkvm_interface::io::ProgramInput;
 
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize)]
-pub struct Cache {
-    pub block: Block,
-    pub parent_block_header: BlockHeader,
-    pub db: ExecutionDB,
-}
-
-pub fn load_cache(block_number: usize) -> Result<Cache, String> {
+pub fn load_cache(block_number: usize) -> Result<ProgramInput, String> {
     let file_name = format!("cache_{}.json", block_number);
     let file = BufReader::new(File::open(file_name).map_err(|err| err.to_string())?);
     serde_json::from_reader(file).map_err(|err| err.to_string())
 }
 
-pub fn write_cache(cache: &Cache) -> Result<(), String> {
+pub fn write_cache(cache: &ProgramInput) -> Result<(), String> {
     let file_name = format!("cache_{}.json", cache.block.header.number);
-    let mut file = BufWriter::new(File::create(file_name).map_err(|err| err.to_string())?);
+    let file = BufWriter::new(File::create(file_name).map_err(|err| err.to_string())?);
     serde_json::to_writer(file, cache).map_err(|err| err.to_string())
 }
