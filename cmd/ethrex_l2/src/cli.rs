6d137cc7c62d1a6cdbcdb0071ca9ea7fa35a96c7
Tomás Paradelo <tomas.paradelo@lambdaclass.com>
2025-02-24 10:50:02 -0300

    remove cli feat

--- cmd/ethrex_l2/src/cli.rs
+++ cmd/ethrex_l2/src/cli.rs
@@ -1,5 +1,5 @@
 use crate::{
-    commands::{autocomplete, config, info, prove, stack, test, utils, wallet},
+    commands::{autocomplete, config, info, prove, stack, test, wallet},
     config::load_selected_config,
 };
 use clap::{Parser, Subcommand};
@@ -33,8 +33,6 @@ enum EthrexL2Command {
     Info(info::Command),
     #[clap(about = "Read a test chain from disk and prove a block.")]
     Prove(prove::Command),
-    #[clap(subcommand, about = "Utils commands.")]
-    Utils(utils::Command),
 }
 
 pub async fn start() -> eyre::Result<()> {
@@ -55,7 +53,6 @@ pub async fn start() -> eyre::Result<()> {
         EthrexL2Command::Test(cmd) => cmd.run(cfg).await?,
         EthrexL2Command::Info(cmd) => cmd.run(cfg).await?,
         EthrexL2Command::Prove(_) => unreachable!(),
-        EthrexL2Command::Utils(cmd) => cmd.run()?,
     };
     Ok(())
 }
