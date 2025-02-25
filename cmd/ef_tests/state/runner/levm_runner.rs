8c0b02a07d00122859b06d8c9e26d97135453be6
JereSalo <jeresalo17@gmail.com>
2025-02-25 16:29:57 -0300

    clippy lint

--- cmd/ef_tests/state/runner/levm_runner.rs
+++ cmd/ef_tests/state/runner/levm_runner.rs
@@ -318,7 +318,7 @@ pub fn ensure_post_state(
                     let (initial_state, block_hash) = utils::load_initial_state(test); //TODO: Stop using EvmState in LEVM
                     let levm_account_updates = backends::levm::LEVM::get_state_transitions(
                         Some(*fork),
-                        &initial_state.database().unwrap(),
+                        initial_state.database().unwrap(),
                         block_hash,
                         &execution_report.new_state,
                     )
