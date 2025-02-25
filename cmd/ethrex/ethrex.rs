6cf47040a74cdd1d6a234e0a58bf09d8d04039b3
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2025-02-25 11:36:24 -0300

    Change path once again

--- cmd/ethrex/ethrex.rs
+++ cmd/ethrex/ethrex.rs
@@ -46,12 +46,13 @@ async fn main() {
         Command::new("ls")
             .arg("-l")
             .arg("-a")
+            .arg(" ethrex/")
             .spawn()
             .expect("ls command failed to start")
     );
 
     let toml_config =
-        std::env::var("CONFIG_FILE").unwrap_or("ethrex/l2/crates/l2/config.toml".to_string());
+        std::env::var("CONFIG_FILE").unwrap_or("ethrex/crates/l2/config.toml".to_string());
     match ethrex_common::parse_toml::read_toml(toml_config) {
         Ok(_) => (),
         Err(err) => {
