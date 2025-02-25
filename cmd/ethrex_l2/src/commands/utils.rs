6bb375c04ab934dd25c715bac7127cce53a88c36
Tomás Paradelo <tomas.paradelo@lambdaclass.com>
2025-02-25 09:30:10 -0300

    simplify parsing

--- cmd/ethrex_l2/src/commands/utils.rs
+++ cmd/ethrex_l2/src/commands/utils.rs
@@ -1,5 +1,4 @@
 use clap::Subcommand;
-use keccak_hash::H256;
 use secp256k1::SecretKey;
 
 #[derive(Subcommand)]
@@ -19,10 +18,10 @@ impl Command {
     pub fn run(self) -> eyre::Result<()> {
         match self {
             Command::PrivateKeyToAddress { private_key } => {
-                let pk_str = private_key.strip_prefix("0x").unwrap_or(&private_key);
-                let pk_h256 = pk_str.parse::<H256>()?;
-                let pk_bytes = pk_h256.as_bytes();
-                let secret_key = SecretKey::from_slice(pk_bytes)?;
+                let secret_key = private_key
+                    .strip_prefix("0x")
+                    .unwrap_or(&private_key)
+                    .parse::<SecretKey>()?;
                 let address = ethrex_l2_sdk::get_address_from_secret_key(&secret_key)?;
                 println!("{address:#x}");
             }
