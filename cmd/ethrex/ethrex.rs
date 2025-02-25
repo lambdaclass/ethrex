1d589dfad7665711f0b8d5224832b3788f2c502c
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2025-02-25 16:43:52 -0300

    Remove commented out code.

--- cmd/ethrex/ethrex.rs
+++ cmd/ethrex/ethrex.rs
@@ -348,7 +348,6 @@ async fn main() {
             let toml_config =
                 std::env::var("CONFIG_FILE").unwrap_or("config.toml".to_string());
 
-            // match ethrex_common::parse_toml::read_toml(toml_config) {
             match ethrex_l2::parse_toml::read_toml(toml_config) {
                 Ok(_) => (),
                 Err(err) => {
