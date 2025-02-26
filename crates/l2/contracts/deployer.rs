707742be09ebc4506b84ee81a5b81d314a185c22
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2025-02-26 13:06:21 -0300

    Remove debug file system read files

--- crates/l2/contracts/deployer.rs
+++ crates/l2/contracts/deployer.rs
@@ -71,14 +71,9 @@ const INITIALIZE_ON_CHAIN_PROPOSER_SIGNATURE: &str =
     "initialize(address,address,address,address[])";
 
 const BRIDGE_INITIALIZER_SIGNATURE: &str = "initialize(address)";
-use std::fs;
+
 #[tokio::main]
 async fn main() -> Result<(), DeployError> {
-    let paths = fs::read_dir("./").unwrap();
-
-    for path in paths {
-        println!("Name: {}", path.unwrap().path().display())
-    }
     let toml_config = std::env::var("CONFIG_FILE").unwrap_or("config.toml".to_string());
 
     match ethrex_l2::parse_toml::read_toml(toml_config) {
