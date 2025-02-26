ca5404ae3e64fb499901111ae28d488c7d007f48
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2025-02-26 17:10:01 -0300

    fix clippy

--- crates/l2/prover/src/backends/mock.rs
+++ crates/l2/prover/src/backends/mock.rs
@@ -2,22 +2,22 @@ use ethrex_l2::utils::prover::proving_systems::{ProofCalldata, ProverType};
 use tracing::warn;
 use zkvm_interface::io::ProgramInput;
 
-pub fn execute(input: ProgramInput) -> Result<(), Box<dyn std::error::Error>> {
+pub fn execute(_input: ProgramInput) -> Result<(), Box<dyn std::error::Error>> {
     warn!("Executing with mock prover does nothing. Use a real prover by enabling the corresponding feature flag");
     Ok(())
 }
 
-pub fn prove(input: ProgramInput) -> Result<(), Box<dyn std::error::Error>> {
+pub fn prove(_input: ProgramInput) -> Result<(), Box<dyn std::error::Error>> {
     warn!("Proving with mock prover does nothing. Use a real prover by enabling the corresponding feature flag");
     Ok(())
 }
 
-pub fn verify(proof: &()) -> Result<(), Box<dyn std::error::Error>> {
+pub fn verify(_proof: &()) -> Result<(), Box<dyn std::error::Error>> {
     warn!("Verifying with mock prover does nothing. Use a real prover by enabling the corresponding feature flag");
     Ok(())
 }
 
-pub fn to_calldata(proof: ()) -> Result<ProofCalldata, Box<dyn std::error::Error>> {
+pub fn to_calldata(_proof: ()) -> Result<ProofCalldata, Box<dyn std::error::Error>> {
     warn!("Calldata of a mock prover is empty. Use a real prover by enabling the corresponding feature flag");
     Ok(ProofCalldata {
         prover_type: ProverType::RISC0,
