bc182b103a7936997674ccbec6fa94825e5dad3f
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2025-02-27 14:35:06 -0300

    Revert "Add conf.toml parsing in deployer.rs and test.rs"
    This reverts commit f385074d072611ca9b281fcb225cb39ed8f95460.

--- crates/l2/contracts/deployer.rs
+++ crates/l2/contracts/deployer.rs
@@ -74,15 +74,6 @@ const BRIDGE_INITIALIZER_SIGNATURE: &str = "initialize(address)";
 
 #[tokio::main]
 async fn main() -> Result<(), DeployError> {
-    let toml_config = std::env::var("CONFIG_FILE").unwrap_or("config.toml".to_string());
-
-    match ethrex_l2::parse_toml::read_toml(toml_config) {
-        Ok(_) => (),
-        Err(err) => {
-            eprintln!("{}", err);
-            std::process::exit(1);
-        }
-    };
     let setup_result = setup()?;
     download_contract_deps(&setup_result.contracts_path)?;
     compile_contracts(&setup_result.contracts_path)?;
