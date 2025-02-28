e1a6a548aabbcf58e4d9683f2dd628daca2ab687
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2025-02-28 15:15:17 -0300

    remove sender param for apply_transaction and apply_plain_transaction

--- crates/blockchain/payload.rs
+++ crates/blockchain/payload.rs
@@ -390,7 +390,7 @@ impl Blockchain {
             metrics!(METRICS_TX.inc_tx());
 
             // Execute tx
-            let receipt = match self.apply_transaction(&head_tx, context, head_tx.tx.sender()) {
+            let receipt = match self.apply_transaction(&head_tx, context) {
                 Ok(receipt) => {
                     txs.shift()?;
                     // Pull transaction from the mempool
@@ -428,11 +428,10 @@ impl Blockchain {
         &self,
         head: &HeadTransaction,
         context: &mut PayloadBuildContext,
-        sender: Address,
     ) -> Result<Receipt, ChainError> {
         match **head {
             Transaction::EIP4844Transaction(_) => self.apply_blob_transaction(head, context),
-            _ => self.apply_plain_transaction(head, context, sender),
+            _ => self.apply_plain_transaction(head, context),
         }
     }
 
@@ -460,7 +459,7 @@ impl Blockchain {
             return Err(EvmError::Custom("max data blobs reached".to_string()).into());
         };
         // Apply transaction
-        let receipt = self.apply_plain_transaction(head, context, head.tx.sender())?;
+        let receipt = self.apply_plain_transaction(head, context)?;
         // Update context with blob data
         let prev_blob_gas = context.payload.header.blob_gas_used.unwrap_or_default();
         context.payload.header.blob_gas_used =
@@ -474,7 +473,6 @@ impl Blockchain {
         &self,
         head: &HeadTransaction,
         context: &mut PayloadBuildContext,
-        sender: Address,
     ) -> Result<Receipt, ChainError> {
         let chain_config = context.chain_config()?;
         let (report, gas_used) = self.vm.execute_tx(
@@ -484,7 +482,7 @@ impl Blockchain {
             &mut context.block_cache,
             &chain_config,
             &mut context.remaining_gas,
-            sender,
+            head.tx.sender(),
         )?;
         context.block_value += U256::from(gas_used) * head.tip;
         Ok(report)
