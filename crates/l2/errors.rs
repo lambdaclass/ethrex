3664ce6354a689659d67a6c707545073e7ca4f7e
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2025-02-28 13:32:28 -0300

    Improve error message output and delete unused error
    Signed-off-by: Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>

--- crates/l2/errors.rs
+++ crates/l2/errors.rs
@@ -3,9 +3,11 @@ pub enum ConfigError {
     #[error(
         "\x1b[91mCould not find crates/l2/config.toml
 Have you tried copying the provided example? Try:
-cp crates/l2/config_example.toml crates/l2/config.toml
+cp {manifest_dir}/config_example.toml {manifest_dir}/config.toml
 \x1b[0m
-"
+",
+        manifest_dir = env!("CARGO_MANIFEST_DIR")
+
     )]
     TomlFileNotFound,
 
@@ -15,9 +17,11 @@ Check the provided example to see if you have all the required fields.
 The example can be found in:
 crates/l2/config_example.toml
 You can also see the differences with:
-diff crates/l2/config_example.toml crates/l2/config.toml
+diff {manifest_dir}/config_example.toml {manifest_dir}/config.toml
 \x1b[0m
-"
+",
+        manifest_dir = env!("CARGO_MANIFEST_DIR")
+
     )]
     TomlFormat,
     #[error(
@@ -25,10 +29,4 @@ diff crates/l2/config_example.toml crates/l2/config.toml
 "
     )]
     EnvWriteError(String),
-    #[error(
-        "\x1b[91m .env file already exits, please check if it has any valuable information. If not, delete it
-\x1b[0m
-"
-    )]
-    EnvFileAlreadyExists,
 }
