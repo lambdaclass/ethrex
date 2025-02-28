87bc8ff89b4d01b6770744b6a6fda7d244249304
Martin Paulucci <martin.c.paulucci@gmail.com>
2025-02-28 17:10:56 +0100

    Change test.

--- cmd/ef_tests/blockchain/benches/import8k.rs
+++ cmd/ef_tests/blockchain/benches/import8k.rs
@@ -11,9 +11,11 @@ use ethrex_storage::Store;
 
 static LARGE_TEST: (&str, &str) = ("vectors/prague/eip2935_historical_block_hashes_from_state/block_hashes/block_hashes_history.json", "tests/prague/eip2935_historical_block_hashes_from_state/test_block_hashes.py::test_block_hashes_history[fork_Prague-blockchain_test-full_history_plus_one_check_blockhash_first]");
 
+static LARGE_TEST_2: (&str, &str) = ("vectors/prague/eip2935_historical_block_hashes_from_state/block_hashes/block_hashes_history_at_transition.json", "tests/prague/eip2935_historical_block_hashes_from_state/test_block_hashes.py::test_block_hashes_history_at_transition[fork_CancunToPragueAtTime15k-blockchain_test-blocks_before_fork_1-blocks_after_fork_257]");
+
 pub fn setup_benchmark() -> (Blockchain, Vec<BlockWithRLP>, Store) {
-    let tests = parse_test_file(Path::new(LARGE_TEST.0));
-    let test = tests.get(LARGE_TEST.1).unwrap();
+    let tests = parse_test_file(Path::new(LARGE_TEST_2.0));
+    let test = tests.get(LARGE_TEST_2.1).unwrap();
 
     let store = build_store_for_test(&test);
 
