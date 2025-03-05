77de3b2df7931b07883569dfdbec7f6b4fa65b45
JereSalo <jeresalo17@gmail.com>
2025-03-05 10:25:30 -0300

    make block_hash mandatory, remove clear_state method (not used anymore)

--- crates/blockchain/payload.rs
+++ crates/blockchain/payload.rs
@@ -187,9 +187,7 @@ impl<'a> PayloadBuildContext<'a> {
                 .map(|schedule| schedule.base_fee_update_fraction)
                 .unwrap_or_default(),
         );
-        let mut vm = Evm::new(evm_engine, storage.clone());
-        // TODO this should probably be set at vm creation
-        vm.clear_state(payload.header.parent_hash);
+        let vm = Evm::new(evm_engine, storage.clone(), payload.header.parent_hash);
 
         Ok(PayloadBuildContext {
             remaining_gas: payload.header.gas_limit,
