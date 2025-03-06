2483df50a5428db917b50191869bf556f1f0de00
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2025-03-06 17:59:47 -0300

    fix pico program

--- crates/l2/prover/zkvm/interface/pico/src/main.rs
+++ crates/l2/prover/zkvm/interface/pico/src/main.rs
@@ -1,14 +1,15 @@
 #![no_main]
 
-pico_sdk::entrypoint!(main);
 use pico_sdk::io::{commit, read_as};
 
 use ethrex_blockchain::{validate_block, validate_gas_used};
-use ethrex_vm::{backends::revm::execute_block, db::EvmState, get_state_transitions};
+use ethrex_vm::{backends::revm_b::REVM, db::EvmState};
 use zkvm_interface::{
     io::{ProgramInput, ProgramOutput},
     trie::{update_tries, verify_db},
 };
+
+pico_sdk::entrypoint!(main);
 
 pub fn main() {
     let ProgramInput {
@@ -17,9 +18,12 @@ pub fn main() {
         db,
     } = read_as();
     let mut state = EvmState::from(db.clone());
+    let chain_config = state
+        .chain_config()
+        .expect("Failed to get chain config from state");
 
     // Validate the block
-    validate_block(&block, &parent_block_header, &state).expect("invalid block");
+    validate_block(&block, &parent_block_header, &chain_config).expect("invalid block");
 
     // Tries used for validating initial and final state root
     let (mut state_trie, mut storage_tries) = db
@@ -35,8 +39,9 @@ pub fn main() {
         panic!("invalid database")
     };
 
-    // Execute
-    let receipts = execute_block(&block, &mut state).expect("failed to execute block");
+    let result = REVM::execute_block(&block, &mut state).expect("failed to execute block");
+    let receipts = result.receipts;
+    let account_updates = result.account_updates;
     validate_gas_used(&receipts, &block.header).expect("invalid gas used");
 
     // Output gas for measurement purposes
@@ -47,7 +52,6 @@ pub fn main() {
     // write(&cumulative_gas_used);
 
     // Update state trie
-    let account_updates = get_state_transitions(&mut state);
     update_tries(&mut state_trie, &mut storage_tries, &account_updates)
         .expect("failed to update state and storage tries");
 
