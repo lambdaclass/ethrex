262064fb52fb5a19e69458a18bf7c297f23efe2d
github-action-benchmark <github@users.noreply.github.com>
2025-03-12 19:25:51 +0000

    add Benchmark (cargo) benchmark result for e655ca46e31befb706e86117503d9c73e26afdd8

---
+++ data.js
@@ -0,0 +1,34 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1741807551266,
+  "repoUrl": "https://github.com/lambdaclass/ethrex",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "lambdaclass",
+            "username": "lambdaclass"
+          },
+          "committer": {
+            "name": "lambdaclass",
+            "username": "lambdaclass"
+          },
+          "id": "e655ca46e31befb706e86117503d9c73e26afdd8",
+          "message": "feat(core): bench workflow",
+          "timestamp": "2025-03-12T18:31:00Z",
+          "url": "https://github.com/lambdaclass/ethrex/pull/2190/commits/e655ca46e31befb706e86117503d9c73e26afdd8"
+        },
+        "date": 1741807550359,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib 20",
+            "value": 20470,
+            "range": "± 562",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
