5d70a642639ca38b89ff8c01b8412d0134f788b5
Javier Chatruc <jrchatruc@gmail.com>
2025-03-12 22:54:14 -0300

    FIx links

--- .github/scripts/publish_link_flamegraphs.sh
+++ .github/scripts/publish_link_flamegraphs.sh
@@ -15,8 +15,8 @@ curl -XPOST -H "Content-type: application/json" -d '{
       "text": {
         "type": "mrkdwn",
         "text": "Flamegraphs are available at *<https://lambdaclass.github.io/ethrex/|https://lambdaclass.github.io/ethrex/flamegraphs>*\n
-        • *<https://lambdaclass.github.io/ethrex/flamegraph/flamegraph_ethrex.svg|Ethrex>*\n
-        • *<https://lambdaclass.github.io/ethrex/flamegraph/flamegraph_reth.svg|Reth>*\n"
+        • *<https://lambdaclass.github.io/ethrex/flamegraphs/flamegraph_ethrex.svg|Ethrex>*\n
+        • *<https://lambdaclass.github.io/ethrex/flamegraphs/flamegraph_reth.svg|Reth>*\n"
       }
     },
   ],
