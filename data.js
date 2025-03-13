cecafcd7587abd4211ed6f142e0c73cb76164f6d
github-action-benchmark <github@users.noreply.github.com>
2025-03-13 02:54:07 +0000

    add Benchmark (cargo) benchmark result for 9cabb0961d7d0e5d1ac96306c40ec16ed1620b3a

---
+++ data.js
@@ -0,0 +1,34 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1741834447537,
+  "repoUrl": "https://github.com/lambdaclass/ethrex",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "lambdaclass",
+            "username": "lambdaclass"
+          },
+          "committer": {
+            "name": "lambdaclass",
+            "username": "lambdaclass"
+          },
+          "id": "9cabb0961d7d0e5d1ac96306c40ec16ed1620b3a",
+          "message": "feat(core): bench workflow",
+          "timestamp": "2025-03-12T18:31:00Z",
+          "url": "https://github.com/lambdaclass/ethrex/pull/2190/commits/9cabb0961d7d0e5d1ac96306c40ec16ed1620b3a"
+        },
+        "date": 1741834445313,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "Block import/Block import ERC20 transfers",
+            "value": 230999121163,
+            "range": "± 404755845",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
