550936e264c585b382b79de81b2005b8b9449891
Tomás Paradelo <tomas.paradelo@lambdaclass.com>
2025-03-13 12:54:16 -0300

    temporal solution to test l2 with levm

--- crates/vm/backends/mod.rs
+++ crates/vm/backends/mod.rs
@@ -290,9 +290,12 @@ impl Evm {
                 self::revm::helpers::simulate_tx_from_generic(tx, header, state, spec_id)
             }
             Evm::LEVM {
-                store_wrapper: _,
+                store_wrapper,
                 block_cache: _,
-            } => Err(EvmError::Custom("Not implemented".to_string())),
+            } => {
+                let mut state = evm_state(store_wrapper.store.clone(), store_wrapper.block_hash);
+                self::revm::helpers::simulate_tx_from_generic(tx, header, &mut state, spec_id)
+            }
         }
     }
 
@@ -307,9 +310,12 @@ impl Evm {
                 self::revm::helpers::create_access_list(tx, header, state, spec_id)
             }
             Evm::LEVM {
-                store_wrapper: _,
+                store_wrapper,
                 block_cache: _,
-            } => Err(EvmError::Custom("Not implemented".to_string())),
+            } => {
+                let mut state = evm_state(store_wrapper.store.clone(), store_wrapper.block_hash);
+                self::revm::helpers::create_access_list(tx, header, &mut state, spec_id)
+            }
         }?;
 
         match res {
