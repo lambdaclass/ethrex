c5193db6dd2a6fdb0694ddb37c6f4382e2540cc2
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2025-03-14 16:50:43 -0300

    Comment out find parent header in handle_new_payload
    Signed-off-by: Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>

--- crates/networking/rpc/engine/payload.rs
+++ crates/networking/rpc/engine/payload.rs
@@ -598,15 +598,15 @@ fn handle_new_payload_v3(
     expected_blob_versioned_hashes: Vec<H256>,
 ) -> Result<PayloadStatus, RpcErr> {
     // Validate if it can be the new head and find the parent
-    let Ok(_) = find_parent_header(&block.header, &context.blockchain.storage) else {
-        // If the parent is not present, we store it as pending and we
-        // return syncing as a response.
-        context
-            .blockchain
-            .storage
-            .add_pending_block(block.clone())?;
-        return Ok(PayloadStatus::syncing());
-    };
+    // let Ok(_) = find_parent_header(&block.header, &context.blockchain.storage) else {
+    //     // If the parent is not present, we store it as pending and we
+    //     // return syncing as a response.
+    //     context
+    //         .blockchain
+    //         .storage
+    //         .add_pending_block(block.clone())?;
+    //     return Ok(PayloadStatus::syncing());
+    // };
 
     // Ignore incoming
     // Check sync status
