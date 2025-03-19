a5107df8711c28cd9567f8188a5c44fe23b3f0be
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2025-03-19 13:05:26 -0300

    chore: address clippy warnings

--- crates/storage/store_db/libmdbx.rs
+++ crates/storage/store_db/libmdbx.rs
@@ -168,9 +168,7 @@ impl StoreEngine for Store {
     }
 
     fn mark_chain_as_canonical(&self, blocks: &[Block]) -> Result<(), StoreError> {
-        let key_values = blocks
-            .iter()
-            .map(|e| (e.header.number.into(), e.hash().into()));
+        let key_values = blocks.iter().map(|e| (e.header.number, e.hash().into()));
 
         self.write_batch::<CanonicalBlockHashes>(key_values)
     }
@@ -873,22 +871,6 @@ impl<T: RLPEncode + RLPDecode> IndexedChunk<T> {
         let decoded = T::decode(&value).map_err(StoreError::RLPDecode)?;
         Ok(Some(decoded))
     }
-}
-
-// In order to use NodeHash as key in a dupsort table we must encode it into a fixed size type
-pub fn node_hash_to_fixed_size(node_hash: Vec<u8>) -> [u8; 33] {
-    // keep original len so we can re-construct it later
-    let original_len = node_hash.len();
-    // original len will always be lower or equal to 32 bytes
-    debug_assert!(original_len <= 32);
-    // Pad the node_hash with zeros to make it fixed_size (in case of inline)
-    let mut node_hash = node_hash;
-    node_hash.resize(32, 0);
-    // Encode the node as [original_len, node_hash...]
-    std::array::from_fn(|i| match i {
-        0 => original_len as u8,
-        n => node_hash[n - 1],
-    })
 }
 
 table!(
