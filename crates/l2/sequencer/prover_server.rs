cd3f55a2b8c778b73c76836005676154bfc760e7
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2025-03-20 13:02:15 -0300

    handle_submit write all

--- crates/l2/sequencer/prover_server.rs
+++ crates/l2/sequencer/prover_server.rs
@@ -384,7 +384,7 @@ impl ProverServer {
 
         let buffer = serde_json::to_vec(&response)?;
         stream
-            .write(&buffer)
+            .write_all(&buffer)
             .await
             .map_err(ProverServerError::ConnectionError)?;
         Ok(())
