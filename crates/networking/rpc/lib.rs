3e0146d4ffeea8fbc593c59fba605acf7865069f
Martin Paulucci <martin.c.paulucci@gmail.com>
2025-03-25 17:40:47 +0100

    Use `lib` as an entrypoint for the crate.

---
+++ crates/networking/rpc/lib.rs
@@ -0,0 +1,16 @@
+mod admin;
+mod authentication;
+mod engine;
+mod eth;
+#[cfg(feature = "l2")]
+mod l2;
+mod net;
+mod rpc;
+mod web3;
+
+pub mod clients;
+pub mod types;
+pub mod utils;
+pub use clients::{EngineClient, EthClient};
+
+pub use rpc::start_api;
