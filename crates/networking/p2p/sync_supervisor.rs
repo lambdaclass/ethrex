72fdbfd36e8e69a0a03d2b4ddda67029c9c77d80
fmoletta <fedemoletta@hotmail.com>
2025-03-26 12:29:19 -0300

    Move resposibility of starting the next cycle to `start_sync` to avoid downtime between cycles

--- crates/networking/p2p/sync_supervisor.rs
+++ crates/networking/p2p/sync_supervisor.rs
@@ -1,6 +1,6 @@
 use std::sync::{
     atomic::{AtomicBool, Ordering},
-    Arc, RwLock,
+    Arc,
 };
 
 use ethrex_blockchain::Blockchain;
@@ -8,6 +8,7 @@ use ethrex_common::H256;
 use ethrex_storage::{error::StoreError, Store};
 use tokio::sync::Mutex;
 use tokio_util::sync::CancellationToken;
+use tracing::warn;
 
 use crate::{
     kademlia::KademliaTable,
@@ -26,7 +27,7 @@ pub struct SyncSupervisor {
     /// It is a READ_ONLY value, as modifications will disrupt the current active sync progress
     snap_enabled: Arc<AtomicBool>,
     syncer: Arc<Mutex<SyncManager>>,
-    last_fcu_head: RwLock<H256>,
+    last_fcu_head: Arc<Mutex<H256>>,
     store: Store,
 }
 
@@ -48,7 +49,7 @@ impl SyncSupervisor {
         Self {
             snap_enabled,
             syncer,
-            last_fcu_head: RwLock::new(H256::zero()),
+            last_fcu_head: Arc::new(Mutex::new(H256::zero())),
             store,
         }
     }
@@ -59,7 +60,7 @@ impl SyncSupervisor {
         Self {
             snap_enabled: Arc::new(AtomicBool::new(false)),
             syncer: Arc::new(Mutex::new(SyncManager::dummy())),
-            last_fcu_head: RwLock::new(H256::zero()),
+            last_fcu_head: Arc::new(Mutex::new(H256::zero())),
             store: Store::new("temp.db", ethrex_storage::EngineType::InMemory)
                 .expect("Failed to create test DB"),
         }
@@ -67,60 +68,62 @@ impl SyncSupervisor {
 
     /// Updates the last fcu head. This may be used on the next sync cycle if needed
     pub fn set_head(&self, fcu_head: H256) {
-        if let Ok(mut last_fcu_head) = self.last_fcu_head.write() {
-            *last_fcu_head = fcu_head
+        if let Ok(mut latest_fcu_head) = self.last_fcu_head.try_lock() {
+            *latest_fcu_head = fcu_head;
+        } else {
+            warn!("Failed to update latest fcu head for syncing")
         }
     }
 
     /// Returns the current sync status, either active or inactive and what the current syncmode is in the case of active
-    /// It will also start the next cycle if there is a pending sync
     pub fn status(&self) -> Result<SyncStatus, StoreError> {
-        // Check current sync status and act accordingly
-        Ok(match self.sync_status_internal()? {
-            SyncStatusInternal::Inactive => SyncStatus::Inactive,
-            SyncStatusInternal::Active => SyncStatus::Active(self.sync_mode()),
-            SyncStatusInternal::Pending => {
-                // Start next cycle
-                self.start_sync();
-                SyncStatus::Active(self.sync_mode())
-            }
+        Ok(if self.syncer.try_lock().is_err() {
+            SyncStatus::Active(self.sync_mode())
+        } else {
+            SyncStatus::Inactive
         })
     }
 
     /// Attempts to sync to the last received fcu head
     /// Will do nothing if the syncer is already involved in a sync process
+    /// If the sync process would require multiple sync cycles (such as snap sync), starts all required sync cycles until the sync is complete
     pub fn start_sync(&self) {
         let syncer = self.syncer.clone();
-        let Ok(sync_head) = self.last_fcu_head.read() else {
-            tracing::error!("Poisoned RwLock, unable to sync");
-            return;
-        };
-        let sync_head = *sync_head;
         let store = self.store.clone();
         let Ok(Some(current_head)) = self.store.get_latest_canonical_block_hash() else {
             tracing::error!("Failed to fecth latest canonical block, unable to sync");
             return;
         };
+        let sync_head = self.last_fcu_head.clone();
         tokio::spawn(async move {
             // If we can't get hold of the syncer, then it means that there is an active sync in process
-            if let Ok(mut syncer) = syncer.try_lock() {
-                syncer.start_sync(current_head, sync_head, store).await
+            let Ok(mut syncer) = syncer.try_lock() else {
+                return;
+            };
+            loop {
+                let sync_head = {
+                    // Read latest fcu head without holding the lock for longer than needed
+                    let Ok(sync_head) = sync_head.try_lock() else {
+                        tracing::error!("Failed to read latest fcu head, unable to sync");
+                        return;
+                    };
+                    *sync_head
+                };
+                // Start the sync cycle
+                syncer
+                    .start_sync(current_head, sync_head, store.clone())
+                    .await;
+                // Continue to the next sync cycle if we have an ongoing snap sync (aka if we still have snap sync checkpoints stored)
+                if store
+                    .get_header_download_checkpoint()
+                    .ok()
+                    .flatten()
+                    .is_none()
+                {
+                    break;
+                }
             }
         });
-    }
-
-    /// Returns the internal sync status, either active, inactive, or pending (aka, the current cycle stopped due to staleness but the sync is not yet complete)
-    fn sync_status_internal(&self) -> Result<SyncStatusInternal, StoreError> {
-        // Try to get hold of the sync manager, if we can't then it means it is currently involved in a sync process
-        Ok(if self.syncer.try_lock().is_err() {
-            SyncStatusInternal::Active
-        // Check if there is a checkpoint left from a previous aborted sync
-        } else if self.store.get_header_download_checkpoint()?.is_some() {
-            SyncStatusInternal::Pending
-        // No trace of a sync being handled
-        } else {
-            SyncStatusInternal::Inactive
-        })
     }
 
     /// Returns the syncer's current syncmode (either snap or full)
@@ -149,15 +152,4 @@ impl SyncSupervisor {
             .map(|mut syncer| syncer.invalid_ancestors.insert(k, v))
             .is_ok()
     }
-}
-
-/// Describes the client's current sync status:
-/// Inactive: There is no active sync process
-/// Active: The client is currently syncing
-/// Pending: The previous sync process became stale, awaiting restart
-#[derive(Debug)]
-pub enum SyncStatusInternal {
-    Inactive,
-    Active,
-    Pending,
 }
