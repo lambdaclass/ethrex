95e82b99843b31f9f3d665fc744109f4b3465fbd
fmoletta <fedemoletta@hotmail.com>
2025-03-28 15:11:14 -0300

    Add quickfix

--- crates/storage/store_db/libmdbx.rs
+++ crates/storage/store_db/libmdbx.rs
@@ -533,10 +533,13 @@ impl StoreEngine for Store {
         &self,
         limit: usize,
     ) -> Result<Vec<(H256, Vec<Nibbles>)>, StoreError> {
-        // self.read::<SnapState>(SnapStateIndex::StorageHealPaths)?
-        //     .map(|ref h| <Vec<(H256, Vec<Nibbles>)>>::decode(h))
-        //     .transpose()
-        //     .map_err(StoreError::RLPDecode)
+        // (QuickFix) Move storage heal paths to their own table
+        if let Some(storage_heal_paths) = self.read::<SnapState>(SnapStateIndex::StorageHealPaths)?
+            .map(|ref h| <Vec<(H256, Vec<Nibbles>)>>::decode(h))
+            .transpose()
+            .map_err(StoreError::RLPDecode)? {
+            self.set_storage_heal_paths(storage_heal_paths)?;
+        };
         let txn = self.db.begin_read().map_err(StoreError::LibmdbxError)?;
         let cursor = txn
             .cursor::<StorageHealPaths>()
