31dad5770aed405c24e8a68b3bc5f4de120d636b
avilagaston9 <gaston.avila@lambdaclass.com>
2025-03-31 17:15:52 -0300

    Fix merge

--- crates/vm/backends/levm/mod.rs
+++ crates/vm/backends/levm/mod.rs
@@ -263,8 +263,8 @@ impl LEVM {
                 None
             } else {
                 // Look into the current database to see if the bytecode hash is already present
-                let current_bytecode = &initial_account_state.bytecode;
-                let code = new_state_account.info.bytecode.clone();
+                let current_bytecode = &initial_account_state.code;
+                let code = new_state_account.code.clone();
                 // The code is present in the current database
                 if *current_bytecode != Bytes::new() {
                     if *current_bytecode != code {
