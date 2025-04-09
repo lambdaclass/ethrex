ac960fb6310898c0b19902123ab64411894ee30f
JereSalo <jeresalo17@gmail.com>
2025-04-09 16:01:05 -0300

    return default storage if not found in executiondb

--- crates/vm/backends/revm/db.rs
+++ crates/vm/backends/revm/db.rs
@@ -67,6 +67,51 @@ impl From<ExecutionDB> for EvmState {
     }
 }
 
+impl DatabaseRef for ExecutionDB {
+    /// The database error type.
+    type Error = ExecutionDBError;
+
+    /// Get basic account information.
+    fn basic_ref(&self, address: RevmAddress) -> Result<Option<RevmAccountInfo>, Self::Error> {
+        let Some(account_info) = self.accounts.get(&CoreAddress::from(address.0.as_ref())) else {
+            return Ok(None);
+        };
+
+        Ok(Some(RevmAccountInfo {
+            balance: RevmU256::from_limbs(account_info.balance.0),
+            nonce: account_info.nonce,
+            code_hash: RevmB256::from_slice(&account_info.code_hash.0),
+            code: None,
+        }))
+    }
+
+    /// Get account code by its hash.
+    fn code_by_hash_ref(&self, code_hash: RevmB256) -> Result<RevmBytecode, Self::Error> {
+        self.code
+            .get(&CoreH256::from(code_hash.as_ref()))
+            .map(|b| RevmBytecode::new_raw(RevmBytes(b.clone())))
+            .ok_or(ExecutionDBError::CodeNotFound(code_hash))
+    }
+
+    /// Get storage value of address at index.
+    fn storage_ref(&self, address: RevmAddress, index: RevmU256) -> Result<RevmU256, Self::Error> {
+        Ok(self
+            .storage
+            .get(&CoreAddress::from(address.0.as_ref()))
+            .and_then(|account_storage| account_storage.get(&CoreH256::from(index.to_be_bytes())))
+            .map(|v| RevmU256::from_limbs(v.0))
+            .unwrap_or_else(|| RevmU256::ZERO))
+    }
+
+    /// Get block hash by block number.
+    fn block_hash_ref(&self, number: u64) -> Result<RevmB256, Self::Error> {
+        self.block_hashes
+            .get(&number)
+            .map(|h| RevmB256::from_slice(&h.0))
+            .ok_or(ExecutionDBError::BlockHashNotFound(number))
+    }
+}
+
 impl revm::Database for StoreWrapper {
     type Error = StoreError;
 
--- crates/vm/db.rs
+++ crates/vm/db.rs
@@ -5,16 +5,13 @@ use ethrex_common::{
     types::{AccountInfo, Block, ChainConfig},
     Address, H256, U256,
 };
-use ethrex_common::{Address as CoreAddress, H256 as CoreH256};
 use ethrex_storage::{AccountUpdate, Store};
 use ethrex_trie::{NodeRLP, Trie, TrieError};
-use revm::primitives::{
-    AccountInfo as RevmAccountInfo, Address as RevmAddress, Bytecode as RevmBytecode,
-    Bytes as RevmBytes, B256 as RevmB256, U256 as RevmU256,
-};
 use revm::{
-    db::CacheDB, inspectors::TracerEip3155, primitives::result::EVMError as RevmError, Database,
-    DatabaseRef, Evm,
+    db::CacheDB,
+    inspectors::TracerEip3155,
+    primitives::{result::EVMError as RevmError, Address as RevmAddress},
+    Database, DatabaseRef, Evm,
 };
 use revm_primitives::SpecId;
 use serde::{Deserialize, Serialize};
@@ -57,51 +54,6 @@ pub struct ExecutionDB {
     ///
     /// Root node is stored separately from the rest as the first tuple member.
     pub storage_proofs: HashMap<Address, (Option<NodeRLP>, Vec<NodeRLP>)>,
-}
-
-impl DatabaseRef for ExecutionDB {
-    /// The database error type.
-    type Error = ExecutionDBError;
-
-    /// Get basic account information.
-    fn basic_ref(&self, address: RevmAddress) -> Result<Option<RevmAccountInfo>, Self::Error> {
-        let Some(account_info) = self.accounts.get(&CoreAddress::from(address.0.as_ref())) else {
-            return Ok(None);
-        };
-
-        Ok(Some(RevmAccountInfo {
-            balance: RevmU256::from_limbs(account_info.balance.0),
-            nonce: account_info.nonce,
-            code_hash: RevmB256::from_slice(&account_info.code_hash.0),
-            code: None,
-        }))
-    }
-
-    /// Get account code by its hash.
-    fn code_by_hash_ref(&self, code_hash: RevmB256) -> Result<RevmBytecode, Self::Error> {
-        self.code
-            .get(&CoreH256::from(code_hash.as_ref()))
-            .map(|b| RevmBytecode::new_raw(RevmBytes(b.clone())))
-            .ok_or(ExecutionDBError::CodeNotFound(code_hash))
-    }
-
-    /// Get storage value of address at index.
-    fn storage_ref(&self, address: RevmAddress, index: RevmU256) -> Result<RevmU256, Self::Error> {
-        Ok(self
-            .storage
-            .get(&CoreAddress::from(address.0.as_ref()))
-            .and_then(|storage| storage.get(&CoreH256::from(index.to_be_bytes())))
-            .map(|v| RevmU256::from_limbs(v.0))
-            .unwrap_or_default())
-    }
-
-    /// Get block hash by block number.
-    fn block_hash_ref(&self, number: u64) -> Result<RevmB256, Self::Error> {
-        self.block_hashes
-            .get(&number)
-            .map(|h| RevmB256::from_slice(&h.0))
-            .ok_or(ExecutionDBError::BlockHashNotFound(number))
-    }
 }
 
 impl ExecutionDB {
