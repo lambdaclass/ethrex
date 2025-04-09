819825516dc633275df56b2886b921061c4d7681
Julian Ventura <julian.ventura@lambdaclass.com>
2025-04-09 11:10:36 -0300

    Remove comment

--- cmd/ef_tests/blockchain/tests/cancun.rs
+++ cmd/ef_tests/blockchain/tests/cancun.rs
@@ -4,15 +4,6 @@ use ef_tests_blockchain::{
 };
 use std::path::Path;
 
-// NOTE: There are many tests which are failing due to the usage of Prague fork.
-// These tests are distributed in almost all json test files.
-// The `parse_and_execute_until_cancun` function will filter those tests after parsing them
-// this will mark said tests as passed, so they will become a false positive.
-// The idea is to move those tests to be executed with the `parse_and_execute_all` function once
-// Prague development starts.
-// This modification should be made on the harness down below, matching the regex with the desired
-// test or set of tests
-
 #[allow(dead_code)]
 fn parse_and_execute(path: &Path) -> datatest_stable::Result<()> {
     let rt = tokio::runtime::Runtime::new().unwrap();
