7e997cdf9973ec8eb05da2ef1340451d161dc849
fmoletta <fedemoletta@hotmail.com>
2025-04-23 15:50:20 -0300

    Remove `get_account_info_by_hash`

--- crates/vm/levm/src/db/mod.rs
+++ crates/vm/levm/src/db/mod.rs
@@ -2,7 +2,7 @@ use crate::account::AccountInfo;
 use bytes::Bytes;
 use error::DatabaseError;
 use ethrex_common::{
-    types::{BlockHash, ChainConfig},
+    types::ChainConfig,
     Address, H256, U256,
 };
 
@@ -16,10 +16,5 @@ pub trait Database: Send + Sync {
     fn get_block_hash(&self, block_number: u64) -> Result<Option<H256>, DatabaseError>;
     fn account_exists(&self, address: Address) -> bool;
     fn get_chain_config(&self) -> ChainConfig;
-    fn get_account_info_by_hash(
-        &self,
-        block_hash: BlockHash,
-        address: Address,
-    ) -> Result<Option<ethrex_common::types::AccountInfo>, DatabaseError>;
     fn get_account_code(&self, code_hash: H256) -> Result<Option<Bytes>, DatabaseError>;
 }
