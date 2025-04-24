e732be852babcb9a40da5b965ab3e9699cbf5845
Lucas Rack <lucas.rack@lambdaclass.com>
2025-04-24 11:26:00 -0300

    remove unnecesary dependencies

--- crates/networking/rpc/build.rs
+++ crates/networking/rpc/build.rs
@@ -2,16 +2,8 @@ use std::error::Error;
 use vergen::*;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let build = BuildBuilder::all_build()?;
-    let cargo = CargoBuilder::all_cargo()?;
     let rustc = RustcBuilder::all_rustc()?;
-    let si = SysinfoBuilder::all_sysinfo()?;
 
-    Emitter::default()
-        .add_instructions(&build)?
-        .add_instructions(&cargo)?
-        .add_instructions(&rustc)?
-        .add_instructions(&si)?
-        .emit()?;
+    Emitter::default().add_instructions(&rustc)?.emit()?;
     Ok(())
 }
