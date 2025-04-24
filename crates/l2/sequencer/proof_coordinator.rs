3ebb4214edc2525aa7fda32b1156bdc473e04a9e
Tomás Paradelo <tomas.paradelo@lambdaclass.com>
2025-04-24 12:16:30 -0300

    merge main

--- crates/l2/sequencer/proof_coordinator.rs
+++ crates/l2/sequencer/proof_coordinator.rs
@@ -131,7 +131,13 @@ impl ProofCoordinator {
         eth_config: EthConfig,
         store: Store,
     ) -> Result<Self, ConfigError> {
-        let eth_client = EthClient::new(&eth_config.rpc_url);
+        let eth_client = EthClient::new_with_config(
+            &eth_config.rpc_url,
+            eth_config.max_number_of_retries,
+            eth_config.backoff_factor,
+            eth_config.min_retry_delay,
+            eth_config.max_retry_delay,
+        );
         let on_chain_proposer_address = committer_config.on_chain_proposer_address;
 
         Ok(Self {
