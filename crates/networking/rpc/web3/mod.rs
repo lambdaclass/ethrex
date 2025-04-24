dad863102e8d42c5bcbe9dabb0b2c47ed3cb1789
Lucas Rack <lucas.rack@lambdaclass.com>
2025-04-24 10:53:34 -0300

    Use const variables

--- crates/networking/rpc/web3/mod.rs
+++ crates/networking/rpc/web3/mod.rs
@@ -3,11 +3,14 @@ use serde_json::Value;
 
 use crate::utils::{RpcErr, RpcRequest};
 
+const ETHREX_PKG_NAME: &str = env!("CARGO_PKG_NAME");
+const ETHREX_PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
+
 pub fn client_version(_req: &RpcRequest, _store: Store) -> Result<Value, RpcErr> {
     Ok(Value::String(format!(
-        "{}/v{}-{}/{}/{}",
-        env!("CARGO_PKG_NAME"),
-        env!("CARGO_PKG_VERSION"),
+        "{}/v{}-{}/{}/rustc-v{}",
+        ETHREX_PKG_NAME,
+        ETHREX_PKG_VERSION,
         "stable-blabl",
         std::env::consts::OS,
         env!("CARGO_PKG_RUST_VERSION")
