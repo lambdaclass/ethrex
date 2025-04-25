lambdaclass/ethrex

commit 91a5cd30f3cf5cfd930461fb2372134fc2104faf

feat (l1): write nodes in batches during storage healing (#2288)
**Motivation**
In a similar fashion to #2270, this PR aims to reduce the time spent
writing data to the DB by writing data in batches. In this case the
nodes received during storage healing are written all at once using the
already existing `put_batch` method of the TrieDB trait.
This could only be done for nodes belonging to the same trie, as it
would otherwise involve leaking and constraining the internal
representation of TrieDB.
This has shown to reduce the time spent writing storage nodes in the DB
from around 4 seconds to less than 20 milliseconds
<!-- Why does this pull request exist? What are its goals? -->

**Description**
* Add `write_node_batch` method for `TrieState` relying on
`TrieDB::put_batch`
* Refactor storage healer code to write all nodes for a trie in a single
operation
<!-- A clear and concise general description of the changes this PR
introduces -->

<!-- Link to issues: Resolves #111, Resolves #222 -->

Closes #issue_number

---------

Co-authored-by: Rodrigo Oliveri <rodrigooliveri10@gmail.com>

crates/networking/p2p/sync/storage_healing.rs
@@ -114,15 +114,17 @@ async fn heal_storage_batch(
         for (acc_path, paths) in batch.iter_mut() {
             let mut trie = store.open_storage_trie(*acc_path, *EMPTY_TRIE_HASH);
             // Get the corresponding nodes
-            for node in nodes.drain(..paths.len().min(nodes.len())) {
-                let path = paths.remove(0);
-                // Add children to batch
-                let children = node_missing_children(&node, &path, trie.state())?;
-                paths.extend(children);
-                let hash = node.compute_hash();
-                trie.state_mut().write_node(node, hash)?;
-            }
-            // Cut the loop if we ran out of nodes
+            let trie_nodes: Vec<ethrex_trie::Node> =
+                nodes.drain(..paths.len().min(nodes.len())).collect();
+            // Add children to batch
+            let children = trie_nodes
+                .iter()
+                .zip(paths.drain(..paths.len().min(nodes.len())))
+                .map(|(node, path)| node_missing_children(node, &path, trie.state()))
+                .collect::<Result<Vec<_>, _>>()?;
+            paths.extend(children.into_iter().flatten());
+            // Write nodes to trie
+            trie.state_mut().write_node_batch(&nodes)?;
             if nodes.is_empty() {
                 break;
             }

crates/common/trie/state.rs
@@ -100,4 +100,18 @@ impl TrieState {
         }
         Ok(())
     }
+
+    /// Writes a node batch directly to the DB bypassing the cache
+    pub fn write_node_batch(&mut self, nodes: &[Node]) -> Result<(), TrieError> {
+        // Don't insert the node if it is already inlined on the parent
+        let key_values = nodes
+            .iter()
+            .filter_map(|node| {
+                let hash = node.compute_hash();
+                matches!(hash, NodeHash::Hashed(_)).then(|| (hash.into(), node.encode_to_vec()))
+            })
+            .collect();
+        self.db.put_batch(key_values)?;
+        Ok(())
+    }
 }

commit 6d71c280df31ba7b4e6f91a1f5e8993558af6953

fix(l1): bug in storage healer (#2468)
**Motivation**
There is currently a bug in the storage healer causing fetched paths to
not be properly updated. This makes storage healing virtually infinite
as fetched paths are constantly being added back to the queue.
This fix should restore regular storage healing behaviour
<!-- Why does this pull request exist? What are its goals? -->

**Description**
* Fix logic error when updating pending paths for the next fetch during
storage healing
<!-- A clear and concise general description of the changes this PR
introduces -->

**Other info**
This bug was unknowingly introduced by #2288 
<!-- Link to issues: Resolves #111, Resolves #222 -->

Closes #issue_number

crates/networking/p2p/sync/storage_healing.rs
@@ -122,10 +122,10 @@ async fn heal_storage_batch(
             // Get the corresponding nodes
             let trie_nodes: Vec<ethrex_trie::Node> =
                 nodes.drain(..paths.len().min(nodes.len())).collect();
-            // Add children to batch
+            // Update batch: remove fetched paths & add children
             let children = trie_nodes
                 .iter()
-                .zip(paths.drain(..paths.len().min(nodes.len())))
+                .zip(paths.drain(..trie_nodes.len()))
                 .map(|(node, path)| node_missing_children(node, &path, trie.state()))
                 .collect::<Result<Vec<_>, _>>()?;
             paths.extend(children.into_iter().flatten());