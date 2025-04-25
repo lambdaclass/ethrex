ffbe069cf9f78cceed064fa0449a2f3ba5752a95
ilitteri <ilitteri@fi.uba.ar>
2025-04-25 16:38:04 -0300

    Fix compilation under based feature

--- cmd/ethrex/l2/mod.rs
+++ cmd/ethrex/l2/mod.rs
@@ -2,6 +2,8 @@ pub mod command;
 pub mod options;
 
 pub use command::Command;
+#[cfg(feature = "based")]
+pub use options::BasedOptions;
 pub use options::{
     CommitterOptions, DeployerOptions, EthOptions, Options as L2Options, ProofCoordinatorOptions,
     ProposerOptions, SequencerOptions, WatcherOptions,
