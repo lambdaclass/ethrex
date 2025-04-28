3df3d2a495e1f6d1ceead32ae3a319eccb5974fd
Martin Paulucci <martin.c.paulucci@gmail.com>
2025-04-28 16:29:13 -0300

    Revert chage.

--- cmd/ethrex/cli.rs
+++ cmd/ethrex/cli.rs
@@ -97,7 +97,7 @@ pub struct Options {
     pub dev: bool,
     #[arg(
         long = "evm",
-        default_value = EvmEngine::default().to_string(),
+        default_value = "levm",
         value_name = "EVM_BACKEND",
         help = "Has to be `levm` or `revm`",
         value_parser = utils::parse_evm_engine,
