a9758764897e977c895eb7c9c2521cc70f6e9df2
ilitteri <ilitteri@fi.uba.ar>
2025-04-29 16:48:51 -0300

    Kill sequencer config toml

--- crates/l2/utils/config/toml_parser.rs
+++ crates/l2/utils/config/toml_parser.rs
@@ -1,102 +1,8 @@
-use crate::utils::config::{
-    errors::{ConfigError, TomlParserError},
-    ConfigMode,
-};
+use crate::utils::config::errors::{ConfigError, TomlParserError};
 use serde::Deserialize;
 use std::fs::OpenOptions;
 use std::io::Write;
-
-#[derive(Deserialize, Debug)]
-struct Deployer {
-    l1_address: String,
-    l1_private_key: String,
-    risc0_contract_verifier: String,
-    sp1_contract_verifier: String,
-    pico_contract_verifier: String,
-    sp1_deploy_verifier: bool,
-    pico_deploy_verifier: bool,
-    salt_is_zero: bool,
-}
-
-impl Deployer {
-    fn to_env(&self) -> String {
-        let prefix = "DEPLOYER";
-        format!(
-            "{prefix}_L1_ADDRESS={}
-{prefix}_L1_PRIVATE_KEY={}
-{prefix}_RISC0_CONTRACT_VERIFIER={}
-{prefix}_SP1_CONTRACT_VERIFIER={}
-{prefix}_PICO_CONTRACT_VERIFIER={}
-{prefix}_SP1_DEPLOY_VERIFIER={}
-{prefix}_PICO_DEPLOY_VERIFIER={}
-{prefix}_SALT_IS_ZERO={}
-",
-            self.l1_address,
-            self.l1_private_key,
-            self.risc0_contract_verifier,
-            self.sp1_contract_verifier,
-            self.pico_contract_verifier,
-            self.sp1_deploy_verifier,
-            self.pico_deploy_verifier,
-            self.salt_is_zero
-        )
-    }
-}
-
-#[derive(Deserialize, Debug)]
-struct Eth {
-    rpc_url: String,
-}
-
-impl Eth {
-    fn to_env(&self) -> String {
-        let prefix = "ETH";
-        format!(
-            "
-{prefix}_RPC_URL={}
-",
-            self.rpc_url,
-        )
-    }
-}
-
-#[derive(Deserialize, Debug)]
-struct Watcher {
-    bridge_address: String,
-}
-
-impl Watcher {
-    pub fn to_env(&self) -> String {
-        let prefix = "L1_WATCHER";
-        format!(
-            "
-{prefix}_BRIDGE_ADDRESS={}
-",
-            self.bridge_address,
-        )
-    }
-}
-
-#[derive(Deserialize, Debug)]
-struct Committer {
-    l1_address: String,
-    on_chain_proposer_address: String,
-    validium: bool,
-}
-
-impl Committer {
-    pub fn to_env(&self) -> String {
-        let prefix = "COMMITTER";
-        format!(
-            "
-{prefix}_L1_ADDRESS={}
-{prefix}_ON_CHAIN_PROPOSER_ADDRESS={}
-{prefix}_VALIDIUM={}
-",
-            self.l1_address, self.on_chain_proposer_address, self.validium
-        )
-    }
-}
+use std::path::{Path, PathBuf};
 
 #[derive(Deserialize, Debug)]
 struct ProverClient {
@@ -116,48 +22,13 @@ impl ProverClient {
     }
 }
 
-#[derive(Deserialize, Debug)]
-struct ProverServer {
-    l1_address: String,
-}
-
-impl ProverServer {
-    fn to_env(&self) -> String {
-        let prefix = "PROVER_SERVER";
-        format!(
-            "
-{prefix}_L1_ADDRESS={}
-",
-            self.l1_address,
-        )
-    }
-}
-
-#[derive(Deserialize, Debug)]
-struct L2Config {
-    deployer: Deployer,
-    eth: Eth,
-    watcher: Watcher,
-    committer: Committer,
-    prover_server: ProverServer,
-}
-
-impl L2Config {
-    fn to_env(&self) -> String {
-        let mut env_representation = String::new();
-
-        env_representation.push_str(&self.deployer.to_env());
-        env_representation.push_str(&self.eth.to_env());
-        env_representation.push_str(&self.watcher.to_env());
-        env_representation.push_str(&self.committer.to_env());
-        env_representation.push_str(&self.prover_server.to_env());
-
-        env_representation
-    }
-}
-
-fn write_to_env(config: String, mode: ConfigMode) -> Result<(), TomlParserError> {
-    let env_file_path = mode.get_env_path_or_default();
+fn write_to_env(config: String) -> Result<(), TomlParserError> {
+    let env_file_path = {
+        let cargo_manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        std::env::var("PROVER_ENV_FILE")
+            .map(Into::into)
+            .unwrap_or(cargo_manifest_dir.join(".env.prover"))
+    };
     let env_file = OpenOptions::new()
         .write(true)
         .create(true)
@@ -185,36 +56,23 @@ fn write_to_env(config: String, mode: ConfigMode) -> Result<(), TomlParserError>
     Ok(())
 }
 
-fn read_config(config_path: String, mode: ConfigMode) -> Result<(), ConfigError> {
-    let toml_path = mode.get_config_file_path(&config_path);
-    let toml_file_name = toml_path
-        .file_name()
-        .ok_or(ConfigError::Custom("Invalid CONFIGS_PATH".to_string()))?
-        .to_str()
-        .ok_or(ConfigError::Custom("Couldn't convert to_str()".to_string()))?
-        .to_owned();
-    let file = std::fs::read_to_string(toml_path).map_err(|err| {
-        TomlParserError::TomlFileNotFound(format!("{err}: {}", toml_file_name.clone()), mode)
-    })?;
-    match mode {
-        ConfigMode::Sequencer => {
-            let config: L2Config = toml::from_str(&file).map_err(|err| {
-                TomlParserError::TomlFormat(format!("{err}: {}", toml_file_name.clone()), mode)
-            })?;
-            write_to_env(config.to_env(), mode)?;
-        }
-        ConfigMode::ProverClient => {
-            let config: ProverClient = toml::from_str(&file).map_err(|err| {
-                TomlParserError::TomlFormat(format!("{err}: {}", toml_file_name.clone()), mode)
-            })?;
-            write_to_env(config.to_env(), mode)?;
-        }
-    }
+fn read_config(config_path: String) -> Result<(), ConfigError> {
+    let toml_path = {
+        let prover_client_config_file_name = std::env::var("PROVER_CLIENT_CONFIG_FILE")
+            .unwrap_or("prover_client_config.toml".to_owned());
+        Path::new(&config_path).join(prover_client_config_file_name)
+    };
+    let file = std::fs::read_to_string(toml_path)
+        .map_err(|err| TomlParserError::TomlFileNotFound(format!("{err}: prover")))?;
+
+    let config: ProverClient = toml::from_str(&file)
+        .map_err(|err| TomlParserError::TomlFormat(format!("{err}: prover")))?;
+    write_to_env(config.to_env())?;
 
     Ok(())
 }
 
-pub fn parse_configs(mode: ConfigMode) -> Result<(), ConfigError> {
+pub fn parse_configs() -> Result<(), ConfigError> {
     #[allow(clippy::expect_fun_call, clippy::expect_used)]
     let config_path = std::env::var("CONFIGS_PATH").expect(
         format!(
@@ -226,5 +84,5 @@ If running locally, a reasonable value would be CONFIGS_PATH=./configs",
         .as_str(),
     );
 
-    read_config(config_path, mode).map_err(From::from)
+    read_config(config_path).map_err(From::from)
 }
