0a21db0652b49f4874eec4e5168798e1fe0d53ac
Lucas Fiegl <iovoid@users.noreply.github.com>
2025-04-30 11:51:04 -0300

    improve demo

--- crates/l2/tee/quote-gen/src/main.rs
+++ crates/l2/tee/quote-gen/src/main.rs
@@ -1,23 +1,39 @@
-use configfs_tsm::OpenQuote;
-use dcap_qvl::collateral::get_collateral_from_pcs;
-use dcap_qvl::verify::verify;
+use configfs_tsm::create_tdx_quote;
+use serde::Serialize;
+use keccak_hash::keccak;
+use std::{thread, time::Duration};
+
+#[derive(Serialize)]
+struct BoundData {
+    input: u64,
+    output: u64
+}
+
+fn inc(n: u64) -> u64 {
+    n + 1
+}
+
+fn run_inc(input: u64) -> BoundData {
+    BoundData {
+        input,
+        output: inc(input)
+    }
+}
+
+type GenericError = Result<(), Box<dyn std::error::Error>>;
 
 #[tokio::main]
-async fn main() {
-    let mut quote = OpenQuote::new(&"test").unwrap();
-
-    // Assert that the provider must be TDX
-    quote.check_provider(vec!["tdx_guest"]).unwrap();
-
-    // Give 64 null bytes as input data
-    quote.write_input([0; 64]).unwrap();
-
-    let output = quote.read_output().unwrap();
-    println!("Quote: {:?}", output);
-    println!("Generation: {}", quote.read_generation().unwrap());
-
-    let collateral = get_collateral_from_pcs(&output, std::time::Duration::from_secs(10)).await.expect("failed to get collateral");
-    let now = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs();
-    let tcb = verify(&output, &collateral, now).expect("failed to verify quote");
-    println!("{:?}", tcb);
+async fn main() -> GenericError {
+    let mut state = 100;
+    loop {
+        let bound_data = run_inc(state);
+        state = bound_data.output;
+        let digest = keccak(serde_json::to_string(&bound_data)?);
+        let mut digest_slice = [0u8; 64];
+        digest_slice.split_at_mut(32).1.copy_from_slice(digest.as_bytes());
+        let quote = create_tdx_quote(digest_slice).unwrap();
+        println!("{} -> {}", bound_data.input, bound_data.output);
+        println!("0x{:x?}", quote);
+        thread::sleep(Duration::from_millis(5000));
+    }
 }
