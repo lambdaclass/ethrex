209bcae6bf304db3ae9a507e12e62fd139fc85aa
github-action-benchmark <github@users.noreply.github.com>
2025-05-02 18:07:32 -0300

    add L1 block proving benchmark (SP1 backend) (customBiggerIsBetter) benchmark result for 722863e189458d8461d30d5a16acbd7135f1be7a

---
+++ dev/l1-prover-test/data.js
@@ -0,0 +1,33 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1746220052817,
+  "repoUrl": "https://github.com/lambdaclass/ethrex",
+  "entries": {
+    "L1 block proving benchmark (SP1 backend)": [
+      {
+        "commit": {
+          "author": {
+            "name": "lambdaclass",
+            "username": "lambdaclass"
+          },
+          "committer": {
+            "name": "lambdaclass",
+            "username": "lambdaclass"
+          },
+          "id": "722863e189458d8461d30d5a16acbd7135f1be7a",
+          "message": "feat(l2): bench job",
+          "timestamp": "2025-04-30T22:49:16Z",
+          "url": "https://github.com/lambdaclass/ethrex/pull/2663/commits/722863e189458d8461d30d5a16acbd7135f1be7a"
+        },
+        "date": 1746220051926,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Proving gas rate",
+            "value": 0.014479522784810126,
+            "unit": "Mgas/s"
+          }
+        ]
+      }
+    ]
+  }
+}
