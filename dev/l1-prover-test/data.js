d8918e6ac98d8c1beb4933e02608c3545885897e
github-action-benchmark <github@users.noreply.github.com>
2025-05-02 18:32:03 -0300

    add L1 block proving benchmark (customBiggerIsBetter) benchmark result for c5bfcb52c208f23d88564f0b2d3622b7bc3b6d6b

--- dev/l1-prover-test/data.js
+++ dev/l1-prover-test/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1746220052817,
+  "lastUpdate": 1746221523034,
   "repoUrl": "https://github.com/lambdaclass/ethrex",
   "entries": {
     "L1 block proving benchmark (SP1 backend)": [
@@ -24,6 +24,33 @@ window.BENCHMARK_DATA = {
           {
             "name": "Proving gas rate",
             "value": 0.014479522784810126,
+            "unit": "Mgas/s"
+          }
+        ]
+      }
+    ],
+    "L1 block proving benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "lambdaclass",
+            "username": "lambdaclass"
+          },
+          "committer": {
+            "name": "lambdaclass",
+            "username": "lambdaclass"
+          },
+          "id": "c5bfcb52c208f23d88564f0b2d3622b7bc3b6d6b",
+          "message": "feat(l2): bench job",
+          "timestamp": "2025-04-30T22:49:16Z",
+          "url": "https://github.com/lambdaclass/ethrex/pull/2663/commits/c5bfcb52c208f23d88564f0b2d3622b7bc3b6d6b"
+        },
+        "date": 1746221522715,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "SP1 backend, CPU",
+            "value": 0.01213640304054054,
             "unit": "Mgas/s"
           }
         ]
