12612af98dcf763e1829e10b2ac2e5ff1fb53cc7
Leandro Serra <leandro.serra@lambdaclass.com>
2025-05-06 16:02:07 -0300

    refactor move get_withdrawals_merkle_root out of impl block

--- crates/l2/sequencer/l1_committer.rs
+++ crates/l2/sequencer/l1_committer.rs
@@ -139,8 +139,7 @@ impl Committer {
             return Ok(());
         }
 
-        let withdrawal_logs_merkle_root =
-            self.get_withdrawals_merkle_root(withdrawal_hashes.clone())?;
+        let withdrawal_logs_merkle_root = get_withdrawals_merkle_root(withdrawal_hashes.clone())?;
 
         info!("Sending commitment for batch {batch_to_commit}. first_block: {first_block_to_commit}, last_block: {last_block_of_batch}");
 
@@ -323,17 +322,6 @@ impl Committer {
             }
         }
         Ok(ret)
-    }
-
-    fn get_withdrawals_merkle_root(
-        &self,
-        withdrawals_hashes: Vec<H256>,
-    ) -> Result<H256, CommitterError> {
-        if !withdrawals_hashes.is_empty() {
-            merkelize(withdrawals_hashes).map_err(CommitterError::FailedToMerkelize)
-        } else {
-            Ok(H256::zero())
-        }
     }
 
     fn get_block_deposits(
@@ -616,4 +604,12 @@ async fn estimate_blob_gas(
     };
 
     Ok(blob_gas)
+}
+
+pub fn get_withdrawals_merkle_root(withdrawals_hashes: Vec<H256>) -> Result<H256, CommitterError> {
+    if !withdrawals_hashes.is_empty() {
+        merkelize(withdrawals_hashes).map_err(CommitterError::FailedToMerkelize)
+    } else {
+        Ok(H256::zero())
+    }
 }
