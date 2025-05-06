8144e723d8ea251587726d6ebd02d9a93304476f
Lucas Fiegl <iovoid@users.noreply.github.com>
2025-05-06 09:09:16 -0300

    add signature-based quote generator

--- crates/l2/tee/quote-gen/src/main.rs
+++ crates/l2/tee/quote-gen/src/main.rs
@@ -1,10 +1,13 @@
 use configfs_tsm::create_tdx_quote;
 use keccak_hash::keccak;
 use std::{thread, time::Duration};
+use zerocopy::IntoBytes;
 
 use eth_encode_packed::{SolidityDataType, TakeLastXBytes};
 use eth_encode_packed::ethabi::ethereum_types::U256;
 use eth_encode_packed::abi::encode_packed;
+
+use alloy::signers::{local::PrivateKeySigner, Signer};
 
 fn inc(n: u64) -> u64 {
     n + 1
@@ -24,16 +27,23 @@ type GenericError = Result<(), Box<dyn std::error::Error>>;
 
 #[tokio::main]
 async fn main() -> GenericError {
+    let signer = PrivateKeySigner::random();
+    println!("{:#?}", signer.address());
+    
+    let mut digest_slice = [0u8; 64];
+    digest_slice.split_at_mut(20).0.copy_from_slice(signer.address().as_bytes());
+    let quote = create_tdx_quote(digest_slice).unwrap();
+    println!("0x{}", hex::encode(quote));
+    
     let mut state = 100;
     loop {
         let (new_state, bound_data) = run_inc(state);
-        let mut digest_slice = [0u8; 64];
-        let digest = keccak(&bound_data);
-        digest_slice.split_at_mut(32).1.copy_from_slice(digest.as_bytes());
-        let quote = create_tdx_quote(digest_slice).unwrap();
+        let signature = signer.sign_message(&bound_data).await?;
+
         println!("{} -> {}", state, new_state);
         state = new_state;
-        println!("0x{}", hex::encode(quote));
+        println!("{signature}");
+        
         thread::sleep(Duration::from_millis(5000));
     }
 }
