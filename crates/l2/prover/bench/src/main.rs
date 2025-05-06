94cbc5a88c48b97d492720d015d3cb39703a9560
Leandro Serra <leandro.serra@lambdaclass.com>
2025-05-06 17:23:33 -0300

    fix lint

--- crates/l2/prover/bench/src/main.rs
+++ crates/l2/prover/bench/src/main.rs
@@ -1,6 +1,8 @@
 use std::{fs::File, io::Write};
 
 use clap::Parser;
+#[cfg(feature = "l2")]
+use ethrex_common::H256;
 use ethrex_prover_bench::{
     cache::{load_cache, write_cache, Cache},
     rpc::{db::RpcDB, get_block, get_latest_block_number},
@@ -87,6 +89,8 @@ async fn main() {
             blocks: vec![block],
             parent_block_header,
             db,
+            #[cfg(feature = "l2")] // ethrex-prover-bench does not support L2 blocks
+            withdrawals_merkle_root: H256::zero(),
         })
         .expect("proving failed");
     } else {
@@ -95,6 +99,8 @@ async fn main() {
             blocks: vec![block],
             parent_block_header,
             db,
+            #[cfg(feature = "l2")]
+            withdrawals_merkle_root: H256::zero(),
         })
         .expect("proving failed");
     }
