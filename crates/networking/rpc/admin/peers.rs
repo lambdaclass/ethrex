c8855e593529b3480f76d73660b85d17cf9077ae
fmoletta <fedemoletta@hotmail.com>
2025-05-09 15:52:30 -0300

    push file

---
+++ crates/networking/rpc/admin/peers.rs
@@ -0,0 +1,74 @@
+use std::net::IpAddr;
+
+use ethrex_common::H512;
+use ethrex_p2p::{kademlia::PeerData, rlpx::p2p::Capability};
+use serde::Serialize;
+use serde_json::Value;
+
+use crate::{rpc::RpcApiContext, utils::RpcErr};
+
+#[derive(Serialize)]
+pub struct RpcPeer {
+    caps: Vec<Capability>,
+    enode: String,
+    id: H512,
+    name: String,
+    network: PeerNetwork,
+    protocols: Protocols,
+}
+
+#[derive(Serialize)]
+struct PeerNetwork {
+    remote_address: IpAddr, // We can add more data about the connection here, such as if it is inbound, the local address, etc
+}
+
+#[derive(Default, Serialize)]
+struct Protocols {
+    snap: Option<ProtocolData>,
+    eth: Option<ProtocolData>,
+    p2p: Option<ProtocolData>,
+}
+
+#[derive(Serialize)]
+struct ProtocolData {
+    version: u32,
+}
+
+impl From<PeerData> for RpcPeer {
+    fn from(peer: PeerData) -> Self {
+        let mut protocols = Protocols::default();
+        // Fill protocol data
+        for cap in &peer.supported_capabilities {
+            match cap {
+                // TODO (https://github.com/lambdaclass/ethrex/issues/1578) Save the versions of each capability
+                // For now we will be hardcoding our supported versions
+                Capability::P2p => protocols.p2p = Some(ProtocolData { version: 5 }),
+                Capability::Eth => protocols.eth = Some(ProtocolData { version: 68 }),
+                Capability::Snap => protocols.snap = Some(ProtocolData { version: 1 }),
+                // Ignore capabilities we don't know of for now as we cannot provide a version
+                Capability::UnsupportedCapability(_) => {}
+            }
+        }
+        RpcPeer {
+            caps: peer.supported_capabilities,
+            enode: peer.node.enode_url(),
+            id: peer.node.node_id,
+            name: "Unknown Node Name".to_string(),
+            network: PeerNetwork {
+                remote_address: peer.node.ip,
+            },
+            protocols,
+        }
+    }
+}
+
+pub fn peers(context: &RpcApiContext) -> Result<Value, RpcErr> {
+    let peers = context
+        .peer_handler
+        .read_connected_peers()
+        .ok_or(RpcErr::Internal(String::from("Failed to read peers")))?
+        .into_iter()
+        .map(|peer| RpcPeer::from(peer))
+        .collect::<Vec<_>>();
+    Ok(serde_json::to_value(peers)?)
+}
