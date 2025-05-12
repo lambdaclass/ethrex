f378165d00e1dfbe613a2640c2e8a3654a11aed1
SDartayet <sofiadartayet@gmail.com>
2025-05-12 11:24:18 -0300

    Change runner to work with the refactor

--- cmd/ef_tests/state/runner/levm_runner.rs
+++ cmd/ef_tests/state/runner/levm_runner.rs
@@ -9,7 +9,10 @@ use ethrex_common::{
     H256, U256,
 };
 use ethrex_levm::{
-    db::gen_db::GeneralizedDatabase, errors::{ExecutionReport, TxValidationError, VMError}, vm::VM, EVMConfig, Environment
+    db::gen_db::GeneralizedDatabase,
+    errors::{ExecutionReport, TxValidationError, VMError},
+    vm::VM,
+    EVMConfig, Environment,
 };
 use ethrex_rlp::encode::RLPEncode;
 use ethrex_storage::AccountUpdate;
