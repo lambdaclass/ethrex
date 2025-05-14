817228d51c2c36ba5f1e53269da571ab72022964
Manuel Iñaki Bilbao <manuel.bilbao@lambdaclass.com>
2025-05-14 15:12:18 -0300

    Lint

--- crates/l2/tee/quote-pusher/src/test.rs
+++ crates/l2/tee/quote-pusher/src/test.rs
@@ -5,7 +5,7 @@ mod quote_pusher_integration_test {
     use std::str::FromStr;
 
     use ethereum_types::{Address, H160, H256};
-    use ethrex_l2_sdk::{calldata::encode_calldata, get_address_from_secret_key};
+    use ethrex_l2_sdk::calldata::encode_calldata;
     use ethrex_rpc::EthClient;
     use secp256k1::SecretKey;
 
