9785635a9d1ecd59a3c7c83a7a518a791eb51c23
Manuel Iñaki Bilbao <manuel.bilbao@lambdaclass.com>
2025-05-16 13:43:45 -0300

    Remove legacy functions

--- crates/l2/utils/parse.rs
+++ crates/l2/utils/parse.rs
@@ -1,23 +1,5 @@
-use std::str::FromStr;
-
 use ethereum_types::{Address, H256};
-use reqwest::Url;
 
 pub fn hash_to_address(hash: H256) -> Address {
     Address::from_slice(&hash.as_fixed_bytes()[12..])
-}
-
-pub fn deserialize_url<'de, D>(deserializer: D) -> Result<Url, D::Error>
-where
-    D: serde::Deserializer<'de>,
-{
-    let url_str: String = serde::Deserialize::deserialize(deserializer)?;
-    Url::from_str(&url_str).map_err(serde::de::Error::custom)
-}
-
-pub fn deserialize_optional_url<'de, D>(deserializer: D) -> Result<Option<Url>, D::Error>
-where
-    D: serde::Deserializer<'de>,
-{
-    Ok(deserialize_url(deserializer).ok())
 }
