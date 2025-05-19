c5fdad416975055fa2dda5add34ed27b0b93a43d
Leandro Serra <leandro.serra@lambdaclass.com>
2025-05-19 16:03:32 -0300

    fix avoid diff

--- crates/l2/sequencer/l1_committer.rs
+++ crates/l2/sequencer/l1_committer.rs
@@ -79,11 +79,7 @@ impl Committer {
     ) -> Result<Self, CommitterError> {
         Ok(Self {
             eth_client: EthClient::new_with_config(
-                eth_config
-                    .rpc_url
-                    .iter()
-                    .map(AsRef::as_ref)
-                    .collect::<Vec<&str>>(),
+                eth_config.rpc_url.iter().map(AsRef::as_ref).collect(),
                 eth_config.max_number_of_retries,
                 eth_config.backoff_factor,
                 eth_config.min_retry_delay,
