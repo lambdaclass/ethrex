c8e9183ec73a7aaad439b42beef240db0c2f14a5
JereSalo <jeresalo17@gmail.com>
2025-05-19 15:24:31 -0300

    erase not used stuff

--- cmd/ef_tests/blockchain/tests/prague.rs
+++ cmd/ef_tests/blockchain/tests/prague.rs
@@ -4,22 +4,14 @@ use ef_tests_blockchain::test_runner::parse_and_execute;
 use ethrex_vm::EvmEngine;
 
 #[cfg(not(feature = "levm"))]
-const SKIPPED_TESTS_REVM: [&str; 1] = [
-    "tests/prague/eip7702_set_code_tx/test_set_code_txs.py::test_set_code_to_non_empty_storage[fork_Prague-blockchain_test-zero_nonce]", // Skipped because REVM doesn't support this.
-];
-
-#[cfg(feature = "levm")]
-const SKIPPED_TESTS_LEVM: [&str; 0] = [];
-
-#[cfg(not(feature = "levm"))]
 fn parse_and_execute_with_revm(path: &Path) -> datatest_stable::Result<()> {
-    parse_and_execute(path, EvmEngine::REVM, Some(&SKIPPED_TESTS_REVM));
+    parse_and_execute(path, EvmEngine::REVM, None);
     Ok(())
 }
 
 #[cfg(feature = "levm")]
 fn parse_and_execute_with_levm(path: &Path) -> datatest_stable::Result<()> {
-    parse_and_execute(path, EvmEngine::LEVM, Some(&SKIPPED_TESTS_LEVM));
+    parse_and_execute(path, EvmEngine::LEVM, None);
     Ok(())
 }
 
