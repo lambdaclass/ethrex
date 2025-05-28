38f42f4319d77d1e0d61e20bf128596130cfea2f
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2025-05-28 17:55:15 -0300

    update risc0 program

--- crates/l2/prover/zkvm/interface/risc0/src/main.rs
+++ crates/l2/prover/zkvm/interface/risc0/src/main.rs
@@ -1,26 +1,36 @@
 use risc0_zkvm::guest::env;
 
 use ethrex_blockchain::{validate_block, validate_gas_used};
-use ethrex_common::Address;
-use ethrex_storage::AccountUpdate;
+use ethrex_common::{
+    types::blobs_bundle::{blob_from_bytes, kzg_commitment_to_versioned_hash},
+    Address,
+};
+use ethrex_common::types::AccountUpdate;
 use ethrex_vm::Evm;
+use kzg_rs::{dtypes::Blob, kzg_proof::KzgProof, trusted_setup::get_kzg_settings};
 use std::collections::HashMap;
-#[cfg(feature = "l2")]
-use zkvm_interface::deposits::{get_block_deposits, get_deposit_hash};
-#[cfg(feature = "l2")]
-use zkvm_interface::withdrawals::{get_block_withdrawals, get_withdrawals_merkle_root};
 use zkvm_interface::{
     io::{ProgramInput, ProgramOutput},
     trie::{update_tries, verify_db},
 };
 
-fn main() {
+#[cfg(feature = "l2")]
+use ethrex_l2_common::{
+    get_block_deposits, get_block_withdrawal_hashes, compute_deposit_logs_hash, compute_withdrawals_merkle_root,
+};
+
+pub fn main() {
     let ProgramInput {
-        block,
+        blocks,
         parent_block_header,
         mut db,
+        elasticity_multiplier,
         #[cfg(feature = "l2")]
-        deposit_logs_hash,
+        state_diff,
+        #[cfg(feature = "l2")]
+        blob_commitment,
+        #[cfg(feature = "l2")]
+        blob_proof,
     } = env::read();
     // Tries used for validating initial and final state root
     let (mut state_trie, mut storage_tries) = db
@@ -44,13 +54,19 @@ fn main() {
     let mut cumulative_gas_used = 0;
 
     #[cfg(feature = "l2")]
-    let mut withdrawals = vec![];
+    let mut withdrawal_hashes = vec![];
     #[cfg(feature = "l2")]
     let mut deposits_hashes = vec![];
 
     for block in blocks {
         // Validate the block
-        validate_block(&block, &parent_header, &db.chain_config).expect("invalid block");
+        validate_block(
+            &block,
+            &parent_header,
+            &db.chain_config,
+            elasticity_multiplier,
+        )
+        .expect("invalid block");
 
         // Execute block
         let mut vm = Evm::from_prover_db(db.clone());
@@ -63,19 +79,26 @@ fn main() {
         // Get L2 withdrawals and deposits for this block
         #[cfg(feature = "l2")]
         {
-            let block_withdrawals = get_block_withdrawals(&block.body.transactions, &receipts)
-                .expect("failed to get block withdrawals");
-            let block_deposits = get_block_deposits(&block.body.transactions);
+            let txs = block.body.transactions;
+            let block_deposits = get_block_deposits(&txs);
+
+            let txs_and_receipts: Vec<_> = txs.into_iter().zip(receipts.clone().into_iter()).collect();
+            let block_withdrawal_hashes = get_block_withdrawal_hashes(&txs_and_receipts).expect("failed to retrieve withdrawal hashes");
             let mut block_deposits_hashes = Vec::with_capacity(block_deposits.len());
-            for deposit in block_deposits {
-                block_deposits_hashes.push(
-                    deposit
-                        .get_deposit_hash()
-                        .expect("Failed to get deposit hash for tx"),
-                );
+            for deposit in &block_deposits {
+                if let Some(hash) = deposit.get_deposit_hash() {
+                    block_deposits_hashes.push(hash);
+                } else {
+                    return Err("Failed to get deposit hash for tx".to_string().into());
+                }
             }
-            withdrawals.extend(block_withdrawals);
-            deposits_hashes.extend(block_deposits_hashes);
+            withdrawal_hashes.extend(block_withdrawal_hashes);
+            deposits_hashes.extend(
+                block_deposits
+                    .iter()
+                    .filter_map(|tx| tx.get_deposit_hash())
+                    .collect::<Vec<_>>(),
+            );
         }
 
         cumulative_gas_used += receipts
@@ -100,22 +123,31 @@ fn main() {
         parent_header = block.header;
     }
 
+    // Check state diffs are valid
+    #[cfg(feature = "l2")]
+    let state_diff_updates = state_diff
+        .to_account_updates(&state_trie)
+        .expect("failed to calculate account updates from state diffs");
+
     // Calculate L2 withdrawals root
     #[cfg(feature = "l2")]
-    let Ok(withdrawals_merkle_root) = get_withdrawals_merkle_root(withdrawals) else {
+    let Ok(withdrawals_merkle_root) = compute_withdrawals_merkle_root(withdrawal_hashes) else {
         panic!("Failed to calculate withdrawals merkle root");
     };
 
     // Calculate L2 deposits logs root
     #[cfg(feature = "l2")]
-    let Ok(deposit_logs_hash) = get_deposit_hash(deposits_hashes) else {
+    let Ok(deposit_logs_hash) = compute_deposit_logs_hash(deposits_hashes) else {
         panic!("Failed to calculate deposits logs hash");
     };
 
     // Update state trie
-    let acc_account_updates: Vec<AccountUpdate> = acc_account_updates.values().cloned().collect();
-    update_tries(&mut state_trie, &mut storage_tries, &acc_account_updates)
-        .expect("failed to update state and storage tries");
+    update_tries(
+        &mut state_trie,
+        &mut storage_tries,
+        &acc_account_updates.values().cloned().collect::<Vec<_>>(),
+    )
+    .expect("failed to update state and storage tries");
 
     // Calculate final state root hash and check
     let final_state_hash = state_trie.hash_no_commit();
@@ -123,14 +155,51 @@ fn main() {
         panic!("invalid final state trie");
     }
 
+    // Check state diffs are valid
+    #[cfg(feature = "l2")]
+    if state_diff_updates != acc_account_updates {
+        panic!("invalid state diffs")
+    }
+
+    // Verify KZG blob proof
+    #[cfg(feature = "l2")]
+    let blob_versioned_hash = {
+        let encoded_state_diff = state_diff.encode().expect("failed to encode state diff");
+        let blob_data = blob_from_bytes(encoded_state_diff)
+            .expect("failed to convert encoded state diff into blob data");
+        let blob = Blob::from_slice(&blob_data).expect("failed to convert blob data into Blob");
+
+        let blob_proof_valid = KzgProof::verify_blob_kzg_proof(
+            blob,
+            &kzg_rs::Bytes48::from_slice(&blob_commitment)
+                .expect("failed type conversion for blob commitment"),
+            &kzg_rs::Bytes48::from_slice(&blob_proof)
+                .expect("failed type conversion for blob proof"),
+            &get_kzg_settings(),
+        )
+        .expect("failed to verify blob proof (neither valid or invalid proof)");
+
+        if !blob_proof_valid {
+            panic!("invalid blob proof");
+        }
+
+        kzg_commitment_to_versioned_hash(&blob_commitment)
+    };
+
+    // Output gas for measurement purposes
     env::write(&cumulative_gas_used);
 
-    env::commit(&ProgramOutput {
-        initial_state_hash,
-        final_state_hash,
-        #[cfg(feature = "l2")]
-        withdrawals_merkle_root,
-        #[cfg(feature = "l2")]
-        deposit_logs_hash,
-    });
+    env::write(
+        &ProgramOutput {
+            initial_state_hash,
+            final_state_hash,
+            #[cfg(feature = "l2")]
+            withdrawals_merkle_root,
+            #[cfg(feature = "l2")]
+            deposit_logs_hash,
+            #[cfg(feature = "l2")]
+            blob_versioned_hash,
+        }
+        .encode(),
+    );
 }
