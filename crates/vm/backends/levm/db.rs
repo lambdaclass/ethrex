5de64375238b1b86ef098728a7eaab090a2ce8ff
Martin Paulucci <martin.c.paulucci@gmail.com>
2025-05-30 17:04:09 +0200

    Remove unused code.

--- crates/vm/backends/levm/db.rs
+++ crates/vm/backends/levm/db.rs
@@ -1,7 +1,6 @@
 use ethrex_common::types::Account;
 use ethrex_common::U256 as CoreU256;
 use ethrex_common::{Address as CoreAddress, H256 as CoreH256};
-// use ethrex_levm::constants::EMPTY_CODE_HASH;
 use ethrex_levm::db::Database as LevmDatabase;
 
 use crate::db::DynVmDatabase;
@@ -146,69 +145,3 @@ impl LevmDatabase for DynVmDatabase {
             .map_err(|e| DatabaseError::Custom(e.to_string()))
     }
 }
-
-// impl LevmDatabase for ProverDBError {
-//     fn get_account(&self, address: CoreAddress) -> Result<Account, DatabaseError> {
-//         let Some(acc_info) = self.accounts.get(&address) else {
-//             return Ok(Account::default());
-//         };
-
-//         let acc_code = if acc_info.code_hash != EMPTY_CODE_HASH {
-//             self.code
-//                 .get(&acc_info.code_hash)
-//                 .ok_or(DatabaseError::Custom(format!(
-//                     "Could not find account's code hash {}",
-//                     &acc_info.code_hash
-//                 )))?
-//         } else {
-//             &bytes::Bytes::new()
-//         };
-
-//         Ok(Account::new(
-//             acc_info.balance,
-//             acc_code.clone(),
-//             acc_info.nonce,
-//             HashMap::new(),
-//         ))
-//     }
-
-//     fn account_exists(&self, address: CoreAddress) -> bool {
-//         self.accounts.contains_key(&address)
-//     }
-
-//     fn get_block_hash(&self, block_number: u64) -> Result<CoreH256, DatabaseError> {
-//         self.block_hashes
-//             .get(&block_number)
-//             .cloned()
-//             .ok_or_else(|| {
-//                 DatabaseError::Custom(format!(
-//                     "Block hash not found for block number {block_number}"
-//                 ))
-//             })
-//     }
-
-//     fn get_storage_value(
-//         &self,
-//         address: CoreAddress,
-//         key: CoreH256,
-//     ) -> Result<CoreU256, DatabaseError> {
-//         let Some(storage) = self.storage.get(&address) else {
-//             return Ok(CoreU256::default());
-//         };
-//         Ok(*storage.get(&key).unwrap_or(&CoreU256::default()))
-//     }
-
-//     fn get_chain_config(&self) -> Result<ethrex_common::types::ChainConfig, DatabaseError> {
-//         Ok(self.get_chain_config())
-//     }
-
-//     fn get_account_code(&self, code_hash: CoreH256) -> Result<bytes::Bytes, DatabaseError> {
-//         match self.code.get(&code_hash) {
-//             Some(code) => Ok(code.clone()),
-//             None => Err(DatabaseError::Custom(format!(
-//                 "Could not find code for hash {}",
-//                 code_hash
-//             ))),
-//         }
-//     }
-// }
--- crates/vm/backends/revm/db.rs
+++ crates/vm/backends/revm/db.rs
@@ -66,51 +66,6 @@ impl From<DynVmDatabase> for EvmState {
     }
 }
 
-// impl DatabaseRef for ProverDB {
-//     /// The database error type.
-//     type Error = ProverDBError;
-
-//     /// Get basic account information.
-//     fn basic_ref(&self, address: RevmAddress) -> Result<Option<RevmAccountInfo>, Self::Error> {
-//         let Some(account_info) = self.accounts.get(&CoreAddress::from(address.0.as_ref())) else {
-//             return Ok(None);
-//         };
-
-//         Ok(Some(RevmAccountInfo {
-//             balance: RevmU256::from_limbs(account_info.balance.0),
-//             nonce: account_info.nonce,
-//             code_hash: RevmB256::from_slice(&account_info.code_hash.0),
-//             code: None,
-//         }))
-//     }
-
-//     /// Get account code by its hash.
-//     fn code_by_hash_ref(&self, code_hash: RevmB256) -> Result<RevmBytecode, Self::Error> {
-//         self.code
-//             .get(&CoreH256::from(code_hash.as_ref()))
-//             .map(|b| RevmBytecode::new_raw(RevmBytes(b.clone())))
-//             .ok_or(ProverDBError::CodeNotFound(code_hash))
-//     }
-
-//     /// Get storage value of address at index.
-//     fn storage_ref(&self, address: RevmAddress, index: RevmU256) -> Result<RevmU256, Self::Error> {
-//         self.storage
-//             .get(&CoreAddress::from(address.0.as_ref()))
-//             .ok_or(ProverDBError::AccountNotFound(address))?
-//             .get(&CoreH256::from(index.to_be_bytes()))
-//             .map(|v| RevmU256::from_limbs(v.0))
-//             .ok_or(ProverDBError::StorageValueNotFound(address, index))
-//     }
-
-//     /// Get block hash by block number.
-//     fn block_hash_ref(&self, number: u64) -> Result<RevmB256, Self::Error> {
-//         self.block_hashes
-//             .get(&number)
-//             .map(|h| RevmB256::from_slice(&h.0))
-//             .ok_or(ProverDBError::BlockHashNotFound(number))
-//     }
-// }
-
 impl revm::Database for DynVmDatabase {
     type Error = EvmError;
 
