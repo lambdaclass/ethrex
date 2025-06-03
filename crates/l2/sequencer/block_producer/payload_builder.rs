00ffd9c409e79eff6102902244770ab166c57c0e
Javier Rodríguez Chatruc <49622509+jrchatruc@users.noreply.github.com>
2025-06-03 14:43:43 -0300

    Update crates/l2/sequencer/block_producer/payload_builder.rs
    Co-authored-by: Ivan Litteri <67517699+ilitteri@users.noreply.github.com>

--- crates/l2/sequencer/block_producer/payload_builder.rs
+++ crates/l2/sequencer/block_producer/payload_builder.rs
@@ -174,10 +174,7 @@ pub async fn fill_transactions(
 
         if let Some(acc_info) = maybe_sender_acc_info {
             if head_tx.nonce() < acc_info.nonce {
-                debug!(
-                    "Removing transaction with nonce too low from mempool: {}",
-                    tx_hash
-                );
+                debug!("Removing transaction with nonce too low from mempool: {tx_hash:#x}");
                 txs.pop();
                 blockchain.remove_transaction_from_pool(&tx_hash)?;
                 continue;
