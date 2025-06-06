1688d4e9171d46439b9d07b19374e33ada935c67
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2025-06-06 18:46:01 -0300

    nit imports zkvm

--- crates/l2/prover/zkvm/interface/pico/src/main.rs
+++ crates/l2/prover/zkvm/interface/pico/src/main.rs
@@ -1,21 +1,13 @@
 #![no_main]
 
 use pico_sdk::io::{commit, read_as};
-
-use zkvm_interface::io::ProgramInput;
-
-#[cfg(feature = "l2")]
-use ethrex_l2_common::{
-    get_block_deposits, get_block_withdrawal_hashes, compute_deposit_logs_hash, compute_withdrawals_merkle_root,
-};
-#[cfg(feature = "l2")]
-use ethrex_common::types::blobs_bundle::{blob_from_bytes, kzg_commitment_to_versioned_hash};
+use zkvm_interface::{io::ProgramInput, execution::execution_program};
 
 pico_sdk::entrypoint!(main);
 
 pub fn main() {
     let input: ProgramInput = read_as();
-    let output = zkvm_interface::execution::execution_program(input).unwrap();
+    let output = execution_program(input).unwrap();
 
     commit(&output);
 }
