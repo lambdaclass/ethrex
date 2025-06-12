c7ba2a094a35f677a62536a02894b1c0409baba4
Camila Di Ielsi <camila.diielsi@lambdaclass.com>
2025-06-12 13:25:04 -0300

    fix imports alphabetical order

--- crates/networking/p2p/kademlia.rs
+++ crates/networking/p2p/kademlia.rs
@@ -4,8 +4,8 @@ use crate::{
     types::{Node, NodeRecord},
 };
 use ethrex_common::{H256, U256};
-use std::sync::Arc;
 use rand::random;
+use std::sync::Arc;
 use tokio::sync::mpsc::UnboundedSender;
 use tokio::sync::{mpsc, Mutex};
 use tracing::debug;
