01a270c69cb9eb49570d1b118d89dd39d8aced2a
Leandro Serra <leandro.serra@lambdaclass.com>
2025-06-13 11:59:16 -0300

    feat add RETURN_TRANSFER_PRIVATE_KEY

--- crates/l2/tests/tests.rs
+++ crates/l2/tests/tests.rs
@@ -24,6 +24,11 @@ const DEFAULT_L1_RICH_WALLET_PRIVATE_KEY: H256 = H256([
     0x94, 0x1e, 0x10, 0x33, 0x20, 0x61, 0x5d, 0x39, 0x4a, 0x55, 0x70, 0x8b, 0xe1, 0x3e, 0x45, 0x99,
     0x4c, 0x7d, 0x93, 0xb9, 0x32, 0xb0, 0x64, 0xdb, 0xcb, 0x2b, 0x51, 0x1f, 0xe3, 0x25, 0x4e, 0x2e,
 ]);
+// 0xbcdf20249abf0ed6d944c0288fad489e33f66b3960d9e6229c1cd214ed3bbe31
+const DEFAULT_L2_RETURN_TRANSFER_PRIVATE_KEY: H256 = H256([
+    0xbc, 0xdf, 0x20, 0x24, 0x9a, 0xbf, 0x0e, 0xd6, 0xd9, 0x44, 0xc0, 0x28, 0x8f, 0xad, 0x48, 0x9e,
+    0x33, 0xf6, 0x6b, 0x39, 0x60, 0xd9, 0xe6, 0x22, 0x9c, 0x1c, 0xd2, 0x14, 0xed, 0x3b, 0xbe, 0x31,
+]);
 // 0x8ccf74999c496e4d27a2b02941673f41dd0dab2a
 const DEFAULT_BRIDGE_ADDRESS: Address = H160([
     0x8c, 0xcf, 0x74, 0x99, 0x9c, 0x49, 0x6e, 0x4d, 0x27, 0xa2, 0xb0, 0x29, 0x41, 0x67, 0x3f, 0x41,
@@ -49,9 +54,12 @@ async fn l2_integration_test() -> Result<(), Box<dyn std::error::Error>> {
     let eth_client = eth_client();
     let proposer_client = proposer_client();
     let rich_wallet_private_key = l1_rich_wallet_private_key();
+    let transfer_return_private_key = l2_return_transfer_private_key();
     let bridge_address = common_bridge_address();
-    let deposit_recipient_address = Address::random();
-    let transfer_recipient_address = Address::random();
+    let deposit_recipient_address = get_address_from_secret_key(&rich_wallet_private_key)
+        .expect("Failed to get address from l1 rich wallet pk");
+    let transfer_recipient_address = get_address_from_secret_key(&transfer_return_private_key)
+        .expect("Failed to get address from l2 transfer return pk");
 
     test_deposit(
         &rich_wallet_private_key,
@@ -911,6 +919,14 @@ fn l1_rich_wallet_private_key() -> SecretKey {
         .map(|pk| pk.parse().expect("Invalid l1 rich wallet pk"))
         .unwrap_or(DEFAULT_L1_RICH_WALLET_PRIVATE_KEY);
     SecretKey::from_slice(l1_rich_wallet_pk.as_bytes()).unwrap()
+}
+
+fn l2_return_transfer_private_key() -> SecretKey {
+    let l2_return_deposit_private_key =
+        std::env::var("INTEGRATION_TEST_RETURN_TRANSFER_PRIVATE_KEY")
+            .map(|pk| pk.parse().expect("Invalid l1 rich wallet pk"))
+            .unwrap_or(DEFAULT_L2_RETURN_TRANSFER_PRIVATE_KEY);
+    SecretKey::from_slice(l2_return_deposit_private_key.as_bytes()).unwrap()
 }
 
 async fn wait_for_l2_deposit_receipt(
