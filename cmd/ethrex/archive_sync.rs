80b20e5571418057262ab94903635b04f23c6e7c
fmoletta <fedemoletta@hotmail.com>
2025-06-13 10:50:08 -0300

    Progress

--- cmd/ethrex/archive_sync.rs
+++ cmd/ethrex/archive_sync.rs
@@ -2,12 +2,13 @@ lazy_static::lazy_static! {
     static ref CLIENT: reqwest::Client = reqwest::Client::new();
 }
 
-use std::collections::HashMap;
+use std::collections::{BTreeMap, HashMap};
 
 use ethrex_common::{types::BlockNumber, Bytes, H256, U256};
 use ethrex_rpc::authentication::generate_jwt_token;
 use ethrex_rpc::clients::auth::RpcResponse;
-use serde::Deserialize;
+use keccak_hash::keccak;
+use serde::{Deserialize, Deserializer};
 use serde_json::{json, Value};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::UnixStream;
@@ -18,7 +19,10 @@ use ethrex_common::{serde_utils, Address};
 struct Dump {
     #[serde(rename = "root")]
     state_root: H256,
-    accounts: HashMap<Address, DumpAccount>,
+    #[serde(deserialize_with = "deser_account_dump_map")]
+    accounts: BTreeMap<H256, DumpAccount>,
+    #[serde(default)]
+    next: Option<String>
 }
 
 #[derive(Deserialize, Debug)]
@@ -31,14 +35,16 @@ struct DumpAccount {
     code_hash: H256,
     #[serde(default, with = "serde_utils::bytes")]
     code: Bytes,
-    address: Address,
+    #[serde(default)]
+    // Didnt parse a dump with storage yet, may fail
+    storage: HashMap<H256, U256>,
+    address: Option<Address>,
     #[serde(rename = "key")]
-    hashed_address: H256,
+    hashed_address: Option<H256>,
 }
 
 pub async fn archive_sync_2(archive_ipc_path: &str, block_number: BlockNumber) -> eyre::Result<()> {
     let mut stream = UnixStream::connect(archive_ipc_path).await?;
-
     let request = &json!({
     "id": 1,
     "jsonrpc": "2.0",
@@ -46,8 +52,21 @@ pub async fn archive_sync_2(archive_ipc_path: &str, block_number: BlockNumber) -
     "params": [format!("{:#x}", block_number)]
     });
     let response = send_ipc_json_request(&mut stream, request).await?;
+    //dbg!(&response);
     let dump: Dump = serde_json::from_value(response)?;
     dbg!(&dump);
+    // We may need to perform more fetches if we didn't get the full state
+    if let Some(next) = dump.next {
+        let request = &json!({
+            "id": 1,
+            "jsonrpc": "2.0",
+            "method": "debug_accountRange",
+            "params": [format!("{:#x}", block_number), format!("{:#x}", dump.accounts.last_key_value().unwrap().0), 10, false, false, false]
+            });
+        let response = send_ipc_json_request(&mut stream, request).await?;
+        let dump: Dump = serde_json::from_value(response)?;
+        dbg!(&dump);
+    }
 
     Ok(())
 }
@@ -95,3 +114,17 @@ pub async fn archive_sync(
     dbg!(&res);
     Ok(())
 }
+
+/// Deserializes a map of Address -> DumpAccount into a sorted map of HashedAddress -> DumpAccount
+/// This is necessary as `debug_getAccountRange` sorts accounts by hashed address
+fn deser_account_dump_map<'de, D>(d: D) -> Result<BTreeMap<H256, DumpAccount>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let map = HashMap::<Address, DumpAccount>::deserialize(d)?;
+        // Order dump accounts by hashed address
+        Ok(map.into_iter().map(|(addr, acc)| {
+            let hashed_addr = acc.hashed_address.unwrap_or_else(|| keccak(addr));
+            (hashed_addr, acc)
+        }).collect())
+    }
