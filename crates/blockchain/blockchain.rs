d0dc5c3f27c022bcd019a654801db8d2cc8a6609
fkrause98 <fkrausear@gmail.com>
2025-06-17 14:23:01 -0300

    chore: remove unneded changes

--- crates/blockchain/blockchain.rs
+++ crates/blockchain/blockchain.rs
@@ -651,6 +651,7 @@ impl Blockchain {
             .get_block_header(header_no)?
             .ok_or(MempoolError::NoBlockHeaderError)?;
         let config = self.storage.get_chain_config()?;
+
         // NOTE: We could add a tx size limit here, but it's not in the actual spec
 
         // Check init code size
@@ -689,6 +690,7 @@ impl Blockchain {
         };
 
         let maybe_sender_acc_info = self.storage.get_account_info(header_no, sender).await?;
+
         if let Some(sender_acc_info) = maybe_sender_acc_info {
             if nonce < sender_acc_info.nonce || nonce == u64::MAX {
                 return Err(MempoolError::NonceTooLow);
@@ -697,6 +699,7 @@ impl Blockchain {
             let tx_cost = tx
                 .cost_without_base_fee()
                 .ok_or(MempoolError::InvalidTxGasvalues)?;
+
             if tx_cost > sender_acc_info.balance {
                 return Err(MempoolError::NotEnoughBalance);
             }
@@ -860,6 +863,8 @@ pub fn validate_gas_used(
 ) -> Result<(), ChainError> {
     if let Some(last) = receipts.last() {
         // Note: This is commented because it is still being used in development.
+        // dbg!(last.cumulative_gas_used);
+        // dbg!(block_header.gas_used);
         if last.cumulative_gas_used != block_header.gas_used {
             return Err(ChainError::InvalidBlock(InvalidBlockError::GasUsedMismatch));
         }
