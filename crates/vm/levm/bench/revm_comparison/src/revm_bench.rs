aafd8a00611a08c0730ccd4f543aad77d9dbcff5
JereSalo <jeresalo17@gmail.com>
2025-06-17 17:56:50 -0300

    little change in revm_bench

--- crates/vm/levm/bench/revm_comparison/src/revm_bench.rs
+++ crates/vm/levm/bench/revm_comparison/src/revm_bench.rs
@@ -8,7 +8,7 @@ use std::hint::black_box;
 pub fn run_with_revm(contract_code: &str, runs: u64, calldata: &str) {
     let rich_acc_address = address!("1000000000000000000000000000000000000000");
     let bytes = hex::decode(contract_code).unwrap();
-    let raw = Bytecode::new_raw(bytes.clone().into());
+    let raw_bytecode = Bytecode::new_raw(bytes.clone().into());
 
     let mut evm = Evm::builder()
         .modify_tx_env(|tx| {
@@ -16,11 +16,8 @@ pub fn run_with_revm(contract_code: &str, runs: u64, calldata: &str) {
             tx.transact_to = TransactTo::Call(Address::ZERO);
             tx.data = hex::decode(calldata).unwrap().into();
         })
-        .with_db(BenchmarkDB::new_bytecode(raw))
+        .with_db(BenchmarkDB::new_bytecode(raw_bytecode))
         .build();
-
-    let result = evm.transact().unwrap();
-    assert!(result.result.is_success());
 
     for _ in 0..runs - 1 {
         let result = black_box(evm.transact()).unwrap();
