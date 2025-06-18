603d42f30ab70624d735a7ced7a6c80778b76c2f
github-action-benchmark <github@users.noreply.github.com>
2025-06-18 14:06:10 -0300

    add L1 block proving benchmark (customBiggerIsBetter) benchmark result for e9249d599a8676e9493e433db80ae802e6a6a292

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1750264715757,
+  "lastUpdate": 1750266370083,
   "repoUrl": "https://github.com/lambdaclass/ethrex",
   "entries": {
     "Benchmark": [
@@ -16156,6 +16156,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "SP1, RTX A6000",
             "value": 0.008444190246516613,
+            "unit": "Mgas/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "47506558+MegaRedHand@users.noreply.github.com",
+            "name": "Tomás Grüner",
+            "username": "MegaRedHand"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "e9249d599a8676e9493e433db80ae802e6a6a292",
+          "message": "docs(l2): add ethrex based L2 roadmap (#3163)\n\n**Description**\n\nThis PR adds a draft of the ethrex based L2 roadmap.",
+          "timestamp": "2025-06-17T16:54:31Z",
+          "tree_id": "da751d5f90c1c014268a5d16d01514fd7010aa0e",
+          "url": "https://github.com/lambdaclass/ethrex/commit/e9249d599a8676e9493e433db80ae802e6a6a292"
+        },
+        "date": 1750266362113,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "SP1, RTX A6000",
+            "value": 0.008399178571428571,
             "unit": "Mgas/s"
           }
         ]
