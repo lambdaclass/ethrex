21b33a05d1b6e2dad3a79b7001a8a5ecdf57f80b
github-action-benchmark <github@users.noreply.github.com>
2025-06-18 20:57:44 +0000

    add Benchmark (cargo) benchmark result for e973e9688f3e0ec9c425eb3c5eb89b3ab5e369fe

---
+++ benchmarks/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1750280264562,
+  "repoUrl": "https://github.com/lambdaclass/ethrex",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "18153834+klaus993@users.noreply.github.com",
+            "name": "Klaus @ LambdaClass",
+            "username": "klaus993"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "e973e9688f3e0ec9c425eb3c5eb89b3ab5e369fe",
+          "message": "ci(l1,l2): publish Ethrex docs on https://docs.ethrex.xyz/ (#3217)\n\n**Motivation**\n\nPublish the mdbook of this repo (book.toml) to https://docs.ethrex.xyz/\n\n**Description**\n\nThese changes are to leave the setup like this:\n\n* https://docs.ethrex.xyz/ will have the mdbook\n* https://docs.ethrex.xyz/benchmarks will have the benchmarks graphs\n* https://docs.ethrex.xyz/flamegraphs will have the flamegraphs",
+          "timestamp": "2025-06-18T19:55:44Z",
+          "tree_id": "f19b7a45c9e78782d48bfd6e3c88a95e2f7fd5b1",
+          "url": "https://github.com/lambdaclass/ethrex/commit/e973e9688f3e0ec9c425eb3c5eb89b3ab5e369fe"
+        },
+        "date": 1750280257990,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "Block import/Block import ERC20 transfers",
+            "value": 222697754655,
+            "range": "± 836006884",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
