1557c8e1b1de0e2dc56fba4928bef7621ce7e2f9
Santiago Morales <s.moralesvranic@gmail.com>
2025-06-18 09:33:59 -0300

    solved test issues

--- crates/common/types/genesis.rs
+++ crates/common/types/genesis.rs
@@ -387,7 +387,7 @@ mod tests {
     #[test]
     fn deserialize_genesis_file() {
         // Deserialize genesis file
-        let file = File::open("../../../test_data/fixtures/genesis/kurtosis.json")
+        let file = File::open("../../test_data/fixtures/genesis/kurtosis.json")
             .expect("Failed to open genesis file");
         let reader = BufReader::new(file);
         let genesis: Genesis =
@@ -486,7 +486,7 @@ mod tests {
     #[test]
     fn genesis_block() {
         // Deserialize genesis file
-        let file = File::open("../../../test_data/fixtures/genesis/kurtosis.json")
+        let file = File::open("../../test_data/fixtures/genesis/kurtosis.json")
             .expect("Failed to open genesis file");
         let reader = BufReader::new(file);
         let genesis: Genesis =
@@ -528,7 +528,7 @@ mod tests {
     #[test]
     // Parses genesis received by kurtosis and checks that the hash matches the next block's parent hash
     fn read_and_compute_kurtosis_hash() {
-        let file = File::open("../../../test_data/fixtures/genesis/kurtosis.json")
+        let file = File::open("../../test_data/fixtures/genesis/kurtosis.json")
             .expect("Failed to open genesis file");
         let reader = BufReader::new(file);
         let genesis: Genesis =
@@ -543,7 +543,7 @@ mod tests {
 
     #[test]
     fn parse_hive_genesis_file() {
-        let file = File::open("../../../test_data/fixtures/genesis/hive.json")
+        let file = File::open("../../test_data/fixtures/genesis/hive.json")
             .expect("Failed to open genesis file");
         let reader = BufReader::new(file);
         let _genesis: Genesis =
@@ -552,7 +552,7 @@ mod tests {
 
     #[test]
     fn read_and_compute_hive_hash() {
-        let file = File::open("../../../test_data/fixtures/genesis/hive.json")
+        let file = File::open("../../test_data/fixtures/genesis/hive.json")
             .expect("Failed to open genesis file");
         let reader = BufReader::new(file);
         let genesis: Genesis =
