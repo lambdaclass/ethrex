d902587200a9ecf03b6e68116fc796d1b7eb0f2f
Lucas Fiegl <iovoid@users.noreply.github.com>
2025-06-18 09:57:56 -0300

    fix behavior on single-node tree

--- crates/networking/rpc/utils.rs
+++ crates/networking/rpc/utils.rs
@@ -272,11 +272,16 @@ pub fn parse_json_hex(hex: &serde_json::Value) -> Result<u64, String> {
 }
 
 pub fn merkle_proof(data: Vec<H256>, mut index: usize) -> Option<Vec<H256>> {
+    if index >= data.len() {
+        return None;
+    }
     use keccak_hash::keccak;
 
     let mut proof = vec![];
     let mut current = data.clone();
-    while current.len() > 1 {
+    let mut first = true;
+    while current.len() > 1 || first {
+        first = false;
         proof.push(*current.get(index ^ 1).or(current.get(index))?);
         index /= 2;
         current = current
