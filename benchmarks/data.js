5262c2c401a3420b5b889bac01ddc5eaea99db3a
github-action-benchmark <github@users.noreply.github.com>
2025-06-24 17:46:34 +0000

    add Benchmark (cargo) benchmark result for 196a17b1e734d7510cb48192b944361641ea29c3

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1750786188422,
+  "lastUpdate": 1750787194097,
   "repoUrl": "https://github.com/lambdaclass/ethrex",
   "entries": {
     "Benchmark": [
@@ -989,6 +989,36 @@ window.BENCHMARK_DATA = {
             "name": "Block import/Block import ERC20 transfers",
             "value": 222276700322,
             "range": "± 276327006",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "estefano.bargas@fing.edu.uy",
+            "name": "Estéfano Bargas",
+            "username": "xqft"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "196a17b1e734d7510cb48192b944361641ea29c3",
+          "message": "chore(l2): remove execution cache (#3091)\n\n**Motivation**\n\nWe can use the rollup store for this purpose (by adding a table to store\naccount updates)\n\n**Description**\n\n- deletes `ExecutionCache`, replaces it with the `StoreRollup`\n- adds new tables in all store backends for storing account updates\n- now the block producer has a reference to the rollup store, to push\naccount updates\n\n---------\n\nCo-authored-by: Tomás Paradelo <112426153+tomip01@users.noreply.github.com>\nCo-authored-by: Javier Rodríguez Chatruc <49622509+jrchatruc@users.noreply.github.com>",
+          "timestamp": "2025-06-24T16:46:08Z",
+          "tree_id": "2da6943241aaa38aba591c90dbd14323dd54d0ab",
+          "url": "https://github.com/lambdaclass/ethrex/commit/196a17b1e734d7510cb48192b944361641ea29c3"
+        },
+        "date": 1750787186615,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "Block import/Block import ERC20 transfers",
+            "value": 220310290530,
+            "range": "± 857262126",
             "unit": "ns/iter"
           }
         ]
