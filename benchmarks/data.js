e1f7e02c601b4aba9690043d34df7ccb2f6a269e
github-action-benchmark <github@users.noreply.github.com>
2025-06-26 16:25:24 -0300

    add L1 block proving benchmark (customBiggerIsBetter) benchmark result for e12f615f1a6e67c1fb719f6693d6627d8e2a2e70

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1750965209917,
+  "lastUpdate": 1750965924656,
   "repoUrl": "https://github.com/lambdaclass/ethrex",
   "entries": {
     "Benchmark": [
@@ -2144,6 +2144,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "SP1, RTX A6000",
             "value": 0.008671909190974133,
+            "unit": "Mgas/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "47506558+MegaRedHand@users.noreply.github.com",
+            "name": "Tomás Grüner",
+            "username": "MegaRedHand"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "e12f615f1a6e67c1fb719f6693d6627d8e2a2e70",
+          "message": "docs(l1): fix readme links and improve L1 documentation (#3340)\n\n**Motivation**\n\nThe current readme links to the unrendered documentation instead of our\nhosted book. Also, the general landing page doesn't have any links or\npointers on where to go next, while the L1 landing page is empty.\n\n**Description**\n\nThis PR addresses the previous issues, adding some content to the L1\nlanding page and generally cleaning up the docs. It also merges the two\ndocumentation sections in the readme and updates links to point to\ndocs.ethrex.xyz",
+          "timestamp": "2025-06-26T18:13:10Z",
+          "tree_id": "9da9d65d7fc47ea4423505e1996ffbbca8421bcc",
+          "url": "https://github.com/lambdaclass/ethrex/commit/e12f615f1a6e67c1fb719f6693d6627d8e2a2e70"
+        },
+        "date": 1750965915365,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "SP1, RTX A6000",
+            "value": 0.00861501312192455,
             "unit": "Mgas/s"
           }
         ]
