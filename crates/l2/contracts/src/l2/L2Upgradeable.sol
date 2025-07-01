342d0a9860f502e92da2dd21ca3ce4716024e12c
Lucas Fiegl <iovoid@users.noreply.github.com>
2025-07-01 17:53:26 -0300

    improve docs

--- crates/l2/contracts/src/l2/L2Upgradeable.sol
+++ crates/l2/contracts/src/l2/L2Upgradeable.sol
@@ -3,9 +3,12 @@ pragma solidity =0.8.29;
 
 import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
-/// @title Interface for an L2-capable token.
+/// @title Proxy for an L2 system contract
 /// @author LambdaClass
+/// @notice This contract is meant to be included in the genesis at the system contract address
+/// @dev The genesis generator sets the initial implementation using the well-known (ERC-1967) proxy slot
 contract UpgradeableSystemContract is TransparentUpgradeableProxy {
+    /// @notice The CommonBridge uses this address to send upgrades
     address constant ADMIN =  0x000000000000000000000000000000000000f000;
 
     constructor() TransparentUpgradeableProxy(address(0), address(0), "") {
@@ -14,6 +17,9 @@ contract UpgradeableSystemContract is TransparentUpgradeableProxy {
         revert("This contract is not meant to be directly deployed.");
     }
 
+    /// @notice We override this so that it can be easily included in the genesis
+    /// @dev The normal constructor has hard to simulate (deploying a ProxyAdmin, setting immutable variables)
+    /// @dev behavior, so this is the simpler way.
     function _proxyAdmin() internal pure override returns (address) {
         return ADMIN;
     }
