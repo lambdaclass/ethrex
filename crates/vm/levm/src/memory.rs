59ea9319490643f4974f3e5f8d77fef30189c7a1
Esteve Soler Arderiu <azteca1998@users.noreply.github.com>
2025-07-02 16:36:40 +0200

    Replace `x.checked_pow(2)` with `x.checked_mul(x)`.

--- crates/vm/levm/src/memory.rs
+++ crates/vm/levm/src/memory.rs
@@ -172,7 +172,10 @@ fn cost(memory_size: usize) -> Result<u64, VMError> {
         .ok_or(OutOfGas)?
         / WORD_SIZE_IN_BYTES_USIZE;
 
-    let gas_cost = (memory_size_word.checked_pow(2).ok_or(OutOfGas)? / MEMORY_EXPANSION_QUOTIENT)
+    let gas_cost = (memory_size_word
+        .checked_mul(memory_size_word)
+        .ok_or(OutOfGas)?
+        / MEMORY_EXPANSION_QUOTIENT)
         .checked_add(3usize.checked_mul(memory_size_word).ok_or(OutOfGas)?)
         .ok_or(OutOfGas)?;
 
