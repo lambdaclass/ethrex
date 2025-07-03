7180e5dad41fa717ec1ec0ddb3a0d286b1935519
Mechardo <30327624+Mechanix97@users.noreply.github.com>
2025-07-03 16:04:41 -0300

    Update crates/l2/contracts/src/l1/based/OnChainProposer.sol
    Co-authored-by: Tomás Paradelo <112426153+tomip01@users.noreply.github.com>

--- crates/l2/contracts/src/l1/based/OnChainProposer.sol
+++ crates/l2/contracts/src/l1/based/OnChainProposer.sol
@@ -60,7 +60,7 @@ contract OnChainProposer is
     /// @notice Committed and verified batches
     /// @dev This mapping contains the batch hashes and tells if they are committed or verified.
     /// @dev If the value is false, the batch has been committed but not verified yet.
-    /// @dev If the value is true, the batch has been committed and verified by the proporser.
+    /// @dev If the value is true, the batch has been committed and verified by the proposer.
     mapping(bytes32 => bool) public verifiedBatches;
 
     address public BRIDGE;
