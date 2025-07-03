55fceac76923e10c8d502839bf4b758f93e440ec
Sofi Azcoaga <sofiaazc3@hotmail.com>
2025-07-03 12:27:20 -0300

    include error block hash in log when syncing fails

--- crates/networking/p2p/sync.rs
+++ crates/networking/p2p/sync.rs
@@ -453,12 +453,14 @@ impl Syncer {
         };
 
         if let Err((error, failure)) = res {
-            warn!("Failed to add block during FullSync: {error}");
+
             if let Some(BatchBlockProcessingFailure {
                 failed_block_hash,
                 last_valid_hash,
             }) = failure
             {
+                warn!("Failed to add block with hash {failed_block_hash} during FullSync - error: {error}");
+                info!("Saving last valid block with hash {last_valid_hash}"); 
                 store
                     .set_latest_valid_ancestor(failed_block_hash, last_valid_hash)
                     .await?;
