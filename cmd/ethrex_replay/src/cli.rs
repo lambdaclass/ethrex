2a8b19148d87fdf26e06a882e4c9d4e9ceffd35d
avilagaston9 <gaston.avila@lambdaclass.com>
2025-07-04 17:57:56 -0300

    docs(core): improve ethrex-replay docs

--- cmd/ethrex_replay/src/cli.rs
+++ cmd/ethrex_replay/src/cli.rs
@@ -318,7 +318,7 @@ enum EthrexReplayCommand {
         about = "Proves blocks, ranges of blocks, or individual transactions."
     )]
     Prove(SubcommandProve),
-    #[command(about = "Proves blocks, ranges of blocks, or individual transactions.")]
+    #[command(about = "Plots the compositiong of a range of blocks.")]
     BlockComposition {
         #[arg(help = "Starting block. (Inclusive)")]
         start: usize,
