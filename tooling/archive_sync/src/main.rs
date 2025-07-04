cb96103a1b6beeaa778b8ad57fd6e4efff8390a5
fmoletta <fedemoletta@hotmail.com>
2025-07-04 15:24:10 -0300

    Document constants

--- tooling/archive_sync/src/main.rs
+++ tooling/archive_sync/src/main.rs
@@ -28,7 +28,9 @@ use tokio::task::JoinSet;
 use tracing::info;
 use tracing_subscriber::FmtSubscriber;
 
+/// Max account dumps to ask for in a single request. The current value matches geth's maximum output.
 const MAX_ACCOUNTS: usize = 256;
+/// Amount of blocks before the target block to request hashes for. These may be needed to execute the next block after the target block.
 const BLOCK_HASH_LOOKUP_DEPH: u64 = 128;
 
 #[derive(Deserialize, Debug)]
