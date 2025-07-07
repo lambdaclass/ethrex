db3c13f1b25a69272044020b5fb6984a7d7ce835
Edgar Luque <git@edgl.dev>
2025-07-07 15:56:03 +0200

    lint

--- crates/vm/levm/src/opcode_handlers/environment.rs
+++ crates/vm/levm/src/opcode_handlers/environment.rs
@@ -210,6 +210,8 @@ impl<'a> VM<'a> {
             return Ok(OpcodeResult::Continue { pc_increment: 1 });
         }
 
+        // Here we verified indexing is in bounds.
+        #[expect(clippy::indexing_slicing, clippy::arithmetic_side_effects)]
         if code_offset < current_call_frame.bytecode.len().into() {
             let code_offset: usize = code_offset
                 .try_into()
