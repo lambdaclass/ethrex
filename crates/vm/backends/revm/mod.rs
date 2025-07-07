220f90d4e8d3f0f5df964aafd6590256a881049b
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2025-07-07 11:22:26 -0300

    TEMP remove let chains

--- crates/vm/backends/revm/mod.rs
+++ crates/vm/backends/revm/mod.rs
@@ -324,9 +324,10 @@ impl REVM {
             // Apply account changes to DB
             let mut account_update = AccountUpdate::new(address);
             // If the account was changed then both original and current info will be present in the bundle account
-            if account.is_info_changed()
-                && let Some(new_acc_info) = account.account_info()
-            {
+            if account.is_info_changed() {
+                let Some(new_acc_info) = account.account_info() else {
+                    panic!();
+                };
                 // Update account info in DB
                 let code_hash = H256::from_slice(new_acc_info.code_hash.as_slice());
                 let account_info = AccountInfo {
@@ -337,8 +338,10 @@ impl REVM {
                 account_update.info = Some(account_info);
                 // Update code in db
                 if account.is_contract_changed()
-                    && let Some(code) = new_acc_info.code
                 {
+                    let Some(code) = new_acc_info.code else {
+                        panic!()
+                    };
                     account_update.code = Some(code.original_bytes().0);
                 }
             }
