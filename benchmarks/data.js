eadb0d6dd891c3d0fdb538d45e7994874e0ef064
github-action-benchmark <github@users.noreply.github.com>
2025-07-08 18:16:19 +0000

    add Benchmark (cargo) benchmark result for d1ceb86cb32f8949ecd3fc279084f6921c3e757f

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1751996007031,
+  "lastUpdate": 1751998579321,
   "repoUrl": "https://github.com/lambdaclass/ethrex",
   "entries": {
     "Benchmark": [
@@ -2995,6 +2995,36 @@ window.BENCHMARK_DATA = {
             "name": "Block import/Block import ERC20 transfers",
             "value": 209527821851,
             "range": "± 527992986",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "47506558+MegaRedHand@users.noreply.github.com",
+            "name": "Tomás Grüner",
+            "username": "MegaRedHand"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "d1ceb86cb32f8949ecd3fc279084f6921c3e757f",
+          "message": "fix(l1): ignore unknown protocols in capability exchange (#3543)\n\n**Motivation**\n\nFailing due to a peer having extra capabilities can make us lose\nexceptional peers. Hence, we want to ignore any extra capabilities they\nhave.\n\n**Description**\n\nThis PR changes `Capability.protocol` to be an 8-byte array instead of a\nstring, allowing us to store any string we receive.",
+          "timestamp": "2025-07-08T17:12:21Z",
+          "tree_id": "b73d081e492be3f36630c76381bba95066e5b585",
+          "url": "https://github.com/lambdaclass/ethrex/commit/d1ceb86cb32f8949ecd3fc279084f6921c3e757f"
+        },
+        "date": 1751998569223,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "Block import/Block import ERC20 transfers",
+            "value": 209724867206,
+            "range": "± 374609234",
             "unit": "ns/iter"
           }
         ]
