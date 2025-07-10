2ec1fed1454465319336d9ca70cd616fc2a298cb
Lucas Fiegl <iovoid@users.noreply.github.com>
2025-07-10 11:33:54 -0300

    add safety annotations and tests

--- crates/common/crypto/blake2f/avx.rs
+++ crates/common/crypto/blake2f/avx.rs
@@ -34,7 +34,7 @@ const IV: AlignTo32<[u64; 8]> = AlignTo32([
 
 // https://github.com/sneves/blake2-avx2/blob/master/blake2b.c#L64
 #[inline(always)]
-unsafe fn word_permutation(
+fn word_permutation(
     rounds_to_permute: usize,
     mut a: __m256i,
     mut b: __m256i,
@@ -42,6 +42,16 @@ unsafe fn word_permutation(
     mut d: __m256i,
     m: &[u64; 16],
 ) -> (__m256i, __m256i) {
+    /// # Safety
+    /// The only operations of concern here are:
+    /// - _mm_load_si128: loads memory and requires data to be aligned to 16 bytes
+    /// - _mm256_i32gather_epi64<N>: loads memory
+    /// The loads are justified at each call. Note that 128 bits corresponds to 4x u32.
+    /// For the gather operations, it's safety relies on:
+    ///     - The scale parameter is 8 (u64), the type of m's elements
+    ///     - All values in SIGMA are < m.length
+    ///     - This means load(m.as_ptr() + SIGMA[M][N] * 8) is valid
+    ///     - Gather is is equivalent to a sequence of such loads, therefore it's safe
     unsafe {
         let rotate24 = _mm256_setr_epi8(
             3, 4, 5, 6, 7, 0, 1, 2, 11, 12, 13, 14, 15, 8, 9, 10, 3, 4, 5, 6, 7, 0, 1, 2, 11, 12,
@@ -52,10 +62,11 @@ unsafe fn word_permutation(
             12, 13, 14, 15, 8, 9,
         );
         for round in 0..rounds_to_permute {
+            // SAFETY: SIGMA[N].0 is aligned to 16 bytes thanks to the AlignTo16 wrapper
             let subsigma = SIGMA[round % 10].0.as_ptr();
-
             let b0 = _mm256_i32gather_epi64::<8>(
                 m.as_ptr() as *const i64,
+                // SAFETY: loads subsigma[0..4], which is within bounds
                 _mm_load_si128(subsigma as *const __m128i),
             );
             // G1
@@ -71,6 +82,7 @@ unsafe fn word_permutation(
             //
             let b0 = _mm256_i32gather_epi64::<8>(
                 m.as_ptr() as *const i64,
+                // SAFETY: loads subsigma[4..8], which is within bounds
                 _mm_load_si128(subsigma.add(4) as *const __m128i),
             );
             // G2
@@ -90,6 +102,7 @@ unsafe fn word_permutation(
             //
             let b0 = _mm256_i32gather_epi64::<8>(
                 m.as_ptr() as *const i64,
+                // SAFETY: loads subsigma[8..12], which is within bounds
                 _mm_load_si128(subsigma.add(8) as *const __m128i),
             );
             // G1
@@ -105,6 +118,7 @@ unsafe fn word_permutation(
             //
             let b0 = _mm256_i32gather_epi64::<8>(
                 m.as_ptr() as *const i64,
+                // SAFETY: loads subsigma[12..16], which is within bounds
                 _mm_load_si128(subsigma.add(12) as *const __m128i),
             );
             // G2
@@ -126,38 +140,79 @@ unsafe fn word_permutation(
     }
 }
 
-/// Based on https://datatracker.ietf.org/doc/html/rfc7693#section-3.2
+// SAFETY: must check that avx2 is available before calling
 #[target_feature(enable = "avx2")]
-pub fn blake2f_compress_f_inner(
+pub unsafe fn blake2f_compress_f_inner(
     rounds: usize, // Specifies the rounds to permute
     h: &[u64; 8],  // State vector, defines the work vector (v) and affects the XOR process
     m: &[u64; 16], // The message block to compress
     t: &[u64; 2],  // Affects the work vector (v) before permutations
     f: bool,       // If set as true, inverts all bits
 ) -> [u64; 8] {
+    /// # Safety
+    /// The only operations of concern here are:
+    ///     - _mm256_load_si256: loads memory, which must be 32B-aligned
+    ///     - _mm256_store_si256: stores memory, which must be 32B-aligned
+    /// Both IV and output are made 32B-aligned using the AlignTo32 wrapper
     unsafe {
+        // This way the caller doesn't need to supply aligned memory
         let a = _mm256_setr_epi64x(h[0] as i64, h[1] as i64, h[2] as i64, h[3] as i64);
         let b = _mm256_setr_epi64x(h[4] as i64, h[5] as i64, h[6] as i64, h[7] as i64);
 
+        // SAFETY: loads IV[0..4], which is within bounds
         let c = _mm256_load_si256(IV.0.as_ptr() as *const __m256i);
         let d = _mm256_xor_si256(
+            // SAFETY: loads IV[0..4], which is within bounds
             _mm256_load_si256(IV.0.as_ptr().add(4) as *const __m256i),
             _mm256_setr_epi64x(t[0] as i64, t[1] as i64, -(f as i64), 0),
         );
 
         let (out0, out1) = word_permutation(rounds, a, b, c, d, &m);
 
-        let mut output = [0; 8];
+        /// output needs to be aligned to 32 bytes
+        let mut output = AlignTo32([0; 8]);
 
+        // SAFETY: stores to output[0..4], which is within bounds
         _mm256_store_si256(
-            output.as_mut_ptr() as *mut __m256i,
+            output.0.as_mut_ptr() as *mut __m256i,
             _mm256_xor_si256(out0, a),
         );
+        // SAFETY: stores to output[4..8], which is within bounds
         _mm256_store_si256(
-            output.as_mut_ptr().add(4) as *mut __m256i,
+            output.0.as_mut_ptr().add(4) as *mut __m256i,
             _mm256_xor_si256(out1, b),
         );
 
         output
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::blake2f::avx::blake2f_compress_f_inner;
+    #[test]
+    fn test_12r() {
+        let out = blake2f_compress_f_inner(
+            12,
+            &[1, 2, 3, 4, 5, 6, 7, 8],
+            &[
+                101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116,
+            ],
+            &[1000, 1001],
+            true,
+        );
+        assert_eq!(
+            out,
+            [
+                16719151077261791083,
+                2946084527549390899,
+                18258373236029374890,
+                15305391278487550604,
+                16233503039257535911,
+                17654926667207417465,
+                12194914407095793501,
+                13409096818966589674
+            ]
+        );
     }
 }
--- crates/common/crypto/blake2f/mod.rs
+++ crates/common/crypto/blake2f/mod.rs
@@ -10,8 +10,10 @@ type Blake2Func = unsafe fn(usize, &[u64; 8], &[u64; 16], &[u64; 2], bool) -> [u
 static BLAKE2_FUNC: LazyLock<Blake2Func> = LazyLock::new(|| {
     #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
     if is_x86_feature_detected!("avx2") {
+        // SAFETY: avx2 verified to be available
         return avx::blake2f_compress_f_inner;
     }
+    // SAFETY: safe function
     portable::blake2f_compress_f
 });
 
