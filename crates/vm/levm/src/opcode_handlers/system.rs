7f5c2ecfc7891317f43d4a6cb97d8de6d5c82b8c
fmoletta <fedemoletta@hotmail.com>
2025-07-10 13:16:21 -0300

    Resize memory before executing CALL

--- crates/vm/levm/src/opcode_handlers/system.rs
+++ crates/vm/levm/src/opcode_handlers/system.rs
@@ -78,6 +78,10 @@ impl<'a> VM<'a> {
                 eip7702_gas_consumed,
                 callee,
             )?;
+        // Make sure we have enough memory to write the return data
+        // This is also needed to make sure we expand the memory even in cases where we don't use return values (such as transfers)
+        try_resize(&mut self.current_call_frame_mut()?.memory, new_memory_size)?;
+
         let (cost, gas_limit) = gas_cost::call(
             new_memory_size,
             current_memory_size,
