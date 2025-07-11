84e5876c505e40bc046f4c6f0308c400259d472d
Lucas Rack <lucas.rack@lambdaclass.com>
2025-07-11 15:24:18 -0300

    Put space again

--- crates/l2/sequencer/l1_proof_sender.rs
+++ crates/l2/sequencer/l1_proof_sender.rs
@@ -324,6 +324,7 @@ pub async fn send_proof_to_contract(
             .as_slice(),
     ]
     .concat();
+
     let calldata = encode_calldata(VERIFY_FUNCTION_SIGNATURE, &calldata_values)?;
 
     let verify_tx_hash = send_verify_tx(
