774533aa45e1f88478496ff0993aed55cb36a1ff
ilitteri <ilitteri@fi.uba.ar>
2025-07-12 21:43:31 -0300

    Add spaces

--- crates/networking/p2p_2/monitor/mod.rs
+++ crates/networking/p2p_2/monitor/mod.rs
@@ -12,18 +12,26 @@ mod widgets;
 
 pub fn init_terminal() -> io::Result<Terminal<CrosstermBackend<io::Stdout>>> {
     enable_raw_mode()?;
+
     execute!(io::stdout(), EnterAlternateScreen, EnableMouseCapture)?;
+
     let backend = CrosstermBackend::new(io::stdout());
+
     Terminal::new(backend)
 }
 
 pub fn restore_terminal(terminal: &mut Terminal<CrosstermBackend<io::Stdout>>) -> io::Result<()> {
     disable_raw_mode()?;
+
     execute!(
         terminal.backend_mut(),
         LeaveAlternateScreen,
         DisableMouseCapture
     )?;
+
+    terminal.flush()?;
+
     terminal.show_cursor()?;
+
     Ok(())
 }
