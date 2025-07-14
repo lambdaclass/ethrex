d3d2353cb7b30231b37445c476bd888190bc9386
Leandro Serra <leandro.serra@lambdaclass.com>
2025-07-14 09:03:30 -0300

    fix(tdx): read git-revision from file or from vergen

--- crates/l2/build.rs
+++ crates/l2/build.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use std::{error::Error, fs};
 use vergen_git2::{Emitter, Git2Builder};
 
 // This build code is needed to add some env vars in order to construct the code version
@@ -6,6 +6,10 @@ use vergen_git2::{Emitter, Git2Builder};
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Export git commit hash and branch name as environment variables
+    if let Ok(sha) = fs::read_to_string("git-revision") {
+        println!("cargo:rustc-env=VERGEN_GIT_SHA={}", sha.trim());
+        return Ok(());
+    }
     let git2 = Git2Builder::default().sha(true).build()?;
 
     Emitter::default().add_instructions(&git2)?.emit()?;
