c96d35c684ec916f7ca7922963021d825a60fb7f
avilagaston9 <gaston.avila@lambdaclass.com>
2025-07-14 11:30:46 -0300

    refactor build script

--- crates/l2/contracts/bin/build.rs
+++ crates/l2/contracts/bin/build.rs
@@ -1,8 +1,8 @@
 //! Build script for the contract deployer binary.
 //! This script downloads dependencies and compiles contracts to be embedded as constants in the deployer.
 #![allow(clippy::unwrap_used, clippy::expect_used)]
-use std::env;
-use std::path::Path;
+
+use std::{env, fs, path::Path};
 
 fn main() {
     let out_dir = env::var_os("OUT_DIR").unwrap();
@@ -10,16 +10,16 @@ fn main() {
 
     download_contract_deps(&contracts_path);
 
-    // Compile the ERC1967Proxy contract;
-    compile_contract_to_bytecode(
+    // ERC1967Proxy contract.
+    compile_contract(
         &contracts_path,
-        &contracts_path.join("lib/openzeppelin-contracts-upgradeable/lib/openzeppelin-contracts/contracts/proxy/ERC1967"),
+        "lib/openzeppelin-contracts-upgradeable/lib/openzeppelin-contracts/contracts/proxy/ERC1967",
         "ERC1967Proxy",
         false,
         None,
     );
 
-    // Compile the SP1VerifierGroth16 contract
+    // SP1VerifierGroth16 contract
     ethrex_l2_sdk::compile_contract(
         &contracts_path,
         &contracts_path.join("lib/sp1-contracts/contracts/src/v5.0.0/SP1VerifierGroth16.sol"),
@@ -31,7 +31,7 @@ fn main() {
     decode_to_bytecode(&contracts_path, "SP1Verifier", false);
 
     // Get the openzeppelin contracts remappings
-    let remappings_raw = [
+    let remappings = [
         (
             "@openzeppelin/contracts",
             contracts_path.join(
@@ -43,42 +43,19 @@ fn main() {
             contracts_path.join("lib/openzeppelin-contracts-upgradeable/contracts"),
         ),
     ];
-    let remappings: Vec<(&str, &Path)> = remappings_raw
-        .iter()
-        .map(|(s, p)| (*s, p.as_path()))
-        .collect();
+    let remappings: Vec<(&str, &Path)> =
+        remappings.iter().map(|(s, p)| (*s, p.as_path())).collect();
 
-    // Compile the L1 contracts
-    compile_contract_to_bytecode(
-        &contracts_path,
-        Path::new("src/l1"),
-        "OnChainProposer",
-        false,
-        Some(&remappings),
-    );
-    compile_contract_to_bytecode(
-        &contracts_path,
-        Path::new("src/l1"),
-        "CommonBridge",
-        false,
-        Some(&remappings),
-    );
-
-    // Compile the L2 contracts
-    compile_contract_to_bytecode(
-        &contracts_path,
-        Path::new("src/l2"),
-        "CommonBridgeL2",
-        true,
-        Some(&remappings),
-    );
-    compile_contract_to_bytecode(
-        &contracts_path,
-        Path::new("src/l2"),
-        "L2ToL1Messenger",
-        true,
-        Some(&remappings),
-    );
+    // L1 contracts
+    let l1_contracts = [("src/l1", "OnChainProposer"), ("src/l1", "CommonBridge")];
+    for (dir, name) in l1_contracts {
+        compile_contract(&contracts_path, dir, name, false, Some(&remappings));
+    }
+    // L2 contracts
+    let l2_contracts = [("src/l2", "CommonBridgeL2"), ("src/l2", "L2ToL1Messenger")];
+    for (dir, name) in l2_contracts {
+        compile_contract(&contracts_path, dir, name, true, Some(&remappings));
+    }
 
     ethrex_l2_sdk::compile_contract(
         &contracts_path,
@@ -87,13 +64,12 @@ fn main() {
         Some(&remappings),
     )
     .unwrap();
-    println!("Successfully compiled L2Upgradeable contract");
     decode_to_bytecode(&contracts_path, "UpgradeableSystemContract", true);
 
-    // Compile based contracts
-    compile_contract_to_bytecode(
+    // Based contracts
+    compile_contract(
         &contracts_path,
-        Path::new("src/l1/based"),
+        "src/l1/based",
         "SequencerRegistry",
         false,
         Some(&remappings),
@@ -105,90 +81,78 @@ fn main() {
         Some(&remappings),
     )
     .unwrap();
-    let contract_bytecode_hex =
-        std::fs::read_to_string(contracts_path.join("solc_out").join("OnChainProposer.bin"))
-            .unwrap();
-    let contract_bytecode = hex::decode(contract_bytecode_hex.trim()).unwrap();
 
     // To avoid colision with the original OnChainProposer bytecode, we rename it to OnChainProposerBased
-    std::fs::write(
-        contracts_path
-            .join("solc_out")
-            .join("OnChainProposerBased.bytecode"),
-        contract_bytecode,
+    let original_path = contracts_path.join("solc_out/OnChainProposer.bin");
+    let bytecode_hex =
+        fs::read_to_string(&original_path).expect("Failed to read OnChainProposer.bin");
+    let bytecode = hex::decode(bytecode_hex.trim()).expect("Failed to decode bytecode");
+    fs::write(
+        contracts_path.join("solc_out/OnChainProposerBased.bytecode"),
+        bytecode,
     )
-    .unwrap();
+    .expect("Failed to write renamed bytecode");
 
     println!("cargo::rerun-if-changed=build.rs");
 }
 
 /// Clones OpenZeppelin and SP1 contracts into the specified path.
 fn download_contract_deps(contracts_path: &Path) {
-    std::fs::create_dir_all(contracts_path.join("lib")).unwrap();
+    fs::create_dir_all(contracts_path.join("lib")).expect("Failed to create contracts/lib dir");
 
     ethrex_l2_sdk::git_clone(
         "https://github.com/OpenZeppelin/openzeppelin-contracts-upgradeable.git",
-        contracts_path
+        &contracts_path
             .join("lib/openzeppelin-contracts-upgradeable")
-            .to_str()
-            .unwrap(),
+            .to_string_lossy(),
         None,
         true,
     )
-    .unwrap();
+    .expect("Failed to clone openzeppelin-contracts-upgradeable");
 
     ethrex_l2_sdk::git_clone(
         "https://github.com/succinctlabs/sp1-contracts.git",
-        contracts_path.join("lib/sp1-contracts").to_str().unwrap(),
+        &contracts_path.join("lib/sp1-contracts").to_string_lossy(),
         None,
         false,
     )
-    .unwrap();
+    .expect("Failed to clone sp1-contracts");
 }
 
-fn compile_contract_to_bytecode(
+fn compile_contract(
     output_dir: &Path,
-    contract_path: &Path,
+    relative_dir: &str,
     contract_name: &str,
     runtime_bin: bool,
     remappings: Option<&[(&str, &Path)]>,
 ) {
     println!("Compiling {contract_name} contract");
-    ethrex_l2_sdk::compile_contract(
-        output_dir,
-        &contract_path.join(format!("{contract_name}.sol")),
-        runtime_bin,
-        remappings,
-    )
-    .unwrap();
+    let contract_path = Path::new(relative_dir).join(format!("{contract_name}.sol"));
+    ethrex_l2_sdk::compile_contract(output_dir, &contract_path, runtime_bin, remappings)
+        .expect("Failed to compile {contract_name}");
     println!("Successfully compiled {contract_name} contract");
     decode_to_bytecode(output_dir, contract_name, runtime_bin);
     println!("Successfully generated {contract_name} bytecode");
 }
 
-fn decode_to_bytecode(contracts_path: &Path, contract: &str, runtime_bin: bool) {
-    let contract_bytecode_hex = if runtime_bin {
-        std::fs::read_to_string(
-            contracts_path
-                .join("solc_out")
-                .join(format!("{contract}.bin-runtime")),
-        )
-        .unwrap()
+fn decode_to_bytecode(output_dir: &Path, contract: &str, runtime_bin: bool) {
+    let filename = if runtime_bin {
+        format!("{contract}.bin-runtime")
     } else {
-        std::fs::read_to_string(
-            contracts_path
-                .join("solc_out")
-                .join(format!("{contract}.bin")),
-        )
-        .unwrap()
+        format!("{contract}.bin")
     };
-    let contract_bytecode = hex::decode(contract_bytecode_hex.trim()).unwrap();
 
-    std::fs::write(
-        contracts_path
+    let bytecode_hex = fs::read_to_string(output_dir.join("solc_out").join(&filename))
+        .expect("Failed to read {filename}");
+
+    let bytecode =
+        hex::decode(bytecode_hex.trim()).expect("Failed to decode bytecode for {contract}");
+
+    fs::write(
+        output_dir
             .join("solc_out")
-            .join(format!("{contract}.bytecode",)),
-        contract_bytecode,
+            .join(format!("{contract}.bytecode")),
+        bytecode,
     )
-    .unwrap();
+    .expect("Failed to write bytecode for {contract}");
 }
