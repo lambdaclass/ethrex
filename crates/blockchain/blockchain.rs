716398d4719825c2b9ddd030d0c3929fc0c345e0
Tomás Arjovsky <tomas.arjovsky@lambdaclass.com>
2025-07-15 19:58:06 +0200

    add gas used diff in error

--- crates/blockchain/blockchain.rs
+++ crates/blockchain/blockchain.rs
@@ -940,7 +940,10 @@ pub fn validate_gas_used(
         // dbg!(last.cumulative_gas_used);
         // dbg!(block_header.gas_used);
         if last.cumulative_gas_used != block_header.gas_used {
-            return Err(ChainError::InvalidBlock(InvalidBlockError::GasUsedMismatch));
+            return Err(ChainError::InvalidBlock(InvalidBlockError::GasUsedMismatch(
+                last.cumulative_gas_used,
+                block_header.gas_used,
+            )));
         }
     }
     Ok(())
