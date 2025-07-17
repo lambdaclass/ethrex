3be56532294bc9b8a0b80f0d76e4908fa3e2b137
github-action-benchmark <github@users.noreply.github.com>
2025-07-17 16:41:29 -0300

    add L1 block proving benchmark (customBiggerIsBetter) benchmark result for c0e0ce2933c1c72d943771abd58563355081c09f

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1752781241324,
+  "lastUpdate": 1752781289853,
   "repoUrl": "https://github.com/lambdaclass/ethrex",
   "entries": {
     "Benchmark": [
@@ -9360,6 +9360,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "SP1, RTX A6000",
             "value": 0.006094666666666667,
+            "unit": "Mgas/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "martin.c.paulucci@gmail.com",
+            "name": "Martin Paulucci",
+            "username": "mpaulucci"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "c0e0ce2933c1c72d943771abd58563355081c09f",
+          "message": "ci(l1): support multiple hive versions depending on simulation. (#3661)\n\n**Motivation**\nWe want to get rid of our hive fork and use the upstream. Unfortunately,\nwe can't completely rely on it yet because it would break.\n\n**Description**\n- While we fix the upstream, lets rely on two versions of Hive, our fork\none and the upstream",
+          "timestamp": "2025-07-17T13:31:45Z",
+          "tree_id": "3365e4d99a9a881624b7f54dfd3e3d7e9295904a",
+          "url": "https://github.com/lambdaclass/ethrex/commit/c0e0ce2933c1c72d943771abd58563355081c09f"
+        },
+        "date": 1752781276765,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Risc0, RTX A6000",
+            "value": 0.0012267757352941177,
             "unit": "Mgas/s"
           }
         ]
