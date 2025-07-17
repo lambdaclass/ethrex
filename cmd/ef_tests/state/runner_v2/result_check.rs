ee77b0e746bc59d8e50d3890b75ee268834f0a3e
Sofi Azcoaga <sofiaazc3@hotmail.com>
2025-07-17 15:08:59 -0300

    add test results check structure

--- cmd/ef_tests/state/runner_v2/result_check.rs
+++ cmd/ef_tests/state/runner_v2/result_check.rs
@@ -1,4 +1,7 @@
-use ethrex_common::types::AccountUpdate;
+use std::collections::HashMap;
+
+use bytes::Bytes;
+use ethrex_common::{types::{Account, AccountUpdate}, Address, U256};
 use ethrex_levm::{
     db::gen_db::GeneralizedDatabase,
     errors::{ExecutionReport, TxValidationError, VMError},
@@ -10,9 +13,21 @@ use ethrex_vm::backends;
 use keccak_hash::{H256, keccak};
 
 use crate::runner_v2::{
-    error::RunnerError,
+    error::{AccountMismatch, RunnerError},
     types::{TestCase, TransactionExpectedException},
 };
+#[derive(Debug, Clone)]
+pub struct AccountMismatch {
+    pub address: Address,
+    pub expected_balance: U256,
+    pub actual_balance: U256,
+    pub expected_nonce: u64,
+    pub actual_nonce: u64,
+    pub expected_code: Bytes,
+    pub actual_code: Bytes,
+    pub expected_storage: HashMap<U256, U256>,
+    pub actual_storage:  HashMap<H256, U256>,
+}
 
 /// Verify if the test has reached the expected results: if an exception was expected, check it was the corresponding
 /// exception. If no exception was expected verify the result root.
@@ -22,22 +37,25 @@ pub async fn check_test_case_results(
     store: Store,
     test_case: &TestCase,
     execution_result: Result<ExecutionReport, VMError>,
-) -> Result<bool, RunnerError> {
+) -> Result<PostCheckResult, RunnerError> {
+    let mut checks_result = PostCheckResult::default();
+    checks_result.passed = true;
     if test_case.expects_exception() {
         // Verify in case an exception was expected.
-        let exception_ok = check_exception(
+        check_exception(
             test_case.post.expected_exceptions.clone().unwrap(),
             execution_result,
+            &mut checks_result
         );
-        Ok(exception_ok)
+        Ok(checks_result)
     } else {
         // Verify hashed logs.
-        let logs_ok = check_logs(test_case, &execution_result.clone().unwrap());
+        check_logs(test_case, &execution_result.clone().unwrap(), &mut checks_result);
         // Verify accounts' post state.
-        let accounts_state_ok = check_accounts_state(vm.db, test_case);
+        check_accounts_state(vm.db, test_case, &mut checks_result);
         // Verify expected root hash.
-        let root_ok = check_root(vm, initial_block_hash, store, test_case).await?;
-        Ok(logs_ok && accounts_state_ok && root_ok)
+        check_root(vm, initial_block_hash, store, test_case, &mut checks_result).await?;
+        Ok(checks_result)
     }
 }
 
@@ -46,14 +64,25 @@ pub async fn check_root(
     initial_block_hash: H256,
     store: Store,
     test_case: &TestCase,
-) -> Result<bool, RunnerError> {
+    check_result: &mut PostCheckResult
+) -> Result<(), RunnerError> {
     let account_updates = backends::levm::LEVM::get_state_transitions(vm.db)
         .map_err(|_| RunnerError::FailedToGetAccountsUpdates)?;
     let post_state_root = post_state_root(&account_updates, initial_block_hash, store).await;
     if post_state_root != test_case.post.hash {
-        return Ok(false);
+        check_result.passed = false;
+        check_result.root_dif = Some((test_case.post.hash, post_state_root));
     }
-    Ok(true)
+    Ok(())
+}
+
+#[derive(Default)]
+pub struct PostCheckResult {
+    pub passed: bool,
+    pub root_dif: Option<(H256, H256)>,
+    pub accounts_diff: Option<Vec<AccountMismatch>>,
+    pub logs_diff: Option<(H256, H256)>,
+    pub exception_diff: Option<(Vec<TransactionExpectedException>, Option<VMError>)>
 }
 
 pub async fn post_state_root(
@@ -72,11 +101,17 @@ pub async fn post_state_root(
 pub fn check_exception(
     expected_exceptions: Vec<TransactionExpectedException>,
     execution_result: Result<ExecutionReport, VMError>,
-) -> bool {
+    check_result: &mut PostCheckResult
+)  {
     if execution_result.is_err() {
-        exception_is_expected(expected_exceptions, execution_result.err().unwrap())
+        let execution_err = execution_result.err().unwrap();
+        if !exception_is_expected(expected_exceptions.clone(), execution_err.clone()) {
+            check_result.exception_diff = Some((expected_exceptions, Some(execution_err)));
+            check_result.passed = false;
+        }
     } else {
-        false
+        check_result.exception_diff = Some((expected_exceptions, None));
+        check_result.passed = false;
     }
 }
 
@@ -158,7 +193,8 @@ fn exception_is_expected(
     })
 }
 
-pub fn check_accounts_state(db: &GeneralizedDatabase, test_case: &TestCase) -> bool {
+pub fn check_accounts_state(db: &GeneralizedDatabase, test_case: &TestCase, check_result: &mut PostCheckResult)  {
+    let mut accounts_diff= Vec::new();
     if test_case.post.state.is_some() {
         let expected_accounts_state = test_case.post.state.clone().unwrap();
         let current_accounts_state = db.current_accounts_state.clone();
@@ -166,16 +202,17 @@ pub fn check_accounts_state(db: &GeneralizedDatabase, test_case: &TestCase) -> b
             let current_state = if current_accounts_state.contains_key(&addr) {
                 current_accounts_state.get(&addr).unwrap()
             } else {
-                return false;
+                // This should make checks comparison fail
+                &Account::default()
             };
             let code_matches = current_state.code == state.code;
             let balance_matches = current_state.info.balance == state.balance;
             let nonce_matches = current_state.info.nonce == state.nonce;
             let mut storage_matches = true;
-            for (storage_key, content) in state.storage {
+            for (storage_key, content) in &state.storage {
                 let key = &H256::from(storage_key.to_big_endian());
                 if current_state.storage.contains_key(key) {
-                    if *current_state.storage.get(key).unwrap() != content {
+                    if current_state.storage.get(key).unwrap() != content {
                         storage_matches = false;
                     }
                 } else {
@@ -183,16 +220,33 @@ pub fn check_accounts_state(db: &GeneralizedDatabase, test_case: &TestCase) -> b
                 }
             }
             if !(code_matches && balance_matches && nonce_matches && storage_matches) {
-                return false;
+                let account_mismatch = AccountMismatch {
+                    address: addr,
+                    expected_balance: state.balance,
+                    actual_balance: current_state.info.balance,
+                    expected_nonce: state.nonce,
+                    actual_nonce: current_state.info.nonce,
+                    expected_code: state.code,
+                    actual_code: current_state.code.clone(),
+                    expected_storage: state.storage,
+                    actual_storage: current_state.storage.clone()
+                };
+                accounts_diff.push(account_mismatch);
             }
         }
     }
-    true
+    if !accounts_diff.is_empty() {
+        check_result.accounts_diff = Some(accounts_diff);
+        check_result.passed = false;
+    }
 }
 
-pub fn check_logs(test_case: &TestCase, execution_report: &ExecutionReport) -> bool {
+pub fn check_logs(test_case: &TestCase, execution_report: &ExecutionReport, checks_result: &mut PostCheckResult) {
     let mut encoded_logs = Vec::new();
     execution_report.logs.encode(&mut encoded_logs);
     let hashed_logs = keccak(encoded_logs);
-    test_case.post.logs == hashed_logs
+    if test_case.post.logs != hashed_logs {
+        checks_result.passed = false;
+        checks_result.logs_diff = Some((test_case.post.logs, hashed_logs));
+    }
 }
