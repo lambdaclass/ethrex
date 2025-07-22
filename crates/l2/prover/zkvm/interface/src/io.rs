deea5cf3ee0498fd338cae8f9da5094ae7db7cde
Leandro Serra <leandro.serra@lambdaclass.com>
2025-07-22 11:08:16 -0300

    perf(l2): avoid cloning all fields from ExecutionWitnessResult

--- crates/l2/prover/zkvm/interface/src/io.rs
+++ crates/l2/prover/zkvm/interface/src/io.rs
@@ -1,9 +1,14 @@
 use ethrex_common::{
-    H256, U256,
-    types::{Block, block_execution_witness::ExecutionWitnessResult},
+    Address, Bytes, H256, U256,
+    types::{
+        AccountInfo, AccountUpdate, Block, BlockHeader, ChainConfig,
+        block_execution_witness::{ExecutionWitnessError, ExecutionWitnessResult},
+    },
 };
+use ethrex_vm::{EvmError, VmDatabase};
 use serde::{Deserialize, Serialize};
 use serde_with::{DeserializeAs, SerializeAs, serde_as};
+use std::sync::{Arc, Mutex, MutexGuard};
 
 #[cfg(feature = "l2")]
 use ethrex_common::types::blobs_bundle;
@@ -112,5 +117,84 @@ impl ProgramOutput {
             self.non_privileged_count.to_big_endian(),
         ]
         .concat()
+    }
+}
+
+#[derive(Clone)]
+pub struct ExecutionWitnessWrapper {
+    inner: Arc<Mutex<ExecutionWitnessResult>>,
+}
+
+impl ExecutionWitnessWrapper {
+    pub fn new(db: ExecutionWitnessResult) -> Self {
+        Self {
+            inner: Arc::new(Mutex::new(db)),
+        }
+    }
+
+    pub fn lock_mutex(&self) -> Result<MutexGuard<ExecutionWitnessResult>, ExecutionWitnessError> {
+        self.inner
+            .lock()
+            .map_err(|_| ExecutionWitnessError::Database("Failed to lock DB".to_string()))
+    }
+
+    pub fn apply_account_updates(
+        &mut self,
+        account_updates: &[AccountUpdate],
+    ) -> Result<(), ExecutionWitnessError> {
+        self.lock_mutex()?.apply_account_updates(account_updates)
+    }
+
+    pub fn state_trie_root(&self) -> Result<H256, ExecutionWitnessError> {
+        self.lock_mutex()?.state_trie_root()
+    }
+    pub fn get_first_invalid_block_hash(&self) -> Result<Option<u64>, ExecutionWitnessError> {
+        self.lock_mutex()?.get_first_invalid_block_hash()
+    }
+
+    pub fn get_block_parent_header(
+        &self,
+        block_number: u64,
+    ) -> Result<BlockHeader, ExecutionWitnessError> {
+        self.lock_mutex()?
+            .get_block_parent_header(block_number)
+            .cloned()
+    }
+}
+
+impl VmDatabase for ExecutionWitnessWrapper {
+    fn get_account_code(&self, code_hash: H256) -> Result<Bytes, EvmError> {
+        self.lock_mutex()
+            .map_err(|_| EvmError::DB("Failed to lock db".to_string()))?
+            .get_account_code(code_hash)
+            .map_err(|_| EvmError::DB("Failed to get account code".to_string()))
+    }
+
+    fn get_account_info(&self, address: Address) -> Result<Option<AccountInfo>, EvmError> {
+        self.lock_mutex()
+            .map_err(|_| EvmError::DB("Failed to lock db".to_string()))?
+            .get_account_info(address)
+            .map_err(|_| EvmError::DB("Failed to get account info".to_string()))
+    }
+
+    fn get_block_hash(&self, block_number: u64) -> Result<H256, EvmError> {
+        self.lock_mutex()
+            .map_err(|_| EvmError::DB("Failed to lock db".to_string()))?
+            .get_block_hash(block_number)
+            .map_err(|_| EvmError::DB("Failed get block hash".to_string()))
+    }
+
+    fn get_chain_config(&self) -> Result<ChainConfig, EvmError> {
+        self.lock_mutex()
+            .map_err(|_| EvmError::DB("Failed to lock db".to_string()))?
+            .get_chain_config()
+            .map_err(|_| EvmError::DB("Failed get chain config".to_string()))
+    }
+
+    fn get_storage_slot(&self, address: Address, key: H256) -> Result<Option<U256>, EvmError> {
+        self.lock_mutex()
+            .map_err(|_| EvmError::DB("Failed to lock db".to_string()))?
+            .get_storage_slot(address, key)
+            .map_err(|_| EvmError::DB("Failed get storage slot".to_string()))
     }
 }
