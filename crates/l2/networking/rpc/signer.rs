lambdaclass/ethrex

commit 0ca8fc7ec0eba8f010080506cb6271ae1f9f6175

feat(l2): add support for web3signer (#2714)
**Motivation**

<!-- Why does this pull request exist? What are its goals? -->
Many operators will want to use a remote signer instead of having the
private keys on the same server as the sequencer.

**Description**

<!-- A clear and concise general description of the changes this PR
introduces -->
Replace all uses of a private key with a new `Signer` enum. This signer
can be either `Local` or `Remote` and can be lately extended. This aims
to standardise the way all kind of messages are signed across the L2 and
facilitate the setup via flags or environment

<!-- Link to issues: Resolves #111, Resolves #222 -->

---------

Co-authored-by: Javier Rodríguez Chatruc <49622509+jrchatruc@users.noreply.github.com>
Co-authored-by: fedacking <francisco.gauna@lambdaclass.com>
Co-authored-by: Avila Gastón <72628438+avilagaston9@users.noreply.github.com>

crates/l2/networking/rpc/signer.rs
@@ -1,0 +1,228 @@
+use bytes::Bytes;
+use ethereum_types::{Address, Signature};
+use ethrex_common::{
+    U256,
+    types::{
+        EIP1559Transaction, EIP2930Transaction, EIP4844Transaction, EIP7702Transaction,
+        LegacyTransaction, Transaction, TxType,
+    },
+};
+use ethrex_rlp::encode::PayloadRLPEncode;
+use keccak_hash::keccak;
+use reqwest::{Client, Url};
+use rustc_hex::FromHexError;
+use secp256k1::{Message, PublicKey, SECP256K1, SecretKey};
+use url::ParseError;
+
+#[derive(Clone, Debug)]
+pub enum Signer {
+    Local(LocalSigner),
+    Remote(RemoteSigner),
+}
+
+impl Signer {
+    pub async fn sign(&self, data: Bytes) -> Result<Signature, SignerError> {
+        match self {
+            Self::Local(signer) => Ok(signer.sign(data)),
+            Self::Remote(signer) => signer.sign(data).await,
+        }
+    }
+
+    pub fn address(&self) -> Address {
+        match self {
+            Self::Local(signer) => signer.address,
+            Self::Remote(signer) => signer.address,
+        }
+    }
+}
+
+impl From<LocalSigner> for Signer {
+    fn from(value: LocalSigner) -> Self {
+        Self::Local(value)
+    }
+}
+
+impl From<RemoteSigner> for Signer {
+    fn from(value: RemoteSigner) -> Self {
+        Self::Remote(value)
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct LocalSigner {
+    private_key: SecretKey,
+    pub address: Address,
+}
+
+impl LocalSigner {
+    pub fn new(private_key: SecretKey) -> Self {
+        let address = Address::from(keccak(
+            &private_key.public_key(SECP256K1).serialize_uncompressed()[1..],
+        ));
+        Self {
+            private_key,
+            address,
+        }
+    }
+
+    pub fn sign(&self, data: Bytes) -> Signature {
+        let hash = keccak(data);
+        let msg = Message::from_digest(hash.0);
+        let (recovery_id, signature) = SECP256K1
+            .sign_ecdsa_recoverable(&msg, &self.private_key)
+            .serialize_compact();
+
+        Signature::from_slice(&[signature.as_slice(), &[recovery_id.to_i32() as u8]].concat())
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct RemoteSigner {
+    pub url: Url,
+    pub public_key: PublicKey,
+    pub address: Address,
+}
+
+impl RemoteSigner {
+    pub fn new(url: Url, public_key: PublicKey) -> Self {
+        let address = Address::from(keccak(&public_key.serialize_uncompressed()[1..]));
+        Self {
+            url,
+            public_key,
+            address,
+        }
+    }
+
+    pub async fn sign(&self, data: Bytes) -> Result<Signature, SignerError> {
+        let url = self
+            .url
+            .join("api/v1/eth1/sign/")?
+            .join(&hex::encode(&self.public_key.serialize_uncompressed()[1..]))?;
+        let body = format!("{{\"data\": \"0x{}\"}}", hex::encode(data));
+
+        let client = Client::new();
+        client
+            .post(url)
+            .body(body)
+            .header("content-type", "application/json")
+            .send()
+            .await?
+            .text()
+            .await?
+            .parse::<Signature>()
+            .map_err(SignerError::FromHexError)
+    }
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum SignerError {
+    #[error("Url Parse Error: {0}")]
+    ParseError(#[from] ParseError),
+    #[error("Failed with a reqwest error: {0}")]
+    ReqwestError(#[from] reqwest::Error),
+    #[error("Failed to parse value: {0}")]
+    FromHexError(#[from] FromHexError),
+    #[error("Tried to sign Privileged L2 transaction")]
+    PrivilegedL2TxUnsupported,
+}
+
+fn parse_signature(signature: Signature) -> (U256, U256, bool) {
+    let r = U256::from_big_endian(&signature[..32]);
+    let s = U256::from_big_endian(&signature[32..64]);
+    let y_parity = signature[64] != 0 && signature[64] != 27;
+
+    (r, s, y_parity)
+}
+
+pub trait Signable {
+    fn sign(
+        &self,
+        signer: &Signer,
+    ) -> impl std::future::Future<Output = Result<Self, SignerError>> + Send
+    where
+        Self: Sized + Sync + Send + Clone,
+    {
+        async {
+            let mut signable = self.clone();
+            signable.sign_inplace(signer).await?;
+            Ok(signable)
+        }
+    }
+
+    fn sign_inplace(
+        &mut self,
+        signer: &Signer,
+    ) -> impl std::future::Future<Output = Result<(), SignerError>> + Send;
+}
+
+impl Signable for Transaction {
+    async fn sign_inplace(&mut self, signer: &Signer) -> Result<(), SignerError> {
+        match self {
+            Transaction::LegacyTransaction(tx) => tx.sign_inplace(signer).await,
+            Transaction::EIP2930Transaction(tx) => tx.sign_inplace(signer).await,
+            Transaction::EIP1559Transaction(tx) => tx.sign_inplace(signer).await,
+            Transaction::EIP4844Transaction(tx) => tx.sign_inplace(signer).await,
+            Transaction::EIP7702Transaction(tx) => tx.sign_inplace(signer).await,
+            Transaction::PrivilegedL2Transaction(_) => Err(SignerError::PrivilegedL2TxUnsupported), // Privileged Transactions are not signed
+        }
+    }
+}
+
+impl Signable for LegacyTransaction {
+    async fn sign_inplace(&mut self, signer: &Signer) -> Result<(), SignerError> {
+        let signature = signer.sign(self.encode_payload_to_vec().into()).await?;
+
+        self.v = U256::from(signature[64]);
+        (self.r, self.s, _) = parse_signature(signature);
+
+        Ok(())
+    }
+}
+
+impl Signable for EIP1559Transaction {
+    async fn sign_inplace(&mut self, signer: &Signer) -> Result<(), SignerError> {
+        let mut payload = vec![TxType::EIP1559 as u8];
+        payload.append(self.encode_payload_to_vec().as_mut());
+
+        let signature = signer.sign(payload.into()).await?;
+        (self.signature_r, self.signature_s, self.signature_y_parity) = parse_signature(signature);
+
+        Ok(())
+    }
+}
+
+impl Signable for EIP2930Transaction {
+    async fn sign_inplace(&mut self, signer: &Signer) -> Result<(), SignerError> {
+        let mut payload = vec![TxType::EIP2930 as u8];
+        payload.append(self.encode_payload_to_vec().as_mut());
+
+        let signature = signer.sign(payload.into()).await?;
+        (self.signature_r, self.signature_s, self.signature_y_parity) = parse_signature(signature);
+
+        Ok(())
+    }
+}
+
+impl Signable for EIP4844Transaction {
+    async fn sign_inplace(&mut self, signer: &Signer) -> Result<(), SignerError> {
+        let mut payload = vec![TxType::EIP4844 as u8];
+        payload.append(self.encode_payload_to_vec().as_mut());
+
+        let signature = signer.sign(payload.into()).await?;
+        (self.signature_r, self.signature_s, self.signature_y_parity) = parse_signature(signature);
+
+        Ok(())
+    }
+}
+
+impl Signable for EIP7702Transaction {
+    async fn sign_inplace(&mut self, signer: &Signer) -> Result<(), SignerError> {
+        let mut payload = vec![TxType::EIP7702 as u8];
+        payload.append(self.encode_payload_to_vec().as_mut());
+
+        let signature = signer.sign(payload.into()).await?;
+        (self.signature_r, self.signature_s, self.signature_y_parity) = parse_signature(signature);
+
+        Ok(())
+    }
+}

crates/l2/sequencer/l1_proof_sender.rs
@@ -8,7 +8,6 @@ use ethrex_l2_common::{
 use ethrex_l2_sdk::calldata::encode_calldata;
 use ethrex_rpc::EthClient;
 use ethrex_storage_rollup::StoreRollup;
-use secp256k1::SecretKey;
 use spawned_concurrency::{
     messages::Unused,
     tasks::{CastResponse, GenServer, GenServerHandle, send_after},
@@ -30,7 +29,6 @@ use aligned_sdk::{
     verification_layer::{estimate_fee as aligned_estimate_fee, get_nonce_from_batcher, submit},
 };
 
-// TODO: Remove this import once it's no longer required by the SDK.
 use ethers::signers::{Signer, Wallet};
 
 const VERIFY_FUNCTION_SIGNATURE: &str = "verifyBatch(uint256,bytes,bytes,bytes,bytes,bytes,bytes)";
@@ -38,8 +36,7 @@ const VERIFY_FUNCTION_SIGNATURE: &str = "verifyBatch(uint256,bytes,bytes,bytes,b
 #[derive(Clone)]
 pub struct L1ProofSenderState {
     eth_client: EthClient,
-    l1_address: Address,
-    l1_private_key: SecretKey,
+    signer: ethrex_l2_rpc::signer::Signer,
     on_chain_proposer_address: Address,
     needed_proof_types: Vec<ProverType>,
     proof_send_interval_ms: u64,
@@ -71,8 +68,7 @@ impl L1ProofSenderState {
         if cfg.dev_mode {
             return Ok(Self {
                 eth_client,
-                l1_address: cfg.l1_address,
-                l1_private_key: cfg.l1_private_key,
+                signer: cfg.signer.clone(),
                 on_chain_proposer_address: committer_cfg.on_chain_proposer_address,
                 needed_proof_types: vec![ProverType::Exec],
                 proof_send_interval_ms: cfg.proof_send_interval_ms,
@@ -87,8 +83,7 @@ impl L1ProofSenderState {
 
         Ok(Self {
             eth_client,
-            l1_address: cfg.l1_address,
-            l1_private_key: cfg.l1_private_key,
+            signer: cfg.signer.clone(),
             on_chain_proposer_address: committer_cfg.on_chain_proposer_address,
             needed_proof_types,
             proof_send_interval_ms: cfg.proof_send_interval_ms,
@@ -243,7 +238,7 @@ async fn send_proof_to_aligned(
     let verification_data = VerificationData {
         proving_system: ProvingSystemId::SP1,
         proof: aligned_proof.proof(),
-        proof_generator_addr: state.l1_address.0.into(),
+        proof_generator_addr: state.signer.address().0.into(),
         vm_program_code: Some(elf),
         verification_key: None,
         pub_input: None,
@@ -251,13 +246,13 @@ async fn send_proof_to_aligned(
 
     let fee_estimation = estimate_fee(state).await?;
 
-    let nonce = get_nonce_from_batcher(state.network.clone(), state.l1_address.0.into())
+    let nonce = get_nonce_from_batcher(state.network.clone(), state.signer.address().0.into())
         .await
         .map_err(|err| {
             ProofSenderError::AlignedGetNonceError(format!("Failed to get nonce: {err:?}"))
         })?;
 
-    let wallet = Wallet::from_bytes(state.l1_private_key.as_ref())
+    let wallet = Wallet::from_bytes(&state.signer.address().0)
         .map_err(|_| ProofSenderError::InternalError("Failed to create wallet".to_owned()))?;
 
     let wallet = wallet.with_chain_id(state.l1_chain_id);
@@ -331,8 +326,7 @@ pub async fn send_proof_to_contract(
         calldata,
         &state.eth_client,
         state.on_chain_proposer_address,
-        state.l1_address,
-        &state.l1_private_key,
+        &state.signer,
     )
     .await?;
 

.github/workflows/pr-main_l2.yaml
@@ -58,8 +58,16 @@ jobs:
         include:
           - name: "Validium"
             validium: true
+            web3signer: false
+            compose_targets: [docker-compose-l2.yaml]
           - name: "Vanilla"
             validium: false
+            web3signer: false
+            compose_targets: [docker-compose-l2.yaml]
+          - name: "Vanilla with Web3signer"
+            validium: false
+            web3signer: true
+            compose_targets: [docker-compose-l2.yaml, docker-compose-l2-web3signer.yaml]
     steps:
       - name: Checkout sources
         uses: actions/checkout@v4
@@ -83,6 +91,12 @@ jobs:
         run: |
           cargo test l2 --no-run --release
 
+      - name: Start Web3Signer
+        if: matrix.web3signer
+        run: |
+          cd crates/l2
+          docker compose -f ${{ join(matrix.compose_targets, ' -f ') }} up --detach web3signer
+          
       - name: Build L1 docker image
         uses: docker/build-push-action@v6
         with:
@@ -109,7 +123,7 @@ jobs:
           CI_ETHREX_WORKDIR=/usr/local/bin \
           ETHREX_L2_VALIDIUM=${{ matrix.validium }} \
           ETHREX_WATCHER_BLOCK_DELAY=0 \
-          docker compose -f docker-compose-l2.yaml up --detach ethrex_l2
+          docker compose -f ${{ join(matrix.compose_targets, ' -f ') }} up --detach ethrex_l2
 
       - name: Run test
         run: |

Cargo.toml
@@ -96,6 +96,8 @@ axum = "0.8.1"
 clap = { version = "4.3", features = ["derive", "env"] }
 clap_complete = "4.5.17"
 eyre = "0.6.12"
+rustc-hex = "2.1.0"
+url = "2.5.4"
 kzg-rs = "0.2.6"
 libsql = "0.9.10"
 futures = "0.3.31"

cmd/ethrex/Cargo.toml
@@ -41,6 +41,7 @@ lazy_static.workspace = true
 secp256k1 = { workspace = true }
 keccak-hash.workspace = true
 reqwest.workspace = true
+thiserror.workspace = true
 itertools = "0.14.0"
 tui-logger.workspace = true
 
@@ -48,6 +49,7 @@ cfg-if = "1.0.0"
 
 ethrex-dev = { path = "../../crates/blockchain/dev", optional = true }
 ethrex-metrics = { path = "../../crates/blockchain/metrics" }
+url.workspace = true
 
 [[bin]]
 name = "ethrex"

cmd/ethrex/bench/build_block_benchmark.rs
@@ -14,12 +14,13 @@ use ethrex_blockchain::{
     payload::{BuildPayloadArgs, PayloadBuildResult, create_payload},
 };
 use ethrex_common::{
-    Address, H160, U256,
+    Address, H160,
     types::{
-        Block, EIP1559Transaction, Genesis, GenesisAccount, LegacyTransaction, Signable,
-        Transaction, TxKind, payload::PayloadBundle,
+        Block, EIP1559Transaction, Genesis, GenesisAccount, Transaction, TxKind,
+        payload::PayloadBundle,
     },
 };
+use ethrex_l2_rpc::signer::{LocalSigner, Signable, Signer};
 use ethrex_storage::{EngineType, Store};
 use ethrex_vm::EvmEngine;
 use secp256k1::SecretKey;
@@ -112,19 +113,8 @@ fn read_private_keys() -> Vec<SecretKey> {
 }
 
 fn recover_address_for_sk(sk: &SecretKey) -> Address {
-    let mut tx = Transaction::LegacyTransaction(LegacyTransaction {
-        nonce: 0,
-        gas_price: 1,
-        to: TxKind::Call(H160::random()),
-        value: U256::zero(),
-        data: Bytes::new(),
-        v: U256::one(),
-        r: U256::one(),
-        s: U256::one(),
-        gas: 21000,
-    });
-    tx.sign_inplace(sk).expect("Unreachable error: Digest to sign should be correct size since it's calculated with the keccak algorithm");
-    tx.sender().expect("Failed to recover sender's address")
+    let signer = Signer::Local(LocalSigner::new(*sk));
+    signer.address()
 }
 
 async fn setup_genesis(accounts: &Vec<Address>) -> (Store, Genesis) {
@@ -171,6 +161,7 @@ async fn create_payload_block(genesis_block: &Block, store: &Store) -> (Block, u
 async fn fill_mempool(b: &Blockchain, accounts: Vec<SecretKey>) {
     let mut txs = vec![];
     for sk in accounts {
+        let signer = Signer::Local(LocalSigner::new(sk));
         for n in 0..1000 {
             let mut tx = Transaction::EIP1559Transaction(EIP1559Transaction {
                 nonce: n,
@@ -182,7 +173,7 @@ async fn fill_mempool(b: &Blockchain, accounts: Vec<SecretKey>) {
                 to: TxKind::Call(H160::random()),
                 ..Default::default()
             });
-            tx.sign_inplace(&sk).expect("Unreachable error: Digest to sign should be correct size since it's calculated with the keccak algorithm");
+            let _ = tx.sign_inplace(&signer).await;
             txs.push(tx);
         }
     }

cmd/ethrex/l2/command.rs
@@ -41,7 +41,7 @@ use std::{
 };
 use tokio::sync::Mutex;
 use tokio_util::task::TaskTracker;
-use tracing::info;
+use tracing::{error, info};
 
 #[allow(clippy::large_enum_variant)]
 #[derive(Subcommand)]
@@ -199,7 +199,10 @@ impl Command {
                     info!("P2P is disabled");
                 }
 
-                let l2_sequencer_cfg = SequencerConfig::from(opts.sequencer_opts);
+                let l2_sequencer_cfg =
+                    SequencerConfig::try_from(opts.sequencer_opts).inspect_err(|err| {
+                        error!("{err}");
+                    })?;
 
                 let l2_sequencer = ethrex_l2::start_l2(
                     store,

cmd/ethrex/l2/options.rs
@@ -1,4 +1,7 @@
-use crate::{cli::Options as NodeOptions, utils};
+use crate::{
+    cli::Options as NodeOptions,
+    utils::{self},
+};
 use clap::Parser;
 use ethrex_common::Address;
 use ethrex_l2::{
@@ -9,15 +12,16 @@ use ethrex_l2::{
         utils::resolve_aligned_network,
     },
 };
+use ethrex_l2_rpc::signer::{LocalSigner, RemoteSigner, Signer};
 use ethrex_rpc::clients::eth::{
     BACKOFF_FACTOR, MAX_NUMBER_OF_RETRIES, MAX_RETRY_DELAY, MIN_RETRY_DELAY,
-    get_address_from_secret_key,
 };
 use reqwest::Url;
-use secp256k1::SecretKey;
+use secp256k1::{PublicKey, SecretKey};
 use std::net::{IpAddr, Ipv4Addr};
 
 #[derive(Parser)]
+#[group(id = "L2Options")]
 pub struct Options {
     #[command(flatten)]
     pub node_opts: NodeOptions,
@@ -94,9 +98,49 @@ pub struct SequencerOptions {
     pub monitor: bool,
 }
 
-impl From<SequencerOptions> for SequencerConfig {
-    fn from(opts: SequencerOptions) -> Self {
-        Self {
+pub fn parse_signer(
+    private_key: Option<SecretKey>,
+    url: Option<Url>,
+    public_key: Option<PublicKey>,
+) -> Result<Signer, SequencerOptionsError> {
+    Ok(match url {
+        Some(url) => RemoteSigner::new(
+            url,
+            public_key.ok_or(SequencerOptionsError::RemoteUrlWithoutPubkey)?,
+        )
+        .into(),
+        None => LocalSigner::new(private_key.ok_or(SequencerOptionsError::NoSigner(
+            "ProofCoordinator".to_string(),
+        ))?)
+        .into(),
+    })
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum SequencerOptionsError {
+    #[error("Remote signer URL was provided without a public key")]
+    RemoteUrlWithoutPubkey,
+    #[error("No signer was set up for {0}")]
+    NoSigner(String),
+}
+
+impl TryFrom<SequencerOptions> for SequencerConfig {
+    type Error = SequencerOptionsError;
+
+    fn try_from(opts: SequencerOptions) -> Result<Self, Self::Error> {
+        let committer_signer = parse_signer(
+            opts.committer_opts.committer_l1_private_key,
+            opts.committer_opts.committer_remote_signer_url,
+            opts.committer_opts.committer_remote_signer_public_key,
+        )?;
+
+        let proof_coordinator_signer = parse_signer(
+            opts.proof_coordinator_opts.proof_coordinator_l1_private_key,
+            opts.proof_coordinator_opts.remote_signer_url,
+            opts.proof_coordinator_opts.remote_signer_public_key,
+        )?;
+
+        Ok(Self {
             block_producer: BlockProducerConfig {
                 block_time_ms: opts.block_producer_opts.block_time_ms,
                 coinbase_address: opts.block_producer_opts.coinbase_address,
@@ -104,13 +148,9 @@ impl From<SequencerOptions> for SequencerConfig {
             },
             l1_committer: CommitterConfig {
                 on_chain_proposer_address: opts.committer_opts.on_chain_proposer_address,
-                l1_address: get_address_from_secret_key(
-                    &opts.committer_opts.committer_l1_private_key,
-                )
-                .unwrap(),
-                l1_private_key: opts.committer_opts.committer_l1_private_key,
                 commit_time_ms: opts.committer_opts.commit_time_ms,
                 arbitrary_base_blob_gas_price: opts.committer_opts.arbitrary_base_blob_gas_price,
+                signer: committer_signer,
                 validium: opts.validium,
             },
             eth: EthConfig {
@@ -131,15 +171,14 @@ impl From<SequencerOptions> for SequencerConfig {
                 watcher_block_delay: opts.watcher_opts.watcher_block_delay,
             },
             proof_coordinator: ProofCoordinatorConfig {
-                l1_address: get_address_from_secret_key(
-                    &opts.proof_coordinator_opts.proof_coordinator_l1_private_key,
-                )
-                .unwrap(),
-                l1_private_key: opts.proof_coordinator_opts.proof_coordinator_l1_private_key,
                 listen_ip: opts.proof_coordinator_opts.listen_ip,
                 listen_port: opts.proof_coordinator_opts.listen_port,
                 proof_send_interval_ms: opts.proof_coordinator_opts.proof_send_interval_ms,
                 dev_mode: opts.proof_coordinator_opts.dev_mode,
+                signer: proof_coordinator_signer,
+                tdx_private_key: opts
+                    .proof_coordinator_opts
+                    .proof_coordinator_tdx_private_key,
                 validium: opts.validium,
             },
             based: BasedConfig {
@@ -171,7 +210,7 @@ impl From<SequencerOptions> for SequencerConfig {
                 enabled: opts.monitor,
                 tick_rate: opts.monitor_opts.tick_rate,
             },
-        }
+        })
     }
 }
 
@@ -337,8 +376,30 @@ pub struct CommitterOptions {
         env = "ETHREX_COMMITTER_L1_PRIVATE_KEY",
         help_heading = "L1 Committer options",
         help = "Private key of a funded account that the sequencer will use to send commit txs to the L1.",
+        conflicts_with_all = &["committer_remote_signer_url", "committer_remote_signer_public_key"],
+        required_unless_present = "committer_remote_signer_url",
     )]
-    pub committer_l1_private_key: SecretKey,
+    pub committer_l1_private_key: Option<SecretKey>,
+    #[arg(
+        long = "committer.remote-signer-url",
+        value_name = "URL",
+        env = "ETHREX_COMMITTER_REMOTE_SIGNER_URL",
+        help_heading = "L1 Committer options",
+        help = "URL of a Web3Signer-compatible server to remote sign instead of a local private key.",
+        requires = "committer_remote_signer_public_key",
+        required_unless_present = "committer_l1_private_key"
+    )]
+    pub committer_remote_signer_url: Option<Url>,
+    #[arg(
+        long = "committer.remote-signer-public-key",
+        value_name = "PUBLIC_KEY",
+        value_parser = utils::parse_public_key,
+        env = "ETHREX_COMMITTER_REMOTE_SIGNER_PUBLIC_KEY",
+        help_heading = "L1 Committer options",
+        help = "Public key to request the remote signature from.",
+        requires = "committer_remote_signer_url",
+    )]
+    pub committer_remote_signer_public_key: Option<PublicKey>,
     #[arg(
         long = "l1.on-chain-proposer-address",
         value_name = "ADDRESS",
@@ -371,12 +432,14 @@ impl Default for CommitterOptions {
             committer_l1_private_key: utils::parse_private_key(
                 "0x385c546456b6a603a1cfcaa9ec9494ba4832da08dd6bcf4de9a71e4a01b74924",
             )
-            .unwrap(),
+            .ok(),
             on_chain_proposer_address: "0xea6d04861106c1fb69176d49eeb8de6dd14a9cfe"
                 .parse()
                 .unwrap(),
             commit_time_ms: 1000,
             arbitrary_base_blob_gas_price: 1_000_000_000,
+            committer_remote_signer_url: None,
+            committer_remote_signer_public_key: None,
         }
     }
 }
@@ -390,8 +453,39 @@ pub struct ProofCoordinatorOptions {
         env = "ETHREX_PROOF_COORDINATOR_L1_PRIVATE_KEY",
         help_heading = "Proof coordinator options",
         long_help = "Private key of of a funded account that the sequencer will use to send verify txs to the L1. Has to be a different account than --committer-l1-private-key.",
+        conflicts_with_all = &["remote_signer_url", "remote_signer_public_key"],
+        required_unless_present = "remote_signer_url",
     )]
-    pub proof_coordinator_l1_private_key: SecretKey,
+    pub proof_coordinator_l1_private_key: Option<SecretKey>,
+    #[arg(
+        long = "proof-coordinator.tdx-private-key",
+        value_name = "PRIVATE_KEY",
+        value_parser = utils::parse_private_key,
+        env = "ETHREX_PROOF_COORDINATOR_TDX_PRIVATE_KEY",
+        help_heading = "Proof coordinator options",
+        long_help = "Private key of of a funded account that the TDX tool that will use to send the tdx attestation to L1.",
+    )]
+    pub proof_coordinator_tdx_private_key: SecretKey,
+    #[arg(
+        long = "proof-coordinator.remote-signer-url",
+        value_name = "URL",
+        env = "ETHREX_PROOF_COORDINATOR_REMOTE_SIGNER_URL",
+        help_heading = "Proof coordinator options",
+        help = "URL of a Web3Signer-compatible server to remote sign instead of a local private key.",
+        requires = "remote_signer_public_key",
+        required_unless_present = "proof_coordinator_l1_private_key"
+    )]
+    pub remote_signer_url: Option<Url>,
+    #[arg(
+        long = "proof-coordinator.remote-signer-public-key",
+        value_name = "PUBLIC_KEY",
+        value_parser = utils::parse_public_key,
+        env = "ETHREX_PROOF_COORDINATOR_REMOTE_SIGNER_PUBLIC_KEY",
+        help_heading = "Proof coordinator options",
+        help = "Public key to request the remote signature from.",
+        requires = "remote_signer_url",
+    )]
+    pub remote_signer_public_key: Option<PublicKey>,
     #[arg(
         long = "proof-coordinator.addr",
         default_value = "127.0.0.1",
@@ -433,13 +527,19 @@ impl Default for ProofCoordinatorOptions {
         let proof_coordinator_l1_private_key = utils::parse_private_key(
             "0x39725efee3fb28614de3bacaffe4cc4bd8c436257e2c8bb887c4b5c4be45e76d",
         )
-        .unwrap();
+        .ok();
         Self {
+            remote_signer_url: None,
+            remote_signer_public_key: None,
             proof_coordinator_l1_private_key,
             listen_ip: IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
             listen_port: 3900,
             proof_send_interval_ms: 5000,
             dev_mode: false,
+            proof_coordinator_tdx_private_key:
+                "0x39725efee3fb28614de3bacaffe4cc4bd8c436257e2c8bb887c4b5c4be45e76d"
+                    .parse()
+                    .unwrap(),
         }
     }
 }

commit 37f73f942c328d80936751b100cb2d5d9e437ef7

fix(l2): fix L1 proof sender's wallet/signer (#3747)
**Motivation**

The L1 proof sender was broken in #2714 by creating an invalid ethers'
`Wallet`
[here](github.com/lambdaclass/ethrex/pull/2714/files#r2216602944). This
PR fixes it but only allows running the proof sender with a local
signer.

To support a remote signer we must investigate if there's a way to
create an ethers' signer that uses web3signer.

Thanks @avilagaston9 for noticing the bug!

crates/l2/networking/rpc/signer.rs
@@ -50,7 +50,7 @@ impl From<RemoteSigner> for Signer {
 
 #[derive(Clone, Debug)]
 pub struct LocalSigner {
-    private_key: SecretKey,
+    pub private_key: SecretKey,
     pub address: Address,
 }
 

crates/l2/sequencer/l1_proof_sender.rs
@@ -5,6 +5,7 @@ use ethrex_l2_common::{
     calldata::Value,
     prover::{BatchProof, ProverType},
 };
+use ethrex_l2_rpc::signer::Signer;
 use ethrex_l2_sdk::calldata::encode_calldata;
 use ethrex_rpc::EthClient;
 use ethrex_storage_rollup::StoreRollup;
@@ -29,7 +30,7 @@ use aligned_sdk::{
     verification_layer::{estimate_fee as aligned_estimate_fee, get_nonce_from_batcher, submit},
 };
 
-use ethers::signers::{Signer, Wallet};
+use ethers::signers::{Signer as EthersSigner, Wallet};
 
 const VERIFY_FUNCTION_SIGNATURE: &str = "verifyBatch(uint256,bytes,bytes,bytes,bytes,bytes,bytes)";
 
@@ -252,7 +253,13 @@ async fn send_proof_to_aligned(
             ProofSenderError::AlignedGetNonceError(format!("Failed to get nonce: {err:?}"))
         })?;
 
-    let wallet = Wallet::from_bytes(&state.signer.address().0)
+    let Signer::Local(local_signer) = &state.signer else {
+        return Err(ProofSenderError::InternalError(
+            "Aligned mode only supports local signer".to_string(),
+        ));
+    };
+
+    let wallet = Wallet::from_bytes(local_signer.private_key.as_ref())
         .map_err(|_| ProofSenderError::InternalError("Failed to create wallet".to_owned()))?;
 
     let wallet = wallet.with_chain_id(state.l1_chain_id);