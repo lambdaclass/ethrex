720272b61fce8be62ab59c663355a2a1322262f6
ilitteri <ilitteri@fi.uba.ar>
2025-07-22 17:08:28 -0300

    Remove old prover crate

--- crates/l2/prover_old/src/config.rs
+++
@@ -1,9 +0,0 @@
-use serde::Deserialize;
-
-#[derive(Deserialize, Debug)]
-pub struct ProverConfig {
-    pub http_addr: String,
-    pub http_port: u16,
-    pub proving_time_ms: u64,
-    pub aligned_mode: bool,
-}
