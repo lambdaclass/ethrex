d55ab548f3ca709fcfbd13c7ee1fe4df56f05707
juanbono <juan.bono@lambdaclass.com>
2025-07-23 11:54:20 -0300

    fixes

--- crates/l2/contracts/src/l1/based/RewardVault.sol
+++ crates/l2/contracts/src/l1/based/RewardVault.sol
@@ -5,15 +5,17 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./interfaces/IOnChainProposer.sol";
 
 contract RewardVault {
-    address public onChainProposer;
-    address public rewardToken;
+    IOnChainProposer public onChainProposer;
+    IERC20 public rewardToken;
 
     // TODO: we should replace this value with a mechanism that changes over time.
-    uint public tokensUnlockedPerDay;
+    uint256 public tokensUnlockedPerDay;
 
-    constructor(address _onChainProposer, address _rewardToken) {
-        onChainProposer = _onChainProposer;
-        rewardToken = _rewardToken;
+    constructor(address _onChainProposer, address _rewardToken, uint256 _tokensUnlockedPerDay) {
+        onChainProposer = IOnChainProposer(_onChainProposer);
+        rewardToken = IERC20(_rewardToken);
+        // TODO: remove this once the mechanism is in place.
+        tokensUnlockedPerDay = _tokensUnlockedPerDay;
     }
 
 
@@ -24,20 +26,19 @@ contract RewardVault {
         address sender = msg.sender;
 
         uint256 numberOfBatches = _batchNumbers.length;
-        IOnChainProposer prover = IOnChainProposer(onChainProposer);
         uint256 gasProvenByClaimer = 0;
         for (uint256 i = 0; i < numberOfBatches; i++) {
             uint256 batchNumber = _batchNumbers[i];
-            (address proverAddress, uint256 gasProven) = prover.verifiedBatches(batchNumber);
+            (address proverAddress, uint256 gasProven) = onChainProposer.verifiedBatches(batchNumber);
             require(proverAddress == sender, "Sender is not the prover");
             gasProvenByClaimer += gasProven;
         }
 
         // calculate the rewards for the prover and transfer them
-        uint256 totalGasProven = prover.getTotalGasProven();
-        uint256 dailyRewardPool = tokensUnlockedPerDay * 10 ** IERC20(rewardToken).decimals();
+        uint256 totalGasProven = onChainProposer.getTotalGasProven();
+        uint256 dailyRewardPool = tokensUnlockedPerDay * 10 ** rewardToken.decimals();
         uint256 totalRewards = dailyRewardPool * gasProvenByClaimer / totalGasProven;
 
-        IERC20(rewardToken).transfer(sender, totalRewards);
+        rewardToken.transfer(sender, totalRewards);
     }
 }
