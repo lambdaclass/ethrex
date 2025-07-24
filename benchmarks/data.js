5b48fa02a61bcfab48e3e6784aac98e1ac559935
github-action-benchmark <github@users.noreply.github.com>
2025-07-24 15:01:43 +0000

    add Benchmark (cargo) benchmark result for df3a9bd81724520f527cc837775419629eebcfec

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1753367764010,
+  "lastUpdate": 1753369303455,
   "repoUrl": "https://github.com/lambdaclass/ethrex",
   "entries": {
     "Benchmark": [
@@ -5785,6 +5785,36 @@ window.BENCHMARK_DATA = {
             "name": "Block import/Block import ERC20 transfers",
             "value": 167882348176,
             "range": "± 329419272",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "39842759+gianbelinche@users.noreply.github.com",
+            "name": "Gianbelinche",
+            "username": "gianbelinche"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "df3a9bd81724520f527cc837775419629eebcfec",
+          "message": "feat(l2): enhance monitor performance (#3757)\n\n**Motivation**\nIf a sequencer runs for a long time, it stops, and we run it again\nactivating the monitor, it takes a long time to start and is slow.\n<!-- Why does this pull request exist? What are its goals? -->\n\n**Description**\nMakes the monitor load and work faster by simplifying the batches\nprocessing.\n<!-- A clear and concise general description of the changes this PR\nintroduces -->\n\n**How to Test**\n\n- Run a Sequencer (I suggest `make restart` in `crates/l2`).\n- Run the prover with `make init-prover` in `crates/l2`.\n- Let the sequencer ran for some time (at least 60 batches)\n- Kill the sequencer\n- Add `--monitor` to the `init-l2-no-metrics` target in\n`crates/l2/Makefile`.\n- Run `make init-l2-no-metrics`\n\n<!-- Link to issues: Resolves #111, Resolves #222 -->",
+          "timestamp": "2025-07-24T14:16:59Z",
+          "tree_id": "aea4bef1fd38b28adda61ffe55e827444f640da9",
+          "url": "https://github.com/lambdaclass/ethrex/commit/df3a9bd81724520f527cc837775419629eebcfec"
+        },
+        "date": 1753369289952,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "Block import/Block import ERC20 transfers",
+            "value": 165309285062,
+            "range": "± 251370086",
             "unit": "ns/iter"
           }
         ]
