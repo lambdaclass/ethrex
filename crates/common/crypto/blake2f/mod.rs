da192928ee5520f1b53dd5081a331cc012fa2be0
Esteve Soler Arderiu <azteca1998@users.noreply.github.com>
2025-07-25 21:55:41 +0200

    Rewrite algorithm in assembly.

--- crates/common/crypto/blake2f/mod.rs
+++ crates/common/crypto/blake2f/mod.rs
@@ -9,14 +9,7 @@ type Blake2Func = fn(usize, &mut [u64; 8], &[u64; 16], &[u64; 2], bool);
 static BLAKE2_FUNC: LazyLock<Blake2Func> = LazyLock::new(|| {
     #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
     if is_x86_feature_detected!("avx2") {
-        #[inline(always)]
-        fn inner(r: usize, h: &mut [u64; 8], m: &[u64; 16], t: &[u64; 2], f: bool) {
-            unsafe {
-                self::x86_64::blake2b_f(r, h, m, t, f);
-            }
-        }
-
-        return inner;
+        return self::x86_64::blake2b_f;
     }
 
     self::portable::blake2b_f
