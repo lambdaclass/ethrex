8bbb28819f9858a87acbd3c45dcc7c0d26173e1e
ilitteri <ilitteri@fi.uba.ar>
2025-07-28 19:38:31 -0300

    Remove leftovers

--- crates/common/rlp/rlp.rs
+++ crates/common/rlp/rlp.rs
@@ -6,15 +6,3 @@ pub mod structs;
 
 use ethereum_types::H256;
 use std::str::FromStr;
-
-pub fn get_hashed_keys() -> Vec<&'static str> {
-    include!("../../../hashed_keys.rs")
-}
-
-pub fn get_proofs() -> Vec<Vec<u8>> {
-    include!("../../../proof.rs")
-}
-
-pub fn get_encoded_values() -> Vec<Vec<u8>> {
-    include!("../../../values.rs")
-}
