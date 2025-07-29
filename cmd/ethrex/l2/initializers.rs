f49e9df522a9ffb2252a056094731ec0fb6239da
Tomás Paradelo <tomas.paradelo@lambdaclass.com>
2025-07-29 15:11:02 -0300

    refactor: move function to match original order

--- cmd/ethrex/l2/initializers.rs
+++ cmd/ethrex/l2/initializers.rs
@@ -35,6 +35,117 @@ use crate::utils::{
     NodeConfigFile, get_client_version, read_jwtsecret_file, set_datadir, store_node_config_file,
 };
 
+#[allow(clippy::too_many_arguments)]
+async fn init_rpc_api(
+    opts: &L1Options,
+    l2_opts: &L2Options,
+    peer_table: Arc<Mutex<KademliaTable>>,
+    local_p2p_node: Node,
+    local_node_record: NodeRecord,
+    store: Store,
+    blockchain: Arc<Blockchain>,
+    cancel_token: CancellationToken,
+    tracker: TaskTracker,
+    rollup_store: StoreRollup,
+) {
+    let peer_handler = PeerHandler::new(peer_table);
+
+    // Create SyncManager
+    let syncer = SyncManager::new(
+        peer_handler.clone(),
+        opts.syncmode.clone(),
+        cancel_token,
+        blockchain.clone(),
+        store.clone(),
+    )
+    .await;
+
+    let rpc_api = ethrex_l2_rpc::start_api(
+        get_http_socket_addr(opts),
+        get_authrpc_socket_addr(opts),
+        store,
+        blockchain,
+        read_jwtsecret_file(&opts.authrpc_jwtsecret),
+        local_p2p_node,
+        local_node_record,
+        syncer,
+        peer_handler,
+        get_client_version(),
+        get_valid_delegation_addresses(l2_opts),
+        l2_opts.sponsor_private_key,
+        rollup_store,
+    );
+
+    tracker.spawn(rpc_api);
+}
+
+fn get_valid_delegation_addresses(l2_opts: &L2Options) -> Vec<Address> {
+    let Some(ref path) = l2_opts.sponsorable_addresses_file_path else {
+        warn!("No valid addresses provided, ethrex_SendTransaction will always fail");
+        return Vec::new();
+    };
+    let addresses: Vec<Address> = read_to_string(path)
+        .unwrap_or_else(|_| panic!("Failed to load file {path}"))
+        .lines()
+        .filter(|line| !line.trim().is_empty())
+        .map(|line| line.to_string().parse::<Address>())
+        .filter_map(Result::ok)
+        .collect();
+    if addresses.is_empty() {
+        warn!("No valid addresses provided, ethrex_SendTransaction will always fail");
+    }
+    addresses
+}
+
+pub async fn init_rollup_store(data_dir: &str) -> StoreRollup {
+    cfg_if::cfg_if! {
+        if #[cfg(feature = "rollup_storage_sql")] {
+            let engine_type = EngineTypeRollup::SQL;
+        } else if #[cfg(feature = "rollup_storage_redb")] {
+            let engine_type = EngineTypeRollup::RedB;
+        } else if #[cfg(feature = "rollup_storage_libmdbx")] {
+            let engine_type = EngineTypeRollup::Libmdbx;
+        } else {
+            let engine_type = EngineTypeRollup::InMemory;
+        }
+    }
+    let rollup_store =
+        StoreRollup::new(data_dir, engine_type).expect("Failed to create StoreRollup");
+    rollup_store
+        .init()
+        .await
+        .expect("Failed to init rollup store");
+    rollup_store
+}
+
+fn init_metrics(opts: &L1Options, tracker: TaskTracker) {
+    tracing::info!(
+        "Starting metrics server on {}:{}",
+        opts.metrics_addr,
+        opts.metrics_port
+    );
+    let metrics_api = ethrex_metrics::l2::api::start_prometheus_metrics_api(
+        opts.metrics_addr.clone(),
+        opts.metrics_port.clone(),
+    );
+    tracker.spawn(metrics_api);
+}
+
+fn init_tracing(opts: &L2Options) {
+    if opts.sequencer_opts.monitor {
+        let level_filter = EnvFilter::builder()
+            .parse_lossy("debug,tower_http::trace=debug,reqwest_tracing=off,hyper=off,libsql=off,ethrex::initializers=off,ethrex::l2::initializers=off,ethrex::l2::command=off");
+        let subscriber = tracing_subscriber::registry()
+            .with(TuiTracingSubscriberLayer)
+            .with(level_filter);
+        tracing::subscriber::set_global_default(subscriber)
+            .expect("setting default subscriber failed");
+        tui_logger::init_logger(LevelFilter::max()).expect("Failed to initialize tui_logger");
+    } else {
+        initializers::init_tracing(&opts.node_opts);
+    }
+}
+
 pub async fn init_l2(opts: L2Options) -> eyre::Result<()> {
     if opts.node_opts.evm == EvmEngine::REVM {
         panic!("L2 Doesn't support REVM, use LEVM instead.");
@@ -165,115 +276,4 @@ pub async fn init_l2(opts: L2Options) -> eyre::Result<()> {
     tokio::time::sleep(Duration::from_secs(1)).await;
     info!("Server shutting down!");
     Ok(())
-}
-
-#[allow(clippy::too_many_arguments)]
-async fn init_rpc_api(
-    opts: &L1Options,
-    l2_opts: &L2Options,
-    peer_table: Arc<Mutex<KademliaTable>>,
-    local_p2p_node: Node,
-    local_node_record: NodeRecord,
-    store: Store,
-    blockchain: Arc<Blockchain>,
-    cancel_token: CancellationToken,
-    tracker: TaskTracker,
-    rollup_store: StoreRollup,
-) {
-    let peer_handler = PeerHandler::new(peer_table);
-
-    // Create SyncManager
-    let syncer = SyncManager::new(
-        peer_handler.clone(),
-        opts.syncmode.clone(),
-        cancel_token,
-        blockchain.clone(),
-        store.clone(),
-    )
-    .await;
-
-    let rpc_api = ethrex_l2_rpc::start_api(
-        get_http_socket_addr(opts),
-        get_authrpc_socket_addr(opts),
-        store,
-        blockchain,
-        read_jwtsecret_file(&opts.authrpc_jwtsecret),
-        local_p2p_node,
-        local_node_record,
-        syncer,
-        peer_handler,
-        get_client_version(),
-        get_valid_delegation_addresses(l2_opts),
-        l2_opts.sponsor_private_key,
-        rollup_store,
-    );
-
-    tracker.spawn(rpc_api);
-}
-
-fn get_valid_delegation_addresses(l2_opts: &L2Options) -> Vec<Address> {
-    let Some(ref path) = l2_opts.sponsorable_addresses_file_path else {
-        warn!("No valid addresses provided, ethrex_SendTransaction will always fail");
-        return Vec::new();
-    };
-    let addresses: Vec<Address> = read_to_string(path)
-        .unwrap_or_else(|_| panic!("Failed to load file {path}"))
-        .lines()
-        .filter(|line| !line.trim().is_empty())
-        .map(|line| line.to_string().parse::<Address>())
-        .filter_map(Result::ok)
-        .collect();
-    if addresses.is_empty() {
-        warn!("No valid addresses provided, ethrex_SendTransaction will always fail");
-    }
-    addresses
-}
-
-pub async fn init_rollup_store(data_dir: &str) -> StoreRollup {
-    cfg_if::cfg_if! {
-        if #[cfg(feature = "rollup_storage_sql")] {
-            let engine_type = EngineTypeRollup::SQL;
-        } else if #[cfg(feature = "rollup_storage_redb")] {
-            let engine_type = EngineTypeRollup::RedB;
-        } else if #[cfg(feature = "rollup_storage_libmdbx")] {
-            let engine_type = EngineTypeRollup::Libmdbx;
-        } else {
-            let engine_type = EngineTypeRollup::InMemory;
-        }
-    }
-    let rollup_store =
-        StoreRollup::new(data_dir, engine_type).expect("Failed to create StoreRollup");
-    rollup_store
-        .init()
-        .await
-        .expect("Failed to init rollup store");
-    rollup_store
-}
-
-fn init_metrics(opts: &L1Options, tracker: TaskTracker) {
-    tracing::info!(
-        "Starting metrics server on {}:{}",
-        opts.metrics_addr,
-        opts.metrics_port
-    );
-    let metrics_api = ethrex_metrics::l2::api::start_prometheus_metrics_api(
-        opts.metrics_addr.clone(),
-        opts.metrics_port.clone(),
-    );
-    tracker.spawn(metrics_api);
-}
-
-fn init_tracing(opts: &L2Options) {
-    if opts.sequencer_opts.monitor {
-        let level_filter = EnvFilter::builder()
-            .parse_lossy("debug,tower_http::trace=debug,reqwest_tracing=off,hyper=off,libsql=off,ethrex::initializers=off,ethrex::l2::initializers=off,ethrex::l2::command=off");
-        let subscriber = tracing_subscriber::registry()
-            .with(TuiTracingSubscriberLayer)
-            .with(level_filter);
-        tracing::subscriber::set_global_default(subscriber)
-            .expect("setting default subscriber failed");
-        tui_logger::init_logger(LevelFilter::max()).expect("Failed to initialize tui_logger");
-    } else {
-        initializers::init_tracing(&opts.node_opts);
-    }
 }
