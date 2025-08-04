373225ecd3f418f6ec9e2deb3a6cd5a307145a1f
github-action-benchmark <github@users.noreply.github.com>
2025-08-04 11:50:10 -0300

    add L1 block proving benchmark (customBiggerIsBetter) benchmark result for 2b5f17e54d97e5d29ef77858e67d780a134a77b9

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1754095142872,
+  "lastUpdate": 1754319010477,
   "repoUrl": "https://github.com/lambdaclass/ethrex",
   "entries": {
     "Benchmark": [
@@ -16211,6 +16211,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "SP1, RTX A6000",
             "value": 0.006266347417840375,
+            "unit": "Mgas/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mrugiero@gmail.com",
+            "name": "Mario Rugiero",
+            "username": "Oppen"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2b5f17e54d97e5d29ef77858e67d780a134a77b9",
+          "message": "fix(l1): avoid broadcasting duplicate transactions (#3897)\n\n**Motivation**\n\nWire protocol correctness. Crucially, sending a transaction back to its\nsource is punishable by connection drop.\n\n**Description**\n\nAdd an early check for presence in the mempool for received\ntransactions, update the per-peer filters, and use them to avoid sending\nthem transactions they already know about in the `Transactions` relay\nand the `SendNewPooledTransactionHashes` paths.",
+          "timestamp": "2025-08-04T14:07:32Z",
+          "tree_id": "8d31b85b81f96d69ae44bddc24f467718c3d6c33",
+          "url": "https://github.com/lambdaclass/ethrex/commit/2b5f17e54d97e5d29ef77858e67d780a134a77b9"
+        },
+        "date": 1754319009881,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "SP1, RTX A6000",
+            "value": 0.005854087719298246,
             "unit": "Mgas/s"
           }
         ]
