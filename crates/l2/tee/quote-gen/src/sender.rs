3bfe0f21d6e9e34c89ee9ef1e80b248ffe3bd4a8
JereSalo <jeresalo17@gmail.com>
2025-08-08 17:15:03 -0300

    add chain_config in missing places

--- crates/l2/tee/quote-gen/src/sender.rs
+++ crates/l2/tee/quote-gen/src/sender.rs
@@ -33,6 +33,7 @@ pub async fn get_batch(commit_hash: String) -> Result<(u64, ProgramInput), Strin
                     blob_commitment: input.blob_commitment,
                     #[cfg(feature = "l2")]
                     blob_proof: input.blob_proof,
+                    chain_config: input.db.chain_config,
                 },
             )),
             _ => Err("No blocks to prove.".to_owned()),
