8e2546b2ef8c5c940e13ab76c478bed97b1e2da5
ilitteri <ilitteri@fi.uba.ar>
2025-08-11 15:04:52 -0300

    Add support for sending reports to slack

--- cmd/ethrex_replay/src/bin/replayer/block_execution_report.rs
+++ cmd/ethrex_replay/src/bin/replayer/block_execution_report.rs
@@ -1,46 +1,287 @@
-use std::fmt::Display;
+use std::{fmt::Display, time::Duration};
 
 use ethrex_common::types::Block;
-use ethrex_replay::networks::Network;
+use ethrex_replay::networks::{Network, PublicNetwork};
+
+use crate::slack::{SlackWebHookActionElement, SlackWebHookBlock, SlackWebHookRequest};
 
 pub struct BlockExecutionReport {
     pub network: Network,
     pub number: u64,
     pub gas: u64,
     pub txs: u64,
-    pub execution_result: String,
+    pub execution_result: Result<(), eyre::Report>,
+    pub time_taken: Duration,
 }
 
 impl BlockExecutionReport {
-    pub fn new(block: Block, network: Network, execution_result: String) -> Self {
+    pub fn new_for(
+        block: Block,
+        network: Network,
+        execution_result: Result<(), eyre::Report>,
+        time_taken: Duration,
+    ) -> Self {
         Self {
             network,
             number: block.header.number,
             gas: block.header.gas_used,
             txs: block.body.transactions.len() as u64,
             execution_result,
+            time_taken,
+        }
+    }
+
+    pub fn to_slack_message(&self) -> SlackWebHookRequest {
+        SlackWebHookRequest {
+            blocks: vec![
+                SlackWebHookBlock::Header {
+                    text: Box::new(SlackWebHookBlock::PlainText {
+                        text: if self.execution_result.is_err() {
+                            String::from("⚠️ Failed to Execute Block with SP1")
+                        } else {
+                            String::from("✅ Successfully Executed Block with SP1")
+                        },
+                        emoji: true,
+                    }),
+                },
+                SlackWebHookBlock::Section {
+                    text: Box::new(SlackWebHookBlock::Markdown {
+                        text: format!(
+                            "*Network:* `{network}`\n*Block:* {number}\n*Gas:* {gas}\n*#Txs:* {txs}\n*Execution Result:* {execution_result}",
+                            network = self.network,
+                            number = self.number,
+                            gas = self.gas,
+                            txs = self.txs,
+                            execution_result = if self.execution_result.is_err() {
+                                "Error: ".to_string()
+                                    + &self.execution_result.as_ref().err().unwrap().to_string()
+                            } else {
+                                "Success".to_string()
+                            }
+                        ),
+                    }),
+                },
+                SlackWebHookBlock::Actions {
+                    elements: vec![SlackWebHookActionElement::Button {
+                        text: SlackWebHookBlock::PlainText {
+                            text: String::from("View on Etherscan"),
+                            emoji: false,
+                        },
+                        url: if let Network::PublicNetwork(PublicNetwork::Mainnet) = self.network {
+                            format!("https://etherscan.io/block/{}", self.number)
+                        } else {
+                            format!(
+                                "https://{}.etherscan.io/block/{}",
+                                self.network, self.number
+                            )
+                        },
+                    }],
+                },
+            ],
         }
     }
 }
 
 impl Display for BlockExecutionReport {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let execution_result = if let Err(e) = &self.execution_result {
+            format!("Error: {e}")
+        } else {
+            "Success".to_string()
+        };
         if let Network::PublicNetwork(_) = self.network {
             write!(
                 f,
-                "[{network}] Block #{number}, Gas Used: {gas}, Tx Count: {txs}, Execution Result: {execution_result} | https://{network}.etherscan.io/block/{number}",
+                "[{network}] Block #{number}, Gas Used: {gas}, Tx Count: {txs}, Execution Result: {execution_result}, Time Taken: {time_taken} | https://{network}.etherscan.io/block/{number}",
                 network = self.network,
                 number = self.number,
                 gas = self.gas,
                 txs = self.txs,
-                execution_result = self.execution_result,
+                execution_result = execution_result,
+                time_taken = format_duration(self.time_taken),
             )
         } else {
             write!(
                 f,
-                "[{}] Block #{}, Gas Used: {}, Tx Count: {}, Execution Result: {}",
-                self.network, self.number, self.gas, self.txs, self.execution_result
+                "[{network}] Block #{number}, Gas Used: {gas}, Tx Count: {txs}, Execution Result: {execution_result}, Time Taken: {time_taken}",
+                network = self.network,
+                number = self.number,
+                gas = self.gas,
+                txs = self.txs,
+                execution_result = execution_result,
+                time_taken = format_duration(self.time_taken),
             )
         }
+    }
+}
+
+fn format_duration(duration: Duration) -> String {
+    let total_seconds = duration.as_secs();
+    let minutes = (total_seconds % 3600) / 60;
+    let seconds = total_seconds % 60;
+    let milliseconds = duration.subsec_millis();
+
+    if minutes == 0 {
+        return format!("{seconds:02}s {milliseconds:03}ms");
+    }
+
+    format!("{minutes:02}m {seconds:02}s")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_slack_message_failed_mainnet_execution() {
+        let report = BlockExecutionReport::new_for(
+            Block::default(),
+            Network::PublicNetwork(PublicNetwork::Mainnet),
+            Err(eyre::Report::msg("Execution failed")),
+            Duration::from_secs(1),
+        );
+
+        let slack_message = report.to_slack_message();
+
+        let slack_message_json_request = serde_json::to_string_pretty(&slack_message)
+            .expect("Failed to serialize Slack message");
+
+        let expected_json = r#"{
+  "blocks": [
+    {
+      "type": "header",
+      "text": {
+        "type": "plain_text",
+        "text": "⚠️ Failed to Execute Block with SP1",
+        "emoji": true
+      }
+    },
+    {
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text": "*Network:* `mainnet`\n*Block:* 0\n*Gas:* 0\n*#Txs:* 0\n*Execution Result:* Error: Execution failed"
+      }
+    },
+    {
+      "type": "actions",
+      "elements": [
+        {
+          "type": "button",
+          "text": {
+            "type": "plain_text",
+            "text": "View on Etherscan",
+            "emoji": false
+          },
+          "url": "https://etherscan.io/block/0"
+        }
+      ]
+    }
+  ]
+}"#;
+
+        assert_eq!(slack_message_json_request, expected_json);
+    }
+
+    #[test]
+    fn test_slack_message_failed_hoodi_execution() {
+        let report = BlockExecutionReport::new_for(
+            Block::default(),
+            Network::PublicNetwork(PublicNetwork::Hoodi),
+            Err(eyre::Report::msg("Execution failed")),
+            Duration::from_secs(1),
+        );
+
+        let slack_message = report.to_slack_message();
+
+        let slack_message_json_request = serde_json::to_string_pretty(&slack_message)
+            .expect("Failed to serialize Slack message");
+
+        let expected_json = r#"{
+  "blocks": [
+    {
+      "type": "header",
+      "text": {
+        "type": "plain_text",
+        "text": "⚠️ Failed to Execute Block with SP1",
+        "emoji": true
+      }
+    },
+    {
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text": "*Network:* `hoodi`\n*Block:* 0\n*Gas:* 0\n*#Txs:* 0\n*Execution Result:* Error: Execution failed"
+      }
+    },
+    {
+      "type": "actions",
+      "elements": [
+        {
+          "type": "button",
+          "text": {
+            "type": "plain_text",
+            "text": "View on Etherscan",
+            "emoji": false
+          },
+          "url": "https://hoodi.etherscan.io/block/0"
+        }
+      ]
+    }
+  ]
+}"#;
+
+        assert_eq!(slack_message_json_request, expected_json);
+    }
+
+    #[test]
+    fn test_slack_message_failed_sepolia_execution() {
+        let report = BlockExecutionReport::new_for(
+            Block::default(),
+            Network::PublicNetwork(PublicNetwork::Sepolia),
+            Err(eyre::Report::msg("Execution failed")),
+            Duration::from_secs(1),
+        );
+
+        let slack_message = report.to_slack_message();
+
+        let slack_message_json_request = serde_json::to_string_pretty(&slack_message)
+            .expect("Failed to serialize Slack message");
+
+        let expected_json = r#"{
+  "blocks": [
+    {
+      "type": "header",
+      "text": {
+        "type": "plain_text",
+        "text": "⚠️ Failed to Execute Block with SP1",
+        "emoji": true
+      }
+    },
+    {
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text": "*Network:* `sepolia`\n*Block:* 0\n*Gas:* 0\n*#Txs:* 0\n*Execution Result:* Error: Execution failed"
+      }
+    },
+    {
+      "type": "actions",
+      "elements": [
+        {
+          "type": "button",
+          "text": {
+            "type": "plain_text",
+            "text": "View on Etherscan",
+            "emoji": false
+          },
+          "url": "https://sepolia.etherscan.io/block/0"
+        }
+      ]
+    }
+  ]
+}"#;
+
+        assert_eq!(slack_message_json_request, expected_json);
     }
 }
--- cmd/ethrex_replay/src/bin/replayer/main.rs
+++ cmd/ethrex_replay/src/bin/replayer/main.rs
@@ -6,64 +6,99 @@ use ethrex_replay::{
 };
 use ethrex_rpc::{EthClient, clients::EthClientError, types::block_identifier::BlockIdentifier};
 use reqwest::Url;
+use tokio::task::JoinHandle;
+
+use crate::block_execution_report::BlockExecutionReport;
 
 mod block_execution_report;
+mod slack;
 
 #[tokio::main]
 async fn main() {
+    if std::env::var("SLACK_WEBHOOK_URL").is_err() {
+        tracing::warn!(
+            "SLACK_WEBHOOK_URL environment variable is not set. Slack notifications will not be sent."
+        );
+    }
+
     init_tracing();
 
+    // TODO: These RPC URLs should be configurable via environment variables or command line arguments.
     let hoodi_rpc_url = "http://65.108.69.58:8545";
     let sepolia_rpc_url = "";
     let mainnet_rpc_url = "http://157.180.1.98:8545";
 
-    let hoodi_task_handle = tokio::spawn(async {
-        replay(hoodi_rpc_url, Network::PublicNetwork(PublicNetwork::Hoodi)).await
-    });
-    let hoodi_rpc_revalidation_handle = tokio::spawn(async { revalidate_rpc(hoodi_rpc_url).await });
+    let replayers = [
+        (Network::PublicNetwork(PublicNetwork::Hoodi), hoodi_rpc_url),
+        (
+            Network::PublicNetwork(PublicNetwork::Sepolia),
+            sepolia_rpc_url,
+        ),
+        (
+            Network::PublicNetwork(PublicNetwork::Mainnet),
+            mainnet_rpc_url,
+        ),
+    ];
 
-    let sepolia_task_handle =
-        tokio::spawn(async { replay("", Network::PublicNetwork(PublicNetwork::Sepolia)).await });
+    let mut replayers_handles = Vec::new();
+
+    for (network, rpc_url) in replayers {
+        tracing::info!("Starting replayer for network: {network} with RPC URL: {rpc_url}");
+
+        let handle = tokio::spawn(async move { replay(rpc_url, network).await });
+
+        replayers_handles.push(handle);
+    }
+
+    // TODO: These tasks are spawned outside the above loop to be able to handled
+    // in the tokio::select!. We should find a way to spawn them inside the loop
+    // and still be able to handle them in the tokio::select!.
+    let hoodi_rpc_revalidation_handle = tokio::spawn(async { revalidate_rpc(hoodi_rpc_url).await });
     let sepolia_rpc_revalidation_handle =
         tokio::spawn(async { revalidate_rpc(sepolia_rpc_url).await });
-
-    let mainnet_task_handle = tokio::spawn(async {
-        replay(
-            mainnet_rpc_url,
-            Network::PublicNetwork(PublicNetwork::Mainnet),
-        )
-        .await
-    });
     let mainnet_rpc_revalidation_handle =
         tokio::spawn(async { revalidate_rpc(mainnet_rpc_url).await });
 
     tokio::select! {
         _ = tokio::signal::ctrl_c() => {
             tracing::info!("Received Ctrl+C, shutting down...");
-            hoodi_task_handle.abort();
-            sepolia_task_handle.abort();
-            mainnet_task_handle.abort();
+            shutdown(replayers_handles);
         }
         res = hoodi_rpc_revalidation_handle => {
             if let Err(e) = res {
                 tracing::error!("Hoodi RPC failed: {e}");
+                try_notify_no_longer_valid_rpc_to_slack(
+                    hoodi_rpc_url,
+                    Network::PublicNetwork(PublicNetwork::Hoodi),
+                ).await.unwrap_or_else(|e| {
+                    tracing::error!("Failed to notify Slack about invalid Hoodi RPC: {e}");
+                });
             }
-            sepolia_task_handle.abort();
-            mainnet_task_handle.abort();
+            shutdown(replayers_handles);
         }
         res = sepolia_rpc_revalidation_handle => {
             if let Err(e) = res {
                 tracing::error!("Sepolia RPC failed: {e}");
+                try_notify_no_longer_valid_rpc_to_slack(
+                    sepolia_rpc_url,
+                    Network::PublicNetwork(PublicNetwork::Sepolia),
+                ).await.unwrap_or_else(|e| {
+                    tracing::error!("Failed to notify Slack about invalid Sepolia RPC: {e}");
+                });
             }
-            hoodi_task_handle.abort();
-            mainnet_task_handle.abort();
+            shutdown(replayers_handles);
         }
         res = mainnet_rpc_revalidation_handle => {
             if let Err(e) = res {
                 tracing::error!("Mainnet RPC failed: {e}");
+                try_notify_no_longer_valid_rpc_to_slack(
+                    mainnet_rpc_url,
+                    Network::PublicNetwork(PublicNetwork::Mainnet),
+                ).await.unwrap_or_else(|e| {
+                    tracing::error!("Failed to notify Slack about invalid Mainnet RPC: {e}");
+                });
             }
-            hoodi_task_handle.abort();
-            sepolia_task_handle.abort();
+            shutdown(replayers_handles);
         }
     }
 }
@@ -103,7 +138,7 @@ async fn replay(rpc_url: &str, network: Network) -> Result<(), EthClientError> {
 
         let start = SystemTime::now();
 
-        let _execution_result = SubcommandExecute::Block {
+        let execution_result = SubcommandExecute::Block {
             block: Some(latest_block), // This will execute the latest block
             rpc_url: Url::parse(rpc_url).unwrap(),
             network: network.clone(),
@@ -118,6 +153,19 @@ async fn replay(rpc_url: &str, network: Network) -> Result<(), EthClientError> {
         let elapsed = start.elapsed().unwrap_or_else(|e| {
             panic!("SystemTime::elapsed failed: {e}");
         });
+
+        let block_execution_report =
+            BlockExecutionReport::new_for(block, network.clone(), execution_result, elapsed);
+
+        tracing::info!("{block_execution_report}");
+
+        if block_execution_report.execution_result.is_err() {
+            try_send_failed_execution_report_to_slack(block_execution_report)
+                .await
+                .unwrap_or_else(|e| {
+                    tracing::error!("Failed to post to Slack webhook: {e}");
+                });
+        }
 
         // Wait at most 12 seconds for executing the next block.
         // This will only wait if the execution took less than 12 seconds.
@@ -142,5 +190,62 @@ async fn revalidate_rpc(rpc_url: &str) -> Result<(), EthClientError> {
         eth_client.get_block_number().await.map(|_| ())?;
 
         interval.tick().await;
+    }
+}
+
+async fn try_send_failed_execution_report_to_slack(
+    report: BlockExecutionReport,
+) -> Result<(), reqwest::Error> {
+    let Ok(webhook_url) = std::env::var("SLACK_WEBHOOK_URL") else {
+        return Ok(());
+    };
+
+    let client = reqwest::Client::new();
+
+    let payload = report.to_slack_message();
+
+    client.post(webhook_url).json(&payload).send().await?;
+
+    Ok(())
+}
+
+async fn try_notify_no_longer_valid_rpc_to_slack(
+    rpc_url: &str,
+    network: Network,
+) -> Result<(), reqwest::Error> {
+    let Ok(webhook_url) = std::env::var("SLACK_WEBHOOK_URL") else {
+        return Ok(());
+    };
+
+    let client = reqwest::Client::new();
+
+    let payload = slack::SlackWebHookRequest {
+        blocks: vec![
+            slack::SlackWebHookBlock::Header {
+                text: Box::new(slack::SlackWebHookBlock::PlainText {
+                    text: "⚠️ RPC URL is no longer valid".to_string(),
+                    emoji: true,
+                }),
+            },
+            slack::SlackWebHookBlock::Section {
+                text: Box::new(slack::SlackWebHookBlock::Markdown {
+                    text: format!("`{network}`'s RPC URL `{rpc_url}` is no longer valid."),
+                }),
+            },
+        ],
+    };
+
+    client.post(webhook_url).json(&payload).send().await?;
+
+    Ok(())
+}
+
+fn shutdown(handles: Vec<JoinHandle<Result<(), EthClientError>>>) {
+    tracing::info!("Shutting down...");
+
+    for handle in handles {
+        if !handle.is_finished() {
+            handle.abort();
+        }
     }
 }
