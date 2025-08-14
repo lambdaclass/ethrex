5d5544d9a23691dac4023437f15b867ae5403cff
ilitteri <ilitteri@fi.uba.ar>
2025-08-14 11:45:03 -0300

    Fix format duration (show hours now)

--- cmd/ethrex_replay/src/bin/replayer/block_execution_report.rs
+++ cmd/ethrex_replay/src/bin/replayer/block_execution_report.rs
@@ -142,9 +142,14 @@ impl Display for BlockRunReport {
 
 fn format_duration(duration: Duration) -> String {
     let total_seconds = duration.as_secs();
+    let hours = total_seconds / 3600;
     let minutes = (total_seconds % 3600) / 60;
     let seconds = total_seconds % 60;
-    let milliseconds = duration.subsec_millis();
+    let milliseconds = total_seconds / 1000;
+
+    if hours > 0 {
+        return format!("{hours:02}h {minutes:02}m {seconds:02}s {milliseconds:03}ms");
+    }
 
     if minutes == 0 {
         return format!("{seconds:02}s {milliseconds:03}ms");
