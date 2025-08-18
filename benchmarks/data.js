c0e8bf10601649fc15db5e459c7cf829886f1a42
github-action-benchmark <github@users.noreply.github.com>
2025-08-18 12:37:10 -0300

    add L1 block proving benchmark (customBiggerIsBetter) benchmark result for e1da8bff3abbafd6dc202385bc56f72d3f9ddf78

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1755530302946,
+  "lastUpdate": 1755531430399,
   "repoUrl": "https://github.com/lambdaclass/ethrex",
   "entries": {
     "Benchmark": [
@@ -20612,6 +20612,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Risc0, RTX A6000",
             "value": 0.0013774324045407638,
+            "unit": "Mgas/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "git@edgl.dev",
+            "name": "Edgar",
+            "username": "edg-l"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "e1da8bff3abbafd6dc202385bc56f72d3f9ddf78",
+          "message": "chore(levm): remove unused bigint dep from levm (#4060)\n\n**Motivation**\n\nWith the change from bigint to malachite for modexp, bigint is now an\nunused dependency.\n\nfixes #4057",
+          "timestamp": "2025-08-18T14:18:06Z",
+          "tree_id": "00953dde9885a6844f348a43fec8aea364c993c6",
+          "url": "https://github.com/lambdaclass/ethrex/commit/e1da8bff3abbafd6dc202385bc56f72d3f9ddf78"
+        },
+        "date": 1755531429890,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "SP1, RTX A6000",
+            "value": 0.0061793148148148146,
             "unit": "Mgas/s"
           }
         ]
