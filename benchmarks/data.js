0920df42656471d6022c59bc5360880a2d2b8788
github-action-benchmark <github@users.noreply.github.com>
2025-08-19 20:56:05 -0300

    add L1 block proving benchmark (customBiggerIsBetter) benchmark result for da96b3878ff45cb8a1789f0a52124ec624fa4e38

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1755633534192,
+  "lastUpdate": 1755647764883,
   "repoUrl": "https://github.com/lambdaclass/ethrex",
   "entries": {
     "Benchmark": [
@@ -21345,6 +21345,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "SP1, RTX A6000",
             "value": 0.008394540880503145,
+            "unit": "Mgas/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "72628438+avilagaston9@users.noreply.github.com",
+            "name": "Avila Gastón",
+            "username": "avilagaston9"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "da96b3878ff45cb8a1789f0a52124ec624fa4e38",
+          "message": "refactor(l2): remove WrappedTransaction (#3984)\n\n**Motivation**\n\n`WrappedTransaction` is an abstraction that is not really necessary. We\ncan use `GenericTransaction` instead, which serves a similar purpose but\nis easier to manipulate, as we don't have to match Enum variants to\naccess its attributes.\n\n**Description**\n\n- Removes `WrappedTransaction`.\n- Updates all `WrappedTransaction` related functions (send,bump) to use\n`GenericTransactions` instead.\n- Implements mapper functions to convert a `GenericTransaction` to a\nspecific transaction type.\n- Replaces `build_eipxxx`/`send_eipxxx` functions with generic ones:\n`build_generic_tx(type)`/`send_generic_tx()`.\n\nCloses #3787\n\n---------\n\nCo-authored-by: Manuel Iñaki Bilbao <manuel.bilbao@lambdaclass.com>",
+          "timestamp": "2025-08-19T23:26:47Z",
+          "tree_id": "ccbe706b9e892b782a083e1f3d6a4e1fc9c5ef6d",
+          "url": "https://github.com/lambdaclass/ethrex/commit/da96b3878ff45cb8a1789f0a52124ec624fa4e38"
+        },
+        "date": 1755647764423,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "SP1, RTX A6000",
+            "value": 0.008290260869565217,
             "unit": "Mgas/s"
           }
         ]
