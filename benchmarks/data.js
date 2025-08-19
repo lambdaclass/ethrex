f12a59cce169a98bdbe43cfc0b7f85f7fe8bcded
github-action-benchmark <github@users.noreply.github.com>
2025-08-19 16:58:34 +0000

    add Benchmark (cargo) benchmark result for d744176e86df0f0aa6239607decdf96f1312937c

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1755621294535,
+  "lastUpdate": 1755622713977,
   "repoUrl": "https://github.com/lambdaclass/ethrex",
   "entries": {
     "Benchmark": [
@@ -9085,6 +9085,36 @@ window.BENCHMARK_DATA = {
             "name": "Block import/Block import ERC20 transfers",
             "value": 162631155932,
             "range": "± 330927094",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "72628438+avilagaston9@users.noreply.github.com",
+            "name": "Avila Gastón",
+            "username": "avilagaston9"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "d744176e86df0f0aa6239607decdf96f1312937c",
+          "message": "chore(l2): avoid CI_ETHREX_WORKDIR docker warnings (#4064)\n\n**Motivation**\n\nRunning `make down` or `make init` prints the following warnings:\n\n```\nWARN[0000] The \"CI_ETHREX_WORKDIR\" variable is not set. Defaulting to a blank string.\nWARN[0000] The \"CI_ETHREX_WORKDIR\" variable is not set. Defaulting to a blank string.\nWARN[0000] The \"CI_ETHREX_WORKDIR\" variable is not set. Defaulting to a blank string.\nWARN[0000] The \"CI_ETHREX_WORKDIR\" variable is not set. Defaulting to a blank string.\nWARN[0000] The \"CI_ETHREX_WORKDIR\" variable is not set. Defaulting to a blank string.\nWARN[0000] The \"CI_ETHREX_WORKDIR\" variable is not set. Defaulting to a blank string.\nWARN[0000] The \"CI_ETHREX_WORKDIR\" variable is not set. Defaulting to a blank string.\nWARN[0000] The \"CI_ETHREX_WORKDIR\" variable is not set. Defaulting to a blank string.\nWARN[0000] The \"CI_ETHREX_WORKDIR\" variable is not set. Defaulting to a blank string.\nWARN[0000] The \"CI_ETHREX_WORKDIR\" variable is not set. Defaulting to a blank string.\nWARN[0000] The \"CI_ETHREX_WORKDIR\" variable is not set. Defaulting to a blank string.\nWARN[0000] The \"CI_ETHREX_WORKDIR\" variable is not set. Defaulting to a blank string.\nWARN[0000] The \"CI_ETHREX_WORKDIR\" variable is not set. Defaulting to a blank string.\nWARN[0000] The \"CI_ETHREX_WORKDIR\" variable is not set. Defaulting to a blank string.\n```\n\nThe same was happening for the `L1_RPC_URL` variable.\n\n**Description**\n\n- Sets the variables when running docker compose targets.\n- Renames `CI_ETHREX_WORKDIR` to `DOCKER_ETHREX_WORKDIR`.\n\nCloses #3810",
+          "timestamp": "2025-08-19T15:59:20Z",
+          "tree_id": "08994c1f264588261d858b53a144692de442d348",
+          "url": "https://github.com/lambdaclass/ethrex/commit/d744176e86df0f0aa6239607decdf96f1312937c"
+        },
+        "date": 1755622698681,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "Block import/Block import ERC20 transfers",
+            "value": 163275290928,
+            "range": "± 297007885",
             "unit": "ns/iter"
           }
         ]
