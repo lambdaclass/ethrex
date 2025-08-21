1c79d817b9fb735048802dcc6ec5739357a19d3b
Rodrigo Oliveri <rodrigooliveri10@gmail.com>
2025-08-21 16:04:22 -0300

    formatting

--- crates/blockchain/metrics/metrics_process.rs
+++ crates/blockchain/metrics/metrics_process.rs
@@ -29,7 +29,12 @@ impl MetricsProcess {
         {
             use prometheus::process_collector::ProcessCollector;
             r.register(Box::new(ProcessCollector::for_self()))
-                .map_err(|e| MetricsError::PrometheusErr(format!("Failed to register process collector: {}", e)))?;
+                .map_err(|e| {
+                    MetricsError::PrometheusErr(format!(
+                        "Failed to register process collector: {}",
+                        e
+                    ))
+                })?;
         }
 
         let encoder = TextEncoder::new();
