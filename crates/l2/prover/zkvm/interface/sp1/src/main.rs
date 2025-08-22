6a6cbecc99b0531ccac363a06e9cb930005cc783
ilitteri <ilitteri@fi.uba.ar>
2025-08-21 23:17:34 -0300

    Remove times from zkvm program

--- crates/l2/prover/zkvm/interface/sp1/src/main.rs
+++ crates/l2/prover/zkvm/interface/sp1/src/main.rs
@@ -1,12 +1,7 @@
 #![no_main]
 
-use std::time::SystemTime;
-
 use rkyv::rancor::Error;
-use zkvm_interface::{
-    execution::{execution_program, format_duration},
-    io::ProgramInput,
-};
+use zkvm_interface::{execution::execution_program, io::ProgramInput};
 
 sp1_zkvm::entrypoint!(main);
 
@@ -15,42 +10,9 @@ pub fn main() {
 
     let input = sp1_zkvm::io::read_vec();
 
-    let input_read_duration = start.elapsed().unwrap_or_else(|e| {
-        panic!("SystemTime::elapsed failed: {e}");
-    });
-
-    println!("Read input in {}", format_duration(input_read_duration));
-
     let input = rkyv::from_bytes::<ProgramInput, Error>(&input).unwrap();
-
-    let input_decode_duration = start.elapsed().unwrap_or_else(|e| {
-        panic!("SystemTime::elapsed failed: {e}");
-    });
-
-    println!(
-        "Decoded input in {}",
-        format_duration(input_decode_duration - input_read_duration)
-    );
 
     let output = execution_program(input).unwrap();
 
-    let execution_duration = start.elapsed().unwrap_or_else(|e| {
-        panic!("SystemTime::elapsed failed: {e}");
-    });
-
-    println!(
-        "Executed program in {}",
-        format_duration(execution_duration - input_decode_duration)
-    );
-
     sp1_zkvm::io::commit(&output.encode());
-
-    let commit_duration = start.elapsed().unwrap_or_else(|e| {
-        panic!("SystemTime::elapsed failed: {e}");
-    });
-
-    println!(
-        "Committed output in {}",
-        format_duration(commit_duration - execution_duration)
-    );
 }
