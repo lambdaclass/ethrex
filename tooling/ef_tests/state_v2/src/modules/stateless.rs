c4ececd63bd3c82dee188ab4693013f062915737
JereSalo <jeresalo17@gmail.com>
2025-08-26 15:17:04 -0300

    set headers properly

--- tooling/ef_tests/state_v2/src/modules/stateless.rs
+++ tooling/ef_tests/state_v2/src/modules/stateless.rs
@@ -3,7 +3,7 @@ use ethrex_blockchain::{Blockchain, BlockchainType, fork_choice::apply_fork_choi
 use ethrex_common::H256;
 use ethrex_common::constants::DEFAULT_REQUESTS_HASH;
 use ethrex_common::types::{
-    Block, BlockBody, BlockHeader, Receipt, compute_receipts_root, compute_transactions_root,
+    Block, BlockBody, BlockHeader, Fork, Receipt, compute_receipts_root, compute_transactions_root,
 };
 use ethrex_levm::{
     tracing::LevmCallTracer,
@@ -12,6 +12,7 @@ use ethrex_levm::{
 use ethrex_vm::EvmEngine;
 use std::str::FromStr;
 
+use crate::modules::types::TestCase;
 use crate::modules::{
     error::RunnerError,
     runner::{get_tx_from_test_case, get_vm_env_for_test},
@@ -20,22 +21,17 @@ use crate::modules::{
 };
 
 pub async fn run_tests(tests: Vec<Test>) -> Result<(), RunnerError> {
-    // Remove previous report if it exists.
-    // This is for testing purposes
-    let test = tests
-        .iter()
-        .find(|t| t.name.contains("fork_Prague") && !t.test_cases.is_empty())
-        .expect("No test with name containing 'fork_Prague' and non-empty test_cases");
-
-    // run_test(&test).await?;
-    block_run(test).await?;
+    for test in &tests {
+        for test_case in &test.test_cases {
+            single_block_run(test, test_case).await?;
+        }
+    }
 
     Ok(())
 }
 
-pub async fn block_run(test: &Test) -> Result<(), RunnerError> {
+pub async fn single_block_run(test: &Test, test_case: &TestCase) -> Result<(), RunnerError> {
     println!("Test name: {}", test.name);
-    let test_case = &test.test_cases[0];
     let env = get_vm_env_for_test(test.env, test_case)?;
     let tx = get_tx_from_test_case(test_case).await?;
     let tracer = LevmCallTracer::disabled();
@@ -70,6 +66,32 @@ pub async fn block_run(test: &Test) -> Result<(), RunnerError> {
         ..Default::default()
     };
 
+    let fork = test_case.fork;
+    let (excess_blob_gas, blob_gas_used, parent_beacon_block_root, requests_hash) = match fork {
+        Fork::Prague | Fork::Cancun => {
+            let excess_blob_gas = Some(
+                test.env
+                    .current_excess_blob_gas
+                    .unwrap_or_default()
+                    .as_u64(),
+            );
+            let blob_gas_used = Some(env.block_blob_gas_used.map(|v| v.as_u64()).unwrap_or(0));
+            let parent_beacon_block_root = Some(H256::zero());
+            let requests_hash = if fork == Fork::Prague {
+                Some(*DEFAULT_REQUESTS_HASH)
+            } else {
+                None
+            };
+            (
+                excess_blob_gas,
+                blob_gas_used,
+                parent_beacon_block_root,
+                requests_hash,
+            )
+        }
+        _ => (None, None, None, None),
+    };
+
     let header = BlockHeader {
         hash: Default::default(), // I initialize it later with block.hash().
         parent_hash: initial_block_hash,
@@ -95,10 +117,10 @@ pub async fn block_run(test: &Test) -> Result<(), RunnerError> {
             H256::from_str("0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421")
                 .unwrap(),
         ),
-        blob_gas_used: Some(env.block_blob_gas_used.map(|v| v.as_u64()).unwrap_or(0)), //TODO: Blob gas used should only be post Cancun. Also, I think for this I need to do a pre-execution to know blob gas used?
-        excess_blob_gas: test.env.current_excess_blob_gas.map(|v| v.as_u64()),
-        parent_beacon_block_root: Some(H256::zero()),
-        requests_hash: Some(*DEFAULT_REQUESTS_HASH),
+        blob_gas_used, //TODO: I think for this I need to do a pre-execution to know blob gas used? Does it matter?
+        excess_blob_gas,
+        parent_beacon_block_root,
+        requests_hash,
     };
     header.hash();
 
