bb719bf3fa2b384fa97343dbf33df22f348e8ad6
Tomás Paradelo <tomas.paradelo@lambdaclass.com>
2025-08-27 16:14:23 -0300

    fix?: state nodes

--- crates/blockchain/blockchain.rs
+++ crates/blockchain/blockchain.rs
@@ -373,7 +373,7 @@ impl Blockchain {
         }
 
         let mut state_nodes = HashMap::new();
-        for node in trie.into_iter().map(|(_nibble, node)| node.encode_raw()) {
+        for node in used_trie_nodes.into_iter() {
             let hash = Keccak256::digest(&node);
             state_nodes.insert(H256::from_slice(hash.as_slice()), node);
         }
