e107c4f567877a4e66b3d2099a45be3407615ffe
fmoletta <fedemoletta@hotmail.com>
2025-08-28 17:00:05 -0300

    Revert "nostorage=true"
    This reverts commit 76e3f98ea17e4b08611969dccb63097a3904fe93.

--- tooling/archive_sync/src/main.rs
+++ tooling/archive_sync/src/main.rs
@@ -548,7 +548,7 @@ impl DumpIpcReader {
         "id": 1,
         "jsonrpc": "2.0",
         "method": "debug_accountRange",
-        "params": [format!("{:#x}", self.block_number), format!("{:#x}", self.start), MAX_ACCOUNTS, false, true, true]
+        "params": [format!("{:#x}", self.block_number), format!("{:#x}", self.start), MAX_ACCOUNTS, false, false, true]
         });
         let response = send_ipc_json_request(&mut self.stream, request).await?;
         info!("IPC res: {response}");
