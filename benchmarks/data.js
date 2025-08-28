b63eef400e83a498dae1d86fffa82b0921e50a7e
github-action-benchmark <github@users.noreply.github.com>
2025-08-28 11:12:06 +0000

    add Benchmark (cargo) benchmark result for f6dcb766bd3d274a36664a2074228257061e6387

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1756331134565,
+  "lastUpdate": 1756379526232,
   "repoUrl": "https://github.com/lambdaclass/ethrex",
   "entries": {
     "Benchmark": [
@@ -10465,6 +10465,36 @@ window.BENCHMARK_DATA = {
             "name": "Block import/Block import ERC20 transfers",
             "value": 161950926444,
             "range": "± 291517440",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "git@edgl.dev",
+            "name": "Edgar",
+            "username": "edg-l"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f6dcb766bd3d274a36664a2074228257061e6387",
+          "message": "perf(levm): improvements to precompiles (#4168)\n\nMinor improvements:\n\n- In fill_with_zeros, when the size is already target or more avoid\nallocating a vec.\n- Also avoided allocating vecs in other contexts.\n- Improved further the modexp code, making it more predictable to\ncompiler optimizations.\n- Changed some infallible functions from returning a Result to a value\ndirectly.\n\n\nNeed to measure if it improves.\n\n<img width=\"1347\" height=\"260\" alt=\"image\"\nsrc=\"https://github.com/user-attachments/assets/a934cf61-aed9-4f7a-bfc6-861ee8c16ebc\"\n/>",
+          "timestamp": "2025-08-28T10:21:53Z",
+          "tree_id": "661b8aec00535bb7c6028f520fe84aea0814819a",
+          "url": "https://github.com/lambdaclass/ethrex/commit/f6dcb766bd3d274a36664a2074228257061e6387"
+        },
+        "date": 1756379510757,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "Block import/Block import ERC20 transfers",
+            "value": 161995963371,
+            "range": "± 339855110",
             "unit": "ns/iter"
           }
         ]
