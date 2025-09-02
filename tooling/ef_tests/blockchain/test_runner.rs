c657a92c8f175dcced181de194716ebcdc011b75
Camila Di Ielsi <camila.diielsi@lambdaclass.com>
2025-09-02 14:46:17 -0300

    cargo fmt for the merge done with the web editor

--- tooling/ef_tests/blockchain/test_runner.rs
+++ tooling/ef_tests/blockchain/test_runner.rs
@@ -36,7 +36,6 @@ pub fn parse_and_execute(
     //EIPs should be added as strings in the format 'eip-XXXX'
     let fusaka_eips_to_test: Vec<&str> = vec!["eip-7823", "eip-7939"];
 
-
     //Hashes of any other tests to run, that don't correspond to an especific EIP (for examples, some integration tests)
     //We should really remove this once we're finished with implementing Fusaka, but it's a good-enough workaround to run specific tests for now
     let hashes_of_fusaka_tests_to_run: Vec<&str> = vec![
