dd190fadf0faf0c319992249ec914eccf3e5a7bf
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2025-09-04 12:46:45 -0300

    Use unwrap or else

--- cmd/ethrex/initializers.rs
+++ cmd/ethrex/initializers.rs
@@ -295,10 +295,8 @@ pub fn get_local_p2p_node(opts: &Options, signer: &SecretKey) -> Node {
     let tcp_socket_addr =
         parse_socket_addr("::", &opts.p2p_port).expect("Failed to parse addr and port");
 
-    let p2p_node_ip = match local_ip() {
-        Ok(ip) => ip,
-        Err(_) => local_ipv6().expect("Failed to get local ip v6"),
-    };
+    let p2p_node_ip =
+        local_ip().unwrap_or_else(|_| local_ipv6().expect("Failed to get local ip v6"));
 
     let local_public_key = public_key_from_signing_key(signer);
 
