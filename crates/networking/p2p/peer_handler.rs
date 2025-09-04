e1b223303488c8b891fd6a18ee91e07274e6f645
Javier Chatruc <jrchatruc@gmail.com>
2025-09-04 18:47:47 -0300

    clippy

--- crates/networking/p2p/peer_handler.rs
+++ crates/networking/p2p/peer_handler.rs
@@ -51,7 +51,7 @@ pub const MAX_HEADER_CHUNK: u64 = 500_000;
 // How much we store in memory of request_account_range and request_storage_ranges
 // before we dump it into the file. This tunes how much memory ethrex uses during
 // the first steps of snap sync
-pub const RANGE_FILE_CHUNK_SIZE: u64 = 1024 * 1024 * 512 * 1; // 512MB
+pub const RANGE_FILE_CHUNK_SIZE: u64 = 1024 * 1024 * 512; // 512MB
 pub const SNAP_LIMIT: usize = 128;
 
 // Request as many as 128 block bodies per request
