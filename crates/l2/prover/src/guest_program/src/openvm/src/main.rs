2ca220bbb375553c70635809c6bcf4e73bd6e3a8
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2025-09-08 18:53:50 -0300

    move openvm to guest_program

--- crates/l2/prover/src/guest_program/src/openvm/src/main.rs
+++ crates/l2/prover/src/guest_program/src/openvm/src/main.rs
@@ -1,10 +1,8 @@
-#![no_main]
-
 use openvm_keccak256::keccak256;
 use rkyv::rancor::Error;
 use zkvm_interface::{execution::execution_program, io::ProgramInput};
 
-openvm::entry!(main);
+openvm::init!();
 
 pub fn main() {
     openvm::io::println("start reading input");
