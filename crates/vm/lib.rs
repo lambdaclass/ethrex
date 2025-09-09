7312938038d495073736b858d86437b657bd35ab
Tomás Paradelo <tomas.paradelo@lambdaclass.com>
2025-09-09 09:58:34 -0300

    remove comment

--- crates/vm/lib.rs
+++ crates/vm/lib.rs
@@ -12,6 +12,5 @@ pub use backends::{BlockExecutionResult, Evm};
 pub use db::{DynVmDatabase, VmDatabase};
 pub use errors::{EvmError, ProverDBError};
 pub use execution_result::ExecutionResult;
-// pub use helpers::{SpecId, create_contract_address, fork_to_spec_id};
 pub use prover_db::ProverDB;
 pub use witness_db::ExecutionWitnessWrapper;
