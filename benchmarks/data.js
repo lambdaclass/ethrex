1893d5db360b0adcba4cb33d40b476919cc334f8
github-action-benchmark <github@users.noreply.github.com>
2025-09-09 22:59:38 +0000

    add Benchmark (cargo) benchmark result for 3565e63abdfeb7fdbb7bfd6b40943b0c891cc6eb

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1757453764604,
+  "lastUpdate": 1757458778058,
   "repoUrl": "https://github.com/lambdaclass/ethrex",
   "entries": {
     "Benchmark": [
@@ -13375,6 +13375,36 @@ window.BENCHMARK_DATA = {
             "name": "Block import/Block import ERC20 transfers",
             "value": 169344039416,
             "range": "± 529984252",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mrugiero@gmail.com",
+            "name": "Mario Rugiero",
+            "username": "Oppen"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "3565e63abdfeb7fdbb7bfd6b40943b0c891cc6eb",
+          "message": "fix(l1): make account and storage ranges handlers async (#4401)\n\n**Motivation**\n\nThese handlers are currently sync and can block the runtime for quite a\nlong time. This is not the ideal solution for this; we still need to do\nthings better so the handler itself doesn't take a long time to fetch\nthe requested nodes. We'll tackle this on a subsequent PR.\n\n**Description**\n\n<!-- A clear and concise general description of the changes this PR\nintroduces -->\n\n<!-- Link to issues: Resolves #111, Resolves #222 -->\n\nCloses #issue_number\n\n---------\n\nCo-authored-by: Javier Chatruc <jrchatruc@gmail.com>",
+          "timestamp": "2025-09-09T22:05:09Z",
+          "tree_id": "aa0487027bcf30b9695271fa583b480e4745ee83",
+          "url": "https://github.com/lambdaclass/ethrex/commit/3565e63abdfeb7fdbb7bfd6b40943b0c891cc6eb"
+        },
+        "date": 1757458758927,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "Block import/Block import ERC20 transfers",
+            "value": 167525092640,
+            "range": "± 698174914",
             "unit": "ns/iter"
           }
         ]
