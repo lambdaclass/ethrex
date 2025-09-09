db0c50672572359bb48cda8899c61c0acaa8fcbb
Esteban Dimitroff Hodi <esteban.dimitroff@lambdaclass.com>
2025-09-09 09:10:52 -0300

    Factoring large function into smaller ones

--- crates/networking/p2p/discv4/server.rs
+++ crates/networking/p2p/discv4/server.rs
@@ -155,9 +155,8 @@ impl DiscoveryServer {
                     return;
                 }
 
-                let sender_ip = unmap_ipv4in6_address(from.ip());
                 let node = Node::new(
-                    sender_ip,
+                    unmap_ipv4in6_address(from.ip()),
                     from.port(),
                     ping_message.from.tcp_port,
                     sender_public_key,
@@ -181,40 +180,8 @@ impl DiscoveryServer {
                     trace!("FindNode expired, skipped");
                     return;
                 }
-                let node_id = node_id(&sender_public_key);
 
-                let table = self.kademlia.table.lock().await;
-
-                let Some(contact) = table.get(&node_id) else {
-                    debug!(received = "FindNode", to = %format!("{sender_public_key:#x}"), "Unknown contact, skipping");
-                    return;
-                };
-                if !contact.was_validated() {
-                    debug!(received = "FindNode", to = %format!("{sender_public_key:#x}"), "Contact not validated, skipping");
-                    return;
-                }
-                let node = contact.node.clone();
-
-                // Check that the IP address from which we receive the request matches the one we have stored to prevent amplification attacks
-                // This prevents an attack vector where the discovery protocol could be used to amplify traffic in a DDOS attack.
-                // A malicious actor would send a findnode request with the IP address and UDP port of the target as the source address.
-                // The recipient of the findnode packet would then send a neighbors packet (which is a much bigger packet than findnode) to the victim.
-                if from.ip() != node.ip {
-                    debug!(received = "FindNode", to = %format!("{sender_public_key:#x}"), "IP address mismatch, skipping");
-                    return;
-                }
-
-                let neighbors = get_closest_nodes(node_id, table.clone());
-
-                drop(table);
-
-                // we are sending the neighbors in 2 different messages to avoid exceeding the
-                // maximum packet size
-                for chunk in neighbors.chunks(8) {
-                    let _ = self.send_neighbors(chunk.to_vec(), &node).await.inspect_err(|e| {
-                        error!(sent = "Neighbors", to = %format!("{sender_public_key:#x}"), err = ?e);
-                    });
-                }
+                self.handle_find_node(sender_public_key, from).await;
             }
             Message::Neighbors(neighbors_message) => {
                 trace!(received = "Neighbors", msg = ?neighbors_message, from = %format!("{sender_public_key:#x}"));
@@ -224,22 +191,7 @@ impl DiscoveryServer {
                     return;
                 }
 
-                // TODO(#3746): check that we requested neighbors from the node
-
-                let mut contacts = self.kademlia.table.lock().await;
-                let discarded_contacts = self.kademlia.discarded_contacts.lock().await;
-
-                for node in neighbors_message.nodes {
-                    let node_id = node.node_id();
-                    if let Entry::Vacant(vacant_entry) = contacts.entry(node_id) {
-                        if !discarded_contacts.contains(&node_id)
-                            && node_id != self.local_node.node_id()
-                        {
-                            vacant_entry.insert(Contact::from(node));
-                            METRICS.record_new_discovery().await;
-                        }
-                    };
-                }
+                self.handle_neighbors(neighbors_message).await;
             }
             Message::ENRRequest(enrrequest_message) => {
                 trace!(received = "ENRRequest", msg = ?enrrequest_message, from = %format!("{sender_public_key:#x}"));
@@ -248,25 +200,8 @@ impl DiscoveryServer {
                     trace!("ENRRequest expired, skipping");
                     return;
                 }
-                let node_id = node_id(&sender_public_key);
 
-                let mut table = self.kademlia.table.lock().await;
-
-                let Some(contact) = table.get(&node_id) else {
-                    debug!(received = "FindNode", to = %format!("{sender_public_key:#x}"), "Unknown contact, skipping");
-                    return;
-                };
-                if !contact.was_validated() {
-                    debug!(received = "ENRRequest", to = %format!("{sender_public_key:#x}"), "Contact not validated, skipping");
-                    return;
-                }
-
-                if let Err(err) = self.send_enr_response(hash, from).await {
-                    error!(sent = "ENRResponse", to = %format!("{from}"), err = ?err);
-                    return;
-                }
-
-                table.entry(node_id).and_modify(|c| c.knows_us = true);
+                self.handle_enr_request(sender_public_key, from, hash).await;
             }
             Message::ENRResponse(enrresponse_message) => {
                 /*
@@ -487,6 +422,85 @@ impl DiscoveryServer {
             return;
         }
         contact.ping_hash = None;
+    }
+
+    async fn handle_find_node(&self, sender_public_key: H512, from: SocketAddr) {
+        let table = self.kademlia.table.lock().await;
+
+        let node_id = node_id(&sender_public_key);
+
+        let Some(contact) = table.get(&node_id) else {
+            debug!(received = "FindNode", to = %format!("{sender_public_key:#x}"), "Unknown contact, skipping");
+            return;
+        };
+        if !contact.was_validated() {
+            debug!(received = "FindNode", to = %format!("{sender_public_key:#x}"), "Contact not validated, skipping");
+            return;
+        }
+        let node = contact.node.clone();
+
+        // Check that the IP address from which we receive the request matches the one we have stored to prevent amplification attacks
+        // This prevents an attack vector where the discovery protocol could be used to amplify traffic in a DDOS attack.
+        // A malicious actor would send a findnode request with the IP address and UDP port of the target as the source address.
+        // The recipient of the findnode packet would then send a neighbors packet (which is a much bigger packet than findnode) to the victim.
+        if from.ip() != node.ip {
+            debug!(received = "FindNode", to = %format!("{sender_public_key:#x}"), "IP address mismatch, skipping");
+            return;
+        }
+
+        let neighbors = get_closest_nodes(node_id, table.clone());
+
+        drop(table);
+
+        // we are sending the neighbors in 2 different messages to avoid exceeding the
+        // maximum packet size
+        for chunk in neighbors.chunks(8) {
+            let _ = self
+                .send_neighbors(chunk.to_vec(), &node)
+                .await
+                .inspect_err(|e| {
+                    error!(sent = "Neighbors", to = %format!("{sender_public_key:#x}"), err = ?e);
+                });
+        }
+    }
+
+    async fn handle_neighbors(&self, neighbors_message: NeighborsMessage) {
+        // TODO(#3746): check that we requested neighbors from the node
+
+        let mut contacts = self.kademlia.table.lock().await;
+        let discarded_contacts = self.kademlia.discarded_contacts.lock().await;
+
+        for node in neighbors_message.nodes {
+            let node_id = node.node_id();
+            if let Entry::Vacant(vacant_entry) = contacts.entry(node_id) {
+                if !discarded_contacts.contains(&node_id) && node_id != self.local_node.node_id() {
+                    vacant_entry.insert(Contact::from(node));
+                    METRICS.record_new_discovery().await;
+                }
+            };
+        }
+    }
+
+    async fn handle_enr_request(&self, sender_public_key: H512, from: SocketAddr, hash: H256) {
+        let node_id = node_id(&sender_public_key);
+
+        let mut table = self.kademlia.table.lock().await;
+
+        let Some(contact) = table.get(&node_id) else {
+            debug!(received = "FindNode", to = %format!("{sender_public_key:#x}"), "Unknown contact, skipping");
+            return;
+        };
+        if !contact.was_validated() {
+            debug!(received = "ENRRequest", to = %format!("{sender_public_key:#x}"), "Contact not validated, skipping");
+            return;
+        }
+
+        if let Err(err) = self.send_enr_response(hash, from).await {
+            error!(sent = "ENRResponse", to = %format!("{from}"), err = ?err);
+            return;
+        }
+
+        table.entry(node_id).and_modify(|c| c.knows_us = true);
     }
 
     async fn send(&self, message: Message, addr: SocketAddr) -> Result<(), DiscoveryServerError> {
