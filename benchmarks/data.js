9fe701071023f898fac5377dfaf52f02d5ea5b80
github-action-benchmark <github@users.noreply.github.com>
2025-09-11 14:24:51 +0000

    add Benchmark (cargo) benchmark result for 0bb360a915d908d8efd5e5f7a978dd09197f5602

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1757600305101,
+  "lastUpdate": 1757600691085,
   "repoUrl": "https://github.com/lambdaclass/ethrex",
   "entries": {
     "Benchmark": [
@@ -13765,6 +13765,36 @@ window.BENCHMARK_DATA = {
             "name": "Block import/Block import ERC20 transfers",
             "value": 172068961635,
             "range": "± 1001249763",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "39842759+gianbelinche@users.noreply.github.com",
+            "name": "Gianbelinche",
+            "username": "gianbelinche"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "0bb360a915d908d8efd5e5f7a978dd09197f5602",
+          "message": "fix(l1): avoid sending a transaction to a peer that already knows it (#4363)\n\n**Motivation**\nThe TxBroadcaster does not follow the spec, since it can send a tx to a\npeer that already knows of it\n<!-- Why does this pull request exist? What are its goals? -->\n\n**Description**\nAdds a HashMap stating which transactions each peer knows, and avoiding\nsending a tx if a peer already knows it.\nAfter some time, this HashMap is pruned.\n<!-- A clear and concise general description of the changes this PR\nintroduces -->\n\n<!-- Link to issues: Resolves #111, Resolves #222 -->\n\nCloses https://github.com/lambdaclass/ethrex/issues/4356",
+          "timestamp": "2025-09-11T13:30:11Z",
+          "tree_id": "33e474ad67513989bb9fb478eb69430e7aeef247",
+          "url": "https://github.com/lambdaclass/ethrex/commit/0bb360a915d908d8efd5e5f7a978dd09197f5602"
+        },
+        "date": 1757600669687,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "Block import/Block import ERC20 transfers",
+            "value": 172373400381,
+            "range": "± 784750775",
             "unit": "ns/iter"
           }
         ]
