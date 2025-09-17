35bd1e5bbf733215d73287d92558e7853b73b544
Camila Di Ielsi <camila.diielsi@lambdaclass.com>
2025-09-17 11:51:49 -0300

    add workflow job to run the test on loop

---
+++ .github/scripts/run_hive_flaky_test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+HIVE_CMD="./hive"
+CLIENT_FILE="../fixtures//hive/clients.yaml"
+CLIENT="ethrex"
+SIM="ethereum/engine"
+SIM_LIMIT="engine-cancun/Invalid Missing Ancestor Syncing ReOrg, Transaction Nonce, EmptyTxs=False, CanonicalReOrg=True, Invalid P9 (Cancun)"
+
+for ((i=1; i<=1000; i++)); do
+    echo "Running hive command..."
+    $HIVE_CMD \
+        --client-file "$CLIENT_FILE" \
+        --client "$CLIENT" \
+        --sim "$SIM" \
+        --sim.limit "$SIM_LIMIT" \
+        --sim.parallelism 16 \
+        --sim.loglevel 1
+
+    echo "[$i/1000] Command completed at $(date)"
+done
