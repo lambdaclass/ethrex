f6cefef21a96869ae895d8bea7a8e18cac2547ff
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-09-17 08:06:42 -0300

    feat(storage): implement in memory methods

--- crates/storage/backend/in_memory.rs
+++ crates/storage/backend/in_memory.rs
@@ -11,7 +11,6 @@ use std::sync::{Arc, Mutex};
 /// for testing and development.
 #[derive(Debug, Clone, Default)]
 pub struct InMemoryBackend {
-    // Each namespace is a separate BTreeMap for ordered key iteration
     namespaces: HashMap<String, Arc<Mutex<BTreeMap<Vec<u8>, Vec<u8>>>>>,
 }
 
@@ -22,12 +21,31 @@ impl InMemoryBackend {
     pub fn new() -> Self {
         Self::default()
     }
+
+    fn get_table(
+        &self,
+        namespace: &str,
+    ) -> Result<Arc<Mutex<BTreeMap<Vec<u8>, Vec<u8>>>>, StoreError> {
+        let table = self
+            .namespaces
+            .get(namespace)
+            .ok_or(StoreError::Custom(format!(
+                "Namespace not found: {}",
+                namespace
+            )))?;
+        Ok(table.clone())
+    }
 }
 
 #[async_trait::async_trait]
 impl StorageBackend for InMemoryBackend {
     fn get_sync(&self, namespace: &str, key: Vec<u8>) -> Result<Option<Vec<u8>>, StoreError> {
-        todo!()
+        let table = self.get_table(namespace)?;
+        Ok(table
+            .lock()
+            .map_err(|_| StoreError::LockError)?
+            .get(&key)
+            .cloned())
     }
 
     async fn get_async(
@@ -35,7 +53,7 @@ impl StorageBackend for InMemoryBackend {
         namespace: &str,
         key: Vec<u8>,
     ) -> Result<Option<Vec<u8>>, StoreError> {
-        todo!()
+        self.get_sync(namespace, key)
     }
 
     async fn get_async_batch(
@@ -43,23 +61,56 @@ impl StorageBackend for InMemoryBackend {
         namespace: &str,
         keys: Vec<Vec<u8>>,
     ) -> Result<Vec<Vec<u8>>, StoreError> {
-        todo!()
+        let table = self.get_table(namespace)?;
+        let mut values = Vec::new();
+        for key in keys {
+            let Some(value) = table
+                .lock()
+                .map_err(|_| StoreError::LockError)?
+                .get(&key)
+                .cloned()
+            else {
+                return Err(StoreError::Custom("Key not found".to_string()));
+            };
+            values.push(value);
+        }
+        Ok(values)
     }
 
     fn put_sync(&self, namespace: &str, key: Vec<u8>, value: Vec<u8>) -> Result<(), StoreError> {
-        todo!()
+        let table = self.get_table(namespace)?;
+        table
+            .lock()
+            .map_err(|_| StoreError::LockError)?
+            .insert(key, value);
+        Ok(())
     }
 
     async fn put(&self, namespace: &str, key: Vec<u8>, value: Vec<u8>) -> Result<(), StoreError> {
-        todo!()
+        self.put_sync(namespace, key, value)
     }
 
     async fn delete(&self, namespace: &str, key: Vec<u8>) -> Result<(), StoreError> {
-        todo!()
+        let table = self.get_table(namespace)?;
+        table
+            .lock()
+            .map_err(|_| StoreError::LockError)?
+            .remove(&key);
+        Ok(())
     }
 
     async fn batch_write(&self, ops: Vec<BatchOp>) -> Result<(), StoreError> {
-        todo!()
+        for op in ops {
+            match op {
+                BatchOp::Put {
+                    namespace,
+                    key,
+                    value,
+                } => self.put(&namespace, key, value).await?,
+                BatchOp::Delete { namespace, key } => self.delete(&namespace, key).await?,
+            }
+        }
+        Ok(())
     }
 
     async fn range(
@@ -68,6 +119,15 @@ impl StorageBackend for InMemoryBackend {
         start_key: Vec<u8>,
         end_key: Option<Vec<u8>>,
     ) -> Result<Vec<(Vec<u8>, Vec<u8>)>, StoreError> {
-        todo!()
+        let mut values = Vec::new();
+        for (key, value) in self
+            .get_table(namespace)?
+            .lock()
+            .map_err(|_| StoreError::LockError)?
+            .range(start_key..end_key.unwrap_or_default())
+        {
+            values.push((key.clone(), value.clone()));
+        }
+        Ok(values)
     }
 }
