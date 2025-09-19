e41f0e45f3e7be0070e8a4dc22f9271c9b0bf4dc
Francisco Xavier Gauna <francisco.gauna@lambdaclass.com>
2025-09-19 17:38:21 -0300

    Add hash metrics

--- crates/networking/p2p/sync.rs
+++ crates/networking/p2p/sync.rs
@@ -1322,6 +1322,7 @@ pub async fn update_pivot(
         } else {
             return Err(SyncError::NotInSnapSync);
         }
+        *METRICS.sync_head_hash.lock().await = pivot.hash();
         return Ok(pivot.clone());
     }
 }
