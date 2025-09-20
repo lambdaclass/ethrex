06859a6026137efb45cbacd3ae9bfa1439271e52
github-action-benchmark <github@users.noreply.github.com>
2025-09-20 06:18:14 -0300

    add L1 block proving benchmark (customBiggerIsBetter) benchmark result for 38e0ffc4487548a7b89de02b8b2b13c791a84682

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1758355958739,
+  "lastUpdate": 1758359894694,
   "repoUrl": "https://github.com/lambdaclass/ethrex",
   "entries": {
     "Benchmark": [
@@ -32130,6 +32130,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "SP1, RTX A6000",
             "value": 0.004418356034482759,
+            "unit": "Mgas/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "47506558+MegaRedHand@users.noreply.github.com",
+            "name": "Tomás Grüner",
+            "username": "MegaRedHand"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "38e0ffc4487548a7b89de02b8b2b13c791a84682",
+          "message": "refactor(l1): use `Path` instead of `String` for `--datadir` (#4543)\n\n**Motivation**\n\nWe are currently using `String` when dealing with file paths. This\npushes error-handling later into the node's lifecycle, which could delay\nsome errors.\n\n**Description**\n\nThis PR changes the CLI flag `--datadir` to receive a `PathBuf`, and\nalso changes all uses of the flag to receive a `&Path` or similar type.",
+          "timestamp": "2025-09-19T19:01:16Z",
+          "tree_id": "6272d7a8c23a1c2e94bfc90322a5e634d70b1577",
+          "url": "https://github.com/lambdaclass/ethrex/commit/38e0ffc4487548a7b89de02b8b2b13c791a84682"
+        },
+        "date": 1758359893710,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Risc0, RTX A6000",
+            "value": 0.001533141788812444,
             "unit": "Mgas/s"
           }
         ]
