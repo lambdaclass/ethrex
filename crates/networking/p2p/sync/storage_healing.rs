ac88a4e87c33c408c22bd06180763f02eba3d85a
Francisco Xavier Gauna <francisco.gauna@lambdaclass.com>
2025-09-23 12:09:01 -0300

    Improved error message

--- crates/networking/p2p/sync/storage_healing.rs
+++ crates/networking/p2p/sync/storage_healing.rs
@@ -523,7 +523,7 @@ fn get_initial_downloads(
                 // a self destruct contract creation step
                 let rlp = trie
                     .get(&acc_path.to_fixed_bytes().to_vec())
-                    .expect("The trie shouldn't have holes")?;
+                    .expect("We should be able to open the store")?;
                 let account = AccountState::decode(&rlp).expect("We should have a valid account");
                 if account.storage_root == *EMPTY_TRIE_HASH {
                     return None;
