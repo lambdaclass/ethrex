7f8bc0425a96aa6f1b28061a639413f4184ae50d
fmoletta <fedemoletta@hotmail.com>
2025-09-23 18:10:12 -0300

    Fix revm error comparison for ef blockchain tests

--- tooling/ef_tests/blockchain/test_runner.rs
+++ tooling/ef_tests/blockchain/test_runner.rs
@@ -240,9 +240,6 @@ fn match_alternative_revm_exception_msg(expected_msg: &String, msg: &str) -> boo
         (
             "reject transactions from senders with deployed code",
             SENDER_NOT_EOA_REGEX
-        ) | (
-            "call gas cost exceeds the gas limit",
-            "Intrinsic gas too low"
         ) | ("gas floor exceeds the gas limit", "Intrinsic gas too low")
             | ("empty blobs", "Type 3 transaction without blobs")
             | (
@@ -264,6 +261,9 @@ fn match_alternative_revm_exception_msg(expected_msg: &String, msg: &str) -> boo
             )
             | ("create initcode size limit", "Initcode size exceeded")
     ) || (msg.starts_with("lack of funds") && expected_msg == "Insufficient account funds")
+        || (msg.starts_with("call gas cost")
+            && msg.contains("exceeds the gas limit")
+            && expected_msg == "Intrinsic gas too low")
 }
 
 fn match_expected_regex(expected_error_regex: &str, error_msg: &str) -> bool {
