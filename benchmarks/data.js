e770345e91c8bc7ca96470074d82c6e4409eac3b
github-action-benchmark <github@users.noreply.github.com>
2025-09-25 14:31:26 -0300

    add L1 block proving benchmark (customBiggerIsBetter) benchmark result for 68beb0c114f2d43299e2264ec8c77f7ca6ac8710

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1758818600192,
+  "lastUpdate": 1758821486555,
   "repoUrl": "https://github.com/lambdaclass/ethrex",
   "entries": {
     "Benchmark": [
@@ -34321,6 +34321,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Risc0, RTX A6000",
             "value": 0.0015872195385724585,
+            "unit": "Mgas/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "112426153+tomip01@users.noreply.github.com",
+            "name": "Tomás Paradelo",
+            "username": "tomip01"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "68beb0c114f2d43299e2264ec8c77f7ca6ac8710",
+          "message": "ci(l1,l2): free unused packages on github runners (#4647)\n\n**Motivation**\n\nThe CI is broken due to not having space left to run the jobs\n\n**Description**\n\n<!-- A clear and concise general description of the changes this PR\nintroduces -->\n\n<!-- Link to issues: Resolves #111, Resolves #222 -->\n\nCloses #issue_number",
+          "timestamp": "2025-09-25T14:40:33Z",
+          "tree_id": "119c291dd045aef130339565b28a1cab6dce7bb4",
+          "url": "https://github.com/lambdaclass/ethrex/commit/68beb0c114f2d43299e2264ec8c77f7ca6ac8710"
+        },
+        "date": 1758821462781,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Risc0, RTX A6000",
+            "value": 0.0015775517735578646,
             "unit": "Mgas/s"
           }
         ]
