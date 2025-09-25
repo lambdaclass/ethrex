e05709219fda0b29ea2aa611fe35062b3f5c6a3e
github-action-benchmark <github@users.noreply.github.com>
2025-09-25 09:50:10 -0300

    add L1 block proving benchmark (customBiggerIsBetter) benchmark result for f68c0015ff7357eda09ba582c516f5d5e14e9b89

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1758803013780,
+  "lastUpdate": 1758804610374,
   "repoUrl": "https://github.com/lambdaclass/ethrex",
   "entries": {
     "Benchmark": [
@@ -34057,6 +34057,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "SP1, RTX A6000",
             "value": 0.004515843076923077,
+            "unit": "Mgas/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "47506558+MegaRedHand@users.noreply.github.com",
+            "name": "Tomás Grüner",
+            "username": "MegaRedHand"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "f68c0015ff7357eda09ba582c516f5d5e14e9b89",
+          "message": "fix(l1): remove \"background_threads\" feature in jemallocator (#4636)\n\n**Motivation**\n\nAny `ethrex` binary run in macOS starts by printing the message:\n\n```text\n<jemalloc>: option background_thread currently supports pthread only\n```\n\nThis is confusing, especially since the node appears to work as normal.\n\n**Description**\n\nThis PR removes the \"background_threads\" feature, which isn't supported\non macOS, it seems. It still enables it for linux, however.",
+          "timestamp": "2025-09-24T20:58:35Z",
+          "tree_id": "770950e05c865b671516f5d6eb2fc123791739af",
+          "url": "https://github.com/lambdaclass/ethrex/commit/f68c0015ff7357eda09ba582c516f5d5e14e9b89"
+        },
+        "date": 1758804609240,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "SP1, RTX A6000",
+            "value": 0.004497392236976507,
             "unit": "Mgas/s"
           }
         ]
