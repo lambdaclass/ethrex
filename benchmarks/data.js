16f16a9ac922d5c6b3e6b67622db0a2cf1b00934
github-action-benchmark <github@users.noreply.github.com>
2025-09-26 21:23:54 +0000

    add Benchmark (cargo) benchmark result for 1ef53ccda5fd3f96118cadbd296fbff6e5c10b22

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1758920819984,
+  "lastUpdate": 1758921834769,
   "repoUrl": "https://github.com/lambdaclass/ethrex",
   "entries": {
     "Benchmark": [
@@ -17065,6 +17065,36 @@ window.BENCHMARK_DATA = {
             "name": "Block import/Block import ERC20 transfers",
             "value": 89639412061,
             "range": "± 738888286",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "72628438+avilagaston9@users.noreply.github.com",
+            "name": "Avila Gastón",
+            "username": "avilagaston9"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1ef53ccda5fd3f96118cadbd296fbff6e5c10b22",
+          "message": "ci(l2): free space in lint workflow (#4671)\n\n**Motivation**\n\nOur lint workflow is failing with a `No space left on device` error.\n\n**Description**\n\nAdds the `Free Disk Space` step that we already use in other workflows.\n\nCloses None",
+          "timestamp": "2025-09-26T20:34:59Z",
+          "tree_id": "ba81c83559f1ec6e7cfb26511f31cf4b80e99c47",
+          "url": "https://github.com/lambdaclass/ethrex/commit/1ef53ccda5fd3f96118cadbd296fbff6e5c10b22"
+        },
+        "date": 1758921813889,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "Block import/Block import ERC20 transfers",
+            "value": 88520083268,
+            "range": "± 196375709",
             "unit": "ns/iter"
           }
         ]
