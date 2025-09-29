41633117d0fd27c7fd06b9b69da18a233c6db50d
Manuel Iñaki Bilbao <manuel.bilbao@lambdaclass.com>
2025-09-29 18:57:23 -0300

    Remove tracing and use expect instead of unwrap

--- tooling/migrations/src/cli.rs
+++ tooling/migrations/src/cli.rs
@@ -1,7 +1,7 @@
 use std::path::PathBuf;
 
 use clap::{Parser as ClapParser, Subcommand as ClapSubcommand};
-use ethrex_blockchain::Blockchain;
+use ethrex_blockchain::{Blockchain, BlockchainOptions, BlockchainType};
 use ethrex_common::types::Block;
 
 use crate::utils::{migrate_block_body, migrate_block_header};
@@ -57,53 +57,76 @@ async fn migrate_libmdbx_to_rocksdb(
         old_storage_path,
         ethrex_storage_libmdbx::EngineType::Libmdbx,
     )
-    .unwrap();
-    old_store.load_initial_state().await.unwrap();
+    .expect("Cannot open libmdbx store");
+    old_store
+        .load_initial_state()
+        .await
+        .expect("Cannot load libmdbx store state");
+
     let new_store = ethrex_storage::Store::new_from_genesis(
         new_storage_path.as_path(),
         ethrex_storage::EngineType::RocksDB,
-        genesis_path.to_str().unwrap(),
+        genesis_path
+            .to_str()
+            .expect("Cannot convert genesis path to str"),
     )
     .await
-    .unwrap();
+    .expect("Cannot create rocksdb store");
 
-    let last_block_number = old_store.get_latest_block_number().await.unwrap();
-    let last_known_block = new_store.get_latest_block_number().await.unwrap();
+    let last_block_number = old_store
+        .get_latest_block_number()
+        .await
+        .expect("Cannot get latest block from libmdbx store");
+    let last_known_block = new_store
+        .get_latest_block_number()
+        .await
+        .expect("Cannot get latest known block from rocksdb store");
 
-    let blockchain = Blockchain::new(
-        new_store.clone(),
-        ethrex_blockchain::BlockchainType::L2,
-        false,
-    );
+    if last_known_block >= last_block_number {
+        println!("Rocksdb store is already up to date");
+        return;
+    }
 
-    println!("Last known number: {last_known_block}");
-    println!("Last block number: {last_block_number}");
+    println!("Migrating from block {last_known_block} to {last_block_number}");
+
+    let mut blockchain_opts = BlockchainOptions::default();
+    blockchain_opts.r#type = BlockchainType::L2;
+    let blockchain = Blockchain::new(new_store.clone(), blockchain_opts);
 
     let block_bodies = old_store
         .get_block_bodies(last_known_block + 1, last_block_number)
         .await
-        .unwrap();
+        .expect("Cannot get bodies from libmdbx store");
 
     let block_headers = (last_known_block + 1..=last_block_number)
         .into_iter()
-        .map(|i| old_store.get_block_header(i).unwrap().unwrap());
+        .map(|i| {
+            old_store
+                .get_block_header(i)
+                .ok()
+                .flatten()
+                .expect("Cannot get block headers from libmdbx store")
+        });
 
     let blocks = block_headers.zip(block_bodies);
     for (header, body) in blocks {
         let header = migrate_block_header(header);
         let body = migrate_block_body(body);
+        let block_number = header.number;
+
         blockchain
             .add_block(&Block::new(header, body))
             .await
-            .unwrap();
+            .expect(&format!("Cannot add block {block_number} to rocksdb store"));
     }
 
     let last_block = old_store
         .get_block_header(last_block_number)
-        .unwrap()
-        .unwrap();
+        .ok()
+        .flatten()
+        .expect("Cannot get last block from libmdbx store");
     new_store
         .forkchoice_update(None, last_block.number, last_block.hash(), None, None)
         .await
-        .unwrap();
+        .expect("Cannot apply forkchoice update");
 }
--- tooling/migrations/src/main.rs
+++ tooling/migrations/src/main.rs
@@ -1,33 +1,12 @@
 mod cli;
 mod utils;
 
-use tracing::Level;
-use tracing_subscriber::prelude::__tracing_subscriber_SubscriberExt;
-use tracing_subscriber::{EnvFilter, Layer, Registry, filter::Directive, fmt, reload};
-
 use crate::cli::CLI;
 use clap::Parser;
-
-pub fn init_tracing() -> reload::Handle<EnvFilter, Registry> {
-    let log_filter = EnvFilter::builder()
-        .with_default_directive(Directive::from(Level::TRACE))
-        .from_env_lossy();
-
-    let (filter, filter_handle) = reload::Layer::new(log_filter);
-
-    let fmt_layer = fmt::layer().with_filter(filter);
-    let subscriber = Box::new(Registry::default().with(fmt_layer));
-
-    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
-
-    filter_handle
-}
 
 #[tokio::main]
 async fn main() {
     let CLI { command } = CLI::parse();
-
-    init_tracing();
 
     command.run().await;
 }
