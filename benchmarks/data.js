9edb4648fac4bb52ad3a6cbcf8c66c62e01f4ae6
github-action-benchmark <github@users.noreply.github.com>
2025-09-29 22:15:08 +0000

    add Benchmark (cargo) benchmark result for c25e90f8a1d9aac64b0124e74f6a20752a0737fe

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1759178479260,
+  "lastUpdate": 1759184108130,
   "repoUrl": "https://github.com/lambdaclass/ethrex",
   "entries": {
     "Benchmark": [
@@ -17425,6 +17425,36 @@ window.BENCHMARK_DATA = {
             "name": "Block import/Block import ERC20 transfers",
             "value": 93299441335,
             "range": "± 325814835",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "francisco.gauna@lambdaclass.com",
+            "name": "fedacking",
+            "username": "fedacking"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c25e90f8a1d9aac64b0124e74f6a20752a0737fe",
+          "message": "docs(l2): improve documentation in `contracts.md` (#3797)\n\n**Motivation**\nDocumentation in `contracts.md` contains todos\n\n**Description**\nFills in the `todos` with the latest L1 and L2 contract implementations\n\nrelated to #3165 and #3575\n\nAuthored-by: JasonVranek <jasonvranek@gmail.com>\n\n---------\n\nCo-authored-by: JasonVranek <jasonvranek@gmail.com>\nCo-authored-by: Ivan Litteri <67517699+ilitteri@users.noreply.github.com>\nCo-authored-by: Javier Chatruc <jrchatruc@gmail.com>",
+          "timestamp": "2025-09-29T21:21:31Z",
+          "tree_id": "6c5c6ae8c64fb140e3393662836ef0460041bbcc",
+          "url": "https://github.com/lambdaclass/ethrex/commit/c25e90f8a1d9aac64b0124e74f6a20752a0737fe"
+        },
+        "date": 1759184086691,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "Block import/Block import ERC20 transfers",
+            "value": 91004351818,
+            "range": "± 224108584",
             "unit": "ns/iter"
           }
         ]
