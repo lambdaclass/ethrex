18df2cc60d79eb2b67ca379c95c29986252ea8ee
Tomás Paradelo <tomas.paradelo@lambdaclass.com>
2025-09-30 13:20:27 -0300

    revert change again

--- fixtures/contracts/ERC20/ERC20.sol
+++ fixtures/contracts/ERC20/ERC20.sol
@@ -3,7 +3,8 @@ import "./deps.sol";
 pragma solidity ^0.8.0;
 
 contract TestToken is ERC20 {
-    uint256 constant defaultMint = 1000000 * (10 ** 18);
+
+    uint256 constant defaultMint = 1000000 * (10**18);
 
     constructor() ERC20("TestToken", "TEST") {
         _mint(msg.sender, defaultMint);
