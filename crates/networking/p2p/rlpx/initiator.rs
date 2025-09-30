b0b792327ccd0bd396400b363d606450d1eb42d0
Pablo Deymonnaz <pdeymon@fi.uba.ar>
2025-09-30 16:05:11 -0300

    using std::convert::Infallible when it's not necessary

--- crates/networking/p2p/rlpx/initiator.rs
+++ crates/networking/p2p/rlpx/initiator.rs
@@ -105,7 +105,7 @@ impl GenServer for RLPxInitiator {
     type CallMsg = Unused;
     type CastMsg = InMessage;
     type OutMsg = OutMessage;
-    type Error = ();
+    type Error = std::convert::Infallible;
 
     async fn handle_cast(
         &mut self,
