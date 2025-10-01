b4be1d69bed860c8345decd4b0b3f7a808b112d8
Leandro Serra <leandro.serra@lambdaclass.com>
2025-10-01 17:07:38 -0300

    refactor: prefix libmdbx types with Libmdbx in utils.rs

--- tooling/migrations/src/utils.rs
+++ tooling/migrations/src/utils.rs
@@ -1,7 +1,15 @@
-pub fn migrate_block_header(
-    header: ethrex_common_libmdbx::types::BlockHeader,
-) -> ethrex_common::types::BlockHeader {
-    ethrex_common::types::BlockHeader {
+use ethrex_common::types::{
+    AuthorizationTuple, BlockBody, BlockHeader, EIP1559Transaction, EIP2930Transaction,
+    EIP4844Transaction, EIP7702Transaction, LegacyTransaction, PrivilegedL2Transaction,
+    Transaction, TxKind, Withdrawal,
+};
+use ethrex_common_libmdbx::types::{
+    BlockBody as LibmdbxBlockBody, BlockHeader as LibmdbxBlockHeader,
+    Transaction as LibmdbxTransaction, TxKind as LibmdbxTxKind,
+};
+
+pub fn migrate_block_header(header: LibmdbxBlockHeader) -> BlockHeader {
+    BlockHeader {
         hash: header.hash,
         parent_hash: header.parent_hash,
         ommers_hash: header.ommers_hash,
@@ -27,10 +35,8 @@ pub fn migrate_block_header(
     }
 }
 
-pub fn migrate_block_body(
-    body: ethrex_common_libmdbx::types::BlockBody,
-) -> ethrex_common::types::BlockBody {
-    ethrex_common::types::BlockBody {
+pub fn migrate_block_body(body: LibmdbxBlockBody) -> BlockBody {
+    BlockBody {
         transactions: body
             .transactions
             .into_iter()
@@ -40,7 +46,7 @@ pub fn migrate_block_body(
         withdrawals: body.withdrawals.map(|withdrawals| {
             withdrawals
                 .iter()
-                .map(|withdrawal| ethrex_common::types::Withdrawal {
+                .map(|withdrawal| Withdrawal {
                     index: withdrawal.index,
                     validator_index: withdrawal.validator_index,
                     address: withdrawal.address,
@@ -51,159 +57,129 @@ pub fn migrate_block_body(
     }
 }
 
-pub fn migrate_transaction(
-    tx: ethrex_common_libmdbx::types::Transaction,
-) -> ethrex_common::types::Transaction {
+pub fn migrate_transaction(tx: LibmdbxTransaction) -> Transaction {
     match tx {
-        ethrex_common_libmdbx::types::Transaction::EIP1559Transaction(tx) => {
-            ethrex_common::types::Transaction::EIP1559Transaction(
-                ethrex_common::types::EIP1559Transaction {
-                    chain_id: tx.chain_id,
-                    nonce: tx.nonce,
-                    max_priority_fee_per_gas: tx.max_priority_fee_per_gas,
-                    max_fee_per_gas: tx.max_fee_per_gas,
-                    gas_limit: tx.gas_limit,
-                    to: match tx.to {
-                        ethrex_common_libmdbx::types::TxKind::Create => {
-                            ethrex_common::types::TxKind::Create
-                        }
-                        ethrex_common_libmdbx::types::TxKind::Call(to) => {
-                            ethrex_common::types::TxKind::Call(to)
-                        }
-                    },
-                    value: tx.value,
-                    data: tx.data,
-                    access_list: tx.access_list,
-                    signature_y_parity: tx.signature_y_parity,
-                    signature_r: tx.signature_r,
-                    signature_s: tx.signature_s,
-                    inner_hash: tx.inner_hash,
+        LibmdbxTransaction::EIP1559Transaction(tx) => {
+            Transaction::EIP1559Transaction(EIP1559Transaction {
+                chain_id: tx.chain_id,
+                nonce: tx.nonce,
+                max_priority_fee_per_gas: tx.max_priority_fee_per_gas,
+                max_fee_per_gas: tx.max_fee_per_gas,
+                gas_limit: tx.gas_limit,
+                to: match tx.to {
+                    LibmdbxTxKind::Create => TxKind::Create,
+                    LibmdbxTxKind::Call(to) => TxKind::Call(to),
                 },
-            )
+                value: tx.value,
+                data: tx.data,
+                access_list: tx.access_list,
+                signature_y_parity: tx.signature_y_parity,
+                signature_r: tx.signature_r,
+                signature_s: tx.signature_s,
+                inner_hash: tx.inner_hash,
+            })
         }
-        ethrex_common_libmdbx::types::Transaction::LegacyTransaction(tx) => {
-            ethrex_common::types::Transaction::LegacyTransaction(
-                ethrex_common::types::LegacyTransaction {
-                    nonce: tx.nonce,
-                    gas_price: tx.gas_price,
-                    gas: tx.gas,
-                    to: match tx.to {
-                        ethrex_common_libmdbx::types::TxKind::Create => {
-                            ethrex_common::types::TxKind::Create
-                        }
-                        ethrex_common_libmdbx::types::TxKind::Call(to) => {
-                            ethrex_common::types::TxKind::Call(to)
-                        }
-                    },
-                    value: tx.value,
-                    data: tx.data,
-                    v: tx.v,
-                    r: tx.r,
-                    s: tx.s,
-                    inner_hash: tx.inner_hash,
+        LibmdbxTransaction::LegacyTransaction(tx) => {
+            Transaction::LegacyTransaction(LegacyTransaction {
+                nonce: tx.nonce,
+                gas_price: tx.gas_price,
+                gas: tx.gas,
+                to: match tx.to {
+                    LibmdbxTxKind::Create => TxKind::Create,
+                    LibmdbxTxKind::Call(to) => TxKind::Call(to),
                 },
-            )
+                value: tx.value,
+                data: tx.data,
+                v: tx.v,
+                r: tx.r,
+                s: tx.s,
+                inner_hash: tx.inner_hash,
+            })
         }
-        ethrex_common_libmdbx::types::Transaction::EIP2930Transaction(tx) => {
-            ethrex_common::types::Transaction::EIP2930Transaction(
-                ethrex_common::types::EIP2930Transaction {
-                    chain_id: tx.chain_id,
-                    nonce: tx.nonce,
-                    gas_price: tx.gas_price,
-                    gas_limit: tx.gas_limit,
-                    to: match tx.to {
-                        ethrex_common_libmdbx::types::TxKind::Create => {
-                            ethrex_common::types::TxKind::Create
-                        }
-                        ethrex_common_libmdbx::types::TxKind::Call(to) => {
-                            ethrex_common::types::TxKind::Call(to)
-                        }
-                    },
-                    value: tx.value,
-                    data: tx.data,
-                    access_list: tx.access_list,
-                    signature_y_parity: tx.signature_y_parity,
-                    signature_r: tx.signature_r,
-                    signature_s: tx.signature_s,
-                    inner_hash: tx.inner_hash,
+        LibmdbxTransaction::EIP2930Transaction(tx) => {
+            Transaction::EIP2930Transaction(EIP2930Transaction {
+                chain_id: tx.chain_id,
+                nonce: tx.nonce,
+                gas_price: tx.gas_price,
+                gas_limit: tx.gas_limit,
+                to: match tx.to {
+                    LibmdbxTxKind::Create => TxKind::Create,
+                    LibmdbxTxKind::Call(to) => TxKind::Call(to),
                 },
-            )
+                value: tx.value,
+                data: tx.data,
+                access_list: tx.access_list,
+                signature_y_parity: tx.signature_y_parity,
+                signature_r: tx.signature_r,
+                signature_s: tx.signature_s,
+                inner_hash: tx.inner_hash,
+            })
         }
-        ethrex_common_libmdbx::types::Transaction::EIP4844Transaction(tx) => {
-            ethrex_common::types::Transaction::EIP4844Transaction(
-                ethrex_common::types::EIP4844Transaction {
-                    chain_id: tx.chain_id,
-                    nonce: tx.nonce,
-                    max_priority_fee_per_gas: tx.max_priority_fee_per_gas,
-                    max_fee_per_gas: tx.max_fee_per_gas,
-                    gas: tx.gas,
-                    to: tx.to,
-                    value: tx.value,
-                    data: tx.data,
-                    access_list: tx.access_list,
-                    max_fee_per_blob_gas: tx.max_fee_per_blob_gas,
-                    blob_versioned_hashes: tx.blob_versioned_hashes,
-                    signature_y_parity: tx.signature_y_parity,
-                    signature_r: tx.signature_r,
-                    signature_s: tx.signature_s,
-                    inner_hash: tx.inner_hash,
-                },
-            )
+        LibmdbxTransaction::EIP4844Transaction(tx) => {
+            Transaction::EIP4844Transaction(EIP4844Transaction {
+                chain_id: tx.chain_id,
+                nonce: tx.nonce,
+                max_priority_fee_per_gas: tx.max_priority_fee_per_gas,
+                max_fee_per_gas: tx.max_fee_per_gas,
+                gas: tx.gas,
+                to: tx.to,
+                value: tx.value,
+                data: tx.data,
+                access_list: tx.access_list,
+                max_fee_per_blob_gas: tx.max_fee_per_blob_gas,
+                blob_versioned_hashes: tx.blob_versioned_hashes,
+                signature_y_parity: tx.signature_y_parity,
+                signature_r: tx.signature_r,
+                signature_s: tx.signature_s,
+                inner_hash: tx.inner_hash,
+            })
         }
-        ethrex_common_libmdbx::types::Transaction::EIP7702Transaction(tx) => {
-            ethrex_common::types::Transaction::EIP7702Transaction(
-                ethrex_common::types::EIP7702Transaction {
-                    chain_id: tx.chain_id,
-                    nonce: tx.nonce,
-                    max_priority_fee_per_gas: tx.max_priority_fee_per_gas,
-                    max_fee_per_gas: tx.max_fee_per_gas,
-                    gas_limit: tx.gas_limit,
-                    to: tx.to,
-                    value: tx.value,
-                    data: tx.data,
-                    access_list: tx.access_list,
-                    authorization_list: tx
-                        .authorization_list
-                        .iter()
-                        .map(|auth| ethrex_common::types::AuthorizationTuple {
-                            chain_id: auth.chain_id,
-                            address: auth.address,
-                            nonce: auth.nonce,
-                            y_parity: auth.y_parity,
-                            r_signature: auth.r_signature,
-                            s_signature: auth.s_signature,
-                        })
-                        .collect(),
-                    signature_y_parity: tx.signature_y_parity,
-                    signature_r: tx.signature_r,
-                    signature_s: tx.signature_s,
-                    inner_hash: tx.inner_hash,
-                },
-            )
+        LibmdbxTransaction::EIP7702Transaction(tx) => {
+            Transaction::EIP7702Transaction(EIP7702Transaction {
+                chain_id: tx.chain_id,
+                nonce: tx.nonce,
+                max_priority_fee_per_gas: tx.max_priority_fee_per_gas,
+                max_fee_per_gas: tx.max_fee_per_gas,
+                gas_limit: tx.gas_limit,
+                to: tx.to,
+                value: tx.value,
+                data: tx.data,
+                access_list: tx.access_list,
+                authorization_list: tx
+                    .authorization_list
+                    .iter()
+                    .map(|auth| AuthorizationTuple {
+                        chain_id: auth.chain_id,
+                        address: auth.address,
+                        nonce: auth.nonce,
+                        y_parity: auth.y_parity,
+                        r_signature: auth.r_signature,
+                        s_signature: auth.s_signature,
+                    })
+                    .collect(),
+                signature_y_parity: tx.signature_y_parity,
+                signature_r: tx.signature_r,
+                signature_s: tx.signature_s,
+                inner_hash: tx.inner_hash,
+            })
         }
-        ethrex_common_libmdbx::types::Transaction::PrivilegedL2Transaction(tx) => {
-            ethrex_common::types::Transaction::PrivilegedL2Transaction(
-                ethrex_common::types::PrivilegedL2Transaction {
-                    chain_id: tx.chain_id,
-                    nonce: tx.nonce,
-                    max_priority_fee_per_gas: tx.max_priority_fee_per_gas,
-                    max_fee_per_gas: tx.max_fee_per_gas,
-                    gas_limit: tx.gas_limit,
-                    to: match tx.to {
-                        ethrex_common_libmdbx::types::TxKind::Create => {
-                            ethrex_common::types::TxKind::Create
-                        }
-                        ethrex_common_libmdbx::types::TxKind::Call(to) => {
-                            ethrex_common::types::TxKind::Call(to)
-                        }
-                    },
-                    value: tx.value,
-                    data: tx.data,
-                    access_list: tx.access_list,
-                    from: tx.from,
-                    inner_hash: tx.inner_hash,
+        LibmdbxTransaction::PrivilegedL2Transaction(tx) => {
+            Transaction::PrivilegedL2Transaction(PrivilegedL2Transaction {
+                chain_id: tx.chain_id,
+                nonce: tx.nonce,
+                max_priority_fee_per_gas: tx.max_priority_fee_per_gas,
+                max_fee_per_gas: tx.max_fee_per_gas,
+                gas_limit: tx.gas_limit,
+                to: match tx.to {
+                    LibmdbxTxKind::Create => TxKind::Create,
+                    LibmdbxTxKind::Call(to) => TxKind::Call(to),
                 },
-            )
+                value: tx.value,
+                data: tx.data,
+                access_list: tx.access_list,
+                from: tx.from,
+                inner_hash: tx.inner_hash,
+            })
         }
     }
 }
