2080e2f071f6d9ae5dde60bfac4b53fcecf6cb7c
SDartayet <sofiadartayet@gmail.com>
2025-10-01 17:56:50 -0300

    Fixed failing test

--- crates/vm/backends/levm/mod.rs
+++ crates/vm/backends/levm/mod.rs
@@ -381,8 +381,10 @@ pub fn generic_system_contract_levm(
         ..Default::default()
     };
 
-    let account_code_length = db.get_account_code(contract_address)?.len();
-    if account_code_length == 0 && contract_address != HISTORY_STORAGE_ADDRESS.address {
+    if !(contract_address == HISTORY_STORAGE_ADDRESS.address
+        || contract_address == BEACON_ROOTS_ADDRESS.address)
+        && db.get_account_code(contract_address)?.len() == 0
+    {
         return Err(EvmError::SystemContractCallFailed(format!(
             "System contract: {contract_address} has no code after deployment"
         )));
