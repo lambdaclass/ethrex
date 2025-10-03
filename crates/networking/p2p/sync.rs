844247fb6c4b825a2a6131f72ebc5d0ae7a2c307
Lucas Fiegl <iovoid@users.noreply.github.com>
2025-10-03 09:50:48 -0300

    also free peers in healing

--- crates/networking/p2p/sync.rs
+++ crates/networking/p2p/sync.rs
@@ -1112,6 +1112,7 @@ impl Syncer {
                 &mut code_hash_collector,
             )
             .await?;
+            free_peers_and_log_if_not_empty(&self.peers).await;
             if !healing_done {
                 continue;
             }
