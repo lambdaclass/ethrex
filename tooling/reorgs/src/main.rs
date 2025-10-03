11ec6081ee29ff115a14113163beb5246b931c21
Camila Di Ielsi <camila.diielsi@lambdaclass.com>
2025-10-03 14:32:09 -0300

    uncomment tooling/reorgs test

--- tooling/reorgs/src/main.rs
+++ tooling/reorgs/src/main.rs
@@ -33,8 +33,7 @@ async fn main() {
 
     run_test(&cmd_path, test_one_block_reorg_and_back).await;
 
-    // TODO: this test is failing
-    // run_test(&cmd_path, test_many_blocks_reorg).await;
+    run_test(&cmd_path, test_many_blocks_reorg).await;
 }
 
 async fn get_ethrex_version(cmd_path: &Path) -> String {
