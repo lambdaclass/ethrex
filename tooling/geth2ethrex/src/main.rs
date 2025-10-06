547da980694407ddd8f523be38a358a8ba980bbc
Mario Rugiero <mrugiero@gmail.com>
2025-10-06 18:44:16 -0300

    better debug

--- tooling/geth2ethrex/src/main.rs
+++ tooling/geth2ethrex/src/main.rs
@@ -431,13 +431,13 @@ fn account_bucket_worker(
             let mut last = [0u8; 32];
             for (hash, encoded) in sort_buffer.drain(..) {
                 println!(
-                    "inserting hash: {:016x}{:016x}",
+                    "inserting hash: {:032x}{:032x}",
                     u128::from_be_bytes(hash[..16].try_into().unwrap()),
                     u128::from_be_bytes(hash[16..].try_into().unwrap())
                 );
                 sst.put(hash, encoded).inspect_err(|_| {
                     println!(
-                        "failed to insert: {:016x}{:016x} (prev: {:016x}{:016x})",
+                        "failed to insert: {:032x}{:032x} (prev: {:032x}{:032x})",
                         u128::from_be_bytes(hash[..16].try_into().unwrap()),
                         u128::from_be_bytes(hash[16..].try_into().unwrap()),
                         u128::from_be_bytes(last[..16].try_into().unwrap()),
