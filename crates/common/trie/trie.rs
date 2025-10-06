39961b0f8d1719742ee1fe204273efaf6ab93c02
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2025-10-06 18:27:59 -0300

    fix

--- crates/common/trie/trie.rs
+++ crates/common/trie/trie.rs
@@ -129,7 +129,7 @@ impl Trie {
             // If the trie is not empty, call the root node's insertion logic.
             self.root
                 .get_node(self.db.as_ref(), Nibbles::default())?
-                .ok_or(TrieError::RootNotFound)?
+                .ok_or(TrieError::InconsistentTree)?
                 .insert(self.db.as_ref(), path, value)?
                 .into()
         } else {
