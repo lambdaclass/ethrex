83475d2ab1cea5d1f32722b7575bc0b4d94bb8b9
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2025-10-06 11:10:38 -0300

    Update tooling/reorgs/src/main.rs
    Co-authored-by: Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>

--- tooling/reorgs/src/main.rs
+++ tooling/reorgs/src/main.rs
@@ -35,6 +35,7 @@ async fn main() {
     run_test(&cmd_path, no_reorgs_full_sync_smoke_test).await;
     run_test(&cmd_path, test_reorg_back_to_base).await;
     // This test is flaky 50% of the time, check that it runs correctly 30 times in a row
+    // TODO(#4775): make it deterministic
     for _ in 0..30 {
         run_test(&cmd_path, test_chain_split).await;
     }
