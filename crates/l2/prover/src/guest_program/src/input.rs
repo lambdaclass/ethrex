f26d7dcc8702c37580fa9d3208e954b60150dd3d
Pablo Deymonnaz <pdeymon@fi.uba.ar>
2025-10-07 11:28:24 -0300

    ProgramInput in guestprogram

--- crates/l2/prover/src/guest_program/src/input.rs
+++ crates/l2/prover/src/guest_program/src/input.rs
@@ -3,11 +3,10 @@ use rkyv::{Archive, Deserialize as RDeserialize, Serialize as RSerialize};
 use serde::{Deserialize, Serialize};
 use serde_with::serde_as;
 
-#[cfg(feature = "l2")]
-use ethrex_common::types::blobs_bundle;
-
 /// Private input variables passed into the zkVM execution program.
+#[cfg(feature = "l2")]
 #[serde_as]
+#[allow(clippy::derivable_impls)]
 #[derive(Serialize, Deserialize, RDeserialize, RSerialize, Archive)]
 pub struct ProgramInput {
     /// blocks to execute
@@ -16,25 +15,35 @@ pub struct ProgramInput {
     pub execution_witness: ExecutionWitness,
     /// value used to calculate base fee
     pub elasticity_multiplier: u64,
-    #[cfg(feature = "l2")]
     /// KZG commitment to the blob data
     #[serde_as(as = "[_; 48]")]
-    pub blob_commitment: blobs_bundle::Commitment,
-    #[cfg(feature = "l2")]
+    pub blob_commitment: ethrex_common::types::blobs_bundle::Commitment,
     /// KZG opening for a challenge over the blob commitment
     #[serde_as(as = "[_; 48]")]
-    pub blob_proof: blobs_bundle::Proof,
+    pub blob_proof: ethrex_common::types::blobs_bundle::Proof,
 }
 
+/// Private input variables passed into the zkVM execution program.
+#[cfg(not(feature = "l2"))]
+#[serde_as]
+#[derive(Default, Serialize, Deserialize, RDeserialize, RSerialize, Archive)]
+pub struct ProgramInput {
+    /// blocks to execute
+    pub blocks: Vec<Block>,
+    /// database containing all the data necessary to execute
+    pub execution_witness: ExecutionWitness,
+    /// value used to calculate base fee
+    pub elasticity_multiplier: u64,
+}
+
+#[cfg(feature = "l2")]
 impl Default for ProgramInput {
     fn default() -> Self {
         Self {
             blocks: Default::default(),
             execution_witness: ExecutionWitness::default(),
             elasticity_multiplier: Default::default(),
-            #[cfg(feature = "l2")]
             blob_commitment: [0; 48],
-            #[cfg(feature = "l2")]
             blob_proof: [0; 48],
         }
     }
