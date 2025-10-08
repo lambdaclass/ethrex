3a9a443e239642beb95a202a801ea363b8b780b7
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2025-10-08 12:47:30 -0300

    chore: UNCOMMENT CONDITION

--- crates/networking/p2p/sync.rs
+++ crates/networking/p2p/sync.rs
@@ -1095,16 +1095,15 @@ impl Syncer {
         let mut healing_done = false;
         while !healing_done {
             // This if is an edge case for the skip snap sync scenario
-            // TODO: UNCOMMENT CONDITION
-            //if block_is_stale(&pivot_header) {
-            pivot_header = update_pivot(
-                pivot_header.number,
-                pivot_header.timestamp,
-                &mut self.peers,
-                block_sync_state,
-            )
-            .await?;
-            //}
+            if block_is_stale(&pivot_header) {
+                pivot_header = update_pivot(
+                    pivot_header.number,
+                    pivot_header.timestamp,
+                    &mut self.peers,
+                    block_sync_state,
+                )
+                .await?;
+            }
             healing_done = heal_state_trie_wrap(
                 pivot_header.state_root,
                 store.clone(),
