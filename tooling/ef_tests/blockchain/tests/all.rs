7d454f957a0bf665a540c6bc004045d16bdddbae
JereSalo <jeresalo17@gmail.com>
2025-10-08 11:55:56 -0300

    update comment

--- tooling/ef_tests/blockchain/tests/all.rs
+++ tooling/ef_tests/blockchain/tests/all.rs
@@ -27,7 +27,7 @@ const SKIPPED_BASE: &[&str] = &[
     "HighGasPriceParis",
     // Skip because it tries to deserialize number > U256::MAX
     "ValueOverflowParis",
-    // Skip because it's a "Create" Blob Transaction, which doesn't actually exist. It never reaches the EVM because we can't parse it.
+    // Skip because it's a "Create" Blob Transaction, which doesn't actually exist. It never reaches the EVM because we can't even parse it as an actual Transaction.
     "createBlobhashTx",
 ];
 
