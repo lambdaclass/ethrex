c32a1a4e7fe1b9a0d8f9368c5a46030b50d254c6
ilitteri <ilitteri@fi.uba.ar>
2025-10-09 12:00:10 -0300

    Add prints to snapshooter

--- cmd/snapshooter/src/main.rs
+++ cmd/snapshooter/src/main.rs
@@ -1,3 +1,5 @@
+use std::time::{Duration, Instant};
+
 use ethrex::utils::default_datadir;
 use ethrex_p2p::sync::build_snapshot;
 use ethrex_storage::{EngineType, Store};
@@ -12,5 +14,29 @@ fn main() {
         .expect("failed to get pivot header")
         .expect("pivot header not found in store");
 
+    println!("Building snapshot...");
+
+    let start = Instant::now();
     build_snapshot(pivot_header.state_root, &store).expect("failed to build snapshot");
+    let elapsed = start.elapsed();
+
+    println!("Snapshot built in: {}", format_duration(elapsed))
+}
+
+fn format_duration(duration: Duration) -> String {
+    let total_seconds = duration.as_secs();
+    let hours = total_seconds / 3600;
+    let minutes = (total_seconds % 3600) / 60;
+    let seconds = total_seconds % 60;
+    let milliseconds = duration.subsec_millis();
+
+    if hours > 0 {
+        return format!("{hours:02}h {minutes:02}m {seconds:02}s {milliseconds:03}ms");
+    }
+
+    if minutes == 0 {
+        return format!("{seconds:02}s {milliseconds:03}ms");
+    }
+
+    format!("{minutes:02}m {seconds:02}s")
 }
