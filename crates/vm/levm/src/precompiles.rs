f09254424a191b03636d6235a61478ceea69d94a
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2025-10-20 15:37:20 -0300

    fix: map error after function call

--- crates/vm/levm/src/precompiles.rs
+++ crates/vm/levm/src/precompiles.rs
@@ -896,6 +896,8 @@ pub fn pairing_check(batch: &[(G1, G2)]) -> Result<bool, VMError> {
 #[cfg(not(feature = "sp1"))]
 #[inline]
 pub fn pairing_check(batch: &[(G1, G2)]) -> Result<bool, VMError> {
+    use lambdaworks_math::errors::PairingError;
+
     type Fq = BN254FieldElement;
     type Fq2 = BN254TwistCurveFieldElement;
     type LambdaworksG1 = BN254Curve;
@@ -945,8 +947,13 @@ pub fn pairing_check(batch: &[(G1, G2)]) -> Result<bool, VMError> {
         valid_batch.push((g1, g2));
     }
     let valid_batch_refs: Vec<_> = valid_batch.iter().map(|(p1, p2)| (p1, p2)).collect();
-    let result = BN254AtePairing::compute_batch(&valid_batch_refs)
-        .map_err(|_| PrecompileError::BN254AtePairingError)?;
+    let result = BN254AtePairing::compute_batch(&valid_batch_refs).map_err(|err| {
+        if err == PairingError::PointNotInSubgroup {
+            PrecompileError::PointNotInSubgroup
+        } else {
+            PrecompileError::BN254AtePairingError
+        }
+    })?;
 
     Ok(result == QuadraticExtensionFieldElement::one())
 }
