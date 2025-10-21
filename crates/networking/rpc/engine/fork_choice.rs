121c49b287ea39797e43c3b0b120f53021312a42
fmoletta <fedemoletta@hotmail.com>
2025-10-21 16:00:26 -0300

    fix(l1): do not trigger snap sync if fcu head is already canonical

--- crates/networking/rpc/engine/fork_choice.rs
+++ crates/networking/rpc/engine/fork_choice.rs
@@ -215,7 +215,12 @@ async fn handle_forkchoice(
         ));
     }
 
-    if context.syncer.sync_mode() == SyncMode::Snap {
+    // Don't trigger a sync if the block is already canonical
+    if context.syncer.sync_mode() == SyncMode::Snap
+        && !context
+            .storage
+            .is_canonical_sync(fork_choice_state.head_block_hash)?
+    {
         context
             .syncer
             .sync_to_head(fork_choice_state.head_block_hash);
