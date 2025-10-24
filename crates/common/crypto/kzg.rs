665bb46bf20698f72a6991a986887ae76283ae6c
ilitteri <ilitteri@fi.uba.ar>
2025-10-24 19:09:49 -0300

    Fix kzg.rs

--- crates/common/crypto/kzg.rs
+++ crates/common/crypto/kzg.rs
@@ -28,7 +28,7 @@ type Proof = Bytes48;
 pub fn warm_up_trusted_setup() {
     #[cfg(feature = "c-kzg")]
     {
-        let _ = c_kzg::ethereum_kzg_settings(8);
+        let _ = c_kzg::ethereum_kzg_settings(KZG_PRECOMPUTE);
     }
 }
 
@@ -155,17 +155,14 @@ pub fn verify_kzg_proof(
 pub fn blob_to_kzg_commitment_and_proof(blob: &Blob) -> Result<(Commitment, Proof), KzgError> {
     let blob: c_kzg::Blob = (*blob).into();
 
-    let commitment = c_kzg::KzgSettings::blob_to_kzg_commitment(
-        c_kzg::ethereum_kzg_settings(KZG_PRECOMPUTE),
-        &blob,
-    )?;
+    let c_kzg_settings = c_kzg::ethereum_kzg_settings(KZG_PRECOMPUTE);
+
+    let commitment = c_kzg::KzgSettings::blob_to_kzg_commitment(c_kzg_settings, &blob)?;
+
     let commitment_bytes = commitment.to_bytes();
 
-    let proof = c_kzg::KzgSettings::compute_blob_kzg_proof(
-        c_kzg::ethereum_kzg_settings(KZG_PRECOMPUTE),
-        &blob,
-        &commitment_bytes,
-    )?;
+    let proof =
+        c_kzg::KzgSettings::compute_blob_kzg_proof(c_kzg_settings, &blob, &commitment_bytes)?;
 
     let proof_bytes = proof.to_bytes();
 
@@ -176,15 +173,19 @@ pub fn blob_to_kzg_commitment_and_proof(blob: &Blob) -> Result<(Commitment, Proo
 pub fn blob_to_commitment_and_cell_proofs(
     blob: &Blob,
 ) -> Result<(Commitment, Vec<Proof>), KzgError> {
-    let c_kzg_settings = c_kzg::ethereum_kzg_settings(8);
+    let c_kzg_settings = c_kzg::ethereum_kzg_settings(KZG_PRECOMPUTE);
+
     let blob: c_kzg::Blob = (*blob).into();
-    let commitment =
-        c_kzg::KzgSettings::blob_to_kzg_commitment(c_kzg::ethereum_kzg_settings(8), &blob)?;
+
+    let commitment = c_kzg::KzgSettings::blob_to_kzg_commitment(c_kzg_settings, &blob)?;
+
     let commitment_bytes = commitment.to_bytes();
 
     let (_cells, cell_proofs) = c_kzg_settings
         .compute_cells_and_kzg_proofs(&blob)
         .map_err(KzgError::CKzg)?;
+
     let cell_proofs = cell_proofs.map(|p| p.to_bytes().into_inner());
+
     Ok((commitment_bytes.into_inner(), cell_proofs.to_vec()))
 }
