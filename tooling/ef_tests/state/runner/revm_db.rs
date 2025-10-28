7d28870c77039643ae41abe48e5264199784e3ae
Martin Paulucci <martin.c.paulucci@gmail.com>
2025-10-28 18:49:45 +0100

    Fix format.

--- tooling/ef_tests/state/runner/revm_db.rs
+++ tooling/ef_tests/state/runner/revm_db.rs
@@ -210,7 +210,9 @@ impl RevmState {
             // Apply account changes to DB
             let mut account_update = AccountUpdate::new(address);
             // If the account was changed then both original and current info will be present in the bundle account
-            if account.is_info_changed() && let Some(new_acc_info) = account.account_info() {
+            if account.is_info_changed()
+                && let Some(new_acc_info) = account.account_info()
+            {
                 // Update account info in DB
                 let code_hash = H256::from_slice(new_acc_info.code_hash.as_slice());
                 let account_info = AccountInfo {
@@ -220,7 +222,9 @@ impl RevmState {
                 };
                 account_update.info = Some(account_info);
                 // Update code in db
-                if account.is_contract_changed() && let Some(code) = new_acc_info.code {
+                if account.is_contract_changed()
+                    && let Some(code) = new_acc_info.code
+                {
                     account_update.code = Some(Code::from_bytecode(code.original_bytes().0));
                 }
             }
