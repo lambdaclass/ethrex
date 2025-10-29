3a1f09b6a7fdb3537e53ad54b805b396873b5e37
ilitteri <ilitteri@fi.uba.ar>
2025-10-29 18:04:41 -0300

    Embed SP1 ELF into guest_program

--- crates/l2/prover/src/guest_program/src/lib.rs
+++ crates/l2/prover/src/guest_program/src/lib.rs
@@ -2,3 +2,10 @@ pub mod execution;
 pub mod input;
 pub mod methods;
 pub mod output;
+
+#[cfg(all(not(clippy), feature = "sp1"))]
+pub static ZKVM_SP1_PROGRAM_ELF: &[u8] = include_bytes!("./sp1/out/riscv32im-succinct-zkvm-elf");
+// If we're running clippy, the file isn't generated.
+// To avoid compilation errors, we override it with an empty slice.
+#[cfg(any(clippy, not(feature = "sp1")))]
+pub const ZKVM_SP1_PROGRAM_ELF: &[u8] = &[];
