bfb7d3c4cb619bef2affae043b6dd42facf6e3ba
Francisco Xavier Gauna <francisco.gauna@lambdaclass.com>
2025-10-30 11:48:54 -0300

    Remove unnecesary clone

--- crates/common/trie/trie_sorted.rs
+++ crates/common/trie/trie_sorted.rs
@@ -199,7 +199,7 @@ where
 
     // The current node that is being used as computing. We compare it with the current
     // parent and the next value to see where it should be written
-    let mut current_node: CenterSide = CenterSide::from_value(initial_value.clone());
+    let mut current_node: CenterSide = CenterSide::from_value(initial_value);
     let mut next_value_opt: Option<(H256, Vec<u8>)> = data_iter.next();
 
     while let Some(next_value) = next_value_opt {
