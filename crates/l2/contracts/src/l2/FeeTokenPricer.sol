0a6524be164af094bba21cf65819001053366489
Tomás Paradelo <tomas.paradelo@lambdaclass.com>
2025-11-03 17:18:01 -0300

    fix identation

--- crates/l2/contracts/src/l2/FeeTokenPricer.sol
+++ crates/l2/contracts/src/l2/FeeTokenPricer.sol
@@ -6,11 +6,11 @@ contract FeeTokenPricer is IFeeTokenPricer {
 	address internal constant FEE_TOKEN =
         0xb7E811662Fa10ac068aeE115AC2e682821630535;
 	
-  /// @inheritdoc IFeeTokenPricer
-  function getFeeTokenRatio(
-      address fee_token
-  ) external view override returns (uint256) {
-		require(fee_token == FEE_TOKEN, "The fee token does not match with the one set for the chain");
-		return 2;
-  }	
+	  /// @inheritdoc IFeeTokenPricer
+		function getFeeTokenRatio(
+			  address fee_token
+		) external view override returns (uint256) {
+			require(fee_token == FEE_TOKEN, "The fee token does not match with the one set for the chain");
+			return 2;
+		}	
 }
