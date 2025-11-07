b18b7e237a5a9999db5a83095fdbf7cb3da3c1e3
Pablo Deymonnaz <pdeymon@fi.uba.ar>
2025-11-07 17:00:22 -0300

    changing freq to 99997

--- crates/blockchain/blockchain.rs
+++ crates/blockchain/blockchain.rs
@@ -856,7 +856,7 @@ impl Blockchain {
         use std::io::Write;
 
         let guard = pprof::ProfilerGuardBuilder::default()
-            .frequency(9997)
+            .frequency(99997)
             .build()
             .unwrap();
         let (res, account_updates_list, merkle_queue_length, instants) =
