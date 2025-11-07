a20302b468dc6160090f11ca45a7cce428f15298
JereSalo <jeresalo17@gmail.com>
2025-11-07 12:13:00 -0300

    imrpove comment

--- crates/common/trie/db.rs
+++ crates/common/trie/db.rs
@@ -59,7 +59,7 @@ impl InMemoryTrieDB {
         }
     }
 
-    // Do not remove or make private as we use this outside of ethrex
+    // Do not remove or make private as we use this in ethrex-replay
     pub fn from_nodes(
         root_hash: H256,
         state_nodes: &BTreeMap<H256, NodeRLP>,
@@ -84,7 +84,7 @@ impl InMemoryTrieDB {
         }
     }
 
-    // Do not remove or make private as we use this outside of ethrex
+    // Do not remove or make private as we use this in ethrex-replay
     pub fn inner(&self) -> NodeMap {
         Arc::clone(&self.inner)
     }
