838879f1e7bd61a434a725b2bb11951ccafb7913
Tomás Paradelo <tomas.paradelo@lambdaclass.com>
2025-11-10 13:09:23 -0300

    refactors and delete debug stuff

--- crates/l2/contracts/src/l2/FeeTokenRegistry.sol
+++ crates/l2/contracts/src/l2/FeeTokenRegistry.sol
@@ -3,17 +3,16 @@ pragma solidity =0.8.29;
 import "./interfaces/IFeeTokenRegistry.sol";
 
 contract FeeTokenRegistry is IFeeTokenRegistry {
-    address public constant BRIDGE =
-        0x000000000000000000000000000000000000FFff;
+    address public constant BRIDGE = 0x000000000000000000000000000000000000FFff;
 
-		mapping(address => bool) private feeTokens;
+    mapping(address => bool) private feeTokens;
 
-		/// @inheritdoc IFeeTokenRegistry
+    /// @inheritdoc IFeeTokenRegistry
     function isFeeToken(address token) external view override returns (bool) {
         return feeTokens[token];
     }
 
-		/// @inheritdoc IFeeTokenRegistry
+    /// @inheritdoc IFeeTokenRegistry
     function registerFeeToken(address token) external override onlyBridge {
         require(token != address(0), "FeeTokenRegistry: zero address");
         require(!feeTokens[token], "Token already registered");
@@ -21,16 +20,16 @@ contract FeeTokenRegistry is IFeeTokenRegistry {
         emit FeeTokenRegistered(token);
     }
 
-		/// @inheritdoc IFeeTokenRegistry
+    /// @inheritdoc IFeeTokenRegistry
     function unregisterFeeToken(address token) external override onlyBridge {
         require(feeTokens[token], "Token not registered");
         feeTokens[token] = false;
         emit FeeTokenUnregistered(token);
-		}
+    }
 
-		modifier onlyBridge() {
+    modifier onlyBridge() {
         require(msg.sender == BRIDGE, "FeeTokenRegistry: not bridge");
         _;
     }
-
 }
+
