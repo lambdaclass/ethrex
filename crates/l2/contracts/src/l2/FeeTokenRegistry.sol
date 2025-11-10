6895ca99b4fd8d417f16dad024ff057b2fe6a72b
Tomás Paradelo <tomas.paradelo@lambdaclass.com>
2025-11-10 10:50:45 -0300

    print receipt

--- crates/l2/contracts/src/l2/FeeTokenRegistry.sol
+++ crates/l2/contracts/src/l2/FeeTokenRegistry.sol
@@ -14,7 +14,7 @@ contract FeeTokenRegistry is IFeeTokenRegistry {
     }
 
 		/// @inheritdoc IFeeTokenRegistry
-    function registerFeeToken(address token) external override {
+    function registerFeeToken(address token) external override onlyBridge {
         require(token != address(0), "FeeTokenRegistry: zero address");
         require(!feeTokens[token], "Token already registered");
         feeTokens[token] = true;
--- crates/l2/sequencer/block_producer/payload_builder.rs
+++ crates/l2/sequencer/block_producer/payload_builder.rs
@@ -205,6 +205,7 @@ pub async fn fill_transactions(
                 continue;
             }
         };
+        dbg!(&receipt);
 
         let tx_backup = context.vm.db.get_tx_backup().map_err(|e| {
             BlockProducerError::FailedToGetDataFrom(format!("transaction backup: {e}"))
