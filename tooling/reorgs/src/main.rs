9c6addc49e54f3934f2ef292ae611f9d975e1d71
SDartayet <44068466+SDartayet@users.noreply.github.com>
2025-11-10 11:41:11 -0300

    Added sleep before forkchoice is updated

--- tooling/reorgs/src/main.rs
+++ tooling/reorgs/src/main.rs
@@ -215,6 +215,7 @@ async fn test_one_block_reorg_and_back(simulator: Arc<Mutex<Simulator>>) {
 
     let side_chain = node1.build_payload(side_chain).await;
     node1.notify_new_payload(&side_chain).await;
+    tokio::time::sleep(Duration::from_millis(500)).await;
     node1.update_forkchoice(&side_chain).await;
 
     // Sanity check: balance hasn't changed
@@ -223,6 +224,7 @@ async fn test_one_block_reorg_and_back(simulator: Arc<Mutex<Simulator>>) {
 
     // Notify the first node of the side chain block, it should reorg
     node0.notify_new_payload(&side_chain).await;
+    tokio::time::sleep(Duration::from_millis(500)).await;
     node0.update_forkchoice(&side_chain).await;
 
     // Check the transfer has been processed
@@ -231,6 +233,7 @@ async fn test_one_block_reorg_and_back(simulator: Arc<Mutex<Simulator>>) {
 
     // Finally, move to the extended base chain, it should reorg back
     node0.notify_new_payload(&extended_base_chain).await;
+    tokio::time::sleep(Duration::from_millis(500)).await;
     node0.update_forkchoice(&extended_base_chain).await;
 
     // Check the transfer has been reverted
