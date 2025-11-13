ac13d0f707d4014dc85ef911d7f07e37e2d082bc
fmoletta <fedemoletta@hotmail.com>
2025-11-13 13:06:29 -0300

    feat(l1): periodically check if there is a newer release available

--- cmd/ethrex/ethrex.rs
+++ cmd/ethrex/ethrex.rs
@@ -5,6 +5,8 @@ use ethrex::{
     utils::{NodeConfigFile, get_client_version, store_node_config_file},
 };
 use ethrex_p2p::{discv4::peer_table::PeerTable, types::NodeRecord};
+use semver::Version;
+use serde::Deserialize;
 use std::{path::Path, time::Duration};
 use tokio::signal::unix::{SignalKind, signal};
 use tokio_util::sync::CancellationToken;
@@ -45,6 +47,63 @@ async fn server_shutdown(
     info!("Server shutting down!");
 }
 
+/// Fetches the latest release version on github
+/// Returns None if there was an error when requesting the latest version
+async fn latest_release_version() -> Option<Version> {
+    #[derive(Deserialize)]
+    struct Release {
+        tag_name: String,
+    }
+    let client = reqwest::Client::new();
+    let response = client
+        .get("https://api.github.com/repos/lambdaclass/ethrex/releases/latest")
+        .header("User-Agent", "ethrex")
+        .send()
+        .await
+        .ok()?;
+    if !response.status().is_success() {
+        None
+    } else {
+        Version::parse(
+            response
+                .json::<Release>()
+                .await
+                .ok()?
+                .tag_name
+                .trim_start_matches("v"),
+        )
+        .ok()
+    }
+}
+
+/// Reads current crate version
+fn current_version() -> Option<Version> {
+    Version::parse(env!("CARGO_PKG_VERSION")).ok()
+}
+
+/// Checks if the latest released version is higher than the current version and emits an info log
+/// Won't emit a log line if the current version is newer or equal, or if there was a problem reading either version
+async fn check_version_update() {
+    if let (Some(current_version), Some(latest_version)) =
+        (current_version(), latest_release_version().await)
+    {
+        if current_version < latest_version {
+            info!(
+                "There is a newer ethrex version available, current version: {current_version} vs latest version: {latest_version}"
+            );
+        }
+    }
+}
+
+/// Checks if there is a newer ethrex verison available every hour
+async fn periodically_check_version_update() {
+    let mut interval = tokio::time::interval(Duration::from_secs(60 * 60));
+    loop {
+        interval.tick().await;
+        check_version_update().await;
+    }
+}
+
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
     let CLI { opts, command } = CLI::parse();
@@ -56,6 +115,7 @@ async fn main() -> eyre::Result<()> {
     let log_filter_handler = init_tracing(&opts);
 
     info!("ethrex version: {}", get_client_version());
+    tokio::spawn(periodically_check_version_update());
 
     let (datadir, cancel_token, peer_table, local_node_record) =
         init_l1(opts, Some(log_filter_handler)).await?;
