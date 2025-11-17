e17ef120a8bb01b3667cab828ab0a17aa12b0ca4
Rodrigo Oliveri <rodrigooliveri10@gmail.com>
2025-11-17 20:04:20 -0300

    fix an unnecesary diff

--- crates/blockchain/metrics/profiling.rs
+++ crates/blockchain/metrics/profiling.rs
@@ -12,7 +12,6 @@ pub static METRICS_BLOCK_PROCESSING_PROFILE: LazyLock<HistogramVec> =
 
 // Metrics defined in this module register into the Prometheus default registry.
 // The metrics API exposes them by calling `gather_default_metrics()`.
-
 fn initialize_histogram_vec() -> HistogramVec {
     register_histogram_vec!(
         "function_duration_seconds",
