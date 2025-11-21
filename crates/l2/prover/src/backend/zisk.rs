1c9ceba6b117172ce821dd4db42b27b3a310716f
ilitteri <ivanlitteri@hotmail.com>
2025-11-21 15:51:18 -0300

    add --unlock-mapped-memory flag

--- crates/l2/prover/src/backend/zisk.rs
+++ crates/l2/prover/src/backend/zisk.rs
@@ -54,6 +54,7 @@ pub fn prove(
         "--output-dir",
         OUTPUT_DIR_PATH,
         "--aggregation",
+        "--unlock-mapped-memory",
     ];
     let conditional_groth16_arg = if let ProofFormat::Groth16 = format {
         vec!["--final-snark"]
