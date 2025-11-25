2537d32b85707a899cc08f3420e7d3bf03dd6aa8
Rodrigo Oliveri <rodrigooliveri10@gmail.com>
2025-11-25 17:11:38 -0300

    use linear buckets instead

--- crates/blockchain/metrics/blocks.rs
+++ crates/blockchain/metrics/blocks.rs
@@ -55,10 +55,7 @@ impl MetricsBlocks {
                     "gigagas_summary",
                     "Summary of the block execution throughput through gigagas/s",
                 )
-                .buckets(vec![
-                    0.0, 0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2,
-                    1.5, 2.0, 5.0, 10.0,
-                ]),
+                .buckets(prometheus::linear_buckets(0.0, 0.05, 60).unwrap()),
             )
             .unwrap(),
             gigagas_block_building: Gauge::new(
