139cca8526ecb66455278e2dd9abf0c2a9c81e36
Rodrigo Oliveri <rodrigooliveri10@gmail.com>
2025-11-25 17:02:33 -0300

    make bucket less sparse

--- crates/blockchain/metrics/blocks.rs
+++ crates/blockchain/metrics/blocks.rs
@@ -56,7 +56,8 @@ impl MetricsBlocks {
                     "Summary of the block execution throughput through gigagas/s",
                 )
                 .buckets(vec![
-                    0.0, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1.0, 5.0, 10.0,
+                    0.0, 0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2,
+                    1.5, 2.0, 5.0, 10.0,
                 ]),
             )
             .unwrap(),
