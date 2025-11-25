d5ee797d800c5c9d964ef32b1f21c5e304574aeb
Esteve Soler Arderiu <azteca1998@users.noreply.github.com>
2025-11-25 13:03:54 +0100

    Implement RLP decoding benches for lists of integers and strings.

--- crates/common/rlp/benches/decode.rs
+++ crates/common/rlp/benches/decode.rs
@@ -4,7 +4,7 @@ use rand::{
     Rng,
     distr::{Alphanumeric, Distribution, SampleString, StandardUniform},
 };
-use std::hint::black_box;
+use std::{hint::black_box, iter::repeat_with};
 
 fn bench_decode_scalars(c: &mut Criterion) {
     let mut group = c.benchmark_group("decode_scalars");
@@ -15,7 +15,7 @@ fn bench_decode_scalars(c: &mut Criterion) {
         T: RLPDecode + RLPEncode,
     {
         b.iter_batched_ref(
-            move || {
+            || {
                 rand::rng()
                     .random_iter::<T>()
                     .take(N)
@@ -121,18 +121,127 @@ fn bench_decode_strings(c: &mut Criterion) {
     group.finish();
 }
 
-fn bench_decode_collections(c: &mut Criterion) {
-    let mut group = c.benchmark_group("decode_collections");
-    group.finish();
-}
+fn bench_decode_lists(c: &mut Criterion) {
+    let mut group = c.benchmark_group("decode_lists");
 
-fn bench_decode_tuples(c: &mut Criterion) {
-    let mut group = c.benchmark_group("decode_tuples");
-    group.finish();
-}
+    fn impl_bench_for<T, const N: usize, const L: usize>(b: &mut Bencher)
+    where
+        StandardUniform: Distribution<T>,
+        T: RLPDecode + RLPEncode,
+    {
+        b.iter_batched_ref(
+            || {
+                let mut data = Vec::with_capacity(N);
+                for _ in 0..N {
+                    data.push(
+                        rand::rng()
+                            .random_iter::<T>()
+                            .take(L)
+                            .collect::<Vec<_>>()
+                            .encode_to_vec(),
+                    );
+                }
+                data
+            },
+            |data| {
+                for data in data.iter() {
+                    black_box(Vec::<T>::decode(data).unwrap());
+                }
+            },
+            BatchSize::SmallInput,
+        );
+    }
 
-fn bench_decode_ips(c: &mut Criterion) {
-    let mut group = c.benchmark_group("decode_ip_types");
+    fn impl_bench_str<const N: usize, const L: usize>(b: &mut Bencher) {
+        b.iter_batched_ref(
+            || {
+                let mut data = Vec::with_capacity(N);
+                for _ in 0..N {
+                    data.push(
+                        repeat_with(|| Alphanumeric.sample_string(&mut rand::rng(), 10))
+                            .take(L)
+                            .collect::<Vec<_>>()
+                            .encode_to_vec(),
+                    );
+                }
+                data
+            },
+            |data| {
+                for data in data.iter() {
+                    black_box(Vec::<String>::decode(data).unwrap());
+                }
+            },
+            BatchSize::SmallInput,
+        );
+    }
+
+    group.bench_function(
+        BenchmarkId::new("u8", "len=10/1000"),
+        impl_bench_for::<u8, 1000, 10>,
+    );
+    group.bench_function(
+        BenchmarkId::new("u8", "len=100/1000"),
+        impl_bench_for::<u8, 1000, 100>,
+    );
+    group.bench_function(
+        BenchmarkId::new("u8", "len=1000/1000"),
+        impl_bench_for::<u8, 1000, 1000>,
+    );
+
+    group.bench_function(
+        BenchmarkId::new("u16", "len=10/1000"),
+        impl_bench_for::<u16, 1000, 10>,
+    );
+    group.bench_function(
+        BenchmarkId::new("u16", "len=100/1000"),
+        impl_bench_for::<u16, 1000, 100>,
+    );
+    group.bench_function(
+        BenchmarkId::new("u16", "len=1000/1000"),
+        impl_bench_for::<u16, 1000, 1000>,
+    );
+
+    group.bench_function(
+        BenchmarkId::new("u32", "len=10/1000"),
+        impl_bench_for::<u32, 1000, 10>,
+    );
+    group.bench_function(
+        BenchmarkId::new("u32", "len=100/1000"),
+        impl_bench_for::<u32, 1000, 100>,
+    );
+    group.bench_function(
+        BenchmarkId::new("u32", "len=1000/1000"),
+        impl_bench_for::<u32, 1000, 1000>,
+    );
+
+    group.bench_function(
+        BenchmarkId::new("u64", "len=10/1000"),
+        impl_bench_for::<u64, 1000, 10>,
+    );
+    group.bench_function(
+        BenchmarkId::new("u64", "len=100/1000"),
+        impl_bench_for::<u64, 1000, 100>,
+    );
+    group.bench_function(
+        BenchmarkId::new("u64", "len=1000/1000"),
+        impl_bench_for::<u64, 1000, 1000>,
+    );
+
+    // group.bench_function("u128", impl_bench_for::<u128>);
+
+    group.bench_function(
+        BenchmarkId::new("str[10]", "len=10/1000"),
+        impl_bench_str::<1000, 10>,
+    );
+    group.bench_function(
+        BenchmarkId::new("str[10]", "len=100/1000"),
+        impl_bench_str::<1000, 100>,
+    );
+    group.bench_function(
+        BenchmarkId::new("str[10]", "len=1000/1000"),
+        impl_bench_str::<1000, 1000>,
+    );
+
     group.finish();
 }
 
@@ -140,8 +249,7 @@ criterion_group!(
     benches,
     bench_decode_scalars,
     bench_decode_bytes,
-    bench_decode_collections,
-    bench_decode_tuples,
-    bench_decode_ips,
+    bench_decode_strings,
+    bench_decode_lists,
 );
 criterion_main!(benches);
