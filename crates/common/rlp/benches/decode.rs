d4b3f0a60a117a40e7213ca710fda8689bb7f9c5
Esteve Soler Arderiu <azteca1998@users.noreply.github.com>
2025-11-26 15:46:19 +0100

    Implement some RLP decoding benches for nibbles.

--- crates/common/rlp/benches/decode.rs
+++ crates/common/rlp/benches/decode.rs
@@ -1,5 +1,7 @@
 use criterion::{BatchSize, Bencher, BenchmarkId, Criterion, criterion_group, criterion_main};
+use ethereum_types::H256;
 use ethrex_rlp::{decode::RLPDecode, encode::RLPEncode};
+use ethrex_trie::{Nibbles, NodeHash};
 use rand::{
     Rng,
     distr::{Alphanumeric, Distribution, SampleString, StandardUniform},
@@ -245,11 +247,110 @@ fn bench_decode_lists(c: &mut Criterion) {
     group.finish();
 }
 
+fn bench_decode_nibbles(c: &mut Criterion) {
+    let mut group = c.benchmark_group("decode_nibbles");
+
+    fn impl_bench<const N: usize, const L: usize, const IS_LEAF: bool>(b: &mut Bencher) {
+        b.iter_batched_ref(
+            || {
+                let mut data = Vec::with_capacity(N);
+                for _ in 0..N {
+                    data.push(
+                        Nibbles::from_raw(
+                            &rand::distr::Uniform::<u8>::new(0, 16)
+                                .unwrap()
+                                .sample_iter(rand::rng())
+                                .take(L)
+                                .collect::<Vec<_>>(),
+                            IS_LEAF,
+                        )
+                        .encode_to_vec(),
+                    );
+                }
+                data
+            },
+            |data| {
+                for data in data.iter() {
+                    black_box(Nibbles::decode(data).unwrap());
+                }
+            },
+            BatchSize::SmallInput,
+        );
+    }
+
+    group.bench_function(
+        BenchmarkId::new("nibbles", "len=65/1000"),
+        impl_bench::<1000, 65, false>,
+    );
+    group.bench_function(
+        BenchmarkId::new("nibbles", "leaf/len=65/1000"),
+        impl_bench::<1000, 65, true>,
+    );
+    group.bench_function(
+        BenchmarkId::new("nibbles", "len=129/1000"),
+        impl_bench::<1000, 129, false>,
+    );
+    group.bench_function(
+        BenchmarkId::new("nibbles", "leaf/len=129/1000"),
+        impl_bench::<1000, 129, true>,
+    );
+    group.bench_function(
+        BenchmarkId::new("nibbles", "len=130/1000"),
+        impl_bench::<1000, 130, false>,
+    );
+    group.bench_function(
+        BenchmarkId::new("nibbles", "leaf/len=130/1000"),
+        impl_bench::<1000, 130, true>,
+    );
+    group.bench_function(
+        BenchmarkId::new("nibbles", "len=500/1000"),
+        impl_bench::<1000, 500, false>,
+    );
+    group.bench_function(
+        BenchmarkId::new("nibbles", "leaf/len=500/1000"),
+        impl_bench::<1000, 500, true>,
+    );
+
+    group.finish();
+}
+
+fn bench_decode_trie(c: &mut Criterion) {
+    let mut group = c.benchmark_group("decode_trie");
+
+    group.bench_function(BenchmarkId::new("NodeHash", "hashed/1000"), |b| {
+        b.iter_batched_ref(
+            || {
+                rand::rng()
+                    .random_iter::<[u8; 32]>()
+                    .map(H256)
+                    .map(NodeHash::Hashed)
+                    .map(|x| x.encode_to_vec())
+                    .collect::<Vec<_>>()
+            },
+            |data| {
+                for data in data.iter() {
+                    black_box(NodeHash::decode(data).unwrap());
+                }
+            },
+            BatchSize::SmallInput,
+        );
+    });
+    // TODO: Benchmark NodeHash::Inline (len in {8, 16, 24}).
+
+    // TODO: Benchmark BranchNode (empty, full, random; hash, node).
+    // TODO: Benchmark ExtensionNode (short path, long path; hash, node).
+    // TODO: Benchmark LeafNode (short path, long path; short value, long value).
+
+    group.finish();
+}
+
 criterion_group!(
     benches,
-    bench_decode_scalars,
     bench_decode_bytes,
-    bench_decode_strings,
     bench_decode_lists,
+    bench_decode_nibbles,
+    bench_decode_scalars,
+    bench_decode_strings,
+    bench_decode_trie,
 );
 criterion_main!(benches);
