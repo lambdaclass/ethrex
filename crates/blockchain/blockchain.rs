81214cc51a73d60611ccb19cc670fdd21a54e9ab
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2025-11-28 13:19:23 -0300

    refactor(l1): avoid recovering sender two times when txs are received (#5419)
    **Motivation**
    
    We were doing sender recovery two times for each transaction received.
    This is inefficient.
    
    **Description**
    
    This PR deduplicates the action, by sharing the computed sender with
    `Mempool::add_transaction`

--- crates/blockchain/blockchain.rs
+++ crates/blockchain/blockchain.rs
@@ -1400,7 +1400,7 @@ impl Blockchain {
 
         // Add transaction and blobs bundle to storage
         self.mempool
-            .add_transaction(hash, MempoolTransaction::new(transaction, sender))?;
+            .add_transaction(hash, sender, MempoolTransaction::new(transaction, sender))?;
         self.mempool.add_blobs_bundle(hash, blobs_bundle)?;
         Ok(hash)
     }
@@ -1426,7 +1426,7 @@ impl Blockchain {
 
         // Add transaction to storage
         self.mempool
-            .add_transaction(hash, MempoolTransaction::new(transaction, sender))?;
+            .add_transaction(hash, sender, MempoolTransaction::new(transaction, sender))?;
 
         Ok(hash)
     }
