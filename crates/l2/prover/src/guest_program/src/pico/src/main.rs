f937713e9f96999d44c9bd20657084bf3036284a
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2025-11-28 16:00:20 -0300

    remove dbg

--- crates/l2/prover/src/guest_program/src/pico/src/main.rs
+++ crates/l2/prover/src/guest_program/src/pico/src/main.rs
@@ -6,14 +6,10 @@ use rkyv::rancor::Error;
 pico_sdk::entrypoint!(main);
 
 pub fn main() {
-    dbg!("reading input");
     let input = pico_sdk::io::read_vec();
-    dbg!("deserializing input");
     let input = rkyv::from_bytes::<ProgramInput, Error>(&input).unwrap();
 
-    dbg!("executing");
     let output = execution_program(input).unwrap();
 
-    dbg!("committing output");
     pico_sdk::io::commit_bytes(&output.encode());
 }
