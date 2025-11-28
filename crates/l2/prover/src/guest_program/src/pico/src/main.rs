8b5a36e72d24b38a27909679122f1808492ec1a7
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2025-11-28 10:21:26 -0300

    add prints

--- crates/l2/prover/src/guest_program/src/pico/src/main.rs
+++ crates/l2/prover/src/guest_program/src/pico/src/main.rs
@@ -6,10 +6,14 @@ use rkyv::rancor::Error;
 pico_sdk::entrypoint!(main);
 
 pub fn main() {
+    println!("reading input");
     let input = pico_sdk::io::read_vec();
+    println!("deserializing input");
     let input = rkyv::from_bytes::<ProgramInput, Error>(&input).unwrap();
 
+    println!("executing");
     let output = execution_program(input).unwrap();
 
+    println!("committing output");
     pico_sdk::io::commit_bytes(&output.encode());
 }
