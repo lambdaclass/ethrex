3e9444f8d79a7048521cc0314604842c08914d25
Edgar Luque <git@edgl.dev>
2025-12-01 14:40:30 +0100

    add trie hash bench

--- crates/common/trie/benches/trie_bench.rs
+++ crates/common/trie/benches/trie_bench.rs
@@ -2,9 +2,11 @@
 // licensed under Apache-2. Modified to suit our needs, and to have a baseline to benchmark our own
 // trie implementation against an existing one.
 
+use std::hint::black_box;
 use std::sync::Arc;
+use std::time::Duration;
 
-use criterion::{Criterion, black_box, criterion_group, criterion_main};
+use criterion::{Criterion, criterion_group, criterion_main};
 
 use ethereum_types::H256;
 use hasher::HasherKeccak;
@@ -16,11 +18,10 @@ use ethrex_trie::Trie as EthrexTrie;
 
 #[allow(clippy::unit_arg)]
 fn insert_worse_case_benchmark(c: &mut Criterion) {
-    let (keys_1k, values_1k) = random_data(1000);
-    let (keys_10k, values_10k) = random_data(10000);
+    let (keys_1k, values_1k) = black_box(random_data(1000));
+    let (keys_10k, values_10k) = black_box(random_data(10000));
 
     let mut group = c.benchmark_group("Trie");
-
     group.bench_function("ethrex-trie insert 1k", |b| {
         let mut trie = EthrexTrie::new(Box::new(EthrexMemDB::new_empty()));
         b.iter(|| {
@@ -85,6 +86,37 @@ fn insert_worse_case_benchmark(c: &mut Criterion) {
             trie.root().unwrap()
         });
     });
+
+    group.measurement_time(Duration::from_secs(15));
+    group.bench_function("ethrex-trie compute_hash 1k", |b| {
+        b.iter_batched_ref(
+            || {
+                let mut trie = EthrexTrie::new(Box::new(EthrexMemDB::new_empty()));
+                for i in 0..keys_1k.len() {
+                    trie.insert(keys_1k[i].clone(), values_1k[i].clone())
+                        .unwrap();
+                }
+                black_box(trie)
+            },
+            |trie| black_box(trie.hash_no_commit()),
+            criterion::BatchSize::LargeInput,
+        );
+    });
+
+    group.bench_function("ethrex-trie compute_hash 10k", |b| {
+        b.iter_batched_ref(
+            || {
+                let mut trie = EthrexTrie::new(Box::new(EthrexMemDB::new_empty()));
+                for i in 0..keys_10k.len() {
+                    trie.insert(keys_10k[i].clone(), values_10k[i].clone())
+                        .unwrap();
+                }
+                black_box(trie)
+            },
+            |trie| black_box(trie.hash_no_commit()),
+            criterion::BatchSize::LargeInput,
+        );
+    });
 }
 
 fn random_data(n: usize) -> (Vec<Vec<u8>>, Vec<Vec<u8>>) {
@@ -100,5 +132,16 @@ fn random_data(n: usize) -> (Vec<Vec<u8>>, Vec<Vec<u8>>) {
     (keys, values)
 }
 
-criterion_group!(benches, insert_worse_case_benchmark);
+fn criterion_config() -> Criterion {
+    Criterion::default()
+        .sample_size(150)
+        .warm_up_time(Duration::from_secs(3))
+        .measurement_time(Duration::from_secs(10))
+}
+
+criterion_group!(
+    name = benches;
+    config = criterion_config();
+    targets =  insert_worse_case_benchmark
+);
 criterion_main!(benches);
