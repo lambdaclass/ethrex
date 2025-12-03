ba1a755e5b83c636e16798c782a58fcdd863fde1
Rodrigo Oliveri <rodrigooliveri10@gmail.com>
2025-12-03 10:59:40 -0300

    initial p2p metrics creation

--- crates/blockchain/metrics/mod.rs
+++ crates/blockchain/metrics/mod.rs
@@ -2,6 +2,8 @@
 pub mod api;
 #[cfg(any(feature = "api", feature = "metrics"))]
 pub mod blocks;
+#[cfg(any(feature = "api", feature = "metrics"))]
+pub mod p2p;
 #[cfg(feature = "api")]
 pub mod l2;
 #[cfg(any(feature = "api", feature = "metrics"))]
