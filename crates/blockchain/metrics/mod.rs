43ef4830c88dec1f4f56918e7470a98cef0e7c4c
Rodrigo Oliveri <rodrigooliveri10@gmail.com>
2025-12-09 13:00:44 -0300

    Revert "feat(l1): exposing peer related metrics + panel dashboards (#5520)"
    This reverts commit 7ec096e5b9a6a7e968b053a608efd15f9201b9b3.

--- crates/blockchain/metrics/mod.rs
+++ crates/blockchain/metrics/mod.rs
@@ -5,8 +5,6 @@ pub mod blocks;
 #[cfg(feature = "api")]
 pub mod l2;
 #[cfg(any(feature = "api", feature = "metrics"))]
-pub mod p2p;
-#[cfg(any(feature = "api", feature = "metrics"))]
 pub mod process;
 #[cfg(feature = "api")]
 pub mod profiling;
