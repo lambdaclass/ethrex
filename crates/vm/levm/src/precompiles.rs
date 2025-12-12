lambdaclass/ethrex

commit 264463e8467c7c9dae47ff3959fbd8788a15e488

chore(l2): use ZisK patched substrate-bn for bn254 G1 mul (#5529)
**Motivation**

We were missing a feature attribute so we weren't using the patched
crate for this precompile.

before:
```
| Block    | Gas Used    | Steps       | Duration (s) | TP (Msteps/s) | Freq (MHz) | Clocks/step |
|----------|-------------|-------------|--------------|----------------|------------|--------------|
| 23919400 | 41,075,722  | 760,022,264 | 18.4439      | 41.2072        | 1999.0000  | 48.5109      |
| 23919500 | 40,237,085  | 798,604,814 | 18.8685      | 42.3247        | 1999.0000  | 47.2301      |
| 23919600 | 24,064,259  | 512,165,934 | 12.0746      | 42.4168        | 1999.0000  | 47.1276      |
| 23919700 | 20,862,238  | 445,582,906 | 10.5385      | 42.2815        | 1999.0000  | 47.2784      |
| 23919800 | 31,813,109  | 596,446,973 | 14.4148      | 41.3774        | 1999.0000  | 48.3114      |
| 23919900 | 22,917,739  | 428,983,514 | 10.6816      | 40.1612        | 1999.0000  | 49.7745      |
| 23920000 | 37,256,487  | 695,820,714 | 16.3619      | 42.5268        | 1999.0000  | 47.0056      |
| 23920100 | 33,542,307  | 598,535,326 | 14.1889      | 42.1834        | 1999.0000  | 47.3883      |
| 23920200 | 22,994,047  | 416,578,732 | 9.9036       | 42.0633        | 1999.0000  | 47.5236      |
| 23920300 | 53,950,967  | 986,179,450 | 23.7537      | 41.5169        | 1999.0000  | 48.1490      |
```

after:
```
| Block    | Gas Used     | Steps         | Duration (s) | TP (Msteps/s) | Freq (MHz) | Clocks/step |
|----------|--------------|---------------|--------------|----------------|------------|--------------|
| 23919400 |   41,075,722 |   691,101,207 |      17.0141 |        40.6192 |  1999.0000 |      49.2132 |
| 23919500 |   40,237,085 |   779,305,442 |      18.6583 |        41.7672 |  1999.0000 |      47.8606 |
| 23919600 |   24,064,259 |   513,012,780 |      12.0374 |        42.6181 |  1999.0000 |      46.9050 |

crates/vm/levm/src/precompiles.rs
@@ -789,7 +789,7 @@ pub fn ecmul(calldata: &Bytes, gas_remaining: &mut u64, _fork: Fork) -> Result<B
     bn254_g1_mul(g1, scalar)
 }
 
-#[cfg(not(feature = "sp1"))]
+#[cfg(not(any(feature = "sp1", feature = "zisk")))]
 #[inline]
 pub fn bn254_g1_mul(point: G1, scalar: U256) -> Result<Bytes, VMError> {
     let x = ark_bn254::Fq::from_be_bytes_mod_order(&point.0.to_big_endian());
@@ -820,7 +820,7 @@ pub fn bn254_g1_mul(point: G1, scalar: U256) -> Result<Bytes, VMError> {
     Ok(Bytes::from(out))
 }
 
-#[cfg(feature = "sp1")]
+#[cfg(any(feature = "sp1", feature = "zisk"))]
 #[inline]
 pub fn bn254_g1_mul(g1: G1, scalar: U256) -> Result<Bytes, VMError> {
     use substrate_bn::{AffineG1, Fq, Fr, G1, Group};
@@ -834,11 +834,17 @@ pub fn bn254_g1_mul(g1: G1, scalar: U256) -> Result<Bytes, VMError> {
         Fq::from_slice(&g1.1.to_big_endian()).map_err(|_| PrecompileError::ParsingInputError)?,
     );
 
-    let g1 = AffineG1::new(g1_x, g1_y).map_err(|_| PrecompileError::InvalidPoint)?;
+    let g1: G1 = AffineG1::new(g1_x, g1_y)
+        .map_err(|_| PrecompileError::InvalidPoint)?
+        .into();
 
     let scalar =
         Fr::from_slice(&scalar.to_big_endian()).map_err(|_| PrecompileError::ParsingInputError)?;
 
+    #[allow(
+        clippy::arithmetic_side_effects,
+        reason = "G1 scalar multiplication doesn't overflow, intermediate operations that could overflow should be handled correctly by the library"
+    )]
     let result = g1 * scalar;
 
     let mut x_bytes = [0u8; 32];

commit 35397ff6bcaa39c92c112e5af9de8deeb33b43cd

fix(l2): fix block execution with SP1 (#5542)
**Motivation**

Execution with SP1 was failing after PR
https://github.com/lambdaclass/ethrex/pull/5529 which changes a line of
the `ecmul` precompile to make it compatible with an API difference
between the substrate-bn patch of SP1 and ZisK.

Apparently this also changes the behavior of the multiplication for the
SP1 patch, and some blocks (like block 23919500 from Mainnet) were
failing to execute due to a gas mismatch error.

Reverting the change for SP1 fixes the issue.

---------

Co-authored-by: Ivan Litteri <67517699+ilitteri@users.noreply.github.com>

crates/vm/levm/src/precompiles.rs
@@ -834,9 +834,11 @@ pub fn bn254_g1_mul(g1: G1, scalar: U256) -> Result<Bytes, VMError> {
         Fq::from_slice(&g1.1.to_big_endian()).map_err(|_| PrecompileError::ParsingInputError)?,
     );
 
-    let g1: G1 = AffineG1::new(g1_x, g1_y)
-        .map_err(|_| PrecompileError::InvalidPoint)?
-        .into();
+    let g1 = AffineG1::new(g1_x, g1_y).map_err(|_| PrecompileError::InvalidPoint)?;
+
+    // Small difference between the patched versions of substrate-bn
+    #[cfg(feature = "zisk")]
+    let g1: G1 = g1.into();
 
     let scalar =
         Fr::from_slice(&scalar.to_big_endian()).map_err(|_| PrecompileError::ParsingInputError)?;