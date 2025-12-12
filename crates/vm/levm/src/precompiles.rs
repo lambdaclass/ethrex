c36ad49f98d75e4fa48f6d4d9ac87040ef7528b3
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2025-12-12 15:44:43 -0300

    fmt

--- crates/vm/levm/src/precompiles.rs
+++ crates/vm/levm/src/precompiles.rs
@@ -834,8 +834,7 @@ pub fn bn254_g1_mul(g1: G1, scalar: U256) -> Result<Bytes, VMError> {
         Fq::from_slice(&g1.1.to_big_endian()).map_err(|_| PrecompileError::ParsingInputError)?,
     );
 
-    let g1 = AffineG1::new(g1_x, g1_y)
-        .map_err(|_| PrecompileError::InvalidPoint)?;
+    let g1 = AffineG1::new(g1_x, g1_y).map_err(|_| PrecompileError::InvalidPoint)?;
 
     // Small difference between the patched versions of substrate-bn
     #[cfg(feature = "zisk")]
