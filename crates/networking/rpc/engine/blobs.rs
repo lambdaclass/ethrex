c31be90d10b2e46230db70267ef3d885e092ca9c
lakshya-sky <lakshya-sky@users.noreply.github.com>
2025-12-15 17:06:08 -0500

    feat(l1): add `engine_getBlobsV3` rpc method

--- crates/networking/rpc/engine/blobs.rs
+++ crates/networking/rpc/engine/blobs.rs
@@ -25,6 +25,11 @@ pub struct BlobsV2Request {
     blob_versioned_hashes: Vec<H256>,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct BlobsV3Request {
+    blob_versioned_hashes: Vec<H256>,
+}
+
 #[derive(Clone, Debug, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct BlobAndProofV1 {
@@ -110,55 +115,83 @@ impl RpcHandler for BlobsV2Request {
 
     async fn handle(&self, context: RpcApiContext) -> Result<Value, RpcErr> {
         info!("Received new engine request: Requested Blobs V2");
-        if self.blob_versioned_hashes.len() >= GET_BLOBS_V1_REQUEST_MAX_SIZE {
-            return Err(RpcErr::TooLargeRequest);
-        }
-
-        if let Some(current_block_header) = context
-            .storage
-            .get_block_header(context.storage.get_latest_block_number().await?)?
-            && !context
-                .storage
-                .get_chain_config()
-                .is_osaka_activated(current_block_header.timestamp)
-        {
-            // validation requested in https://github.com/ethereum/execution-apis/blob/a1d95fb555cd91efb3e0d6555e4ab556d9f5dd06/src/engine/osaka.md?plain=1#L130
-            return Err(RpcErr::UnsuportedFork(
-                "getBlobsV2 engine only supported for Osaka".to_string(),
-            ));
-        };
-
-        let mut res: Vec<Option<BlobAndProofV2>> = vec![None; self.blob_versioned_hashes.len()];
-
-        for blobs_bundle in context.blockchain.mempool.get_blobs_bundle_pool()? {
-            // Go over all blobs bundles from the blobs bundle pool.
-            let blobs_in_bundle = blobs_bundle.blobs;
-            let commitments_in_bundle = blobs_bundle.commitments;
-            let proofs_in_bundle = blobs_bundle.proofs;
-
-            // Go over all the commitments in each blobs bundle to calculate the blobs versioned hash.
-            for (commitment, (blob, proofs)) in commitments_in_bundle.iter().zip(
-                blobs_in_bundle
-                    .iter()
-                    .zip(proofs_in_bundle.chunks(CELLS_PER_EXT_BLOB)),
-            ) {
-                let current_versioned_hash = kzg_commitment_to_versioned_hash(commitment);
-                if let Some(index) = self
-                    .blob_versioned_hashes
-                    .iter()
-                    .position(|&hash| hash == current_versioned_hash)
-                {
-                    // If the versioned hash is one of the requested we save its corresponding blob and proof in the returned vector. We store them in the same position as the versioned hash was received.
-                    res[index] = Some(BlobAndProofV2 {
-                        blob: *blob,
-                        proofs: proofs.to_vec(),
-                    });
-                }
-            }
-        }
+        let res = get_blobs_and_proof_v2(&self.blob_versioned_hashes, context).await?;
         if res.iter().any(|blob| blob.is_none()) {
             return Ok(Value::Null);
         }
         serde_json::to_value(res).map_err(|error| RpcErr::Internal(error.to_string()))
     }
+}
+
+impl RpcHandler for BlobsV3Request {
+    fn parse(params: &Option<Vec<Value>>) -> Result<Self, RpcErr> {
+        let params = params
+            .as_ref()
+            .ok_or(RpcErr::BadParams("No params provided".to_owned()))?;
+        if params.len() != 1 {
+            return Err(RpcErr::BadParams("Expected 1 param".to_owned()));
+        };
+        Ok(BlobsV3Request {
+            blob_versioned_hashes: serde_json::from_value(params[0].clone())?,
+        })
+    }
+
+    async fn handle(&self, context: RpcApiContext) -> Result<Value, RpcErr> {
+        info!("Received new engine request: Requested Blobs V3");
+        let res = get_blobs_and_proof_v2(&self.blob_versioned_hashes, context).await?;
+        serde_json::to_value(res).map_err(|error| RpcErr::Internal(error.to_string()))
+    }
+}
+
+// Get blob data for a given list of blob versioned hashes.
+async fn get_blobs_and_proof_v2(
+    blob_versioned_hashes: &[H256],
+    context: RpcApiContext,
+) -> Result<Vec<Option<BlobAndProofV2>>, RpcErr> {
+    if blob_versioned_hashes.len() >= GET_BLOBS_V1_REQUEST_MAX_SIZE {
+        return Err(RpcErr::TooLargeRequest);
+    }
+
+    if let Some(current_block_header) = context
+        .storage
+        .get_block_header(context.storage.get_latest_block_number().await?)?
+        && !context
+            .storage
+            .get_chain_config()
+            .is_osaka_activated(current_block_header.timestamp)
+    {
+        // validation requested in https://github.com/ethereum/execution-apis/blob/a1d95fb555cd91efb3e0d6555e4ab556d9f5dd06/src/engine/osaka.md?plain=1#L130
+        return Err(RpcErr::UnsuportedFork(
+            "getBlobsV2 engine only supported for Osaka".to_string(),
+        ));
+    };
+
+    let mut res: Vec<Option<BlobAndProofV2>> = vec![None; blob_versioned_hashes.len()];
+
+    for blobs_bundle in context.blockchain.mempool.get_blobs_bundle_pool()? {
+        // Go over all blobs bundles from the blobs bundle pool.
+        let blobs_in_bundle = blobs_bundle.blobs;
+        let commitments_in_bundle = blobs_bundle.commitments;
+        let proofs_in_bundle = blobs_bundle.proofs;
+
+        // Go over all the commitments in each blobs bundle to calculate the blobs versioned hash.
+        for (commitment, (blob, proofs)) in commitments_in_bundle.iter().zip(
+            blobs_in_bundle
+                .iter()
+                .zip(proofs_in_bundle.chunks(CELLS_PER_EXT_BLOB)),
+        ) {
+            let current_versioned_hash = kzg_commitment_to_versioned_hash(commitment);
+            if let Some(index) = blob_versioned_hashes
+                .iter()
+                .position(|&hash| hash == current_versioned_hash)
+            {
+                // If the versioned hash is one of the requested we save its corresponding blob and proof in the returned vector. We store them in the same position as the versioned hash was received.
+                res[index] = Some(BlobAndProofV2 {
+                    blob: *blob,
+                    proofs: proofs.to_vec(),
+                });
+            }
+        }
+    }
+    Ok(res)
 }
