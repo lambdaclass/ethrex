f195c7c4309b7855916a0141ed9b86d4616e47a6
Tomás Paradelo <tomas.paradelo@lambdaclass.com>
2025-12-18 11:24:07 -0300

    fix levm runner

--- tooling/ef_tests/state/runner/levm_runner.rs
+++ tooling/ef_tests/state/runner/levm_runner.rs
@@ -221,7 +221,6 @@ pub fn prepare_vm_for_tx<'a>(
             block_gas_limit: test.env.current_gas_limit,
             is_privileged: false,
             fee_token: None,
-            fee_ratio: None,
         },
         db,
         &tx,
