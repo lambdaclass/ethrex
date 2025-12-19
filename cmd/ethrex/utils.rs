3d3925914ea503960170f016174a36a4fc9ed450
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2025-12-19 19:23:53 -0300

    chore: change error message

--- cmd/ethrex/utils.rs
+++ cmd/ethrex/utils.rs
@@ -48,7 +48,7 @@ pub fn write_jwtsecret_file(jwt_secret_path: &str) -> Bytes {
 
     // Ensure the directory exists
     if let Some(parent_dir) = Path::new(jwt_secret_path).parent() {
-        std::fs::create_dir_all(parent_dir).expect("could not create parent dir");
+        std::fs::create_dir_all(parent_dir).expect("Failed to create parent dir");
     }
 
     std::fs::write(jwt_secret_path, &secret).expect("Unable to write JWT secret file");
