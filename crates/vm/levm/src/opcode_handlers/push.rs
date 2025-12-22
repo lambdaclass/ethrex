1a0b4c815b1c3634260abc0c7d06be22d6e9e32b
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2025-12-22 10:56:12 -0300

    refactor(l1): simplify op_push

--- crates/vm/levm/src/opcode_handlers/push.rs
+++ crates/vm/levm/src/opcode_handlers/push.rs
@@ -15,15 +15,11 @@ impl<'a> VM<'a> {
         call_frame.increase_consumed_gas(gas_cost::PUSHN)?;
 
         // Check to avoid multiple checks.
-        if call_frame.pc.checked_add(N).is_none() {
-            Err(InternalError::Overflow)?;
-        }
+        let Some(new_pc) = call_frame.pc.checked_add(N) else {
+            return Err(InternalError::Overflow.into());
+        };
 
-        let value = if let Some(slice) = call_frame
-            .bytecode
-            .bytecode
-            .get(call_frame.pc..call_frame.pc.wrapping_add(N))
-        {
+        let value = if let Some(slice) = call_frame.bytecode.bytecode.get(call_frame.pc..new_pc) {
             u256_from_big_endian_const(
                 // SAFETY: If the get succeeded, we got N elements so the cast is safe.
                 #[expect(unsafe_code)]
@@ -38,7 +34,7 @@ impl<'a> VM<'a> {
         call_frame.stack.push(value)?;
 
         // Advance the PC by the number of bytes in this instruction's payload.
-        call_frame.pc = call_frame.pc.wrapping_add(N);
+        call_frame.pc = new_pc;
 
         Ok(OpcodeResult::Continue)
     }
