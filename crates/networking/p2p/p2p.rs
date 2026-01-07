94a0fbc85cbf9962934009fc2fa346c474412c7b
Esteban Dimitroff Hodi <esteban.dimitroff@lambdaclass.com>
2026-01-07 19:43:27 -0300

    Reordered p2p.rs

--- crates/networking/p2p/p2p.rs
+++ crates/networking/p2p/p2p.rs
@@ -12,6 +12,9 @@ pub mod tx_broadcaster;
 pub mod types;
 pub mod utils;
 
+pub use network::periodically_show_peer_stats;
+pub use network::start_network;
+
 #[cfg(not(feature = "experimental-discv5"))]
 pub use discv4::peer_table;
 #[cfg(not(feature = "experimental-discv5"))]
@@ -20,5 +23,3 @@ pub use discv4::server as discovery_server;
 pub use discv5::peer_table;
 #[cfg(feature = "experimental-discv5")]
 pub use discv5::server as discovery_server;
-pub use network::periodically_show_peer_stats;
-pub use network::start_network;
