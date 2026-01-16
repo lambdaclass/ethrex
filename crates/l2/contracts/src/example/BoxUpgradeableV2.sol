e126b9601a2be5774396725dde8c92df879f1b9a
Tomás Paradelo <tomas.paradelo@lambdaclass.com>
2026-01-16 10:43:19 -0300

    revert change

--- crates/l2/contracts/src/example/BoxUpgradeableV2.sol
+++ crates/l2/contracts/src/example/BoxUpgradeableV2.sol
@@ -4,9 +4,15 @@ pragma solidity =0.8.31;
 import "./BoxUpgradeable.sol";
 
 contract BoxUpgradeableV2 is BoxUpgradeable {
+    // New storage is appended after BoxUpgradeable.value.
     uint256 public extraValue;
+    uint256 public extraValue2;
 
     function setExtraValue(uint256 newValue) external {
         extraValue = newValue;
+    }
+
+    function setExtraValue2(uint256 newValue) external {
+        extraValue2 = newValue;
     }
 }
