d48e4b753f55721433d67752c67fcc1694ec81ca
ilitteri <ivanlitteri@hotmail.com>
2026-01-21 17:01:41 -0300

    Fix include_bytes paths in crates/l2/zkvm.rs

--- crates/l2/zkvm.rs
+++ crates/l2/zkvm.rs
@@ -3,13 +3,13 @@
 
 #[cfg(all(not(clippy), feature = "sp1"))]
 pub static ZKVM_SP1_PROGRAM_ELF: &[u8] =
-    include_bytes!("../../ethrex-guest/bin/sp1/out/riscv32im-succinct-zkvm-elf");
+    include_bytes!("../ethrex-guest/bin/sp1/out/riscv32im-succinct-zkvm-elf");
 #[cfg(any(clippy, not(feature = "sp1")))]
 pub const ZKVM_SP1_PROGRAM_ELF: &[u8] = &[];
 
 #[cfg(all(not(clippy), feature = "risc0"))]
 pub static ZKVM_RISC0_PROGRAM_VK: &str = include_str!(concat!(
-    "../../ethrex-guest/bin/risc0/out/riscv32im-risc0-vk"
+    "../ethrex-guest/bin/risc0/out/riscv32im-risc0-vk"
 ));
 #[cfg(any(clippy, not(feature = "risc0")))]
 pub const ZKVM_RISC0_PROGRAM_VK: &str = "";
