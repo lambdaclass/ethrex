6bebb741adc5d18c7b58018dbd551da5ecd195e5
avilagaston9 <gaston.avila@lambdaclass.com>
2026-01-21 10:03:55 -0300

    style: fix import ordering in prover.rs and proof_coordinator.rs

--- crates/l2/sequencer/proof_coordinator.rs
+++ crates/l2/sequencer/proof_coordinator.rs
@@ -1,10 +1,10 @@
 use crate::SequencerConfig;
 use crate::sequencer::errors::{ConnectionHandlerError, ProofCoordinatorError};
 use crate::sequencer::setup::{prepare_quote_prerequisites, register_tdx_key};
+use crate::sequencer::utils::get_git_commit_hash;
 use bytes::Bytes;
 use ethrex_common::Address;
 use ethrex_l2_common::prover::{BatchProof, ProofData, ProofFormat, ProverType};
-use crate::sequencer::utils::get_git_commit_hash;
 use ethrex_metrics::metrics;
 use ethrex_rpc::clients::eth::EthClient;
 use ethrex_storage_rollup::StoreRollup;
