4abd5dbf5f6d37c8d84e207181d010b803dd032b
dicethedev <dicethedev@gmail.com>
2026-04-25 10:28:58 +0100

    remove redundant #[allow(unused)] from DB_COMMIT_THRESHOLD

--- crates/storage/store.rs
+++ crates/storage/store.rs
@@ -60,7 +60,6 @@ pub const MAX_WITNESSES: u64 = 128;
 // We use one constant for in-memory and another for on-disk backends.
 // This is due to tests requiring state older than 128 blocks.
 
-#[allow(unused)]
 pub const DB_COMMIT_THRESHOLD: usize = 256;
 const IN_MEMORY_COMMIT_THRESHOLD: usize = 10000;
 
