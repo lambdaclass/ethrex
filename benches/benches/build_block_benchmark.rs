8ef58301ea69875af823f21ebe36fcd02c91e4bb
Javier Rodríguez Chatruc <49622509+jrchatruc@users.noreply.github.com>
2026-04-27 18:31:06 -0300

    autoresearch optimization

--- benches/benches/build_block_benchmark.rs
+++ benches/benches/build_block_benchmark.rs
@@ -1,3 +1,7 @@
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static ALLOC: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+
 use std::{
     collections::BTreeMap,
     str::FromStr,
